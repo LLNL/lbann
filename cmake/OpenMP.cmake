af768dc7d5ae083bf26d4fc41e1a4ccbf64664f5
Moon <moon13@cedarlake.llnl.gov>
2016-09-12 16:59:57 -0700

    Trying to build LBANN on OS X
    Elemental compiles with clang and gfortran 4.9. However, clang does not have nice support for OpenMP.

--- cmake/OpenMP.cmake
+++ cmake/OpenMP.cmake
@@ -1,16 +1,9 @@
 # Try finding OpenMP
 find_package(OpenMP QUIET)
 
-if(OPENMP_FOUND)
+# Status message
+message(STATUS "OpenMP flags: ${OpenMP_CXX_FLAGS}")
 
-  # Status message
-  message(STATUS "Found OpenMP: OpenMP_CXX_FLAGS=\"${OpenMP_CXX_FLAGS}\"")
-
-  # Add C and C++ flags
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-
-  # LBANN has access to OpenMP
-  set(LBANN_HAS_OPENMP TRUE)
-
-endif()
+# Add C and C++ flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
