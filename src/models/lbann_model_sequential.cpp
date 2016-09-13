c744af19ea4cdef2d6338f19e884ab00cc3cf4b8
Sam Ade Jacobs <jacobs32@llnl.gov>
2016-09-13 14:07:55 -0700

    set index layer when layer is added

--- src/models/lbann_model_sequential.cpp
+++ src/models/lbann_model_sequential.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2014-2016, Lawrence Livermore National Security, LLC. 
-// Produced at the Lawrence Livermore National Laboratory. 
+// Copyright (c) 2014-2016, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
 // Written by the LBANN Research Team (B. Van Essen, et al.) listed in
 // the CONTRIBUTORS file. <lbann-dev@llnl.gov>
 //
@@ -9,7 +9,7 @@
 //
 // This file is part of LBANN: Livermore Big Artificial Neural Network
 // Toolkit. For details, see http://software.llnl.gov/LBANN or
-// https://github.com/LLNL/LBANN. 
+// https://github.com/LLNL/LBANN.
 //
 // Licensed under the Apache License, Version 2.0 (the "Licensee"); you
 // may not use this file except in compliance with the License.  You may
@@ -464,6 +464,7 @@ uint lbann::sequential_model::add(const std::string layer_name,
 uint lbann::sequential_model::add(Layer *new_layer)
 {
   const uint layer_index = m_layers.size();
+  new_layer->Index = layer_index;
   m_layers.push_back(new_layer);
   return layer_index;
 }
