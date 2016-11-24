e84ddb06bb5d6884255e3e6f8fc46f1dde4800fe
Brian C. Van Essen <vanessen1@llnl.gov>
2016-11-23 18:06:54 -0800

    Updated the target layer to use the activations rather than Ys for the Axpy

--- src/layers/lbann_target_layer_distributed_minibatch_parallel_io.cpp
+++ src/layers/lbann_target_layer_distributed_minibatch_parallel_io.cpp
@@ -168,8 +168,9 @@ void lbann::target_layer_distributed_minibatch_parallel_io::backProp() {
   /// Compute the error between the target values and the previous layer's activations
   /// Copy the results to the m_error_signal variable for access by the next lower layer
   Copy(*m_prev_activations, *m_error_signal); // delta = (activation - y)
-  Axpy(-1., Ys, *m_error_signal); // Per-neuron error
   Copy(Ys, *m_activations);
+  Axpy(-1., *m_activations, *m_error_signal); // Per-neuron error
+  /// @todo - BVE should we be using views here.
 
   if (m_execution_mode == execution_mode::training) {
     DataType avg_error = neural_network_model->obj_fn->compute_obj_fn(*m_prev_activations_v, *m_activations_v);
