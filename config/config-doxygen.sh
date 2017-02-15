0077663ff94dda4f25aa19480a97e80671c6d1c5
Brian C. Van Essen <vanessen1@llnl.gov>
2017-02-15 15:28:18 -0800

    Improved the version identifier.

--- config/config-doxygen.sh
+++ config/config-doxygen.sh
@@ -4,6 +4,8 @@ set -e
 # Echo on
 set -x
 
+GIT_VER=`git --git-dir .git describe --abbrev=7 --dirty --always --tags`
+
 sed -ie "s#@PROJECT_SOURCE_DIR@#$TRAVIS_BUILD_DIR#g" $DOXYFILE
 sed -ie "s#@DOXYGEN_OUTPUT_DIR@#$TRAVIS_BUILD_DIR/$SCRIPT_DIR#g" $DOXYFILE
-sed -ie "s#@LBANN_MAJOR_VERSION@.@LBANN_MINOR_VERSION@#$TRAVIS_JOB_ID#g"  $DOXYFILE
+sed -ie "s#@LBANN_MAJOR_VERSION@.@LBANN_MINOR_VERSION@#$GIT_VER-ci$TRAVIS_JOB_ID#g"  $DOXYFILE
