bf77f27b26d5fb22ff6b4acb7a726ad4b4035522
Brian C. Van Essen <vanessen1@llnl.gov>
2017-02-15 14:47:23 -0800

    Fixed typo

--- config/config-doxygen.sh
+++ config/config-doxygen.sh
@@ -4,6 +4,6 @@ set -e
 # Echo on
 set -x
 
-sed -ie "s#@PROJECT_SOURCE_DIR@#$TRAVIS_BUILD_DIR#g" $DOXYFILE                                                                                                                  |
-sed -ie "s#@DOXYGEN_OUTPUT_DIR@#$TRAVIS_BUILD_DIR/$SCRIPT_DIR#g" $DOXYFILE                                                                                                      |
+sed -ie "s#@PROJECT_SOURCE_DIR@#$TRAVIS_BUILD_DIR#g" $DOXYFILE
+sed -ie "s#@DOXYGEN_OUTPUT_DIR@#$TRAVIS_BUILD_DIR/$SCRIPT_DIR#g" $DOXYFILE
 sed -ie "s#@LBANN_MAJOR_VERSION@.@LBANN_MINOR_VERSION@#$TRAVIS_JOB_ID#g"  $DOXYFILE
