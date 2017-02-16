e99a3f596d2e1355bf00c44c6809651b5a00efd1
Brian C. Van Essen <vanessen1@llnl.gov>
2017-02-16 13:41:03 -0800

    Finished tuning and tweaking the Travis CI implementation of building gh-pages.

--- config/config-doxygen.sh
+++ config/config-doxygen.sh
@@ -4,12 +4,8 @@ set -e
 # Echo on
 set -x
 
-`git --version`
-
 GIT_VER=`git --git-dir .git describe --abbrev=7 --dirty --always --tags`
 
-echo "$GIT_VER"
-
 sed -ie "s#@PROJECT_SOURCE_DIR@#$TRAVIS_BUILD_DIR#g" $DOXYFILE
-sed -ie "s#@DOXYGEN_OUTPUT_DIR@#$TRAVIS_BUILD_DIR/$SCRIPT_DIR#g" $DOXYFILE
+sed -ie "s#@DOXYGEN_OUTPUT_DIR@#$TRAVIS_BUILD_DIR/$SCRIPT_DIR/$GH_REPO_NAME#g" $DOXYFILE
 sed -ie "s#@LBANN_MAJOR_VERSION@.@LBANN_MINOR_VERSION@#$GIT_VER-ci$TRAVIS_JOB_ID#g"  $DOXYFILE
--- config/publish-doxygen.sh
+++ config/publish-doxygen.sh
@@ -75,9 +75,6 @@ echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
-`ls .`
-`pwd`
-
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
 # Only upload if Doxygen successfully created the documentation.
@@ -90,16 +87,16 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # gh-pages branch.
     # GitHub is smart enough to know which files have changed and which files have
     # stayed the same and will only update the changed files.
-#    git add --all
+    git add --all
 
     # Commit the added files with a title and description containing the Travis CI
     # build number and the GitHub commit reference that issued this build.
-#    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-#    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
