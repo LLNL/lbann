d079a411f09c16fdbe8798a2f1f85a82beeb918e
Brian C. Van Essen <vanessen1@llnl.gov>
2017-02-21 14:55:16 -0800

    Removed dependency on OpenCV nonfree header files.

--- src/data_readers/lbann_image_utils.cpp
+++ src/data_readers/lbann_image_utils.cpp
@@ -32,7 +32,6 @@
 
 #ifdef __LIB_OPENCV
 #include <opencv2/core/core.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 using namespace cv;
