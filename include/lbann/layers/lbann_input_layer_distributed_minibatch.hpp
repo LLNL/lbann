dce5a868d578239d35ae1c3fecfc9da0e84ca2b5
Brian C. Van Essen <vanessen1@llnl.gov>
2017-06-12 08:28:28 -0700

    Updated the serial distributed I/O layers to use templates that specifies their data layout.

--- include/lbann/layers/lbann_input_layer_distributed_minibatch.hpp
+++ include/lbann/layers/lbann_input_layer_distributed_minibatch.hpp
@@ -28,29 +28,92 @@
 #define LBANN_LAYERS_INPUT_LAYER_DISTRIBUTED_MINIBATCH_HPP_INCLUDED
 
 #include "lbann/layers/lbann_input_layer.hpp"
+#include "lbann/models/lbann_model.hpp"
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 namespace lbann {
+template <class T_layout>
 class input_layer_distributed_minibatch : public input_layer {
  public:
-  input_layer_distributed_minibatch(data_layout data_dist, lbann_comm *comm, uint mini_batch_size, std::map<execution_mode, generic_data_reader *> data_readers, std::vector<regularizer *> regs= {});
+  int m_root; /* Which rank is the root of the CircMat */
+  Mat X_local;
+  CircMat Xs;
+  long m_num_data_per_epoch;
 
-  void setup(int num_prev_neurons);
-  bool update(void);
+ public:
+  input_layer_distributed_minibatch(T_layout data_dist, lbann_comm *comm, uint mini_batch_size, std::map<execution_mode, generic_data_reader *> data_readers, std::vector<regularizer *> regs = {})
+    : input_layer(data_dist, comm, mini_batch_size, data_readers, regs), Xs(m_comm->get_model_grid()) {
+
+    m_type = layer_type::input_distributed_minibatch;
+
+    //  m_index = index;
+    m_root = 0;
+    m_num_data_per_epoch = 0;
+
+  }
+
+  void setup(int num_prev_neurons) {
+    if(io_layer::m_data_sets_span_models) {
+      io_layer::setup_data_readers_for_training(0, Layer::m_comm->get_num_models() * Layer::m_mini_batch_size,
+                                                Layer::m_comm->get_model_rank() * Layer::m_mini_batch_size);
+      io_layer::setup_data_readers_for_evaluation(0, m_mini_batch_size);
+    } else {
+      io_layer::setup_data_readers_for_training(0, m_mini_batch_size);
+      io_layer::setup_data_readers_for_evaluation(0, m_mini_batch_size);
+    }
+
+    Zeros(*m_activations, m_num_neurons, m_mini_batch_size);
+    Zeros(X_local, m_num_neurons, m_mini_batch_size);
+  }
+
+ protected:
+  /** Handle forward propagation (arguments are unused.) */
+  void fp_linearity() {
+    generic_data_reader *data_reader = input_layer::select_data_reader();
+    int num_samples_in_batch = 0;
+
+    if (m_comm->get_rank_in_model() == m_root) {
+      Zero(X_local);
+      num_samples_in_batch = data_reader->fetch_data(X_local);
+      bool data_valid = (num_samples_in_batch > 0);
+      if(data_valid) {
+        m_num_data_per_epoch+=num_samples_in_batch;
+      }
+    }
+
+    /// Let each rank know this size of the current mini-batch
+    /// Note that this field has to be updated before distributing the data
+    m_neural_network_model->set_current_mini_batch_size(Layer::m_comm->model_broadcast(m_root, num_samples_in_batch));
+
+    if (m_comm->get_rank_in_model() == m_root) {
+      CopyFromRoot(X_local, Xs);
+    } else {
+      CopyFromNonRoot(Xs);
+    }
+
+    m_comm->model_barrier();
+
+    Copy(Xs, *m_activations);
+  }
+
+ public:
+  /**
+   * Once a mini-batch is processed, resuffle the data for the next batch if necessary
+   */
+  bool update() {
+    generic_data_reader *data_reader = input_layer::select_data_reader();
+    return !data_reader->update();
+  }
+
   Mat *get_local_mat(void) {
     return &X_local;
   };
   CircMat *get_dist_mat(void) {
     return &Xs;
   };
-
- public:
-  int m_root; /* Which rank is the root of the CircMat */
-  Mat X_local;
-  CircMat Xs;
-  long m_num_data_per_epoch;
- protected:
-  /** Handle forward propagation (arguments are unused.) */
-  void fp_linearity(void);
 };
 }
 
