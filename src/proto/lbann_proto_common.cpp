c8820b5497f991b7b6e1976207c99f1de1e9a19d
David A. Hysom <hysom@llnl.gov>
2017-06-14 10:22:33 -0700

    commented code out, so it won't break the build

--- src/proto/lbann_proto_common.cpp
+++ src/proto/lbann_proto_common.cpp
@@ -1,3 +1,4 @@
+#if 0
 #include "lbann/proto/lbann_proto_common.hpp"
 
 #include "lbann/lbann_base.hpp"
@@ -746,3 +747,4 @@ bool writePrototextFile(const char *fn, lbann_data::LbannPB &pb)
   close(fd);
   return true;
 }
+#endif
