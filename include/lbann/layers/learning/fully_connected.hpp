c26d7736f025cd0a4f94524a7ef8238cadec6f22
Nikoli Dryden <dryden1@llnl.gov>
2017-06-14 19:37:09 -0700

    Remove the weighted sum from the fully connected layer; it's no longer needed.

--- include/lbann/layers/learning/fully_connected.hpp
+++ include/lbann/layers/learning/fully_connected.hpp
@@ -179,7 +179,6 @@ class fully_connected_layer : public learning<T_layout> {
       Zeros(*this->m_weights_gradient, this->m_num_neurons, numPrevNeurons);
     }
     Zeros(*this->m_prev_activations, numPrevNeurons, this->m_mini_batch_size);
-    Zeros(*this->m_weighted_sum, this->m_num_neurons, this->m_mini_batch_size);
     Zeros(*this->m_activations, this->m_num_neurons, this->m_mini_batch_size);
     Zeros(*this->m_prev_error_signal, this->m_num_neurons, this->m_mini_batch_size);
     Zeros(*this->m_error_signal, numPrevNeurons, this->m_mini_batch_size); // m_error_signal holds the product of m_weights^T * m_prev_error_signal
@@ -230,11 +229,11 @@ class fully_connected_layer : public learning<T_layout> {
       // Apply bias
       Copy(*m_bias_weights_v, *m_bias_weights_repl);
       const Mat& local_bias_weights = m_bias_weights_repl->Matrix();
-      IndexDependentFill(this->m_weighted_sum_v->Matrix(), (std::function<DataType(El::Int,El::Int)>)
+      IndexDependentFill(this->m_activations_v->Matrix(), (std::function<DataType(El::Int,El::Int)>)
                          ([&local_bias_weights](El::Int r, El::Int c)->DataType {
                            return local_bias_weights.Get(r);
                          }));
-      Scale(m_bias_term, *this->m_weighted_sum_v);
+      Scale(m_bias_term, *this->m_activations_v);
     }
 
     // Apply weight matrix
@@ -244,20 +243,16 @@ class fully_connected_layer : public learning<T_layout> {
            *this->m_activation_weights_v,
            *this->m_prev_activations_v,
            m_has_bias ? DataType(1) : DataType(0),
-           *this->m_weighted_sum_v);
+           *this->m_activations_v);
       break;
     case data_layout::DATA_PARALLEL:
       Gemm(NORMAL, NORMAL, DataType(1),
            this->m_activation_weights_v->LockedMatrix(),
            this->m_prev_activations_v->LockedMatrix(),
            DataType(1),
-           this->m_weighted_sum_v->Matrix());
+           this->m_activations_v->Matrix());
       break;
     }
-
-    // Copy result to output matrix
-    Copy(*this->m_weighted_sum_v, *this->m_activations_v);
-
   }
 
   void bp_compute(void) {
