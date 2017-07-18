5a559ef622dfe78aea098235dced8e7dab82d6b2
Tim Moon <moon13@llnl.gov>
2017-07-18 13:06:27 -0700

    More descriptive variable names in batch norm.

--- include/lbann/layers/regularizers/batch_normalization.hpp
+++ include/lbann/layers/regularizers/batch_normalization.hpp
@@ -52,21 +52,22 @@ class batch_normalization : public regularizer_layer {
    * Set up batch normalization.
    * @param decay Controls the momentum of the running mean/standard
    * deviation averages.
-   * @param gamma The initial value for gamma. The paper recommends 1.0
-   * as a starting point, but other papers have had better results with
-   * a smaller value (e.g. 0.1).
-   * @param beta The initial value for beta. This should almost always
-   * stay at zero.
+   * @param scale The initial value for scaling parameter
+   * \f$\gamma$\f$. The paper recommends 1.0 as a starting point, but
+   * other papers have had better results with a smaller value
+   * (e.g. 0.1).
+   * @param bias The initial value for bias parameter
+   * \f$\beta\f$. This should almost always stay at zero.
    */
   batch_normalization(int index,
                       lbann_comm *comm,
                       int mini_batch_size,
                       DataType decay=0.9,
-                      DataType gamma=1.0,
-                      DataType beta=0.0)
+                      DataType scale_init=1.0,
+                      DataType bias_init=0.0)
     : regularizer_layer(index, comm, mini_batch_size),
-      m_gamma_init(gamma),
-      m_beta_init(beta),
+      m_scale_init(scale_init),
+      m_bias_init(bias_init),
       m_decay(decay) {
     // Setup the data distribution
     initialize_distributed_matrices();
@@ -74,66 +75,66 @@ class batch_normalization : public regularizer_layer {
 
   batch_normalization(const batch_normalization& other) :
     regularizer_layer(other),
-    m_gamma_init(other.m_gamma_init),
-    m_beta_init(other.m_beta_init),
+    m_scale_init(other.m_scale_init),
+    m_bias_init(other.m_bias_init),
     m_decay(other.m_decay) {
-    m_gamma = other.m_gamma->Copy();
-    m_beta = other.m_beta->Copy();
-    m_dgamma = other.m_dgamma->Copy();
-    m_dbeta = other.m_dbeta->Copy();
+    m_scale = other.m_scale->Copy();
+    m_bias = other.m_bias->Copy();
+    m_scale_gradient = other.m_scale_gradient->Copy();
+    m_bias_gradient = other.m_bias_gradient->Copy();
     m_mean = other.m_mean->Copy();
     m_stdev = other.m_stdev->Copy();
     m_running_mean = other.m_running_mean->Copy();
     m_running_stdev = other.m_running_stdev->Copy();
-    m_gamma_optimizer = other.m_gamma_optimizer->copy();
-    m_beta_optimizer = other.m_beta_optimizer->copy();
-    if (m_gamma_optimizer->get_parameters()) {
-      m_gamma_optimizer->set_parameters(m_gamma);
-      m_beta_optimizer->set_parameters(m_beta);
+    m_scale_optimizer = other.m_scale_optimizer->copy();
+    m_bias_optimizer = other.m_bias_optimizer->copy();
+    if (m_scale_optimizer->get_parameters()) {
+      m_scale_optimizer->set_parameters(m_scale);
+      m_bias_optimizer->set_parameters(m_bias);
     }
   }
 
   batch_normalization& operator=(const batch_normalization& other) {
     regularizer_layer::operator=(other);
-    m_gamma_init = other.m_gamma_init;
-    m_beta_init = other.m_beta_init;
+    m_scale_init = other.m_scale_init;
+    m_bias_init = other.m_bias_init;
     m_decay = other.m_decay;
-    if (m_gamma) {
-      delete m_gamma;
-      delete m_beta;
-      delete m_dgamma;
-      delete m_dbeta;
+    if (m_scale) {
+      delete m_scale;
+      delete m_bias;
+      delete m_scale_gradient;
+      delete m_bias_gradient;
       delete m_mean;
       delete m_stdev;
       delete m_running_mean;
       delete m_running_stdev;
     }
-    m_gamma = other.m_gamma->Copy();
-    m_beta = other.m_beta->Copy();
-    m_dgamma = other.m_dgamma->Copy();
-    m_dbeta = other.m_dbeta->Copy();
+    m_scale = other.m_scale->Copy();
+    m_bias = other.m_bias->Copy();
+    m_scale_gradient = other.m_scale_gradient->Copy();
+    m_bias_gradient = other.m_bias_gradient->Copy();
     m_mean = other.m_mean->Copy();
     m_stdev = other.m_stdev->Copy();
     m_running_mean = other.m_running_mean->Copy();
     m_running_stdev = other.m_running_stdev->Copy();
-    if (m_gamma_optimizer) {
-      delete m_gamma_optimizer;
-      delete m_beta_optimizer;
+    if (m_scale_optimizer) {
+      delete m_scale_optimizer;
+      delete m_bias_optimizer;
     }
-    if (other.m_gamma_optimizer) {
-      m_gamma_optimizer = other.m_gamma_optimizer->copy();
-      m_beta_optimizer = other.m_beta_optimizer->copy();
-      m_gamma_optimizer->set_parameters(m_gamma);
-      m_beta_optimizer->set_parameters(m_beta);
+    if (other.m_scale_optimizer) {
+      m_scale_optimizer = other.m_scale_optimizer->copy();
+      m_bias_optimizer = other.m_bias_optimizer->copy();
+      m_scale_optimizer->set_parameters(m_scale);
+      m_bias_optimizer->set_parameters(m_bias);
     }
     return *this;
   }
 
   ~batch_normalization() {
-    delete m_gamma;
-    delete m_beta;
-    delete m_dgamma;
-    delete m_dbeta;
+    delete m_scale;
+    delete m_bias;
+    delete m_scale_gradient;
+    delete m_bias_gradient;
     delete m_mean;
     delete m_stdev;
     delete m_running_mean;
@@ -150,28 +151,28 @@ class batch_normalization : public regularizer_layer {
   void setup_data() {
     regularizer_layer::setup_data();
     // Initialize parameters
-    m_gamma->Resize(this->get_num_neurons(), 1);
-    El::Fill(*m_gamma, m_gamma_init);
-    m_beta->Resize(this->get_num_neurons(), 1);
-    El::Fill(*m_beta, m_beta_init);
-    El::Zeros(*m_dgamma, this->get_num_neurons(), 1);
-    El::Zeros(*m_dbeta, this->get_num_neurons(), 1);
+    m_scale->Resize(this->get_num_neurons(), 1);
+    El::Fill(*m_scale, m_scale_init);
+    m_bias->Resize(this->get_num_neurons(), 1);
+    El::Fill(*m_bias, m_bias_init);
+    El::Zeros(*m_scale_gradient, this->get_num_neurons(), 1);
+    El::Zeros(*m_bias_gradient, this->get_num_neurons(), 1);
     El::Zeros(*m_mean, this->get_num_neurons(), 1);
     El::Zeros(*m_stdev, this->get_num_neurons(), 1);
     El::Zeros(*m_running_mean, this->get_num_neurons(), 1);
     El::Zeros(*m_running_stdev, this->get_num_neurons(), 1);
-    m_gamma_optimizer = this->get_neural_network_model()->create_optimizer();
-    m_beta_optimizer = this->get_neural_network_model()->create_optimizer();
-    m_gamma_optimizer->setup(m_gamma);
-    m_beta_optimizer->setup(m_beta);
+    m_scale_optimizer = this->get_neural_network_model()->create_optimizer();
+    m_bias_optimizer = this->get_neural_network_model()->create_optimizer();
+    m_scale_optimizer->setup(m_scale);
+    m_bias_optimizer->setup(m_bias);
   }
 
   void fp_compute() {
     ElMat *input_acts = this->m_prev_activations;
     Mat& input_acts_local = input_acts->Matrix();
     Mat& acts_local = this->m_activations->Matrix();
-    Mat& gamma_local = m_gamma->Matrix();
-    Mat& beta_local = m_beta->Matrix();
+    Mat& scale_local = m_scale->Matrix();
+    Mat& bias_local = m_bias->Matrix();
     const El::Int local_width = acts_local.Width();
     const El::Int local_height = acts_local.Height();
     if (this->get_execution_mode() == execution_mode::training) {
@@ -187,8 +188,8 @@ class batch_normalization : public regularizer_layer {
           acts_local(row, col) = input_acts_local(row, col) - mean_local(row, 0);
           acts_local(row, col) /= stdev_local(row, 0) + DataType(1e-7);
           // Apply scale/shift.
-          acts_local(row, col) *= gamma_local(row, 0);
-          acts_local(row, col) += beta_local(row, 0);
+          acts_local(row, col) *= scale_local(row, 0);
+          acts_local(row, col) += bias_local(row, 0);
         }
       }
       // Update the running averages.
@@ -208,8 +209,8 @@ class batch_normalization : public regularizer_layer {
           acts_local(row, col) = input_acts_local(row, col) - mean_local(row, 0);
           acts_local(row, col) /= stdev_local(row, 0) + DataType(1e-7);
           // Apply scale/shift.
-          acts_local(row, col) *= gamma_local(row, 0);
-          acts_local(row, col) += beta_local(row, 0);
+          acts_local(row, col) *= scale_local(row, 0);
+          acts_local(row, col) += bias_local(row, 0);
         }
       }
     }
@@ -230,36 +231,36 @@ class batch_normalization : public regularizer_layer {
     const El::Int local_width = input_bp_local.Width();
     const Mat& mean_local = m_mean->LockedMatrix();
     const Mat& stdev_local = m_stdev->LockedMatrix();
-    const Mat& gamma_local = m_gamma->LockedMatrix();
-    Mat& dgamma_local = m_dgamma->Matrix();
-    Mat& dbeta_local = m_dbeta->Matrix();
-    // Compute the derivatives of gamma and beta.
+    const Mat& scale_local = m_scale->LockedMatrix();
+    Mat& scale_gradient_local = m_scale_gradient->Matrix();
+    Mat& bias_gradient_local = m_bias_gradient->Matrix();
+    // Compute the derivatives of scale and bias.
 #pragma omp parallel for
     for (El::Int row = 0; row < local_height; ++row) {
-      dbeta_local(row, 0) = 0.0;
-      dgamma_local(row, 0) = 0.0;
+      bias_gradient_local(row, 0) = 0.0;
+      scale_gradient_local(row, 0) = 0.0;
       for (El::Int col = 0; col < local_width; ++col) {
-        dbeta_local(row, 0) += input_bp_local(row, col);
-        dgamma_local(row, 0) +=
+        bias_gradient_local(row, 0) += input_bp_local(row, col);
+        scale_gradient_local(row, 0) +=
           ((acts_local(row, col) - mean_local(row, 0)) /
            (stdev_local(row, 0) + DataType(1e-7))) *
           input_bp_local(row, col);
       }
     }
-    AllReduce(*m_dbeta, m_dbeta->RedundantComm(), mpi::SUM);
-    AllReduce(*m_dgamma, m_dgamma->RedundantComm(), mpi::SUM);
+    AllReduce(*m_bias_gradient, m_bias_gradient->RedundantComm(), mpi::SUM);
+    AllReduce(*m_scale_gradient, m_scale_gradient->RedundantComm(), mpi::SUM);
     // Update the backprop gradient signal.
 #pragma omp parallel for collapse(2)
     for (El::Int row = 0; row < local_height; ++row) {
       for (El::Int col = 0; col < local_width; ++col) {
         bp_local(row, col) = mbsize * input_bp_local(row, col);
-        bp_local(row, col) -= dbeta_local(row, 0);
+        bp_local(row, col) -= bias_gradient_local(row, 0);
         bp_local(row, col) -=
           ((acts_local(row, col) - mean_local(row, 0)) /
            (stdev_local(row, 0) + DataType(1e-7))) *
-          dgamma_local(row, 0);
+          scale_gradient_local(row, 0);
         bp_local(row, col) /= DataType(mbsize);
-        bp_local(row, col) *= gamma_local(row, 0);
+        bp_local(row, col) *= scale_local(row, 0);
         bp_local(row, col) /= stdev_local(row, 0) + DataType(1e-7);
         bp_local(row, col) += DataType(1e-8);  // Avoid very small values.
       }
@@ -268,28 +269,28 @@ class batch_normalization : public regularizer_layer {
 
   bool update_compute() {
     if (this->get_execution_mode() == execution_mode::training) {
-      m_gamma_optimizer->update(m_dgamma);
-      m_beta_optimizer->update(m_dbeta);
+      m_scale_optimizer->update(m_scale_gradient);
+      m_bias_optimizer->update(m_bias_gradient);
     }
     return true;
   }
 
  protected:
-  /** For learning gamma and beta. */
-  optimizer *m_gamma_optimizer;
-  optimizer *m_beta_optimizer;
-  /** Default initialization value for gamma. */
-  DataType m_gamma_init;
-  /** Default initialization value for beta. */
-  DataType m_beta_init;
+  /** For learning scale and bias. */
+  optimizer *m_scale_optimizer;
+  optimizer *m_bias_optimizer;
+  /** Default initialization value for scale. */
+  DataType m_scale_init;
+  /** Default initialization value for bias. */
+  DataType m_bias_init;
   /** Scale parameter (one scale for each activation). */
-  ElMat *m_gamma;
+  ElMat *m_scale;
   /** Shift parameter (one shift for each activation). */
-  ElMat *m_beta;
-  /** Gradients of gamma. */
-  ElMat *m_dgamma;
-  /** Gradients of beta. */
-  ElMat *m_dbeta;
+  ElMat *m_bias;
+  /** Gradients of scale. */
+  ElMat *m_scale_gradient;
+  /** Gradients of bias. */
+  ElMat *m_bias_gradient;
   /** Decay rate for the running mean/standard deviation. */
   DataType m_decay;
   /** Current minibatch mean of activations. */
@@ -304,10 +305,10 @@ class batch_normalization : public regularizer_layer {
 
 template<> inline void batch_normalization<data_layout::MODEL_PARALLEL>::initialize_distributed_matrices() {
   regularizer_layer::initialize_distributed_matrices<data_layout::MODEL_PARALLEL>();
-  m_gamma = new RowSumMat(this->m_comm->get_model_grid());
-  m_beta = new RowSumMat(this->m_comm->get_model_grid());
-  m_dgamma = new RowSumMat(this->m_comm->get_model_grid());
-  m_dbeta = new RowSumMat(this->m_comm->get_model_grid());
+  m_scale = new RowSumMat(this->m_comm->get_model_grid());
+  m_bias = new RowSumMat(this->m_comm->get_model_grid());
+  m_scale_gradient = new RowSumMat(this->m_comm->get_model_grid());
+  m_bias_gradient = new RowSumMat(this->m_comm->get_model_grid());
   m_mean = new RowSumMat(this->m_comm->get_model_grid());
   m_stdev = new RowSumMat(this->m_comm->get_model_grid());
   m_running_mean = new RowSumMat(this->m_comm->get_model_grid());
@@ -315,10 +316,10 @@ template<> inline void batch_normalization<data_layout::MODEL_PARALLEL>::initial
 }
 template<> inline void batch_normalization<data_layout::DATA_PARALLEL>::initialize_distributed_matrices() {
   regularizer_layer::initialize_distributed_matrices<data_layout::DATA_PARALLEL>();
-  m_gamma = new StarMat(this->m_comm->get_model_grid());
-  m_beta = new StarMat(this->m_comm->get_model_grid());
-  m_dgamma = new StarMat(this->m_comm->get_model_grid());
-  m_dbeta = new StarMat(this->m_comm->get_model_grid());
+  m_scale = new StarMat(this->m_comm->get_model_grid());
+  m_bias = new StarMat(this->m_comm->get_model_grid());
+  m_scale_gradient = new StarMat(this->m_comm->get_model_grid());
+  m_bias_gradient = new StarMat(this->m_comm->get_model_grid());
   m_mean = new StarMat(this->m_comm->get_model_grid());
   m_stdev = new StarMat(this->m_comm->get_model_grid());
   m_running_mean = new StarMat(this->m_comm->get_model_grid());
