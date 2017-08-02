810b3a55fb58e69a4440980cbf6c4c1e84a2265a
Nikoli Dryden <dryden1@llnl.gov>
2017-08-01 17:00:36 -0700

    Feature updates (skipping rows, no header support) and bug fixes in the CSV data reader.

--- src/data_readers/data_reader_csv.cpp
+++ src/data_readers/data_reader_csv.cpp
@@ -31,10 +31,9 @@
 
 namespace lbann {
 
-csv_reader::csv_reader(int batch_size, int label_col, char separator,
-                       int skip_cols, bool shuffle)
-  : generic_data_reader(batch_size, shuffle), m_separator(separator),
-    m_skip_cols(skip_cols), m_label_col(label_col) {}
+csv_reader::csv_reader(int batch_size, int label_col,
+                       bool shuffle)
+  : generic_data_reader(batch_size, shuffle), m_label_col(label_col) {}
 
 csv_reader::~csv_reader() {
   for (auto&& ifs : m_ifstreams) {
@@ -58,6 +57,9 @@ void csv_reader::load() {
   // the results to avoid every rank reading the same file.
   std::string line;
   // Parse the header to determine how many columns there are.
+  // Skip rows if needed.
+  skip_rows(ifs, m_skip_rows);
+  std::streampos header_start = ifs.tellg();
   // TODO: Skip comment lines.
   if (std::getline(ifs, line)) {
     m_num_cols = std::count(line.begin(), line.end(), m_separator) + 1;
@@ -67,9 +69,9 @@ void csv_reader::load() {
     }
     if (m_label_col < 0) {
       // Last column becomes the label column.
-      m_label_col = m_num_cols - 1 - m_skip_cols;
+      m_label_col = m_num_cols - 1;
     }
-    if (m_label_col + m_skip_cols >= m_num_cols) {
+    if (m_label_col >= m_num_cols) {
       throw lbann_exception(
         "csv_reader: label column" + std::to_string(m_label_col) +
         " is not present");
@@ -81,6 +83,11 @@ void csv_reader::load() {
   if (ifs.eof()) {
     throw lbann_exception(
       "csv_reader: reached EOF after reading header");
+  }
+  // If there was no header, skip back to the beginning.
+  if (!m_has_header) {
+    ifs.clear();
+    ifs.seekg(header_start, std::ios::beg);
   }
   // Construct an index mapping each line (sample) to its offset.
   // TODO: Skip comment lines.
@@ -101,10 +108,6 @@ void csv_reader::load() {
     size_t cur_pos = 0;
     for (int col = 0; col < m_num_cols; ++col) {
       size_t end_pos = line.find_first_of(m_separator, cur_pos);
-      if (col < m_skip_cols) {
-        cur_pos = end_pos + 1;
-        continue;
-      }
       if (col == m_label_col) {
         label_classes.insert(
           m_label_transform(line.substr(cur_pos, end_pos - cur_pos)));
@@ -112,7 +115,6 @@ void csv_reader::load() {
       }
       cur_pos = end_pos + 1;
     }
-    throw lbann_exception();
   }
   if (!ifs.eof()) {
     // If we didn't get to EOF, something went wrong.
@@ -130,7 +132,7 @@ void csv_reader::load() {
     throw lbann_exception(
       "csv_reader: label classes are not contiguous");
   }
-  m_num_samples = m_index.size();
+  m_num_samples = m_index.size() - 1;
   m_num_labels = label_classes.size();
   // End of file offset.
   m_index.push_back(ifs.tellg());
@@ -198,6 +200,13 @@ std::pair<std::vector<DataType>, DataType> csv_reader::fetch_line_and_label(
   for (int col = 0; col < m_num_cols; ++col) {
     // Note for last column, this returns std::npos, which substr handles.
     size_t end_pos = line.find_first_of(m_separator, cur_pos);
+    // Handle the label.
+    if (col == m_label_col) {
+      std::string str_val = line.substr(cur_pos, end_pos - cur_pos);
+      label = m_label_transform(str_val);
+      cur_pos = end_pos + 1;
+      continue;
+    }
     // Skip columns if needed.
     if (col < m_skip_cols) {
       cur_pos = end_pos + 1;
@@ -206,18 +215,19 @@ std::pair<std::vector<DataType>, DataType> csv_reader::fetch_line_and_label(
     // Note: This results in a copy, switch to string_view when we can.
     std::string str_val = line.substr(cur_pos, end_pos - cur_pos);
     cur_pos = end_pos + 1;
-    if (col == m_label_col) {
-      label = m_label_transform(str_val);
+    DataType val;
+    if (m_col_transforms.count(col)) {
+      val = m_col_transforms[col](str_val);
     } else {
-      DataType val;
-      if (m_col_transforms.count(col)) {
-        val = m_col_transforms[col](str_val);
-      } else {
-        // No easy way to parameterize based on DataType, so always use double.
+      // No easy way to parameterize based on DataType, so always use double.
+      try {
         val = std::stod(str_val);
+      } catch (std::invalid_argument& e) {
+        throw lbann_exception(
+          "csv_reader: could not convert '" + str_val + "'");
       }
-      parsed_line.push_back(val);
     }
+    parsed_line.push_back(val);
   }
   return std::make_pair(parsed_line, label);
 }
@@ -236,6 +246,15 @@ std::string csv_reader::fetch_raw_line(int data_id) {
       "csv_reader: error on reading " + std::to_string(data_id));
   }
   return line;
+}
+
+void csv_reader::skip_rows(std::ifstream& s, int rows) {
+  std::string unused;  // Unused buffer for extracting lines.
+  for (int i = 0; i < rows; ++i) {
+    if (!std::getline(s, unused)) {
+      throw lbann_exception("csv_reader: error on skipping rows");
+    }
+  }
 }
 
 }  // namespace lbann
