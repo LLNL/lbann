f36078a59ed749ad82d49fb611835dfe1dc5e704
Nikoli Dryden <dryden1@llnl.gov>
2017-09-06 10:13:16 -0700

    First pass at an merge data reader that can merge multiple data readers together. Note that we may wish to revise how it manages labels/responses.

---
+++ src/data_readers/data_reader_merge.cpp
@@ -0,0 +1,120 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014-2016, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+//
+// LLNL-CODE-697807.
+// All rights reserved.
+//
+// This file is part of LBANN: Livermore Big Artificial Neural Network
+// Toolkit. For details, see http://software.llnl.gov/LBANN or
+// https://github.com/LLNL/LBANN.
+//
+// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the license.
+//
+// lbann_data_merge .hpp .cpp - Merge multiple data readers
+////////////////////////////////////////////////////////////////////////////////
+
+#include "lbann/data_readers/data_reader_merge.hpp"
+
+namespace lbann {
+
+data_reader_merge::data_reader_merge(
+  int batch_size, std::vector<generic_data_reader*> data_readers,
+  generic_data_reader *label_reader, bool shuffle) :
+  generic_data_reader(batch_size, shuffle),
+  m_data_readers(data_readers), m_label_reader(label_reader) {
+  if (m_data_readers.empty()) {
+    throw lbann_exception(
+      "data_reader_merge: data reader list is empty");
+  }
+}
+
+data_reader_merge::data_reader_merge(const data_reader_merge& other) :
+  generic_data_reader(other),
+  m_data_size(other.m_data_size) {
+  for (auto&& reader : other.m_data_readers) {
+    m_data_readers.push_back(reader->copy());
+  }
+  m_label_reader = other.m_label_reader->copy();
+}
+
+data_reader_merge& data_reader_merge::operator=(
+  const data_reader_merge& other) {
+  generic_data_reader::operator=(other);
+  m_data_size = other.m_data_size;
+  if (!m_data_readers.empty()) {
+    for (auto&& reader : m_data_readers) {
+      delete reader;
+    }
+    m_data_readers.clear();
+  }
+  if (m_label_reader) {
+    delete m_label_reader;
+  }
+  for (auto&& reader : other.m_data_readers) {
+    m_data_readers.push_back(reader->copy());
+  }
+  m_label_reader = other.m_label_reader->copy();
+  return *this;
+}
+
+data_reader_merge::~data_reader_merge() {
+  for (auto&& reader : m_data_readers) {
+    delete reader;
+  }
+  delete m_label_reader;
+}
+
+void data_reader_merge::load() {
+  // Load each data reader separately.
+  for (auto&& reader : m_data_readers) {
+    reader->load();
+    m_data_size += reader->get_linearized_data_size();
+  }
+  // Verify the readers have the same number of samples.
+  int num_samples = m_data_readers[0]->get_num_data();
+  for (auto&& reader : m_data_readers) {
+    if (num_samples != reader->get_num_data()) {
+      throw lbann_exception(
+        "data_reader_merge: data readers do not have the same amount of data");
+    }
+  }
+  m_label_reader->load();
+  // Reset indices.
+  m_shuffled_indices.resize(num_samples);
+  std::iota(m_shuffled_indices.begin(), m_shuffled_indices.end(), 0);
+  select_subset_of_data();
+}
+
+bool data_reader_merge::fetch_datum(Mat& X, int data_id, int mb_idx, int tid) {
+  int start = 0;
+  for (auto&& reader : m_data_readers) {
+    auto X_view = X(El::IR(start, start + reader->get_linearized_data_size()),
+                    El::ALL);
+    reader->fetch_datum(X_view, data_id, mb_idx, tid);
+    start += reader->get_linearized_data_size();
+  }
+  return true;
+}
+
+bool data_reader_merge::fetch_label(Mat& Y, int data_id, int mb_idx, int tid) {
+  return m_label_reader->fetch_datum(Y, data_id, mb_idx, tid);
+}
+
+bool data_reader_merge::fetch_response(Mat& Y, int data_id, int mb_idx, int tid) {
+  return m_label_reader->fetch_datum(Y, data_id, mb_idx, tid);
+}
+
+}  // namespace lbann
