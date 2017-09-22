0f599a3b6df06d65795862cb16e86821619abe19
Nikoli Dryden <dryden1@llnl.gov>
2017-09-22 07:20:02 -0700

    Add a wrapper around glob to simplify finding many files.

--- include/lbann/lbann.hpp
+++ include/lbann/lbann.hpp
@@ -135,6 +135,7 @@
 #include "lbann/utils/exception.hpp"
 #include "lbann/utils/summary.hpp"
 #include "lbann/utils/options.hpp"
+#include "lbann/utils/glob.hpp"
 #include "lbann/params.hpp"
 #include "lbann/io/file_io.hpp"
 #include "lbann/io/persist.hpp"
