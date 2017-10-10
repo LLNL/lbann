653f83160e42e53e49789a30f09df94c2f43ed3d
Jae-Seung Yeom <yeom2@llnl.gov>
2017-10-10 16:11:21 -0700

    integrating Tim's OpenBlas tag with the existing patch

--- external/OpenBLAS/patchOpenBLAS.sh
+++ external/OpenBLAS/patchOpenBLAS.sh
@@ -2,15 +2,18 @@
 
 thisCommand=`basename $0`
 PATCH_DIR=`dirname $0`
+OpenBLAS_PATCH_OLD=${PATCH_DIR}/old/OpenBLAS.patch
 OpenBLAS_PATCH=${PATCH_DIR}/OpenBLAS.patch
 
-# pick one file to change
-fOrg=Makefile.power
-
-# check if it contains the line to change
-WRONG_OPT=`grep malign-power ${fOrg}`
+WRONG_OPT=`grep malign-power Makefile.power`
+EXTRA_ARCH=`grep I6500 Makefile.system`
 
 if [ "${WRONG_OPT}" != "" ] ; then
-  patch -p0 < ${OpenBLAS_PATCH}
-  echo "Applying patch to OpenBLAS source"
+  if [ "${EXTRA_ARCH}" != "" ] ; then
+    patch -N -s -p0 < ${OpenBLAS_PATCH}
+    echo "Applying patch to OpenBLAS branch by Tim"
+  else
+    patch -N -s -p0 < ${OpenBLAS_PATCH_OLD}
+    echo "Applying patch to OpenBLAS source"
+  fi
 fi
