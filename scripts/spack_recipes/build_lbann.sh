21293a7d29b279e2e7ba4c89b033b1e13202117e
Brian C. Van Essen <vanessen1@llnl.gov>
2017-10-11 17:17:36 -0700

    Added an explicit dependency to force the correct version of the protobuf package until the concretizer is fixed.

--- scripts/spack_recipes/build_lbann.sh
+++ scripts/spack_recipes/build_lbann.sh
@@ -160,7 +160,7 @@ if [ ! -z "${Fortran_FLAGS}" ]; then
     SPACK_FFLAGS="fflags=\"${Fortran_FLAGS}\""
 fi
 
-SPACK_OPTIONS="lbann@local build_type=${BUILD_TYPE} dtype=${DTYPE} ${PLATFORM} ${VARIANTS} %${COMPILER} ${SPACK_CFLAGS} ${SPACK_CXXFLAGS} ${SPACK_FFLAGS} ^elemental@${EL_VER} blas=${BLAS} ^${MPI}"
+SPACK_OPTIONS="lbann@local build_type=${BUILD_TYPE} dtype=${DTYPE} ${PLATFORM} ${VARIANTS} %${COMPILER} ${SPACK_CFLAGS} ${SPACK_CXXFLAGS} ${SPACK_FFLAGS} ^elemental@${EL_VER} blas=${BLAS} ^${MPI} ^protobuf@3.1.0"
 
 SPEC="spack spec ${SPACK_OPTIONS}"
 CMD="spack setup ${SPACK_OPTIONS}"
