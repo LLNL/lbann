2b1bf9c9966392be6701738990f906cee05b2307
Jae-Seung Yeom <yeom2@llnl.gov>
2017-10-20 07:59:02 -0700

    Adding a patch to replace OpenBLAS's gfortran linkage with Intel's fortran library (libifcoremt) to use ifort

--- external/OpenBLAS/patchELOpenBLAS.sh
+++ external/OpenBLAS/patchELOpenBLAS.sh
@@ -5,14 +5,14 @@ PATCH_DIR=`dirname $0`
 OpenBLAS_PATCH_OLD=${PATCH_DIR}/old/EL_OpenBLAS.patch
 OpenBLAS_PATCH=${PATCH_DIR}/EL_OpenBLAS.patch
 
-fOrg=`grep OpenBLAS.cmake ${PATCH_DIR}/ChangedFiles.EL_OpenBLAS.txt`
+fOrg=`grep OpenBLAS.cmake ${PATCH_DIR}/ChangedFiles.EL_OpenBLAS.txt 2> /dev/null`
 
-OpenBLAS_TAG=`grep GIT_TAG ${fOrg} | grep -v OPENBLAS_TAG | awk '{if (NF==2) print $2; else print "0";}' | sed -e 's/\"//g'`
+OpenBLAS_TAG=`grep GIT_TAG ${fOrg} 2> /dev/null | grep -v OPENBLAS_TAG | awk '{if (NF==2) print $2; else print "0";}' | sed -e 's/\"//g'`
 
-if [ "${OpenBLAS_TAG}" = "" ] ; then
+if [ "${OpenBLAS_TAG}" = "" ] && [ "${fOrg}" != "" ] && [ -f ${fOrg} ] ; then
   OpenBLAS_TAG=`grep 'OPENBLAS_TAG' ${PATCH_DIR}/../../cmake/Elemental.cmake | grep set | awk '{if (NF==2) print $2; else print "0";}' | sed -e 's/\"//g' | sed -e 's/)//g'`
 fi
-echo ${OpenBLAS_TAG}
+#echo ${OpenBLAS_TAG}
 
 if [ "${OpenBLAS_TAG}" = "v0.2.15" ] ; then
   patch -N -s -p0 < ${OpenBLAS_PATCH_OLD}
