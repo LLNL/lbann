f612fe28d5f502563aeb0018d0fb1d015af34e0d
DylanMcKinney <mckinney27@llnl.gov>
2017-12-12 11:10:02 -0800

    save weights in model_sequential for now (either would work I think)

--- src/layers/layer.cpp
+++ src/layers/layer.cpp
@@ -658,16 +658,16 @@ bool Layer::loadFromCheckpoint(int fd, const char *filename, size_t *bytes) {
 }
 
 bool Layer::saveToCheckpointShared(persist& p) const {
-  for (weights *w : m_weights) {
-    w->saveToCheckpointShared(p);  
-  }
+  //for (weights *w : m_weights) {
+  //  w->saveToCheckpointShared(p);  
+  //}
   return true;
 }
 
 bool Layer::loadFromCheckpointShared(persist& p) {
-  for (weights *w : m_weights) {
-    w->loadFromCheckpointShared(p);
-  }
+  //for (weights *w : m_weights) {
+  //  w->loadFromCheckpointShared(p);
+  //}
 
   return true;
 }
