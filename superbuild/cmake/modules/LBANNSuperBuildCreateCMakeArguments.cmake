cc6b3e3e0f821ed3c92c829505f76519d4a155b9
Thomas R. Benson <benson31@llnl.gov>
2017-12-13 17:38:37 -0800

    add some extra functionality to cmake arg parser

--- superbuild/cmake/modules/LBANNSuperBuildCreateCMakeArguments.cmake
+++ superbuild/cmake/modules/LBANNSuperBuildCreateCMakeArguments.cmake
@@ -1,10 +1,23 @@
 # This function takes a list of variables and writes a CMake argument string of the format:
 #   -DARGNAME:ARGTYPE=ARGVALUE ...
 #
+# Arguments:
+#   -- REMOVE_PKG_NAME: <PACKAGE_NAME>_<OPTION> gets written as -D<OPTION>=<...>
+#
+#   -- PACKAGE_NAME: The name of the package
+#   -- OUTPUT_VARIABLE: The list of CMake options
+#
+#   -- VARIABLES: The list of variable names to process
+#   -- EXTRA_REMOVE_PREFIXES: Also strip these prefixes. Takes
+#        precendence over SKIP_VARS_WITH_PREFIXES and PACKAGE_NAME.
+#   -- SKIP_VARS_WITH_PREFIXES: Skip a variable with this prefix
+#        unless caught by EXTRA_REMOVE_PREFIXES
+#
 function (create_cmake_arguments)
   set(_OPTIONS REMOVE_PKG_NAME)
   set(_ONE_VALUE_PARAMS PACKAGE_NAME OUTPUT_VARIABLE)
-  set(_MULTI_VALUE_PARAMS VARIABLES)
+  set(_MULTI_VALUE_PARAMS
+    VARIABLES SKIP_VARS_WITH_PREFIXES EXTRA_REMOVE_PREFIXES)
 
   cmake_parse_arguments(_CREATEARGS
     "${_OPTIONS}" "${_ONE_VALUE_PARAMS}" "${_MULTI_VALUE_PARAMS}" ${ARGN})
@@ -14,34 +27,63 @@ function (create_cmake_arguments)
     return()
   endif ()
 
-  set(_DO_NOT_PRINT_TYPES INTERNAL STATIC UNINITIALIZED)
-
-  foreach(_variable ${_CREATEARGS_VARIABLES})
-
-    # Cleanup the variable name
-    if (_CREATEARGS_REMOVE_PKG_NAME)
-      # We must be careful to only remove the first instance of the
-      # package name.
-      string(REGEX REPLACE "^${_CREATEARGS_PACKAGE_NAME}_\(.+\)" "\\1"
-        _CMAKE_ARG_NAME ${_variable})
-    else ()
-      # Handle CMake options
-      string(REGEX REPLACE "${_CREATEARGS_PACKAGE_NAME}_\(CMAKE_.+\)" "\\1"
-        _CMAKE_ARG_NAME ${_variable})
-    endif ()
+  foreach (_variable ${_CREATEARGS_VARIABLES})
 
     # Check the variable's type, if possible.
     get_property(_CMAKE_ARG_TYPE CACHE ${_variable} PROPERTY TYPE)
 
-    # Check if it's a type we want to propagate
-    if (_CMAKE_ARG_TYPE)
-      list(FIND _DO_NOT_PRINT_TYPES ${_CMAKE_ARG_TYPE} _DO_NOT_PRINT_VAR)
-    else ()
-      set(_DO_NOT_PRINT_VAR 0)
+    if (${_CMAKE_ARG_TYPE} STREQUAL "STATIC"
+        OR ${_CMAKE_ARG_TYPE} STREQUAL "INTERNAL")
+      continue()
     endif ()
 
+    set(_variable_done FALSE)
+
+    # EXTRA_REMOVE_PREFIXES takes precedence over everything
+    foreach (_prefix ${_CREATEARGS_EXTRA_REMOVE_PREFIXES})
+      string(REGEX REPLACE "^${_prefix}_\(.+\)" "\\1"
+        _CMAKE_ARG_NAME ${_variable})
+      if (CMAKE_MATCH_COUNT GREATER 0)
+        set(_variable_done TRUE)
+        break ()
+      endif ()
+    endforeach ()
+
+    # Check for skips
+    if (NOT _variable_done AND _CREATEARGS_SKIP_VARS_WITH_PREFIXES)
+
+      foreach (_prefix ${_CREATEARGS_SKIP_VARS_WITH_PREFIXES})
+        string(REGEX MATCH "^${_prefix}_" _MATCH_FOUND ${_variable})
+        if (_MATCH_FOUND)
+          set(_variable_done TRUE)
+          break ()
+        endif ()
+      endforeach ()
+
+      if(_variable_done)
+        continue ()
+      endif ()
+    endif ()
+
+    if (NOT _variable_done)
+      # Cleanup the variable name
+      if (_CREATEARGS_REMOVE_PKG_NAME)
+        # We must be careful to only remove the first instance of the
+        # package name.
+        string(REGEX REPLACE "^${_CREATEARGS_PACKAGE_NAME}_\(.+\)" "\\1"
+          _CMAKE_ARG_NAME ${_variable})
+      else ()
+        # Handle CMake options
+        string(REGEX REPLACE "${_CREATEARGS_PACKAGE_NAME}_\(CMAKE_.+\)" "\\1"
+          _CMAKE_ARG_NAME ${_variable})
+      endif ()
+      set(_variable_done TRUE)
+    endif ()
+
+    message("${_variable} ==> ${_CMAKE_ARG_NAME} (${_CMAKE_ARG_TYPE})")
+
     # Add the variable to the CMake line
-    if (${_DO_NOT_PRINT_VAR} STREQUAL "-1")
+    if (NOT ${_CMAKE_ARG_TYPE} STREQUAL "UNINITIALIZED")
       list(APPEND _output_string
         "-D${_CMAKE_ARG_NAME}:${_CMAKE_ARG_TYPE}=${${_variable}}")
     else ()
