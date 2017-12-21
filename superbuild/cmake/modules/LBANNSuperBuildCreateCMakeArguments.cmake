6c985964a0a438f8efb326a980a72c12afc71814
Thomas R. Benson <benson31@llnl.gov>
2017-12-21 14:26:19 -0800

    fix bug in create cmake args function

--- superbuild/cmake/modules/LBANNSuperBuildCreateCMakeArguments.cmake
+++ superbuild/cmake/modules/LBANNSuperBuildCreateCMakeArguments.cmake
@@ -32,7 +32,7 @@ function (create_cmake_arguments)
     # Check the variable's type, if possible.
     get_property(_CMAKE_ARG_TYPE CACHE ${_variable} PROPERTY TYPE)
 
-    if (${_CMAKE_ARG_TYPE} STREQUAL "STATIC"
+    if (_CMAKE_ARG_TYPE AND ${_CMAKE_ARG_TYPE} STREQUAL "STATIC"
         OR ${_CMAKE_ARG_TYPE} STREQUAL "INTERNAL")
       continue()
     endif ()
