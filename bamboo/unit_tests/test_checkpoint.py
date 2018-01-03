09d4189afc93e32d84845f3ad8cacd1cf9b4c8aa
DylanMcKinney <mckinney27@llnl.gov>
2018-01-03 14:14:59 -0800

    Minor naming fix, changed mpi rank to 1. will still fail on 'train' file

--- bamboo/unit_tests/test_checkpoint.py
+++ bamboo/unit_tests/test_checkpoint.py
@@ -1,21 +1,21 @@
 import os, pytest
 
 def test_checkpoint_lenet(exe, dirname):
-    return_code_nockpt = os.system('salloc -N1 srun -n2 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=2' % (exe, dirname, dirname, dirname))
+    return_code_nockpt = os.system('salloc -N1 srun -n1 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=2' % (exe, dirname, dirname, dirname))
     if return_code_nockpt != 0:
         sys.stderr.write('LeNet execution failed, exiting with error')
         sys.exit(1)
     os.system('patch -f  %s/model_zoo/tests/model_lenet_mnist_ckpt.prototext %s/model_zoo/tests/checkpoint_dir.patch' % (dirname, dirname))
     
-    return_code_ckpt_1 = os.system('salloc -N1 srun -n2 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=1' % (exe, dirname, dirname, dirname))
+    return_code_ckpt_1 = os.system('salloc -N1 srun -n1 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=1' % (exe, dirname, dirname, dirname))
 
-    if return_code_ckpt1 != 0:
+    if return_code_ckpt_1 != 0:
         sys.stderr.write('LeNet execution failed, exiting with error')
         sys.exit(1)
 
-    return_code_ckpt_2 = os.system('salloc -N1 srun -n2 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=2' % (exe, dirname, dirname, dirname))
+    return_code_ckpt_2 = os.system('salloc -N1 srun -n1 %s --model=%s/model_zoo/tests/model_lenet_mnist_ckpt.prototext --optimizer=%s/model_zoo/optimizers/opt_sgd.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext --num_epochs=2' % (exe, dirname, dirname, dirname))
 
-    if return_code_ckpt2 != 0:
+    if return_code_ckpt_2 != 0:
         sys.stderr.write('LeNet execution failed, exiting with error')
         sys.exit(1)
 
