760966a4dc9d6a0ce4f1b9be6b65fddbe8c630b2
Ryan Forsyth <forsyth2@llnl.gov>
2018-01-12 14:12:59 -0800

    Update README and default exe

--- bamboo/integration_tests/conftest.py
+++ bamboo/integration_tests/conftest.py
@@ -3,8 +3,12 @@ import pytest, os, re, subprocess
 def pytest_addoption(parser):
     cluster = re.sub('[0-9]+', '', subprocess.check_output('hostname'.split()).strip())
     default_dirname = subprocess.check_output('git rev-parse --show-toplevel'.split()).strip()
-    plan = os.environ['bamboo_planKey']
-    default_exe = '%s/../%s-BDE/build/%s.llnl.gov/model_zoo/lbann' % (default_dirname, plan, cluster)
+    key = 'bamboo_planKey'
+    if key in os.environ:
+        plan = os.environ['bamboo_planKey']
+        default_exe = '%s/../%s-BDE/build/%s.llnl.gov/model_zoo/lbann' % (default_dirname, plan, cluster)
+    else:
+        default_exe = '%s/build/%s.llnl.gov/model_zoo/lbann' % (default_dirname,cluster)
     parser.addoption('--log', action='store', default=0,
                      help='--log=1 to keep trimmed accuracy files. Default (--log=0) removes files')
     parser.addoption('--exe', action='store', default=default_exe,
