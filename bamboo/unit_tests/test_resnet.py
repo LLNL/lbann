275940eb49420f7e418af18a269388dc35514552
Ryan Forsyth <forsyth2@llnl.gov>
2018-02-16 15:22:27 -0800

    Implement matrix testing

--- bamboo/unit_tests/test_resnet.py
+++
@@ -1,5 +0,0 @@
-import os, pytest
-
-def test_gradient_check(exe, dirname):
-    return_code = os.system('salloc -N1 srun --ntasks=1 %s --model=%s/model_zoo/tests/model_mnist_resnet.prototext --optimizer=%s/model_zoo/optimizers/opt_adam.prototext --reader=%s/model_zoo/data_readers/data_reader_mnist.prototext' % (exe, dirname, dirname, dirname))
-    assert return_code == 0
---
+++ bamboo/unit_tests/test_unit_lbann_invocation.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.insert(0, '../common_python')
+import tools
+import pytest
+import os, sys
+
+def test_unit_no_params_bad(cluster, exe, dirname) :
+    sys.stderr.write('TESTING: run lbann with no params; lbann should throw exception\n')
+    command = tools.get_command(
+        cluster=cluster, executable=exe, exit_after_setup=True)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_one_model_bad(cluster, exe, dirname) :
+    sys.stderr.write('TESTING: run lbann with no optimizer or reader; lbann should throw exception\n')
+    model_path = 'prototext/model_mnist_simple_1.prototext'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, exit_after_setup=True,
+        model_path=model_path)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_two_models_bad(cluster, exe, dirname) :
+    sys.stderr.write('TESTING: run lbann with two models but no optimizer or reader; lbann should throw exception\n')
+    model_path = '{prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext}'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, exit_after_setup=True,
+        model_path=model_path)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_two_models_bad2(cluster, exe,  dirname):
+    sys.stderr.write('TESTING: run lbann with two models with missing {; lbann should throw exception\n')
+    model_path='prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext}'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, exit_after_setup=True,
+        model_path=model_path)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_missing_optimizer(cluster, exe, dirname):
+    sys.stderr.write('TESTING: run lbann with two models, reader, but no optimizer; lbann should throw exception\n')
+    model_path='{prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext}'
+    data_reader_path='prototext/data_reader_mnist.prototext'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, data_reader_path=data_reader_path,
+        exit_after_setup=True, model_path=model_path)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_missing_reader(cluster, exe, dirname):
+    sys.stderr.write('TESTING: run lbann with two models, reader, but no reader; lbann should throw exception\n')
+    model_path = '{prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext}'
+    optimizer_path = 'prototext/opt_sgd.prototext'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, exit_after_setup=True,
+        model_path=model_path, optimizer_path=optimizer_path)
+    return_code = os.system(command)
+    assert return_code != 0
+
+def test_unit_bad_params(cluster, exe, dirname) :
+    sys.stderr.write('TESTING: run lbann with ill-formed param (missing -) lbann should throw exception\n')
+    (command_allocate, command_run, _, _) = tools.get_command(cluster=cluster, executable=exe, return_tuple=True)
+    return_code = os.system('%s%s %s -exit_after_setup --reader=prototext/data_reader_mnist.prototext --model={prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext} --optimizer=prototext/opt_sgd.prototext' % (command_allocate, command_run, exe))
+    assert return_code != 0
+
+def test_unit_should_work(cluster, exe, dirname) :
+    sys.stderr.write('TESTING: run lbann with two models, reader, and optimizer; lbann should NOT throw exception\n')
+    model_path = '{prototext/model_mnist_simple_1.prototext,prototext/model_mnist_simple_1.prototext}'
+    data_reader_path = 'prototext/data_reader_mnist.prototext'
+    optimizer_path = 'prototext/opt_sgd.prototext'
+    command = tools.get_command(
+        cluster=cluster, executable=exe, data_reader_path=data_reader_path,
+        exit_after_setup=True, model_path=model_path,
+        optimizer_path=optimizer_path)
+    return_code = os.system(command)
+    assert return_code != 0
