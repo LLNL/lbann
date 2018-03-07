5ddb50da059d53e3a5b53c137c055fa5e633095b
Corey McNeish <mcneish1@llnl.gov>
2018-03-07 12:23:37 -0800

    External data reader: Currently able to pass zeros to lbann TODO:  * add python to actually get data  * fix labels, responses (want an example?)

--- include/lbann/data_readers/data_reader_external/data_reader_external.hpp
+++ include/lbann/data_readers/data_reader_external/data_reader_external.hpp
@@ -26,9 +26,10 @@
 // lbann_data_reader_external .hpp .cpp - generic_data_reader class for data readers connected over IPC
 ////////////////////////////////////////////////////////////////////////////////
 
-
 #ifndef LBANN_DATA_READER_EXTERNAL_HPP
 #define LBANN_DATA_READER_EXTERNAL_HPP
+
+#include <mutex>
 
 #include <data_reader_communication.pb.h>
 
@@ -42,63 +43,68 @@ class external_reader : public generic_data_reader {
     external_reader(const external_reader&);
     external_reader& operator=(const external_reader&);
     ~external_reader() override;
-    
+
     external_reader* copy() const override;
-    
+
     std::string get_type() const override;
-    
-    void set_has_labels(bool);
-    void set_has_responses(bool);
-    
+
+//    void set_has_labels(bool);
+//    void set_has_responses(bool);
+
     void load() override;
-    
-    int get_num_labels() const override;
+
+//    int get_num_labels() const override;
     int get_linearized_data_size() const override;
-    int get_linearized_label_size() const override;
-    
+//    int get_linearized_label_size() const override;
+
     const std::vector<int> get_data_dims() const override;
-    
+
   protected:
     bool fetch_datum(Mat& X, int data_id, int mb_idx, int tid) override;
-    bool fetch_label(Mat& Y, int data_id, int mb_idx, int tid) override;
-    bool fetch_response(Mat& Y, int data_id, int mb_idx, int tid) override;
-    
+//    bool fetch_label(Mat& Y, int data_id, int mb_idx, int tid) override;
+//    bool fetch_response(Mat& Y, int data_id, int mb_idx, int tid) override;
+
     int m_num_samples = -1;
     int m_num_features = -1;
     int m_num_labels = -1;
     bool m_has_labels = false;
     bool m_has_responses = false;
-    
+
     int m_label_count = -1;
     int m_data_size = -1;
     int m_label_size = -1;
     std::vector<int> m_dims;
-    
-    int m_lbann_to_external_fd = -1;
-    int m_external_to_lbann_fd = -1;
-    bool m_connected = false;
-    
-    std::string m_lbann_comm_dir;
-    
-    std::string m_lbann_to_external_file;
-    std::string m_external_to_lbann_file;
 
   private:
-    mutable uint8_t *m_write_buffer = nullptr;
-    mutable size_t m_write_buffer_size = 0;
-    
-    mutable uint8_t *m_read_buffer = nullptr;
-    mutable size_t m_read_buffer_size = 0;
-
     void connect();
     void disconnect();
-    
+
     void get_config();
 
     Response message_read() const;
     bool message_write(Request request) const;
 
-};    
+    bool m_connected = false;
+
+    std::string m_lbann_comm_dir;
+
+    std::string m_lbann_to_external_file;
+    std::string m_external_to_lbann_file;
+
+    int m_lbann_to_external_fd = -1;
+    int m_external_to_lbann_fd = -1;
+
+    // read_message/write_message are visibly const, but they resize their
+    // buffers to fit the largest message they've seen
+    mutable uint8_t *m_write_buffer = nullptr;
+    mutable size_t m_write_buffer_size = 0;
+
+    mutable uint8_t *m_read_buffer = nullptr;
+    mutable size_t m_read_buffer_size = 0;
+
+    // ensure that writes are paired with a read
+    mutable std::mutex m_read_write_completion;
+};
 }  // namespace lbann
 
 #endif  // LBANN_DATA_READER_EXTERNAL_HPP
--- src/data_readers/data_reader_external/data_reader_external.cpp
+++ src/data_readers/data_reader_external/data_reader_external.cpp
@@ -1,6 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014-2016, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+//
+// LLNL-CODE-697807.
+// All rights reserved.
+//
+// This file is part of LBANN: Livermore Big Artificial Neural Network
+// Toolkit. For details, see http://software.llnl.gov/LBANN or
+// https://github.com/LLNL/LBANN.
+//
+// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the license.
+//
+// lbann_data_reader_external .hpp .cpp - generic_data_reader class for data readers connected over IPC
+////////////////////////////////////////////////////////////////////////////////
+
 #include "lbann/data_readers/data_reader_external/data_reader_external.hpp"
 #include <cstdio>
 #include <string>
+
+// TODO all response sub-messages can probably be const &
 
 namespace lbann {
 
@@ -15,9 +45,13 @@ external_reader& external_reader::operator=(const external_reader& other) {
   return *this;
 }
 
+external_reader* external_reader::copy() const {
+  return new external_reader(*this);
+}
+
 external_reader::~external_reader() {
   disconnect();
-  
+
   if (m_write_buffer) {
     free(m_write_buffer);
     m_write_buffer = nullptr;
@@ -38,39 +72,44 @@ void external_reader::connect() {
   // if done out of that order, open will block or the external reader
   // won't be able to open the fifos
 
-  // Multiple data reader TODO: name these better, e.g. w/ tid
+  // TODO: name these better, e.g. w/ tid
   char lbann_comm_dir[] = "/tmp/lbann_comm.XXXXXX";
   char lbann_to_external_file[] = "/tmp/lbann_comm.XXXXXX/lbann_out";
   char external_to_lbann_file[] = "/tmp/lbann_comm.XXXXXX/lbann_in";
-  
+
   strcpy(lbann_comm_dir, mkdtemp(lbann_comm_dir));
-  
+
   if (strcmp(lbann_comm_dir, "") == 0) {
-    throw lbann_exception("mkdtemp failed");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::connect() - mkdtemp failed");
   }
 
   strncpy(lbann_to_external_file, lbann_comm_dir, strlen(lbann_comm_dir));
   strncpy(external_to_lbann_file, lbann_comm_dir, strlen(lbann_comm_dir));
-  
+
   int retval1, retval2;
   retval1 = mkfifo(lbann_to_external_file, 0600);
   retval2 = mkfifo(external_to_lbann_file, 0600);
   if (retval1 < 0 || retval2 < 0) {
-    throw lbann_exception("mkfifo failed");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::connect() - mkfifo failed");
   }
 
-  // TODO find some way to detect failure to launch data reader,
-  // currently it just doesn't work and lbann has to be killed
-  // https://stackoverflow.com/questions/1584956/how-to-handle-execvp-errors-after-fork
+  // TODO maybe https://stackoverflow.com/questions/1584956/how-to-handle-execvp-errors-after-fork
 
   pid_t child;
 
   switch (child = fork()) {
     case -1: {
-      throw lbann_exception("fork");
+      throw lbann_exception(
+        std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+        " external_reader::connect() - fork failed");
       break;
     }
     case 0: {
+      // TODO all of this should be configurable from a datareader prototext file
       char python[] = "python";
       char python_filename[] = "data_reader.py";
 
@@ -82,9 +121,11 @@ void external_reader::connect() {
       argv[4] = nullptr;
 
       execvp("python", argv);
-      
-      //execvp is noreturn if successful
-      throw lbann_exception("failed to run external data reader");
+
+      // execvp is noreturn if successful
+      throw lbann_exception(
+        std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+        " external_reader::connect() - exec failed, data reader not started");
       break;
     }
     default: {
@@ -97,10 +138,13 @@ void external_reader::connect() {
   m_external_to_lbann_fd = open(external_to_lbann_file, O_RDONLY);
 
   if (m_lbann_to_external_fd < 0 || m_external_to_lbann_fd < 0) {
-    throw lbann_exception("could not open fifo");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::connect() - could not open fifo");
   }
 
   // do these last, everything likes non-const c strings
+  // keep them for cleanup in disconnect
   m_lbann_comm_dir = lbann_comm_dir;
   m_lbann_to_external_file = lbann_to_external_file;
   m_external_to_lbann_file = external_to_lbann_file;
@@ -110,7 +154,16 @@ void external_reader::connect() {
 
 void external_reader::disconnect() {
   if (m_connected) {
-    // TODO send exit message
+
+    // kill external data reader
+    Request request;
+    request.mutable_exit_request();
+    {
+      std::lock_guard<std::mutex> lock{m_read_write_completion};
+      message_write(request);
+    }
+
+    // clean up /tmp/lbann_comm
     close(m_lbann_to_external_fd);
     close(m_external_to_lbann_fd);
 
@@ -123,70 +176,82 @@ void external_reader::disconnect() {
 
 void external_reader::load() {
   if (m_connected) {
-    throw lbann_exception("double load");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::load() - load called twice");
   }
   connect();
-    
+
+  // TODO this needs to have the path too
   std::string infile = get_data_filename();
 
   Request request;
   InitRequest* init_request = request.mutable_init_request();
-  
+
   init_request->set_filename(infile);
-  
-  message_write(request);
-  
-  
-  Response response = message_read();
-  if (!response.has_init_response()) {
-    throw lbann_exception("init response not received");
+
+  Response response;
+  {
+    std::lock_guard<std::mutex> lock{m_read_write_completion};
+    message_write(request);
+    response = message_read();
   }
-  
+
+  if (!response.has_init_response()) {
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::load() - incorrect message type received, expected init");
+  }
+
   InitResponse init_response = response.init_response();
 
   m_num_samples = init_response.num_samples();
   m_num_features = init_response.num_features();
   m_num_labels = init_response.num_labels();
-  
+
   m_has_labels = init_response.has_labels();
   m_has_responses = init_response.has_responses();
 
-  printf("%d %d %d\n", m_num_samples, m_num_features, m_num_labels);
-  
   // Reset indices.
   m_shuffled_indices.clear();
   m_shuffled_indices.resize(m_num_samples);
   std::iota(m_shuffled_indices.begin(), m_shuffled_indices.end(), 0);
   select_subset_of_data();
-  
+
+  // TODO make this a single message for all configuration data, instead of init+config
   get_config();
-//  volatile bool attached = false;
-//  while (!attached);
 }
 
 std::string external_reader::get_type() const {
   Request request;
   request.mutable_type_request();
-  message_write(request);
-  Response response = message_read();
+  Response response;
+  {
+    std::lock_guard<std::mutex> lock{m_read_write_completion};
+    message_write(request);
+    response = message_read();
+  }
   if (!response.has_type_response()) {
-    throw lbann_exception("type response not received");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::get_type() - incorrect message type received, expected type");
   }
   return std::string(response.type_response().type());
-}
-
-external_reader* external_reader::copy() const {
-  // TODO this should be fixed;
-  return new external_reader(*this);
 }
 
 void external_reader::get_config() {
   Request request;
   request.mutable_config_request();
-  message_write(request);
-  Response response = message_read();
+  Response response;
+  {
+    std::lock_guard<std::mutex> lock{m_read_write_completion};
+    message_write(request);
+    response = message_read();
+  }
   if (!response.has_config_response()) {
-    throw lbann_exception("config response not received");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::get_config() - incorrect message type received, expected config");
   }
   ConfigResponse config_response = response.config_response();
   m_label_count = config_response.label_count();
@@ -197,24 +262,28 @@ void external_reader::get_config() {
   }
 }
 
-int external_reader::get_num_labels() const {
-  return m_label_count;
-}
-
 int external_reader::get_linearized_data_size() const {
   return m_data_size;
-}
-
-int external_reader::get_linearized_label_size() const {
-  return m_label_size;
 }
 
 const std::vector<int> external_reader::get_data_dims() const {
   return m_dims;
 }
 
+// TODO test with something that has labels/responses
+/*
+int external_reader::get_num_labels() const {
+  return m_label_count;
+}
+
+int external_reader::get_linearized_label_size() const {
+  return m_label_size;
+}
+*/
+
 Response external_reader::message_read() const {
-  printf("message_read\n");
+  // Read a message from the open named pipe
+
   size_t response_size = 0;
   uint8_t size_bytes[4];
   read(m_external_to_lbann_fd, size_bytes, 4);
@@ -222,45 +291,36 @@ Response external_reader::message_read() const {
   response_size |= size_bytes[1] << 16;
   response_size |= size_bytes[2] <<  8;
   response_size |= size_bytes[3];
-  
+
   if (response_size > m_read_buffer_size) {
     m_read_buffer = static_cast<uint8_t*>(realloc(m_read_buffer, response_size));
     m_read_buffer_size = response_size;
   }
   read(m_external_to_lbann_fd, m_read_buffer, response_size);
-    
+
   Response response;
   response.ParseFromArray(m_read_buffer, response_size);
-  
-  std::cout << "config " << response.has_config_response() << std::endl;
-  std::cout << "datum " <<  response.has_fetch_datum_response() << std::endl;
-  std::cout << "init " <<  response.has_init_response() << std::endl;
-  std::cout << "type " << response.has_type_response() << std::endl;
-  
+
   return response;
 }
 
 bool external_reader::message_write(Request request) const {
-  printf("message_write\n");
-  std::cout << "config " << request.has_config_request() << std::endl;
-  std::cout << "datum " <<  request.has_fetch_datum_request() << std::endl;
-  std::cout << "init " <<  request.has_init_request() << std::endl;
-  std::cout << "type " << request.has_type_request() << std::endl;
+  // Write a message to the open named pipe
 
   size_t request_size = request.ByteSizeLong();
   if (request_size > m_write_buffer_size) {
     m_write_buffer = static_cast<uint8_t*>(realloc(m_write_buffer, request_size));
     m_write_buffer_size = request_size;
   }
-  
+
   request.SerializeToArray(m_write_buffer, request_size);
-  
+
   uint8_t size_bytes[4];
   size_bytes[0] = request_size >> 24;
   size_bytes[1] = request_size >> 16;
   size_bytes[2] = request_size >>  8;
   size_bytes[3] = request_size;
-  
+
   bool ok = (write(m_lbann_to_external_fd, size_bytes, 4) == 4);
   ok &=     (write(m_lbann_to_external_fd, m_write_buffer, request_size) == static_cast<ssize_t>(request_size));
   return ok;
@@ -272,26 +332,35 @@ bool external_reader::fetch_datum(Mat& X, int data_id, int mb_idx, int tid) {
   fetch_datum_request->set_data_id(data_id);
   fetch_datum_request->set_mb_idx(mb_idx);
   fetch_datum_request->set_tid(tid);
-  
-  message_write(request);
-  
-  Response response = message_read();
+
+  Response response;
+  {
+    std::lock_guard<std::mutex> lock{m_read_write_completion};
+    message_write(request);
+    response = message_read();
+  }
   if (!response.has_fetch_datum_response()) {
-    throw lbann_exception("datum response not received");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::fetch_datum() - incorrect message type received, expected fetch_datum");
   }
-  
+
   FetchDatumResponse fetch_datum_response = response.fetch_datum_response();
-  
+
   if (fetch_datum_response.datum_size() != m_num_features) {
-    throw lbann_exception("datum of incorrect size");
+    throw lbann_exception(
+      std::string{} + __FILE__ + " " + std::to_string(__LINE__) +
+      " external_reader::fetch_datum() - incorrect dimensionality for received data");
   }
-  
-  for (int i = 0; i < fetch_datum_response.datum_size(); i++) {\
+
+  for (int i = 0; i < fetch_datum_response.datum_size(); i++) {
     X(i, mb_idx) = fetch_datum_response.datum(i);
   }
   return true;
 }
 
+// TODO finish these
+/*
 bool external_reader::fetch_label(Mat& Y, int data_id, int mb_idx, int tid) {
   if (!m_has_labels) {
     throw lbann_exception("external_reader: do not have labels");
@@ -311,5 +380,5 @@ bool external_reader::fetch_response(Mat& Y, int data_id, int mb_idx, int tid) {
 
   return true;
 }
-
+*/
 }  // namespace lbann
