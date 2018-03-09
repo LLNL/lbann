1a37b08beaed406430d66b492b970c6ba0f95090
Jae-Seung Yeom <yeom2@llnl.gov>
2018-03-08 16:01:24 -0800

    fixing the compilation error with clang 'using integer absolute value function 'abs' when argument is of floating point type'

--- src/objective_functions/loss_functions/mean_absolute_error.cpp
+++ src/objective_functions/loss_functions/mean_absolute_error.cpp
@@ -49,7 +49,7 @@ EvalType mean_absolute_error_loss::evaluate_compute(const AbsDistMat& prediction
       const EvalType true_val = ground_truth_local(row, col);
       const EvalType pred_val = predictions_local(row, col);
       const EvalType error = true_val - pred_val;
-      sum += abs(error);
+      sum += std::abs(error);
     }
   }
 
