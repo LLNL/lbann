fa37ae1498910b8e15454e9c6185ad39687e702d
Brian C. Van Essen <vanessen1@llnl.gov>
2018-03-20 11:53:02 -0700

    Update the primary layer class to return local AbstractMatrices and to take template arguments for matrix instantiation for the device allocation.

--- include/lbann/layers/layer.hpp
+++ include/lbann/layers/layer.hpp
@@ -154,6 +154,13 @@ class Layer {
    *  should override this function to return its template parameter.
    */
   virtual data_layout get_data_layout() const = 0;
+  /** Get the device allocation for the data tensors.
+   *  We assume that the decice allocation of the previous activations,
+   *  activations, previous error signals, and error signals are the
+   *  same. Each concrete layer that is templated on its device allocation
+   *  should override this function to return its template parameter.
+   */
+  virtual El::Device get_device_allocation() const = 0;
   /** Get a human-readable description of the data_layout */
   std::string get_data_layout_string(data_layout d) const;
 
@@ -212,7 +219,7 @@ class Layer {
 
   virtual bool save_to_checkpoint_shared(persist& p,bool val_end) const;
   virtual bool load_from_checkpoint_shared(persist& p);
-  
+
   /** Write layer to proto file */
   virtual void write_proto(lbann_data::Layer* proto) const;
 
@@ -326,21 +333,21 @@ class Layer {
   /** Get error signal tensor. (const) */
   const AbsDistMat& get_error_signals(int parent_index = 0) const;
   /** Get local portion of previous activation tensor. */
-  Mat& get_local_prev_activations(int parent_index = 0);
+  AbsMat& get_local_prev_activations(int parent_index = 0);
   /** Get local portion of activation tensor. */
-  Mat& get_local_activations(int child_index = 0);
+  AbsMat& get_local_activations(int child_index = 0);
   /** Get local portion of previous error signal tensor. */
-  Mat& get_local_prev_error_signals(int child_index = 0);
+  AbsMat& get_local_prev_error_signals(int child_index = 0);
   /** Get local portion of error signal tensor. */
-  Mat& get_local_error_signals(int parent_index = 0);
+  AbsMat& get_local_error_signals(int parent_index = 0);
   /** Get local portion of previous activation tensor. (const) */
-  const Mat& get_local_prev_activations(int parent_index = 0) const;
+  const AbsMat& get_local_prev_activations(int parent_index = 0) const;
   /** Get local portion of activation tensor. (const) */
-  const Mat& get_local_activations(int child_index = 0) const;
+  const AbsMat& get_local_activations(int child_index = 0) const;
   /** Get local portion of previous error signal tensor. (const) */
-  const Mat& get_local_prev_error_signals(int child_index = 0) const;
+  const AbsMat& get_local_prev_error_signals(int child_index = 0) const;
   /** Get local portion of error signal tensor. (const) */
-  const Mat& get_local_error_signals(int parent_index = 0) const;
+  const AbsMat& get_local_error_signals(int parent_index = 0) const;
 
   /** Get reference to cuDNN manager. */
   cudnn::cudnn_manager* get_cudnn_manager() { return m_cudnn; }
@@ -529,7 +536,7 @@ class Layer {
  private:
 
   /** Instantiate distributed matrices. */
-  template <data_layout T>
+  template <data_layout T, El::Device Dev>
   void instantiate_matrices(const El::Grid& grid);
 
   /** Deallocate distributed matrices. */
