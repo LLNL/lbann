d8097c6b003202fcdf0d5e283b6766fbbac5af84
Tim Moon <moon13@llnl.gov>
2018-04-27 11:47:51 -0700

    Follow-up changes to 801ccc8

--- src/objective_functions/weight_regularization/l2.cpp
+++ src/objective_functions/weight_regularization/l2.cpp
@@ -116,13 +116,13 @@ void l2_weight_regularization::start_evaluation() {
       CHECK_CUDA(cudaSetDevice(m_cudnn->get_gpu(gpu)));
       auto&& handle = m_cudnn->get_cublas_handle(gpu);
       CHECK_CUBLAS(cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE));
-      const auto& weights_start = std::min(gpu * weights_per_gpu, num_weights);
-      const auto& weights_end   = std::min((gpu+1) * weights_per_gpu, num_weights);
+      const int weights_start = std::min(gpu * weights_per_gpu, num_weights);
+      const int weights_end   = std::min((gpu+1) * weights_per_gpu, num_weights);
       for (int i = weights_start; i < weights_end; ++i) {
         const auto& w = m_weights[i];
         if (w->get_cudnn_manager() != nullptr) {
           const auto values_d = w->get_values_gpu()[gpu];
-          auto&& sqsum_d = sqsums_d.get_data(gpu) + i - weights_start;
+          auto sqsum_d = sqsums_d.get_data(gpu) + i - weights_start;
           cublas::dot(handle, w->get_size(), values_d, 1, values_d, 1, sqsum_d);
         }
       }
