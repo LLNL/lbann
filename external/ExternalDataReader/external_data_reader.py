07fe3e65a8bbef173b500293bf11ec536e66638f
Corey McNeish <mcneish1@llnl.gov>
2018-05-15 11:32:59 -0700

    Refactor communication in e_d_r.py a little

--- external/ExternalDataReader/external_data_reader.py
+++ external/ExternalDataReader/external_data_reader.py
@@ -9,19 +9,11 @@ class ExternalDataReader(object):
     def __init__(self, connection, address):
         'Initialize external data reader'
         self.connection = Connection(connection, address)
-
         self.running = True
-
         self.params = None
 
-    def receive_init_request(self):
-        data = self.connection.recv_message()
-        message = Request()
-        message.ParseFromString(data)
-        assert (message.HasField('init_request'))
-        return message.init_request
-
     def handle_init_request(self, message):
+        assert (message.HasField('init_request'))
         response = Response()
         response.init_response.num_samples = self.params["num_samples"]
         response.init_response.data_size = self.params["data_size"]
@@ -35,18 +27,14 @@ class ExternalDataReader(object):
         response.init_response.reader_type = self.params["reader_type"]
         return response
 
-    def send_init_response(self, message):
+    def send_response(self, message):
         data = message.SerializeToString()
         self.connection.send_message(data)
 
-    def receive_data_request(self):
+    def receive_request(self):
         data = self.connection.recv_message()
         message = Request()
         message.ParseFromString(data)
-        if not (message.HasField("fetch_data_request") or
-                message.HasField("fetch_responses_request") or
-                message.HasField("fetch_labels_request")):
-            raise RuntimeError("expected data/label/response request")
         return message
 
     def handle_data_request(self, message):
@@ -59,19 +47,21 @@ class ExternalDataReader(object):
                 f = FloatSamples(float_samples=datum)
                 response_message.fetch_data_response.data.samples.add(float_values=f)
         elif message.HasField("fetch_labels_request"):
-            indices = message.fetch_data_request.indices.value
+            indices = message.fetch_labels_request.indices.value
             labels = self.params["labels"][indices]
             response_message = Response()
             for label in labels:
-                f = FloatSamples(bool_samples=label)
-                response_message.fetch_data_response.data.samples.add(float_values=f)
+                f = BoolSamples(bool_samples=label)
+                response_message.fetch_labels_response.labels.samples.add(bool_values=b)
         elif message.HasField("fetch_responses_request"):
-            indices = message.fetch_data_request.indices.value
+            indices = message.fetch_responses_request.indices.value
             responses = self.params["responses"][indices]
             response_message = Response()
             for response in responses:
                 f = FloatSamples(float_samples=response)
-                response_message.fetch_data_response.data.samples.add(float_values=f)
+                response_message.fetch_responses_response.responses.samples.add(float_values=f)
+        else:
+            raise ValueError("expected data/label/response request")
         return response_message
 
     def send_data_response(self, message):
@@ -80,12 +70,12 @@ class ExternalDataReader(object):
 
     def run(self):
         # Init
-        request = self.receive_init_request()
+        request = self.receive_request()
         response = self.handle_init_request(request)
-        self.send_init_response(response)
+        self.send_response(response)
 
         while self.running:
-            # TODO add hangup request so things close cleanly
-            request = self.receive_data_request()
+            # TODO add hangup request so things close cleanly?
+            request = self.receive_request()
             response = self.handle_data_request(request)
-            self.send_data_response(response)
+            self.send_response(response)
