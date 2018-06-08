942aa32e99cf108751b5a755ee6f624e06955b2b
Brian C. Van Essen <vanessen1@llnl.gov>
2018-06-08 14:45:40 -0700

    Updated the input layer to have a data reader target mode that can be set to classification, regression, or reconstruction mode.  Based on the mode, the second output of the input layer will get the correct data.  This allows the reconstruction layer to have two inputs, and avoid having the original_layer pointer.  Note that the greedy layerwise model is temporarily broken and needs to deal with adding a split layer into the iterative model.

--- include/lbann/base.hpp
+++ include/lbann/base.hpp
@@ -122,6 +122,8 @@ enum class pool_mode {invalid, max, average, average_no_pad};
 /** returns a string representation of the pool_mode */
 std::string get_pool_mode_name(pool_mode m);
 
+enum class data_reader_target_mode {CLASSIFICATION, REGRESSION, RECONSTRUCTION};
+
 namespace lbann {
 
 // Forward-declaration.
