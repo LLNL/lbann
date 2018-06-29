ee2dbbcfce6d6a7ba0e0c2d05a450c1a9c7af6f3
Sam Ade Jacobs <jacobs32@llnl.gov>
2018-06-29 11:47:43 -0700

    temporarily undo commit of BCE and MSE losses

--- include/lbann/lbann.hpp
+++ include/lbann/lbann.hpp
@@ -91,8 +91,6 @@
 #include "lbann/layers/transform/stop_gradient.hpp"
 #include "lbann/layers/transform/max.hpp"
 #include "lbann/layers/transform/min.hpp"
-#include "lbann/layers/transform/binary_cross_entropy.hpp"
-#include "lbann/layers/transform/mean_squared_error.hpp"
 
 /// Regularization layers.
 #include "lbann/layers/regularizers/local_response_normalization.hpp"
