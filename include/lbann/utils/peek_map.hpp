a917b5c031202bb8dce3af93b6feae1c8122e587
Jae-Seung Yeom <yeom2@llnl.gov>
2018-07-04 20:15:08 -0700

    - removed the allocator template parameter, which is rarely used, to make the code more clear. - added the same set of interfaces for unordered_map

--- include/lbann/utils/peek_map.hpp
+++ include/lbann/utils/peek_map.hpp
@@ -37,23 +37,21 @@ namespace lbann {
 /**
  * A utility function to peek into a std::map without the side effect of adding
  * the default value when a key is not found.
- * It has the the template parameter list as std::map does.
+ * It has the the template parameter list as std::map does except the allocator.
  * Searches 'idx' in std::map 'map_to_peek', and returns the matching value if
  * found or the default otherwise. 'found' is set to true when the key 'idx' is
  * found, or false when not.
  */
 template < class KEY_T,                    // map::key_type
            class VAL_T,                    // map::mapped_type
-           class CMP_T = std::less<KEY_T>, // map::key_compare
-           class ALC_T = std::allocator<std::pair<const KEY_T, VAL_T> > // map::allocator_type
+           class CMP_T = std::less<KEY_T>  // map::key_compare
          >
-VAL_T peek_map(const std::map<KEY_T, VAL_T, CMP_T, ALC_T>& map_to_peek, KEY_T idx, bool& found) {
-  using map_to_peek_t = std::map<KEY_T, VAL_T, CMP_T, ALC_T>;
+VAL_T peek_map(const std::map<KEY_T, VAL_T, CMP_T>& map_to_peek, KEY_T idx, bool& found) {
+  using map_to_peek_t = std::map<KEY_T, VAL_T, CMP_T>;
   typename map_to_peek_t::const_iterator it = map_to_peek.find(idx);
   if (it == map_to_peek.cend()) {
-    map_to_peek_t tmp;
     found = false;
-    return tmp[idx];
+    return VAL_T();
   }
   found = true;
   return it->second;
@@ -65,15 +63,55 @@ VAL_T peek_map(const std::map<KEY_T, VAL_T, CMP_T, ALC_T>& map_to_peek, KEY_T id
  */
 template < class KEY_T,                    // map::key_type
            class VAL_T,                    // map::mapped_type
-           class CMP_T = std::less<KEY_T>, // map::key_compare
-           class ALC_T = std::allocator<std::pair<const KEY_T, VAL_T> > // map::allocator_type
+           class CMP_T = std::less<KEY_T>  // map::key_compare
          >
-VAL_T peek_map(const std::map<KEY_T, VAL_T, CMP_T, ALC_T>& map_to_peek, KEY_T idx) {
-  using map_to_peek_t = std::map<KEY_T, VAL_T, CMP_T, ALC_T>;
+VAL_T peek_map(const std::map<KEY_T, VAL_T, CMP_T>& map_to_peek, KEY_T idx) {
+  using map_to_peek_t = std::map<KEY_T, VAL_T, CMP_T>;
   typename map_to_peek_t::const_iterator it = map_to_peek.find(idx);
   if (it == map_to_peek.cend()) {
-    map_to_peek_t tmp;
-    return tmp[idx];
+    return VAL_T();
+  }
+  return it->second;
+}
+
+/**
+ * A utility function to peek into a std::unordered_map without the side effect of adding
+ * the default value when a key is not found.
+ * It has the the template parameter list as std::unordered_map does except the allocator.
+ * Searches 'idx' in std::unordered_map 'map_to_peek', and returns the matching value if
+ * found or the default otherwise. 'found' is set to true when the key 'idx' is
+ * found, or false when not.
+ */
+template < class KEY_T,                         // unordered_map::key_type
+	   class VAL_T,                         // unordered_map::mapped_type
+           class HASH_T = std::hash<KEY_T>,     // unordered_map::hasher
+           class KEYeq_T = std::equal_to<KEY_T> // unordered_map::key_equal
+	 >
+VAL_T peek_map(const std::unordered_map<KEY_T, VAL_T, HASH_T, KEYeq_T>& map_to_peek, KEY_T idx, bool& found) {
+  using map_to_peek_t = std::unordered_map<KEY_T, VAL_T, HASH_T, KEYeq_T>;
+  typename map_to_peek_t::const_iterator it = map_to_peek.find(idx);
+  if (it == map_to_peek.cend()) {
+    found = false;
+    return VAL_T();
+  }
+  found = true;
+  return it->second;
+}
+
+/**
+ * Same as the other peek_map interface but does not require the third argument
+ * that indicates the success of searching.
+ */
+template < class KEY_T,                         // unordered_map::key_type
+	   class VAL_T,                         // unordered_map::mapped_type
+           class HASH_T = std::hash<KEY_T>,     // unordered_map::hasher
+           class KEYeq_T = std::equal_to<KEY_T> // unordered_map::key_equal
+	 >
+VAL_T peek_map(const std::unordered_map<KEY_T, VAL_T, HASH_T, KEYeq_T>& map_to_peek, KEY_T idx) {
+  using map_to_peek_t = std::unordered_map<KEY_T, VAL_T, HASH_T, KEYeq_T>;
+  typename map_to_peek_t::const_iterator it = map_to_peek.find(idx);
+  if (it == map_to_peek.cend()) {
+    return VAL_T();
   }
   return it->second;
 }
