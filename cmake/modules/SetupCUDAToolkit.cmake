d655d7acf90d720b9c6c5b3a3d09d3c8f048e955
Thomas R. Benson <benson31@llnl.gov>
2018-08-06 14:31:00 -0700

    fix an issue in the way we setup the cuda::toolkit imported target

--- cmake/modules/SetupCUDAToolkit.cmake
+++ cmake/modules/SetupCUDAToolkit.cmake
@@ -7,16 +7,14 @@ find_package(NVTX REQUIRED)
 find_package(cuDNN REQUIRED)
 
 if (NOT TARGET cuda::toolkit)
-
   add_library(cuda::toolkit INTERFACE IMPORTED)
+endif ()
 
-  set_property(TARGET cuda::toolkit PROPERTY
-    INTERFACE_LINK_LIBRARIES cuda::cub cuda::cudnn cuda::nvtx "${CUDA_CUBLAS_LIBRARIES}")
+set_property(TARGET cuda::toolkit PROPERTY
+  INTERFACE_LINK_LIBRARIES cuda::cub cuda::cudnn cuda::nvtx "${CUDA_CUBLAS_LIBRARIES}")
 
-  set_property(TARGET cuda::toolkit PROPERTY
-    INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CUDA>:-arch=sm_30>)
+set_property(TARGET cuda::toolkit PROPERTY
+  INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CUDA>:-arch=sm_30>)
 
-  set_property(TARGET cuda::toolkit PROPERTY
-    INTERFACE_INCLUDE_DIRECTORIES "${CUDA_INCLUDE_DIRS}")
-
-endif()
+set_property(TARGET cuda::toolkit PROPERTY
+  INTERFACE_INCLUDE_DIRECTORIES "${CUDA_INCLUDE_DIRS}")
