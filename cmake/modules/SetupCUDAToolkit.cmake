fc1c39bfbba9a02b45ad351c924d449e6a4e910e
Thomas R. Benson <benson31@llnl.gov>
2018-08-06 15:04:36 -0700

    append things to the cuda::toolkit properties instead of overwrite

--- cmake/modules/SetupCUDAToolkit.cmake
+++ cmake/modules/SetupCUDAToolkit.cmake
@@ -10,11 +10,11 @@ if (NOT TARGET cuda::toolkit)
   add_library(cuda::toolkit INTERFACE IMPORTED)
 endif ()
 
-set_property(TARGET cuda::toolkit PROPERTY
+set_property(TARGET cuda::toolkit APPEND PROPERTY
   INTERFACE_LINK_LIBRARIES cuda::cub cuda::cudnn cuda::nvtx "${CUDA_CUBLAS_LIBRARIES}")
 
-set_property(TARGET cuda::toolkit PROPERTY
+set_property(TARGET cuda::toolkit APPEND PROPERTY
   INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CUDA>:-arch=sm_30>)
 
-set_property(TARGET cuda::toolkit PROPERTY
+set_property(TARGET cuda::toolkit APPEND PROPERTY
   INTERFACE_INCLUDE_DIRECTORIES "${CUDA_INCLUDE_DIRS}")
