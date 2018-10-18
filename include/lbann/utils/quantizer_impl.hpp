a04cb260380ff5689c1dcd518ab0e82904b54f4f
Brian C. Van Essen <vanessen1@llnl.gov>
2018-10-17 21:30:02 -0700

    Updated the OMP pragmas in the quantizer.

--- include/lbann/utils/quantizer_impl.hpp
+++ include/lbann/utils/quantizer_impl.hpp
@@ -144,9 +144,9 @@ void lbann_quantizer::adaptive_quantize(
   colT total_quantized = std::accumulate(quantized_counts.begin(),
                                          quantized_counts.end(), 0);
   q.resize(header_len + total_quantized);
-  // const int num_copy_threads =
-  //   get_adaptive_quantization_copy_threads(width);
-  LBANN_OMP_PARALLEL_FOR
+  const int num_copy_threads =
+    get_adaptive_quantization_copy_threads(width);
+  LBANN_OMP_PARALLEL_FOR_ARGS(schedule(dynamic, 1) num_threads(num_copy_threads))
   for (unsigned tid = 0; tid < thread_qs.size(); ++tid) {
     std::copy(thread_qs[tid].begin(),
               thread_qs[tid].begin() + quantized_counts[tid],
@@ -187,9 +187,8 @@ void lbann_quantizer::adaptive_unquantize(
 #endif
   const colT ldim = mat.LDim();
   const auto *q_col = (const colT *) q;
-  //  const int num_threads = get_adaptive_quantization_threads(mat.Width());
-  //#pragma omp taskloop default(shared) num_tasks(1) firstprivate(header_len, buf)
-  LBANN_OMP_PARALLEL_FOR_ARGS(firstprivate(header_len, buf))
+  const int num_threads = get_adaptive_quantization_threads(mat.Width());
+  LBANN_OMP_PARALLEL_FOR_ARGS(schedule(dynamic, 1) firstprivate(header_len, buf) num_threads(num_threads))
   for (colT header_loc = 0; header_loc < header_len; header_loc += HEADER_FACTOR) {
     const colT col_offset = (header_loc / HEADER_FACTOR) * ldim;
     // Extract averages.
@@ -241,8 +240,8 @@ void lbann_quantizer::adaptive_unquantize_add(
 #endif
   const colT ldim = mat.LDim();
   const auto *q_col = (const colT *) q;
-  //  const int num_threads = get_adaptive_quantization_threads(mat.Width());
-  LBANN_OMP_PARALLEL_FOR_ARGS(firstprivate(header_len, buf))
+  const int num_threads = get_adaptive_quantization_threads(mat.Width());
+  LBANN_OMP_PARALLEL_FOR_ARGS(schedule(dynamic, 1) firstprivate(header_len, buf) num_threads(num_threads))
   for (colT header_loc = 0; header_loc < header_len; header_loc += HEADER_FACTOR) {
     const colT col_offset = (header_loc / HEADER_FACTOR) * ldim;
     // Extract averages.
@@ -379,9 +378,9 @@ void lbann_quantizer::adaptive_quantize_replace(
   colT total_quantized = std::accumulate(quantized_counts.begin(),
                                          quantized_counts.end(), 0);
   q.resize(header_len + total_quantized);
-  // const int num_copy_threads =
-  //   get_adaptive_quantization_copy_threads(width);
-  LBANN_OMP_PARALLEL_FOR
+  const int num_copy_threads =
+    get_adaptive_quantization_copy_threads(width);
+  LBANN_OMP_PARALLEL_FOR_ARGS(schedule(dynamic, 1) num_threads(num_copy_threads))
     //#pragma omp taskloop default(shared)
   for (unsigned tid = 0; tid < thread_qs.size(); ++tid) {
     std::copy(thread_qs[tid].begin(),
