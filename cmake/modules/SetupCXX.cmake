732b745dd3e512f58e067d7fc7a932206019aa2a
Thomas R. Benson <benson31@llnl.gov>
2018-11-09 14:13:19 -0800

    Correct previous

--- cmake/modules/SetupCXX.cmake
+++ cmake/modules/SetupCXX.cmake
@@ -85,8 +85,8 @@ if (${UPPER_PROJECT_NAME}_WARNINGS_AS_ERRORS)
   separate_arguments(_WERROR_FLAGS NATIVE_COMMAND "${_WERROR_FLAGS}")
   if (NOT TARGET LBANN_CXX_FLAGS_werror)
     add_library(LBANN_CXX_FLAGS_werror INTERFACE)
-    target_compile_options(LBANN_CXX_FLAGS_werror
-      $<$<COMPILE_LANGUAGE:CXX>:${_WERROR_FLAGS}>)
+    set_property(TARGET LBANN_CXX_FLAGS_werror PROPERTY
+      INTERFACE_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CXX>:${_WERROR_FLAGS}>)
 
     # Add the "library" to the export
     install(TARGETS LBANN_CXX_FLAGS_werror EXPORT LBANNTargets)
