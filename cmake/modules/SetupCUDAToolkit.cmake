4a05e43fc29a53373936cef4d4af3dd29f575a03
Thomas R. Benson <benson31@llnl.gov>
2018-11-08 16:02:08 -0800

    patches to LBANNConfig

--- cmake/modules/SetupCUDAToolkit.cmake
+++ cmake/modules/SetupCUDAToolkit.cmake
@@ -5,7 +5,6 @@ if (NOT CUDA_FOUND)
   find_package(CUDA REQUIRED)
 endif ()
 
-find_package(CUB REQUIRED)
 find_package(NVTX REQUIRED)
 find_package(cuDNN REQUIRED)
 
@@ -14,7 +13,7 @@ if (NOT TARGET cuda::toolkit)
 endif ()
 
 set_property(TARGET cuda::toolkit APPEND PROPERTY
-  INTERFACE_LINK_LIBRARIES cuda::cub cuda::cudnn cuda::nvtx
+  INTERFACE_LINK_LIBRARIES cuda::cudnn cuda::nvtx
   "${CUDA_CUBLAS_LIBRARIES}" "${CUDA_CUDA_LIBRARY}")
 
 set_property(TARGET cuda::toolkit APPEND PROPERTY
