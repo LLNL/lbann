23a878b308482e1e6674fbd2fa4c57b43f4c221b
Brian C. Van Essen <vanessen1@llnl.gov>
2018-11-19 15:45:25 -0800

    Cleaned up the OpenMP pragmas.

--- include/lbann/utils/omp_pragma.hpp
+++ include/lbann/utils/omp_pragma.hpp
@@ -30,36 +30,38 @@
 #include "lbann_config.hpp"
 #include <omp.h>
 
-//#define LBANN_HAVE_OMP_TASKLOOP HYDROGEN_HAVE_OMP_TASKLOOP
+/// Allow OpenMP parallel for loops to be replaced with taskloop constructs
+/// Requires OpenMP 5.0 support for taskloop reduction clauses
+#if defined(HYDROGEN_HAVE_OMP_TASKLOOP)
+  #define LBANN_HAVE_OMP_TASKLOOP
+#endif
+
 #if defined(LBANN_HAVE_OMP_TASKLOOP)
-#define LBANN_OMP_PARALLEL_FOR_HELPER(arg) #arg
-#define LBANN_OMP_PARALLEL_FOR_TEXT(arg) LBANN_OMP_PARALLEL_FOR_HELPER(omp taskloop default(shared) num_tasks(omp_get_num_threads()) arg)
-#define LBANN_OMP_PARALLEL_FOR_ARGS(arg) _Pragma(LBANN_OMP_PARALLEL_FOR_TEXT(arg))
+  #pragma message "Using OpenMP taskloops instead of parallel for loops"
+  #define LBANN_OMP_PARALLEL_FOR_HELPER(arg) #arg
+  #define LBANN_OMP_PARALLEL_FOR_TEXT(arg) LBANN_OMP_PARALLEL_FOR_HELPER(omp taskloop default(shared) num_tasks(omp_get_num_threads()) arg)
+  #define LBANN_OMP_PARALLEL_FOR_ARGS(arg) _Pragma(LBANN_OMP_PARALLEL_FOR_TEXT(arg))
 
-#define LBANN_OMP_PARALLEL_FOR _Pragma("omp taskloop default(shared) num_tasks(omp_get_num_threads())")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE2 _Pragma("omp taskloop collapse(2) default(shared) num_tasks(omp_get_num_threads())")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE3 _Pragma("omp taskloop collapse(3) default(shared) num_tasks(omp_get_num_threads())")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE4 _Pragma("omp taskloop collapse(4) default(shared) num_tasks(omp_get_num_threads())")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE5 _Pragma("omp taskloop collapse(5) default(shared) num_tasks(omp_get_num_threads())")
-
-//#define OMP_PARALLEL _Pragma("omp taskloop default(shared) num_tasks(omp_get_num_threads())")
+  #define LBANN_OMP_PARALLEL_FOR _Pragma("omp taskloop default(shared) num_tasks(omp_get_num_threads())")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE2 _Pragma("omp taskloop collapse(2) default(shared) num_tasks(omp_get_num_threads())")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE3 _Pragma("omp taskloop collapse(3) default(shared) num_tasks(omp_get_num_threads())")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE4 _Pragma("omp taskloop collapse(4) default(shared) num_tasks(omp_get_num_threads())")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE5 _Pragma("omp taskloop collapse(5) default(shared) num_tasks(omp_get_num_threads())")
 #else
-#define LBANN_OMP_PARALLEL_FOR_HELPER(arg) #arg
-#define LBANN_OMP_PARALLEL_FOR_TEXT(arg) LBANN_OMP_PARALLEL_FOR_HELPER(omp parallel for arg)
-#define LBANN_OMP_PARALLEL_FOR_ARGS(arg) _Pragma(LBANN_OMP_PARALLEL_FOR_TEXT(arg))
+  #define LBANN_OMP_PARALLEL_FOR_HELPER(arg) #arg
+  #define LBANN_OMP_PARALLEL_FOR_TEXT(arg) LBANN_OMP_PARALLEL_FOR_HELPER(omp parallel for arg)
+  #define LBANN_OMP_PARALLEL_FOR_ARGS(arg) _Pragma(LBANN_OMP_PARALLEL_FOR_TEXT(arg))
 
-#define LBANN_OMP_PARALLEL_FOR _Pragma("omp parallel for")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE2 _Pragma("omp parallel for collapse(2)")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE3 _Pragma("omp parallel for collapse(3)")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE4 _Pragma("omp parallel for collapse(4)")
-#define LBANN_OMP_PARALLEL_FOR_COLLAPSE5 _Pragma("omp parallel for collapse(5)")
-
-//#define OMP_PARALLEL _Pragma("omp parallel for")
+  #define LBANN_OMP_PARALLEL_FOR _Pragma("omp parallel for")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE2 _Pragma("omp parallel for collapse(2)")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE3 _Pragma("omp parallel for collapse(3)")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE4 _Pragma("omp parallel for collapse(4)")
+  #define LBANN_OMP_PARALLEL_FOR_COLLAPSE5 _Pragma("omp parallel for collapse(5)")
+#endif
 
 #define LBANN_OMP_PARALLEL_HELPER(arg) #arg
 #define LBANN_OMP_PARALLEL_TEXT(arg) LBANN_OMP_PARALLEL_HELPER(omp parallel arg)
 #define LBANN_OMP_PARALLEL_ARGS(arg) _Pragma(LBANN_OMP_PARALLEL_TEXT(arg))
-#endif
 
 #define LBANN_OMP_PARALLEL _Pragma("omp parallel")
 #define OMP_CRITICAL _Pragma("omp critical")
