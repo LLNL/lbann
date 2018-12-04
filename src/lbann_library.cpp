f34cf47b3696a053febf1e0a713677032dc3b6d6
Brian C. Van Essen <vanessen1@llnl.gov>
2018-12-04 11:34:50 -0800

    Merged in develops changes

--- src/lbann_library.cpp
+++ src/lbann_library.cpp
@@ -159,16 +159,11 @@ model * __attribute__((used)) build_model_from_prototext(int argc, char **argv,
 
     if (comm->am_world_master()) {
       std::cout << std::endl;
+      model->print_description(std::cout);
       std::cout << "Callbacks:" << std::endl;
       for (lbann_callback *cb : model->get_callbacks()) {
         std::cout << cb->name() << std::endl;
       }
-      std::cout << std::endl;
-      const std::vector<Layer *>& layers = model->get_layers();
-      for (size_t h=0; h<layers.size(); h++) {
-        std::cout << h << " " << layers[h]->get_description() << std::endl;
-      }
-    }
 
 #ifndef LBANN_DETERMINISTIC
       // Under normal conditions, reinitialize the random number generator so
