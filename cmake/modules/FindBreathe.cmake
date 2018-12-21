2d4554f13c3fecbed5659d02f25e9c45262cc098
Thomas R. Benson <benson31@llnl.gov>
2018-12-21 13:21:36 -0800

    Do a simple search for breathe if building with sphinx.

---
+++ cmake/modules/FindBreathe.cmake
@@ -0,0 +1,18 @@
+# Exports the following variables:
+#
+#   BREATHE_EXECUTABLE
+#   BREATHE_FOUND
+#
+
+find_program(BREATHE_EXECUTABLE breathe-apidoc
+  HINTS ${BREATHE_DIR} $ENV{BREATHE_DIR}
+  ${SPHINX_DIR} $ENV{SPHINX_DIR}
+  PATH_SUFFIXES bin
+  DOC "The breathe documentation tool."
+  NO_DEFAULT_PATH)
+find_program(BREATHE_EXECUTABLE breathe-build)
+
+# Standard handling of the package arguments
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(BREATHE
+  DEFAULT_MSG BREATHE_EXECUTABLE)
