ac8263d0955471d9105fb0cab3556a2c470c5d71
David A. Hysom <hysom@llnl.gov>
2019-01-10 12:27:57 -0800

    Revising select_samples to deal with inclusion and exclusion lists correctly, and replcing indices with sample IDs

--- model_zoo/jag_utils/build_index.cpp
+++ model_zoo/jag_utils/build_index.cpp
@@ -31,7 +31,7 @@
 
 #include "conduit/conduit.hpp"
 #include "conduit/conduit_relay.hpp"
-#include "conduit/conduit_relay_hdf5.hpp"
+#include "conduit/conduit_relay_io_hdf5.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
 
     if (argc == 1) {
       if (master) {
-        std::cout << "usage: " << argv[0] << " --filelist=<string> --output_fn=<string>\n"
+        std::cout << "usage: " << argv[0] << " --filelist=<string> --base_dir=<string> --output_fn=<string>\n"
           "where: filelist contains a list of conduit filenames;\n"
           "       base_dir / <name from filelist> should fully specify\n"
           "       a conduit filepath\n"
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]) {
       return EXIT_SUCCESS;
     }
 
-    if (! (opts->has_string("filelist") && opts->has_string("output_fn") )) {
+    if (! (opts->has_string("filelist") && opts->has_string("output_fn") &&  opts->has_string("base_dir") )) {
       throw lbann_exception(std::string{} + __FILE__ + " " + std::to_string(__LINE__) + " :: improper invocation; run with no cmd line args for proper invocation");
     }
 
@@ -86,23 +86,26 @@ int main(int argc, char *argv[]) {
     std::stringstream ss;
     ss << output_fn << "." << rank;
     std::ofstream out(ss.str().c_str());
+    std::cerr << rank << " :: opened for writing: " << ss.str() << "\n";
     if (!out.good()) {
       throw lbann_exception(std::string{} + __FILE__ + " " + std::to_string(__LINE__) + " :: failed to open " + output_fn + " for writing");
     }
     if (master) {
       std::cerr << "writing index file: " << output_fn << "\n";
-      out << base_dir << "\n";
     }
 
     // get list of input filenames
     std::vector<std::string> filenames;
     read_filelist(comm, input_fn, filenames);
 
-    int global_num_samples = 0;
+    int num_samples = 0;
+    int num_samples_bad = 0;
     int np = comm->get_procs_in_world();
-    size_t total_good = 0;
     hid_t hdf5_file_hnd;
     for (size_t j=rank; j<filenames.size(); j+=np) {
+if (j >= 400) break;
+      int local_num_samples = 0;
+      int local_num_samples_bad = 0;
       std::cerr << rank << " :: processing: " << filenames[j] << "\n";
       try {
         std::string fn = filenames[j];
@@ -124,8 +127,6 @@ int main(int argc, char *argv[]) {
          std::cerr << "exception hdf5_group_list_child_names\n";
          continue;
       }
-      size_t is_good = 0;
-      size_t is_bad = 0;
       std::stringstream s5;
       conduit::Node n_ok;
       for (size_t h=0; h<cnames.size(); h++) {
@@ -138,15 +139,15 @@ int main(int argc, char *argv[]) {
         }
         int success = n_ok.to_int64();
         if (success == 1) {
-          ++is_good;
-          ++total_good;
+          ++num_samples;
+          ++local_num_samples;
         } else {
-          s5 << h << " ";
-          ++is_bad;
+          s5 << cnames[h] << " ";
+          ++num_samples_bad;
+          ++local_num_samples_bad;
         }
       }
-      global_num_samples += is_good;
-      out << is_good << " " << is_bad << " " << s5.str() << "\n";
+      out << local_num_samples << " " << local_num_samples_bad << " " << s5.str() << "\n";
       try {
         conduit::relay::io::hdf5_close_file(hdf5_file_hnd);
       } catch (...) {
@@ -157,26 +158,38 @@ int main(int argc, char *argv[]) {
     out.close();
     comm->global_barrier();
 
-    int num_samples;
-    MPI_Reduce(&global_num_samples, &num_samples, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    int global_num_samples;
+    int global_num_samples_bad;
+    MPI_Reduce(&num_samples, &global_num_samples, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&num_samples_bad, &global_num_samples_bad, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (master) {
+
+      std::ofstream out2("num_samples_tmp");
+      if (!out2) {
+        LBANN_ERROR("failed to open output file");
+      }
+      out2 << "CONDUIT_HDF5_EXCLUSION\n" << global_num_samples << " " << global_num_samples_bad 
+           << " " << filenames.size() << "\n" << base_dir << "\n";
+      out2.close();
+
       std::stringstream s3;
-      s3 << "echo " << num_samples << " " << filenames.size() << " >  num_samples_tmp";
-      if (master) std::cerr << "NUM SAMPLES: " << num_samples << "\n";
-      system(s3.str().c_str());
-      s3.clear();
-      s3.str("");
       s3 << "cat num_samples_tmp ";
       for (int k=0; k<np; k++) {
         s3 << output_fn << "." << k << " ";
       }
       s3 << "> " << output_fn;
       system(s3.str().c_str());
+
       s3.clear();
       s3.str("");
       s3 << "chmod 660 " << output_fn;
       system(s3.str().c_str());
+      s3.clear();
+      s3.str("");
+      s3 << "chgrp brain " << output_fn;
+      system(s3.str().c_str());
+
       s3.clear();
       s3.str("");
       s3 << "rm -f num_samples_tmp ";
@@ -184,24 +197,11 @@ int main(int argc, char *argv[]) {
         s3 << output_fn << "." << k << " ";
       }
       system(s3.str().c_str());
-    }
+    } // if (master)
 
-  } catch (exception& e) {
-    std::cerr << "caught exception, outer loop!!!!\n";
-    if (options::get()->has_bool("stack_trace_to_file")) {
-      std::stringstream ss("stack_trace");
-      const auto& rank = get_rank_in_world();
-      if (rank >= 0) { ss << "_rank" << rank; }
-      ss << ".txt";
-      std::ofstream fs(ss.str().c_str());
-      e.print_report(fs);
-    }
-    El::ReportException(e);
+  } catch (std::exception const &e) {
     finalize(comm);
-    return EXIT_FAILURE;
-  } catch (std::exception& e) {
-    El::ReportException(e);
-    finalize(comm);
+    if (master) std::cerr << "caught exception: " << e.what() << "\n";
     return EXIT_FAILURE;
   } catch (...) {
     std::cerr << "unknown exception in main\n";
--- model_zoo/jag_utils/build_sample_id_mapping.cpp
+++ model_zoo/jag_utils/build_sample_id_mapping.cpp
@@ -30,34 +30,62 @@ int main(int argc, char **argv) {
   opts->init(argc, argv);
 
   // sanity check the cmd line
-  if (argc != 3) {
+  if (argc != 2) {
     if (master) {
-      cerr << "\nusage: " << argv[0] << " --filelist=<string> --output_dir=<string>\n";
+      cerr << "\nusage: " << argv[0] << " --base_dir=<string>\n"
+           << "assumes: the file '<base_dir>/index.txt' exists\n"
+           << "output: writes the file <base_dir>/id_mapping.txt\n\n";
     }
     finalize(comm);
     return(0);
   }
-  string dir = opts->get_string("output_dir");
 
   // get list of conduit filenames
+  if (master) cerr << "reading filelist\n";
   vector<string> filenames;
-  read_filelist(comm, opts->get_string("filelist"), filenames);
+  string base_dir = opts->get_string("base_dir");
+  if (!base_dir.back() == '/') {
+    base_dir += '/';
+  }
+  char b[1024];
+  sprintf(b, "%s/index.txt", base_dir.c_str());
+  std::string fn;
+  std::ifstream in(b);
+  if (!in) LBANN_ERROR("can't open file for writing");
+  std::string line;
+  getline(in, line);
+  getline(in, line);
+  getline(in, line);
+  while (getline(in, line)) {
+    if (line.size() < 4) break;
+    std::stringstream s(line);
+    s >> fn;
+    filenames.push_back(fn);
+  }
+  in.close();
 
   // each proc opens a file for output
-  char b[1024];
-  sprintf(b, "%s/TMP_%d", dir.c_str(), rank);
+  if (master) cerr << "opening output files\n";
+  sprintf(b, "%s/TMP_%d", base_dir.c_str(), rank);
   ofstream out(b);
+  if (!out) LBANN_ERROR("can't open file for writing");
 
   // each proc builds a map: sample_id -> local index, for the
-  // conduit files that for which it's responsible
+  // conduit files for which it's responsible
+  size_t q = 0;
+  if (master) cerr << "building map\n";
   for (size_t j=rank; j<filenames.size(); j+= np) {
-    hid_t hdf5_file_hnd = conduit::relay::io::hdf5_open_file_for_read( filenames[j] );
+    out << filenames[j] << " ";
+    ++q;
+    if (q % 10 == 0) cout << rank << " :: " << q/10 << " *10 processed\n";
+    const std::string f_name(base_dir + filenames[j]); 
+    hid_t hdf5_file_hnd = conduit::relay::io::hdf5_open_file_for_read( f_name );
     std::vector<std::string> cnames;
     conduit::relay::io::hdf5_group_list_child_names(hdf5_file_hnd, "/", cnames);
-    conduit::Node n_ok;
     for (size_t h=0; h<cnames.size(); h++) {
-      out << cnames[h] << " " << h << "\n";
+      out << cnames[h] << " ";
     }
+    out << "\n";
   }
 
   out.close();
@@ -65,22 +93,29 @@ int main(int argc, char **argv) {
 
   if (master) {
     stringstream s3;
-    s3 << "cat " << dir << "/TMP_* > " << dir << "/id_mapping.txt";
+    s3 << "cat " << base_dir << "/TMP_* > " << base_dir << "/id_mapping.txt";
     int r = system(s3.str().c_str());
     if (r) {
       throw lbann_exception(std::string{} + __FILE__ + " " + std::to_string(__LINE__) + " :: system call failed: " + s3.str());
     }
+
     s3.clear();
     s3.str("");
-    s3 << "rm -f " << dir << "/TMP_*";
+    s3 << "chmod 660 " << base_dir << "/id_mapping.txt; chgrp brain " << base_dir << "/id_mapping.txt";
+    r = system(s3.str().c_str());
+    if (r) {
+      throw lbann_exception(std::string{} + __FILE__ + " " + std::to_string(__LINE__) + " :: system call failed: " + s3.str());
+    }
+
+    s3.clear();
+    s3.str("");
+
+    s3 << "rm -f " << base_dir << "/TMP_*";
     r = system(s3.str().c_str());
     if (r) {
       throw lbann_exception(std::string{} + __FILE__ + " " + std::to_string(__LINE__) + " :: system call failed: " + s3.str());
     }
   }
-
-
-
 
   finalize(comm);
 }
