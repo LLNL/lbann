20f4e08e03100f9ecefefad3eab5f493e11dd728
Naoya Maruyama <maruyama3@llnl.gov>
2019-01-15 22:44:51 -0800

    Do not disable dilated convolutions

--- include/lbann/layers/learning/convolution.hpp
+++ include/lbann/layers/learning/convolution.hpp
@@ -518,8 +518,8 @@ protected:
     if (!Layer::using_distconv()) return false;
 
     if (!(this->m_kernel_dims[2] == this->m_kernel_dims[3] &&
-          this->m_kernel_dims[2] == this->m_pads[0] * 2 + 1 &&
-          this->m_kernel_dims[3] == this->m_pads[1] * 2 + 1)) {
+          this->m_kernel_dims[2] == this->m_pads[0] / this->m_dilations[0] * 2 + 1 &&
+          this->m_kernel_dims[3] == this->m_pads[1] / this->m_dilations[1] * 2 + 1)) {
       dc::MPIPrintStreamDebug()
           << "Unsupported as padding does not match the kernel size";
       return false;
