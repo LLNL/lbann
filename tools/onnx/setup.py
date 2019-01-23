c0f09b29d4727e3bc0a0c0cf900287737d90eec0
Yosuke Oyama <17844184+oyamay@users.noreply.github.com>
2019-01-23 11:05:18 -0800

    Refactor setup.py

--- tools/onnx/setup.py
+++ tools/onnx/setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup
 import setuptools.command.build_py
-import setuptools.command.develop
+from setuptools.command.install import install
 import os.path
 import subprocess
 
 from lbann_onnx import getLbannRoot
 
-class build_py(setuptools.command.build_py.build_py):
+class installCmd(install):
     def run(self):
         root = getLbannRoot()
         lbannProtoDir = os.path.join(root, "src", "proto")
         lbannProto = os.path.join(lbannProtoDir, "lbann.proto")
         subprocess.check_output(["protoc", "-I={}".format(lbannProtoDir), lbannProto, "--python_out=."])
-
-class develop(setuptools.command.develop.develop):
-    def run(self):
-        self.run_command('build_py')
+        install.run(self)
 
 setup(name="lbann-onnx",
       version="0.1.0",
@@ -25,8 +22,7 @@ setup(name="lbann-onnx",
                         "numpy>=1.16.0",
                         "protobuf>=3.6.1",
                         "nose>=1.3.7"],
-      cmdclass={"build_py": build_py,
-                "develop": develop},
+      cmdclass={"install": installCmd},
       test_suite="nose.collector",
       tests_require=["nose"],
       zip_safe=False)
