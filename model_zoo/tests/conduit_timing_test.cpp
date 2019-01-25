64d645a0a434573da9103cf3bf113a5c20e4551d
Brian C. Van Essen <vanessen1@llnl.gov>
2019-01-24 18:04:01 -0800

    Added more timing options and looking at scalars vs images

--- model_zoo/tests/conduit_timing_test.cpp
+++ model_zoo/tests/conduit_timing_test.cpp
@@ -49,6 +49,31 @@ std::vector<std::string>  emi_v = {
   "(90.0, 78.0)/0.0"
 };
 
+std::vector<std::string> jag_scalar_keys = {
+        "BWx",
+        "BT",
+        "tMAXt",
+        "BWn",
+        "MAXpressure",
+        "BAte",
+        "MAXtion",
+        "tMAXpressure",
+        "BAt",
+        "Yn",
+        "Ye",
+        "Yx",
+        "tMAXte",
+        "BAtion",
+        "MAXte",
+        "tMAXtion",
+        "BTx",
+        "MAXt",
+        "BTn",
+        "BApressure",
+        "tMINradius",
+        "MINradius"
+};
+
 int main(int argc, char *argv[]) {
   int random_seed = lbann_default_random_seed;
   lbann_comm *comm = initialize(argc, argv, random_seed);
@@ -114,6 +139,11 @@ int main(int argc, char *argv[]) {
 void test_conduit(int from, int to, std::vector<std::string> filenames) {
   double tm1 = get_time();
   double total = 0;
+  double n1 = 0;
+  double n2 = 0;
+  double n3 = 0;
+  double n4 = 0;
+  double tm3;
   conduit::Node leaf;
   for (int j=from; j<to; j++) {
     std::cerr << "loading: " << filenames[j] << "\n";
@@ -123,23 +153,46 @@ void test_conduit(int from, int to, std::vector<std::string> filenames) {
     for (size_t i=0; i<cnames.size(); i++) {
       for (size_t k=0; k<emi_v.size(); k++) {
         const std::string key = cnames[i] + "/outputs/images/" + emi_v[k] + "/emi";
+        tm3 = get_time();
         conduit::relay::io::hdf5_read(hdf5_file_hnd, key, leaf);
+        n1 += (get_time() - tm3);
+        tm3 = get_time();
         conduit::float32_array emi = leaf.value();
+        n2 += (get_time() - tm3);
         const size_t image_size = emi.number_of_elements();
         for (size_t x=0; x<image_size; x++) {
           total += emi[x];
         }
       }
+      for (size_t k=0; k<jag_scalar_keys.size(); k++) {
+        const std::string key = cnames[i] + "/outputs/scalars/" + jag_scalar_keys[k];
+        tm3 = get_time();
+        conduit::relay::io::hdf5_read(hdf5_file_hnd, key, leaf);
+        n3 += (get_time() - tm3);
+        tm3 = get_time();
+        const double val = leaf.to_value();
+        n4 += (get_time() - tm3);
+        total += sizeof(val);
+      }
     }
   }
   double tm2 = get_time();
-  std::cerr << "baseline hdf5_read per field; time: " << tm2-tm1 << "  test_total: " << total << "\n\n";
+  std::cerr << "baseline hdf5_read per field; time: " << tm2-tm1 << "  test_total: " << total << "\n";
+  std::cerr << "  time to load individual images from samples: " << n1 << "\n";
+  std::cerr << "  time to access images values:        " << n2 << "\n";
+  std::cerr << "  time to load individual scalars from samples: " << n3 << "\n";
+  std::cerr << "  time to access scalar values:        " << n4 << "\n\n";
 }
 
 // load each outputs/images for each sample, then load each image from that node
 void test_conduit_2(int from, int to, std::vector<std::string> filenames) {
   double tm1 = get_time();
   double total = 0;
+  double n1 = 0;
+  double n2 = 0;
+  double n3 = 0;
+  double n4 = 0;
+  double tm3;
   conduit::Node inode;
   for (int j=from; j<to; j++) {
     std::cerr << "loading: " << filenames[j] << "\n";
@@ -147,11 +200,21 @@ void test_conduit_2(int from, int to, std::vector<std::string> filenames) {
     std::vector<std::string> cnames;
     conduit::relay::io::hdf5_group_list_child_names(hdf5_file_hnd, "/", cnames);
     for (size_t i=0; i<cnames.size(); i++) {
+      tm3 = get_time();
+      const std::string key2 = cnames[i] + "/outputs/scalars/";
+      conduit::relay::io::hdf5_read(hdf5_file_hnd, key2, inode);
+      n4 += (get_time() - tm3);
       const std::string key = cnames[i] + "/outputs/images/";
+      tm3 = get_time();
       conduit::relay::io::hdf5_read(hdf5_file_hnd, key, inode);
+      n1 += (get_time() - tm3);
       for (size_t k=0; k<emi_v.size(); k++) {
+        tm3 = get_time();
         const conduit::Node& leaf = inode[emi_v[k] + "/emi"];
+        n2 += (get_time() - tm3);
+        tm3 = get_time();
         conduit::float32_array emi = leaf.value();
+        n3 += (get_time() - tm3);
         const size_t image_size = emi.number_of_elements();
         for (size_t x=0; x<image_size; x++) {
           total += emi[x];
@@ -160,24 +223,45 @@ void test_conduit_2(int from, int to, std::vector<std::string> filenames) {
     }
   }
   double tm2 = get_time();
-  std::cerr << "multi-conduit node access; time: " << tm2-tm1 << "  test_total: " << total << "\n\n";
+  std::cerr << "multi-conduit node access; time: " << tm2-tm1 << "  test_total: " << total << "\n";
+  std::cerr << "  time to load block of images from samples: " << n1 << "\n";
+  std::cerr << "  time to access images node:        " << n2 << "\n";
+  std::cerr << "  time to access images value:        " << n3 << "\n";
+  std::cerr << "  time to load the scalar samples: " << n4 << "\n\n";
 }
 
 // load each outputs/images for each sample, then load each image from that node
 void test_conduit_3(int from, int to, std::vector<std::string> filenames) {
   double tm1 = get_time();
   double total = 0;
+  double n1 = 0;
+  double n2 = 0;
+  double tm3;
   conduit::Node inode;
   for (int j=from; j<to; j++) {
     std::cerr << "loading: " << filenames[j] << "\n";
+
     hid_t hdf5_file_hnd = conduit::relay::io::hdf5_open_file_for_read( filenames[j].c_str() );
     std::vector<std::string> cnames;
     conduit::relay::io::hdf5_group_list_child_names(hdf5_file_hnd, "/", cnames);
     for (size_t i=0; i<cnames.size(); i++) {
-      const std::string key = cnames[i];
+
+      tm3 = get_time();
+      const std::string key = cnames[i] + "/outputs/";
       conduit::relay::io::hdf5_read(hdf5_file_hnd, key, inode);
+      n1 += (get_time() - tm3);
+      // std::cout << "total bytes: " << inode.total_strided_bytes() << std::endl;
+      // conduit::Node ninfo;
+      // inode.info(ninfo);
+      // ninfo.print();
+
       for (size_t k=0; k<emi_v.size(); k++) {
-        conduit::float32_array emi = inode["/outputs/images/" + emi_v[k] + "/emi"].value();
+
+        tm3 = get_time();
+        const conduit::float32_array &emi = inode["/images/" + emi_v[k] + "/emi"].as_float32_array();
+        //conduit::float32_array emi = inode["/outputs/images/" + emi_v[k] + "/emi"].value();
+        n2 += (get_time() - tm3);
+
         const size_t image_size = emi.number_of_elements();
         for (size_t x=0; x<image_size; x++) {
           total += emi[x];
@@ -186,6 +270,8 @@ void test_conduit_3(int from, int to, std::vector<std::string> filenames) {
     }
   }
   double tm2 = get_time();
-  std::cerr << "direct access via operator[]; time: " << tm2-tm1 << "  test_total: " << total << "\n\n";
+  std::cerr << "direct access via operator[]; time: " << tm2-tm1 << "  test_total: " << total << "\n";
+  std::cerr << "  time to load entire (images) sample: " << n1 << "\n";
+  std::cerr << "  time to access image values: " << n2 << "\n";
 }
 #endif //#ifdef LBANN_HAS_CONDUIT
