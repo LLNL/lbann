e0ab19957f18b9cc15de0e36c870bf80a1f2c77d
Naoya Maruyama <maruyama3@llnl.gov>
2019-02-07 20:05:11 -0800

    Enable HYBRID halo exchange

--- src/utils/distconv.cpp
+++ src/utils/distconv.cpp
@@ -309,6 +309,8 @@ HaloExchangeMethod get_halo_exchange_method() {
     return HaloExchangeMethod::MPI;
   } else if (opt_halo_exchange == "AL") {
     return HaloExchangeMethod::AL;
+  } else if (opt_halo_exchange == "HYBRID") {
+    return HaloExchangeMethod::HYBRID;
   } else if (opt_halo_exchange == "MPI_DERIVED_TYPE") {
     return HaloExchangeMethod::MPI_DERIVED_TYPE;
   } else {
