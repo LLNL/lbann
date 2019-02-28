5ea03a2f9af89fd18cdb7e74e226af2a2916842b
Thomas R. Benson <benson31@llnl.gov>
2019-02-28 12:55:50 -0800

    fix a bug in the type-erased copying constructor; add doxygen documentation

--- src/utils/unit_test/type_erased_matrix_test.cpp
+++ src/utils/unit_test/type_erased_matrix_test.cpp
@@ -29,41 +29,91 @@ TEMPLATE_PRODUCT_TEST_CASE(
 
   GIVEN("A type-erased matrix")
   {
-    auto x = lbann::utils::create_type_erased_matrix<src_type>();
+    auto mat = lbann::utils::create_type_erased_matrix<src_type>();
 
     THEN ("the internal matrix has the correct storage type")
     {
-      REQUIRE_NOTHROW(x->template get<src_type>());
-      REQUIRE_THROWS_AS(x->template get<tgt_type>(),
+      REQUIRE_NOTHROW(mat->template get<src_type>());
+      REQUIRE_THROWS_AS(mat->template get<tgt_type>(),
                         lbann::utils::bad_any_cast);
 
-      auto&& internal_mat = x->template get<src_type>();
+      auto&& internal_mat = mat->template get<src_type>();
       REQUIRE(internal_mat.Height() == 0);
       REQUIRE(internal_mat.Width() == 0);
     }
 
     WHEN ("The matrix is resized")
     {
-      REQUIRE_NOTHROW(x->template get<src_type>().Resize(10,12));
+      REQUIRE_NOTHROW(mat->template get<src_type>().Resize(10,12));
 
       THEN ("The change is reflected in the internal matrix.")
       {
-        auto&& internal_mat = x->template get<src_type>();
+        auto&& internal_mat = mat->template get<src_type>();
         REQUIRE(internal_mat.Height() == 10);
         REQUIRE(internal_mat.Width() == 12);
       }
       AND_WHEN ("The matrix is converted")
       {
-        REQUIRE_NOTHROW(x->template convert<src_type, tgt_type>());
+        REQUIRE_NOTHROW(mat->template convert<src_type, tgt_type>());
 
         THEN ("The internal matrix has the right type and size")
         {
-          REQUIRE_NOTHROW(x->template get<tgt_type>());
-          REQUIRE_THROWS_AS(x->template get<src_type>(),
+          REQUIRE_NOTHROW(mat->template get<tgt_type>());
+          REQUIRE_THROWS_AS(mat->template get<src_type>(),
                             lbann::utils::bad_any_cast);
 
-          REQUIRE(x->template get<tgt_type>().Height() == 10);
-          REQUIRE(x->template get<tgt_type>().Width() == 12);
+          REQUIRE(mat->template get<tgt_type>().Height() == 10);
+          REQUIRE(mat->template get<tgt_type>().Width() == 12);
+        }
+      }
+    }
+  }
+
+  GIVEN("A matrix of a given type")
+  {
+    El::Matrix<src_type> mat(10,12);
+    mat(1,1) = src_type(13);
+
+    WHEN("A type-erased matrix is constructed by copying it")
+    {
+      lbann::utils::type_erased_matrix erased_mat(mat);
+      THEN("The type-erased matrix is a copy")
+      {
+        REQUIRE(erased_mat.template get<src_type>().Height() == 10);
+        REQUIRE(erased_mat.template get<src_type>().Width() == 12);
+        REQUIRE(
+          erased_mat.template get<src_type>().operator()(1,1) == mat(1,1));
+
+        AND_WHEN("The original matrix is resized")
+        {
+          mat.Resize(5,5);
+          THEN("The type-erased matrix is unaffected.")
+          {
+            REQUIRE(erased_mat.template get<src_type>().Height() == 10);
+            REQUIRE(erased_mat.template get<src_type>().Width() == 12);
+          }
+        }
+      }
+    }
+
+    WHEN("A type-erased matrix is constructed by moving it")
+    {
+      lbann::utils::type_erased_matrix erased_mat(std::move(mat));
+      THEN("The type-erased matrix is sized correctly and has good values")
+      {
+        REQUIRE(erased_mat.template get<src_type>().Height() == 10);
+        REQUIRE(erased_mat.template get<src_type>().Width() == 12);
+        REQUIRE(
+          erased_mat.template get<src_type>().operator()(1,1) == src_type(13));
+
+        AND_WHEN("The original matrix is resized")
+        {
+          mat.Resize(5,5);
+          THEN("The type-erased matrix is unaffected.")
+          {
+            REQUIRE(erased_mat.template get<src_type>().Height() == 10);
+            REQUIRE(erased_mat.template get<src_type>().Width() == 12);
+          }
         }
       }
     }
