deb92f3d5831075e6430767ad0e07b2cc3b608ec
Thomas R. Benson <benson31@llnl.gov>
2019-02-28 12:55:50 -0800

    fix a bug in the type-erased copying constructor; add doxygen documentation

--- include/lbann/utils/type_erased_matrix.hpp
+++ include/lbann/utils/type_erased_matrix.hpp
@@ -11,20 +11,59 @@ namespace lbann
 namespace utils
 {
 
+/** @class type_erased_matrix
+ *  @brief A type-erased wrapper around an @c El::Matrix<T,Device::CPU>
+ *
+ *  @warning This class is an implementation detail of the
+ *      preprocessing pipeline and should not be used in general
+ *      LBANN code.
+ */
 class type_erased_matrix
 {
 public:
 
+  /** @brief Construct from a copy of a given matrix.
+   *
+   *  Deep-copy the input matrix into the held matrix.
+   *
+   *  @tparam Field The data type of the input matrix
+   *
+   *  @param in_matrix The input matrix.
+   *
+   *  @warning This performs a deep copy of the matrix.
+   */
   template <typename Field>
   type_erased_matrix(El::Matrix<Field> const& in_matrix)
-    : m_matrix{in_matrix}
-  {}
+  {
+    El::Matrix<Field> held;
+    El::Copy(in_matrix, held);
+    m_matrix.emplace<El::Matrix<Field>>(std::move(held));
+  }
 
+  /** @brief Construct by moving the given matrix into type-erased
+   *      storage.
+   *
+   *  Move the input matrix into the held matrix.
+   *
+   *  @tparam Field The data type of the input matrix
+   *
+   *  @param in_matrix The input matrix.
+   */
   template <typename Field>
   type_erased_matrix(El::Matrix<Field>&& in_matrix)
     : m_matrix{std::move(in_matrix)}
   {}
 
+  /** @brief Access the underlying matrix.
+   *
+   *  Provides read/write access to the underlying matrix if the input
+   *  @c Field matches the data type of the held matrix.
+   *
+   *  @tparam Field The data type of the held matrix
+   *
+   *  @throws bad_any_cast If the datatype of the held matrix does not
+   *      match the input @c Field.
+   */
   template <typename Field>
   El::Matrix<Field>& get()
   {
@@ -33,12 +72,37 @@ public:
         .template get<Field>());
   }
 
+  /** @brief Access the underlying matrix.
+   *
+   *  Provides read-only access to the underlying matrix if the input
+   *  @c Field matches the data type of the held matrix.
+   *
+   *  @tparam Field The data type of the held matrix
+   *
+   *  @return Reference to the underlying matrix
+   *
+   *  @throws bad_any_cast If the datatype of the held matrix does not
+   *      match the input @c Field.
+   */
   template <typename Field>
   El::Matrix<Field> const& get() const
   {
     return any_cast<El::Matrix<Field> const&>(m_matrix);
   }
 
+  /** @brief Access the underlying matrix.
+   *
+   *  Converts (copies) the internal matrix into a matrix of a new
+   *  type, which is then held in place of the original matrix.
+   *
+   *  @tparam OldField The data type of the originally held matrix
+   *  @tparam NewField The data type of the newly held matrix
+   *
+   *  @return @c const reference to the underlying matrix
+   *
+   *  @throws bad_any_cast If the datatype of the held matrix does not
+   *      match the input @c OldField.
+   */
   template <typename OldField, typename NewField>
   El::Matrix<NewField>& convert()
   {
@@ -49,11 +113,18 @@ public:
   }
 
 private:
+  /** @brief Type-erased matrix storage */
   any m_matrix;
 };// class type_erased_matrix
 
-// Helper function for what will probably be the usual construction
-// process.
+/** @brief Create an empty type-erased matrix with given underlying
+ *      data type.
+ *
+ *  @tparam Field The type of the underlying matrix.
+ *
+ *  @return A pointer to an empty type-erased matrix with data type @c
+ *      Field.
+ */
 template <typename Field>
 std::unique_ptr<type_erased_matrix>
 create_type_erased_matrix()
