543387bd516304c6dba5de27966532647a64bf6a
Jae-Seung Yeom <yeom2@llnl.gov>
2019-03-14 11:19:19 -0700

    remove file_handle_t from the template parameter list of sample_list_hdf5 and make it inherit sample_list<hid_t, sample_name_t>

--- include/lbann/data_readers/sample_list_hdf5.hpp
+++ include/lbann/data_readers/sample_list_hdf5.hpp
@@ -9,66 +9,66 @@
 
 namespace lbann {
 
-template <typename file_handle_t, typename sample_name_t>
-class sample_list_hdf5 : public sample_list<file_handle_t, sample_name_t> {
+template <typename sample_name_t>
+class sample_list_hdf5 : public sample_list<hid_t, sample_name_t> {
  public:
-  using typename sample_list<file_handle_t, sample_name_t>::sample_file_id_t;
-  using typename sample_list<file_handle_t, sample_name_t>::sample_t;
-  using typename sample_list<file_handle_t, sample_name_t>::samples_t;
-  using typename sample_list<file_handle_t, sample_name_t>::file_id_stats_t;
-  using typename sample_list<file_handle_t, sample_name_t>::file_id_stats_v_t;
-  using typename sample_list<file_handle_t, sample_name_t>::fd_use_map_t;
+  using typename sample_list<hid_t, sample_name_t>::sample_file_id_t;
+  using typename sample_list<hid_t, sample_name_t>::sample_t;
+  using typename sample_list<hid_t, sample_name_t>::samples_t;
+  using typename sample_list<hid_t, sample_name_t>::file_id_stats_t;
+  using typename sample_list<hid_t, sample_name_t>::file_id_stats_v_t;
+  using typename sample_list<hid_t, sample_name_t>::fd_use_map_t;
 
   sample_list_hdf5();
   ~sample_list_hdf5() override;
 
  protected:
-  void obtain_sample_names(file_handle_t& h, std::vector<std::string>& sample_names) const override;
-  bool is_file_handle_valid(const file_handle_t& h) const override;
-  file_handle_t open_file_handle_for_read(const std::string& path) override;
-  void close_file_handle(file_handle_t& h) override;
-  void clear_file_handle(file_handle_t& h) override;
+  void obtain_sample_names(hid_t& h, std::vector<std::string>& sample_names) const override;
+  bool is_file_handle_valid(const hid_t& h) const override;
+  hid_t open_file_handle_for_read(const std::string& path) override;
+  void close_file_handle(hid_t& h) override;
+  void clear_file_handle(hid_t& h) override;
 };
 
 
-template <typename file_handle_t, typename sample_name_t>
-inline sample_list_hdf5<file_handle_t, sample_name_t>::sample_list_hdf5()
-: sample_list<file_handle_t, sample_name_t>() {} 
+template <typename sample_name_t>
+inline sample_list_hdf5<sample_name_t>::sample_list_hdf5()
+: sample_list<hid_t, sample_name_t>() {} 
 
-template <typename file_handle_t, typename sample_name_t>
-inline sample_list_hdf5<file_handle_t, sample_name_t>::~sample_list_hdf5() {
+template <typename sample_name_t>
+inline sample_list_hdf5<sample_name_t>::~sample_list_hdf5() {
 }
 
-template <typename file_handle_t, typename sample_name_t>
-inline void sample_list_hdf5<file_handle_t, sample_name_t>
-::obtain_sample_names(file_handle_t& h, std::vector<std::string>& sample_names) const {
+template <typename sample_name_t>
+inline void sample_list_hdf5<sample_name_t>
+::obtain_sample_names(hid_t& h, std::vector<std::string>& sample_names) const {
   conduit::relay::io::hdf5_group_list_child_names(h, "/", sample_names);
 }
 
-template <typename file_handle_t, typename sample_name_t>
-inline bool sample_list_hdf5<file_handle_t, sample_name_t>
-::is_file_handle_valid(const file_handle_t& h) const {
-  return (h > static_cast<file_handle_t>(0));
+template <typename sample_name_t>
+inline bool sample_list_hdf5<sample_name_t>
+::is_file_handle_valid(const hid_t& h) const {
+  return (h > static_cast<hid_t>(0));
 }
 
-template <typename file_handle_t, typename sample_name_t>
-inline file_handle_t sample_list_hdf5<file_handle_t, sample_name_t>
+template <typename sample_name_t>
+inline hid_t sample_list_hdf5< sample_name_t>
 ::open_file_handle_for_read(const std::string& file_path) {
   return conduit::relay::io::hdf5_open_file_for_read(file_path);
 }
 
-template <typename file_handle_t, typename sample_name_t>
-inline void sample_list_hdf5<file_handle_t, sample_name_t>
-::close_file_handle(file_handle_t& h) {
+template <typename sample_name_t>
+inline void sample_list_hdf5<sample_name_t>
+::close_file_handle(hid_t& h) {
   if(is_file_handle_valid(h)) {
     conduit::relay::io::hdf5_close_file(h);
   }
 }
 
-template <typename file_handle_t, typename sample_name_t>
-inline void sample_list_hdf5<file_handle_t, sample_name_t>
-::clear_file_handle(file_handle_t& h) {
-  h = file_handle_t(0);
+template <typename sample_name_t>
+inline void sample_list_hdf5<sample_name_t>
+::clear_file_handle(hid_t& h) {
+  h = static_cast<hid_t>(0);
 }
 
 
