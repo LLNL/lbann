58cd155d5237e507a41b43ddf0d2a410a6c9602a
Tim Moon <moon13@llnl.gov>
2019-03-15 18:43:04 -0700

    Cleaning up in Python LC utility functions.
    Slurm run function can accommodate arbitrary executables and has an option to create a batch script without running.

--- scripts/proto/lbann/lc/slurm.py
+++ scripts/proto/lbann/lc/slurm.py
@@ -2,45 +2,48 @@
 import os
 import os.path
 import subprocess
-from lbann.lc.systems import system, partition, account, time_limit, procs_per_node
+from lbann.lc.systems import (system, partition, account, has_gpu,
+                              procs_per_node, time_limit)
 from lbann.lc.paths import lbann_exe
 
-def run(work_dir = os.getcwd(),
-        lbann_exe = lbann_exe(),
-        lbann_args = '',
-        prototext_file = 'experiment.prototext',
+def run(experiment_dir = os.getcwd(),
+        exe = lbann_exe(),
+        exe_args = '',
         srun_args = '',
         job_name = 'lbann',
         nodes = 1,
         procs_per_node = procs_per_node(),
+        system = system(),
         partition = partition(),
         account = account(),
-        time_limit = time_limit()):
-    """Run LBANN experiment with Slurm.
+        time_limit = time_limit(),
+        setup_only = False):
+    """Run executable with Slurm.
 
-    Creates a Slurm batch script in the work directory. If a Slurm job
-    allocation is detected, the script is run directly. Otherwise, the
-    script is submitted to sbatch.
+    Creates a Slurm batch script in the experiment directory. If a
+    Slurm job allocation is detected, the script is run
+    directly. Otherwise, the script is submitted to sbatch.
 
     Args:
-        work_dir (str, optional): Work directory.
-        lbann_exe (str, optional): LBANN executable.
-        lbann_args (str, optional): Command-line arguments to LBANN
-            executable.
-        prototext_file (str, optional): Prototext file for LBANN
-            experiment.
+        experiment_dir (str, optional): Experiment directory.
+        exe (str, optional): Executable.
+        exe_args (str, optional): Command-line arguments to executable.
         srun_args (str, optional): Command-line arguments to srun.
         job_name (str, optional): Batch job name.
         nodes (int, optional): Number of compute nodes.
         procs_per_node (int, optional): Number of processes per compute
             node.
+        system (str, optional): Target system.
         partition (str, optional): Scheduler partition.
         account (str, optional): Scheduler account.
         time_limit (int, optional): Job time limit, in minutes.
+        setup_only (bool, optional): If true, the experiment is not
+            run after the batch script is created.
 
     """
 
-    # Determine if nodes have already been allocated by Slurm
+    # Check for an existing job allocation from Slurm
+    # Note: Settings for current job allocation take precedence
     has_allocation = 'SLURM_JOB_ID' in os.environ
     if has_allocation:
         job_name = os.environ['SLURM_JOB_NAME']
@@ -48,96 +51,19 @@ def run(work_dir = os.getcwd(),
         account = os.environ['SLURM_JOB_ACCOUNT']
         time_limit = -1
 
-    # Initialize experiment directory
-    batch_file, out_file, err_file = setup(
-        prototext_file = prototext_file,
-        work_dir = work_dir,
-        job_name = job_name,
-        lbann_exe = lbann_exe,
-        lbann_args = lbann_args,
-        nodes = nodes,
-        procs_per_node = procs_per_node,
-        system = system(),
-        partition = partition,
-        account = account,
-        time_limit = time_limit)
-
-    # Launch job
-    # Note: Pipes output to log files
-    run_exe = 'sh' if has_allocation else 'sbatch'
-    run_proc = subprocess.Popen([run_exe, batch_file],
-                                stdout = subprocess.PIPE,
-                                stderr = subprocess.PIPE,
-                                cwd = work_dir)
-    out_proc = subprocess.Popen(['tee', out_file],
-                                stdin = run_proc.stdout,
-                                cwd = work_dir)
-    err_proc = subprocess.Popen(['tee', err_file],
-                                stdin = run_proc.stderr,
-                                cwd = work_dir)
-    run_proc.stdout.close()
-    run_proc.stderr.close()
-    run_proc.wait()
-    out_proc.wait()
-    err_proc.wait()
-
-def setup(work_dir = os.getcwd(),
-          lbann_exe = lbann_exe(),
-          lbann_args = '',
-          prototext_file = '',
-          srun_args = '',
-          job_name = 'lbann',
-          nodes = 1,
-          procs_per_node = procs_per_node(),
-          system = system(),
-          partition = partition(),
-          account = account(),
-          time_limit = time_limit()):
-    """Create Slurm batch script in work directory.
-
-    Args:
-        work_dir (str, optional): Work directory.
-        lbann_exe (str, optional): LBANN executable.
-        lbann_args (str, optional): Command-line arguments to LBANN
-            executable.
-        prototext_file (str, optional): Prototext file for LBANN
-            experiment.
-        lbann_args (str, optional): Command-line arguments to srun.
-        job_name (str, optional): Batch job name.
-        nodes (int, optional): Number of compute nodes.
-        procs_per_node (int, optional): Number of processes per compute
-            node.
-        system (str, optional): Target system for batch job.
-        partition (str, optional): Scheduler partition.
-        account (str, optional): Scheduler account.
-        time_limit (int, optional): Job time limit, in minutes.
-
-    Returns:
-        batch_file (str): Batch script.
-        out_file (str): Log file for stdout.
-        err_file (str): Log file for stderr.
-
-    """
-
-    # Create work directory if needed
-    work_dir = os.path.abspath(work_dir)
-    os.makedirs(work_dir, exist_ok=True)
-
-    # Batch script and generated files
-    batch_file = os.path.join(work_dir, 'batch.sh')
-    out_file = os.path.join(work_dir, 'out.log')
-    err_file = os.path.join(work_dir, 'err.log')
-    nodes_file = os.path.join(work_dir, 'nodes.txt')
+    # Experiment directory
+    experiment_dir = os.path.abspath(experiment_dir)
+    os.makedirs(experiment_dir, exist_ok=True)
+    batch_file = os.path.join(experiment_dir, 'batch.sh')
+    out_file = os.path.join(experiment_dir, 'out.log')
+    err_file = os.path.join(experiment_dir, 'err.log')
+    nodes_file = os.path.join(experiment_dir, 'nodes.txt')
 
     # srun command-line arguments
-    if system in ('pascal', 'surface'):
+    if has_gpu(system):
         srun_args += ' --mpibind=off --nvidia_compute_mode=default'
-    if system == 'pascal':
+    if system == 'pascal' and procs_per_node == 2:
         srun_args += ' --cpu_bind=mask_cpu:0x000001ff,0x0003fe00'
-
-    # LBANN command-line arguments
-    if prototext_file:
-        lbann_args += ' --prototext=' + prototext_file
 
     # Write batch script
     with open(batch_file, 'w') as f:
@@ -151,7 +77,7 @@ def setup(work_dir = os.getcwd(),
             f.write('#SBATCH --partition={}\n'.format(partition))
         if account:
             f.write('#SBATCH --account={}\n'.format(account))
-        f.write('#SBATCH --workdir={}\n'.format(work_dir))
+        f.write('#SBATCH --workdir={}\n'.format(experiment_dir))
         f.write('#SBATCH --output={}\n'.format(out_file))
         f.write('#SBATCH --error={}\n'.format(err_file))
         if time_limit >= 0:
@@ -181,10 +107,27 @@ def setup(work_dir = os.getcwd(),
         f.write('# ==== Experiment ====\n')
         f.write('srun {} --nodes={} --ntasks={} {} {}\n'
                 .format(srun_args, nodes, nodes * procs_per_node,
-                        lbann_exe, lbann_args))
+                        exe, exe_args))
 
     # Make batch script executable
     os.chmod(batch_file, 0o755)
 
-    # Return batch script path
-    return batch_file, out_file, err_file
+    # Launch job if needed
+    # Note: Pipes output to log files
+    if not setup_only:
+        run_exe = 'sh' if has_allocation else 'sbatch'
+        run_proc = subprocess.Popen([run_exe, batch_file],
+                                    stdout = subprocess.PIPE,
+                                    stderr = subprocess.PIPE,
+                                    cwd = experiment_dir)
+        out_proc = subprocess.Popen(['tee', out_file],
+                                    stdin = run_proc.stdout,
+                                    cwd = experiment_dir)
+        err_proc = subprocess.Popen(['tee', err_file],
+                                    stdin = run_proc.stderr,
+                                    cwd = experiment_dir)
+        run_proc.stdout.close()
+        run_proc.stderr.close()
+        run_proc.wait()
+        out_proc.wait()
+        err_proc.wait()
