b8d8ecf4745655ee228c462d4affeb1f99d2f639
Tim Moon <moon13@llnl.gov>
2019-04-30 13:13:21 -0700

    CMake installs Python frontend using setuptools.

--- python/lbann/util/__init__.py
+++ python/lbann/util/__init__.py
@@ -1,17 +1,6 @@
 from collections.abc import Iterable
 from os.path import abspath, dirname, isfile, join
 
-def lbann_exe():
-    """LBANN executable."""
-    install_dir = dirname(dirname(dirname(dirname(__file__))))
-    exe = join(install_dir, 'bin', 'lbann')
-    if isfile(exe):
-        return exe
-    else:
-        # LBANN has been built with `build_lbann_lc.sh`
-        import lbann.contrib.lc.paths
-        return lbann.contrib.lc.paths.lbann_exe()
-
 def make_iterable(obj):
     """Convert to an iterable object.
 
