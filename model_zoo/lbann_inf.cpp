a465612e755d674dabf71f7ab86f34b039b321f2
Sam Ade Jacobs <jacobs32@llnl.gov>
2019-05-14 14:35:18 -0700

    lbann_inf gracefully exits if checkpoint dir is not given

--- model_zoo/lbann_inf.cpp
+++ model_zoo/lbann_inf.cpp
@@ -60,11 +60,16 @@ int main(int argc, char *argv[]) {
         build_model_from_prototext(argc, argv, *pb_model,
                                    comm.get(), io_thread_pool, models.size() == 0));
     }
+      for(auto&& m : models) {
+        bool loaded = lbann_callback_save_model::load_model_weights(opts->get_string("ckpt_dir"), m.get());
+        if(!loaded)  LBANN_ERROR("Unable to reload model"); 
+      }
 
     // Load layer weights from checkpoint if checkpoint directory given
     if(opts->has_string("ckpt_dir")){
       for(auto&& m : models) {
-        lbann_callback_save_model::load_model_weights(opts->get_string("ckpt_dir"), m.get());
+        bool loaded = lbann_callback_save_model::load_model_weights(opts->get_string("ckpt_dir"), m.get());
+        if(!loaded)  LBANN_ERROR("Unable to reload model"); 
       }
     }else {
       LBANN_ERROR("Unable to reload model");
