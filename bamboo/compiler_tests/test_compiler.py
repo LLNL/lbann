de183eb7bd63abded43cb52a5a5ee94a041d80e2
Ryan Forsyth <forsyth2@llnl.gov>
2019-05-16 16:55:57 -0700

    Spack setup

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -6,7 +6,7 @@ import os, re, subprocess
 
 
 def test_compiler_build_script(cluster, dirname):
-    if cluster not in ['corona', 'lassen', 'pascal']:
+    if cluster not in []:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
@@ -21,149 +21,110 @@ def test_compiler_build_script(cluster, dirname):
 def test_compiler_clang6_release(cluster, dirname):
     skeleton_clang6(cluster, dirname, False)
     path = '%s/bamboo/compiler_tests/builds/%s_clang-6.0.0_rel/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/clang.Release.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def test_compiler_clang6_debug(cluster, dirname):
     skeleton_clang6(cluster, dirname, True)
     path = '%s/bamboo/compiler_tests/builds/%s_clang-6.0.0_debug/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/clang.Debug.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def test_compiler_gcc7_release(cluster, dirname):
     skeleton_gcc7(cluster, dirname, False)
     path = '%s/bamboo/compiler_tests/builds/%s_gcc-7.1.0_rel/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/gnu.Release.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def test_compiler_gcc7_debug(cluster, dirname):
     skeleton_gcc7(cluster, dirname, True)
     path = '%s/bamboo/compiler_tests/builds/%s_gcc-7.1.0_debug/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/gnu.Debug.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def test_compiler_intel19_release(cluster, dirname):
     skeleton_intel19(cluster, dirname, False)
     path = '%s/bamboo/compiler_tests/builds/%s_intel-19.0.0_rel/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/intel.Release.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def test_compiler_intel19_debug(cluster, dirname):
     skeleton_intel19(cluster, dirname, True)
     path = '%s/bamboo/compiler_tests/builds/%s_intel-19.0.0_debug/build/model_zoo/lbann' % (dirname, cluster)
-    if not os.path.exists(path):
-        path = '%s/build/intel.Debug.%s.llnl.gov/install/bin/lbann' % (dirname, cluster)
-        assert os.path.exists(path)
+    assert os.path.exists(path)
 
 
 def skeleton_clang6(cluster, dir_name, debug):
-    if cluster not in ['catalyst']:
+    if cluster in ['catalyst']:
+        compile(dir_name, 'clang@6.0.0', debug, 'mvapich2@2.2')
+    else:
         e = 'skeleton_clang6: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
-    try:
-        spack_skeleton(dir_name, 'clang@6.0.0', 'mvapich2@2.2', debug)
-        build_skeleton(dir_name, 'clang@6.0.0', debug)
-    except AssertionError as e:
-        print(e)
-        build_script(cluster, dir_name, 'clang6', debug)
 
 
 def skeleton_gcc7(cluster, dir_name, debug):
-    if cluster not in ['catalyst', 'pascal']:
+    if cluster in ['catalyst', 'corona', 'lassen', 'pascal']:
+        compile(dir_name, 'gcc@7.1.0', debug, 'mvapich2@2.2')
+    else:
         e = 'skeleton_gcc7: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
-    try:
-        spack_skeleton(dir_name, 'gcc@7.1.0', 'mvapich2@2.2', debug)
-        build_skeleton(dir_name, 'gcc@7.1.0', debug)
-    except AssertionError as e:
-        print(e)
-        build_script(cluster, dir_name, 'gcc7', debug)
 
 
 def skeleton_intel19(cluster, dir_name, debug):
-    if cluster not in []:  # Taking out 'catalyst'
+    if cluster in ['catalyst']:
+        compile(dir_name, 'intel@19.0.0', debug, 'mvapich2@2.2')
+    else:
         e = 'skeleton_intel19: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
-    try:
-        spack_skeleton(dir_name, 'intel@19.0.0', 'mvapich2@2.2', debug)
-        build_skeleton(dir_name, 'intel@19.0.0', debug)
-    except AssertionError as e:
-        print(e)
-        build_script(cluster, dir_name, 'intel19', debug)
 
 
-def spack_skeleton(dir_name, compiler, mpi_lib, debug):
-    compiler_underscored = re.sub('[@\.]', '_', compiler)
-    if debug:
-        build_type = 'debug'
+def compile(dir_name, compiler, debug, mpi):
+    cluster = re.sub('[0-9]+', '',
+                     subprocess.check_output('hostname'.split()).decode(
+                         'utf-8').strip())
+    if cluster in ['catalyst', 'corona', 'pascal']:
+        architecture = 'x86_64_cuda'
+    elif cluster in ['lassen']:
+        architecture = 'ppc64le_cuda'
     else:
-        build_type = 'rel'
-    output_file_name = '%s/bamboo/compiler_tests/output/%s_%s_spack_output.txt' % (dir_name, compiler_underscored, build_type)
-    error_file_name = '%s/bamboo/compiler_tests/error/%s_%s_spack_error.txt' % (dir_name, compiler_underscored, build_type)
-    os.chdir('%s/bamboo/compiler_tests/builds' % dir_name)
-    debug_flag = ''
+        raise Exception('Invalid cluster={c}'.format(c=cluster))
     if debug:
-        debug_flag = ' -d'
-    command = '%s/scripts/spack_recipes/build_lbann.sh -c %s -m %s%s > %s 2> %s' % (
-        dir_name, compiler, mpi_lib, debug_flag, output_file_name, error_file_name)
-    return_code = os.system(command)
-    os.chdir('..')
+        debug_command = ' +debug'
+        debug_string = '_debug'
+    else:
+        debug_command = ''
+        debug_string = ''
+    compiler_string = compiler.replace('@', '_')
+    compiler_string = compiler_string.replace('.', '_')
+
+    spack = os.environ['bamboo_SPACK']
+
+    # https://lbann.readthedocs.io/en/latest/building_lbann.html#building-installing-lbann-as-a-user
+    # cd <path to LBANN repo>/spack_environments/users/llnl_lc/<arch>_gpu/
+    os.chdir('{lbann_path}/spack_environments/users/llnl_lc/{arch}'.format(
+        lbann_path=dir_name, arch=architecture))
+
+    spack_command = '{s} compiler find; {s} install lbann@develop %{c}{d} +gpu +nccl ^{m} ^hydrogen@develop ^aluminum@master'.format(
+        s=spack, c=compiler, d=debug_command, m=mpi)
+    print('spack_command={s}'.format(s=spack_command))
+    output_file_name = '{d}/bamboo/compiler_tests/output/{cl}_{cs}{ds}_spack_output.txt'.format(
+        d=dir_name, cl=cluster, cs=compiler_string, ds=debug_string)
+    error_file_name = '{d}/bamboo/compiler_tests/error/{cl}_{cs}{ds}_spack_error.txt'.format(
+        d=dir_name, cl=cluster, cs=compiler_string, ds=debug_string)
+    spack_command += '> {o} 2> {e}'.format(
+        o=output_file_name, e=error_file_name)
+    return_code = os.system(spack_command)
     tools.assert_success(return_code, error_file_name)
 
-
-def build_skeleton(dir_name, compiler, debug):
-    compiler_underscored = re.sub('[@\.]', '_', compiler)
-    if debug:
-        build_type = 'debug'
-    else:
-        build_type = 'rel'
-    output_file_name = '%s/bamboo/compiler_tests/output/%s_%s_build_output.txt' % (dir_name, compiler_underscored, build_type)
-    error_file_name = '%s/bamboo/compiler_tests/error/%s_%s_build_error.txt' % (dir_name, compiler_underscored, build_type)
-    compiler = compiler.replace('@', '-')
-    #mpi_lib = mpi_lib.replace('@', '-')
-    cluster = re.sub('[0-9]+', '', subprocess.check_output('hostname'.split()).decode('utf-8').strip())
-    # For reference:
-    # Commenting out for now. These additions to path name will likely return
-    # one day, so I am not removing them entirely.
-    # x86_64 <=> catalyst, pascal
-    # ppc64le <=> ray
-    #architecture = subprocess.check_output('uname -m'.split()).decode('utf-8').strip()
-    #if cluster == 'ray':
-    #    architecture += '_gpu_cuda-9.2.64_cudnn-7.0'
-    #elif cluster == 'pascal':
-    #    architecture += '_gpu_cuda-9.1.85_cudnn-7.1'
-    os.chdir('%s/bamboo/compiler_tests/builds/%s_%s_%s/build' % (dir_name, cluster, compiler, build_type))
-    command = 'make -j all > %s 2> %s' % (output_file_name, error_file_name)
-    return_code = os.system(command)
-    os.chdir('../..')
-    tools.assert_success(return_code, error_file_name)
-
-
-def build_script(cluster, dirname, compiler, debug):
-    print(('Running build_script for cluster={cluster},'
-           ' compiler={compiler}, debug={debug}.').format(
-        cluster=cluster, compiler=compiler, debug=debug))
-    if debug:
-        build = 'debug'
-        debug_flag = '--debug'
-    else:
-        build = 'release'
-        debug_flag = ''
-    output_file_name = '%s/bamboo/compiler_tests/output/%s_%s_%s_build_script_output.txt' % (dirname, cluster, compiler, build)
-    error_file_name = '%s/bamboo/compiler_tests/error/%s_%s_%s_build_script_error.txt' % (dirname, cluster, compiler, build)
-    command = '%s/bamboo/compiler_tests/build_script_specific.sh --compiler %s %s> %s 2> %s' % (dirname, compiler, debug_flag, output_file_name, error_file_name)
-    return_code = os.system(command)
+    build_command = 'ml load lbann'
+    output_file_name = '{d}/bamboo/compiler_tests/output/{cl}_{cs}{ds}_build_output.txt'.format(
+        d=dir_name, cl=cluster, cs=compiler_string, ds=debug_string)
+    error_file_name = '{d}/bamboo/compiler_tests/error/{cl}_{cs}{ds}_build_error.txt'.format(
+        d=dir_name, cl=cluster, cs=compiler_string, ds=debug_string)
+    build_command += '> {o} 2> {e}'.format(
+        o=output_file_name, e=error_file_name)
+    return_code = os.system(build_command)
     tools.assert_success(return_code, error_file_name)
