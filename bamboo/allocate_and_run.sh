1a1731c99ee7c7ea199be1e65bad4b48dd800be9
Ryan Forsyth <forsyth2@llnl.gov>
2019-05-20 16:44:19 -0700

    Update Weekly Develop

--- bamboo/allocate_and_run.sh
+++ bamboo/allocate_and_run.sh
@@ -4,7 +4,31 @@ if [ "${CLUSTER}" = 'catalyst' ]; then
     salloc -N16 -t 600 ./run.sh
 fi
 
+WEEKLY=0
+while :; do
+    case ${1} in
+        --weekly)
+            # Run all tests. This is a weekly build.
+            WEEKLY=1
+            ;;
+        -?*)
+            # Unknown option
+            echo "Unknown option (${1})" >&2
+            exit 1
+            ;;
+        *)
+            # Break loop if there are no more options
+            break
+    esac
+    shift
+done
+
 if [ "${CLUSTER}" = 'pascal' ]; then
     export MV2_USE_CUDA=1
-    salloc -N16 -t 600 ./run.sh
+    if [ ${WEEKLY} -ne 0 ]; then
+        salloc -N16 -t 600 ./run.sh --weekly
+    else
+        salloc -N16 -t 600 ./run.sh
+    fi
+
 fi
--- bamboo/run.sh
+++ bamboo/run.sh
@@ -8,6 +8,25 @@ if [ "${CLUSTER}" = 'pascal' ]; then
     PYTHON=$bamboo_PYTHON_x86_gpu/python
 fi
 
+WEEKLY=0
+while :; do
+    case ${1} in
+        --weekly)
+            # Run all tests. This is a weekly build.
+            WEEKLY=1
+            ;;
+        -?*)
+            # Unknown option
+            echo "Unknown option (${1})" >&2
+            exit 1
+            ;;
+        *)
+            # Break loop if there are no more options
+            break
+    esac
+    shift
+done
+
 echo "Task: Cleaning"
 ./clean.sh
 
@@ -19,7 +38,12 @@ cd ..
 
 echo "Task: Integration Tests"
 cd integration_tests
-$PYTHON -m pytest -s --junitxml=results.xml
+if [ ${WEEKLY} -ne 0 ]; then
+    $PYTHON -m pytest -s --weekly --junitxml=results.xml
+else
+    $PYTHON -m pytest -s --junitxml=results.xml
+fi
+
 cd ..
 
 echo "Task: Unit Tests"
