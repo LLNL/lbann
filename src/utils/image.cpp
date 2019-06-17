8b1b0a1684b4770b3615bf74ac1dca1ded7c678b
Katie Lynn Graham <graham63@pascal83.llnl.gov>
2019-06-17 09:49:51 -0700

    Added function to convert images from El::Matrix<DataType to El::Matrix<uint8_t>, and a second function to encode into std::string.

--- src/utils/image.cpp
+++ src/utils/image.cpp
@@ -238,4 +238,49 @@ void save_image(const std::string& filename, const CPUMat& src,
   save_image(filename, cv_mat, dims);
 }
 
+El::Matrix<uint8_t> get_uint8_t_image(const lbann::CPUMat& image,
+                            const std::vector<size_t>& dims)
+{
+  // Need to convert to uint8_t matrix in OpenCV format.
+  // We will normalize to [0, 1], then map to [0, 255].
+  const size_t size = utils::get_linearized_size(dims);
+  El::Matrix<uint8_t> cv_mat = El::Matrix<uint8_t>(size, 1);
+  // Find the minimum and maximum to normalize with.
+  const lbann::DataType* __restrict__ img_buf = image.LockedBuffer();
+  lbann::DataType min = std::numeric_limits<lbann::DataType>::max();
+  lbann::DataType max = std::numeric_limits<lbann::DataType>::lowest();
+  for (size_t i = 0; i < size; ++i) {
+    min = std::min(min, img_buf[i]);
+    max = std::max(max, img_buf[i]);
+  }
+  const lbann::DataType norm_denom = max - min;
+  // Construct the OpenCV buffer.
+  uint8_t* __restrict__ cv_buf = cv_mat.Buffer();
+  for (size_t channel = 0; channel < dims[0]; ++channel) {
+    const size_t img_offset = channel*dims[1]*dims[2];
+    for (size_t col = 0; col < dims[2]; ++col) {
+      for (size_t row = 0; row < dims[1]; ++row) {
+        const lbann::DataType norm_img_val =
+          (img_buf[img_offset + row + col*dims[1]] - min) / norm_denom;
+        cv_buf[dims[0]*(col + row*dims[2]) + channel] =
+          static_cast<uint8_t>(norm_img_val * 255);
+      }
+    }
+  }
+  return cv_mat;
+}
+
+std::string encode_image(const El::Matrix<uint8_t>& image,
+                                 const std::vector<size_t>& dims)
+{
+  cv::Mat Mat_img = lbann::utils::get_opencv_mat(
+    const_cast<El::Matrix<uint8_t>&>(image), dims);
+  std::vector<uint8_t> encoded_img;
+  std::vector<int> params = {cv::IMWRITE_JPEG_QUALITY, 20};
+
+  cv::imencode(".jpg", Mat_img, encoded_img, params);
+
+  return std::string{encoded_img.begin(), encoded_img.end()};
+}
+
 }  // namespace lbann
