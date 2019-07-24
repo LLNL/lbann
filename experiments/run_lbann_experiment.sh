4cba013fe8c4b95d9e2dfde2393e227ceaea7f5d
Tim Moon <moon13@llnl.gov>
2019-07-24 15:22:16 -0700

    Reverting accidentally changed experiment script.

--- experiments/run_lbann_experiment.sh
+++ experiments/run_lbann_experiment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Experiment parameters
-EXPERIMENT_NAME=lbann_ridgeregression
+EXPERIMENT_NAME=lbann_alexnet
 LBANN_DIR=$(git rev-parse --show-toplevel)
-MODEL_PROTO="--model=${LBANN_DIR}/model_zoo/tests/model_mnist_ridge_regression.prototext"
-READER_PROTO="--reader=${LBANN_DIR}/model_zoo/data_readers/data_reader_mnist.prototext"
+MODEL_PROTO="--model=${LBANN_DIR}/model_zoo/models/alexnet/model_alexnet.prototext --num_epochs=10"
+READER_PROTO="--reader=${LBANN_DIR}/model_zoo/data_readers/data_reader_imagenet.prototext"
 OPTIMIZER_PROTO="--optimizer=${LBANN_DIR}/model_zoo/optimizers/opt_sgd.prototext"
 IMAGENET_CLASSES= # options: 10, 100, 300, 1000 (leave blank to use other dataset)
 BUILD=            # default: Release
