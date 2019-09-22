f93cbc181dc8de70c21a51b45bafbbff21c45776
Thomas R. Benson <benson31@llnl.gov>
2019-09-21 17:12:46 -0700

    add documentation of trainers, training algorithms, and execution contexts to sphinx directory pages

--- docs/RSTDocsFlavorText.py
+++ docs/RSTDocsFlavorText.py
@@ -56,6 +56,21 @@ code.''',
 The data store provides in-memory caching of the data set and
 inter-epoch data shuffling.''',
 
+    'execution_contexts' : '''
+When a model is attached to a trainer the execution context of the
+training algorithm is stored in an execution_context class (or
+sub-class) per execution mode.  Thus there is one execution context
+per model and mode that contains all of the state with respect to the
+training algorithm being applied to the model.
+
+For example it tracks the current:
+
+* step
+* execution mode
+* epoch
+* and a pointer back to the trainer
+''',
+
     'layers' : '''
 LBANN models are defined in model prototext files. The bulk of these
 defintions will be the series of layers which make up the model
@@ -106,6 +121,30 @@ architecture of neural networks, data readers, and optimizers. It
 serves as the "assembly language" interface to the toolkit. The
 python front end of LBANN will emit a network description in the
 protobuf format that is ingested at runtime.''',
+
+    'trainers' : '''
+A trainer is a collection of compute resources and defines a explicit
+communication domain.  It provides the execution for both the training
+and inference of a trained model.  Once constructed a trainer owns an
+LBANN comm object that defines both intra- and inter-trainer
+communication domains.  Additionally, a trainer will contain an I/O
+thread pool that is used to fetch and pre-process data that will be
+provided to the trainer's models.
+
+A trainer owns:
+
+* comm object
+* I/O thread pool
+* One or more models
+* Execution context for each model
+* In the future, it will also contain the data readers.
+''',
+
+    'training_algorithms' : '''
+The training algorithm defines the optimization that is to be
+applied to the model(s) being trained.  Additionally, it can
+specify how to evaluate the model.
+''',
 
     'utils' : 'Utility classes and functions.',
 
