0aa67adfb1a02d711b14a150bad7183f61b50c3b
Thomas R. Benson <benson31@llnl.gov>
2019-09-30 08:36:13 -0700

    make build script more general

--- bamboo/compiler_tests/build_script.sh
+++ bamboo/compiler_tests/build_script.sh
@@ -6,26 +6,89 @@ source /etc/profile.d/00-modulepath.sh
 LBANN_DIR=$(git rev-parse --show-toplevel)
 CLUSTER=$(hostname | sed 's/[0-9]*//g')
 USER=$(whoami)
-WORKSPACE_DIR=$(ls --color=no -d /usr/workspace/*/${USER})
-DEPENDENCY_DIR=${WORKSPACE_DIR}/stable_dependencies/${CLUSTER}/gcc-7.3.0/mvapich2-2.3
+WORKSPACE_DIR=$(ls --color=no -d /usr/workspace/ws*/${USER})
+DEPENDENCY_DIR_BASE=${WORKSPACE_DIR}/stable_dependencies/${CLUSTER}
+
+# For this script, we only care about GCC.
+LATEST_GCC=$(ls -1 ${DEPENDENCY_DIR_BASE} | grep gcc | tail -n1)
+COMPILER_DIR=${DEPENDENCY_DIR_BASE}/${LATEST_GCC}
+
+# For now, there's only one MPI library. The pipe to tail ensures that
+# we just pick one thing, just in case.
+MPI_LIBRARY=$(ls -1 --color=no ${COMPILER_DIR} | tail -n1)
+MPI_DIR=${COMPILER_DIR}/${MPI_LIBRARY}
+
+# All the dependencies are installed at the MPI level (even though
+# most are MPI-independent).
+DEPENDENCY_DIR=${MPI_DIR}
+
 if [ -e ${DEPENDENCY_DIR} ];
 then
-    ml gcc/7.3.0 cmake/3.14.5 cuda
-    export NCCL_DIR=/usr/workspace/wsb/brain/nccl2/nccl_2.4.2-1+cuda10.0_x86_64
-    export CUDNN_DIR=/usr/WS2/brain/cudnn/cudnn-7.6.2/cuda-10.1_x86_64
-    export PATH=${WORKSPACE_DIR}/stable_dependencies/${CLUSTER}/ninja/bin:${PATH}
+    SAVELIST_NAME=$(echo ${CLUSTER}_${LATEST_GCC}_${MPI_LIBRARY} | sed -e 's/\./x/g')
 
-    # Setup paths to match the build script (ugh)
+    if ml -t savelist |& grep ${SAVELIST_NAME}
+    then
+        ml restore ${SAVELIST_NAME}
+    else
+        # Compilers are easy...
+        COMPILER_MODULE=$(echo ${LATEST_GCC} | sed -e 's|-|/|g')
+
+        if [[ ${MPI_LIBRARY} =~ "spectrum-mpi-.*" ]]
+        then
+            MPI_MODULE=$(echo ${MPI_LIBRARY} | sed -e 's|spectrum-mpi-|spectrum-mpi/|g')
+        else
+            MPI_MODULE=$(echo ${MPI_LIBRARY} | sed -e 's|-|/|g')
+        fi
+        
+        # Use the latest CUDA 10, since it's compatible with other
+        # CUDA 10.* libraries
+        CUDA_MODULE=$(ml --terse avail cuda |& sed -n '/\/10\./p' | tail -n1)
+
+        # Load up the appropriate modules
+        module load ${COMPILER_MODULE} ${MPI_MODULE} ${CUDA_MODULE} cmake/3.14.5
+        ml save ${SAVELIST_NAME}
+    fi
+
+    BRAIN_DIR=/usr/workspace/wsb/brain
+
+    # CUDA-y things (Use the newest)
+    ARCH=$(uname -i)
+    export NCCL_DIR=$(ls -d --color=no ${BRAIN_DIR}/nccl2/*cuda10*${ARCH} | tail -n1)
+    export CUDNN_DIR=$(find ${BRAIN_DIR}/cudnn -maxdepth 2 -type d | grep "cuda-10.*_${ARCH}" | tail -n1)
+
+    # Unit testing framework
+    export CATCH2_DIR=${WORKSPACE_DIR}/stable_dependencies/catch2
+
+    # Add Ninja support
+    export PATH=${DEPENDENCY_DIR_BASE}/ninja/bin:${PATH}
+    
+    # Setup paths to match the build_lbann_lc.sh script (ugh)
     BUILD_DIR_BASE=${LBANN_DIR}/build/gnu.Release.${CLUSTER}.llnl.gov
     BUILD_DIR=${BUILD_DIR_BASE}/lbann/build
     INSTALL_DIR=${BUILD_DIR_BASE}/install
     
     # Setup a path for Catch2 to use
     CATCH2_OUTPUT_DIR=${LBANN_DIR}/bamboo/compiler_tests
+
+    # Decide if CUDA should be used.
+    if [[ "${CLUSTER}" =~ ^(pascal|lassen|ray)$ ]];
+    then
+            USE_CUDA=ON
+    else
+            USE_CUDA=OFF
+    fi
     
     # Cleanup
-    [ -e ${BUILD_DIR_BASE} ] && rm -rf ${BUILD_DIR_BASE}
+    [[ -e ${BUILD_DIR_BASE} ]] && rm -rf ${BUILD_DIR_BASE}
     mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+
+    # Hack to be nice to others.
+    if [[ "${CLUSTER}" =~ ^(lassen|ray)$ ]];
+    then
+        LAUNCH_CMD="lrun -1"
+    else
+        unset LAUNCH_CMD
+    fi
     
     cmake \
         -GNinja \
@@ -63,7 +126,7 @@ then
         -DPROTOBUF_DIR=${DEPENDENCY_DIR} \
         -Dprotobuf_MODULE_COMPATIBLE=ON \
         \
-        ${LBANN_DIR} && ninja && ninja install && ./unit_test/seq-catch-tests -r junit -o ${CATCH2_OUTPUT_DIR}/seq_catch_tests_output.xml
+        ${LBANN_DIR} && ${LAUNCH_CMD} ninja && ${LAUNCH_CMD} ninja install && ${LAUNCH_CMD} ./unit_test/seq-catch-tests -r junit -o ${CATCH2_OUTPUT_DIR}/seq_catch_tests_output.xml
 else
     ${LBANN_DIR}/scripts/build_lbann_lc.sh --with-conduit
 fi
