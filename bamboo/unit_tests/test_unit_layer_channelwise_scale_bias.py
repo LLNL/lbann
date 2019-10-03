7f2a97562b514127a3e452808bfbde91fc502153
Tim Moon <moon13@llnl.gov>
2019-10-02 17:24:31 -0700

    Remove PyTest boilerplate from layer unit tests

--- bamboo/unit_tests/test_unit_layer_channelwise_scale_bias.py
+++ bamboo/unit_tests/test_unit_layer_channelwise_scale_bias.py
@@ -4,7 +4,6 @@ import os
 import os.path
 import sys
 import numpy as np
-import pytest
 
 # Local files
 current_file = os.path.realpath(__file__)
@@ -174,78 +173,8 @@ def construct_data_reader(lbann):
 # Setup PyTest
 # ==============================================
 
-# Generate test name based on file name
+# Create test functions that can interact with PyTest
+# Note: Create test name by removing ".py" from file name
 _test_name = os.path.splitext(os.path.basename(current_file))[0]
-
-# Primary test method
-def _test(cluster, executables, dir_name, compiler_name):
-    tools.process_executable(_test_name, compiler_name, executables)
-
-    # Import LBANN Python frontend
-    if compiler_name == 'exe':
-        exe = executables[compiler_name]
-        bin_dir = os.path.dirname(exe)
-        install_dir = os.path.dirname(bin_dir)
-        build_path = '{i}/lib/python3.7/site-packages'.format(i=install_dir)
-    else:
-        if compiler_name == 'clang6':
-            path = 'clang.Release'
-        elif compiler_name == 'clang6_debug':
-            path = 'clang.Debug'
-        elif compiler_name == 'gcc7':
-            path = 'gnu.Release'
-        elif compiler_name == 'clang6_debug':
-            path = 'gnu.Debug'
-        elif compiler_name == 'intel19':
-            path = 'intel.Release'
-        elif compiler_name == 'intel19_debug':
-            path = 'intel.Debug'
-        path = '{p}.{c}.llnl.gov'.format(p=path, c=cluster)
-        build_path = '{d}/build/{p}/install/lib/python3.7/site-packages'.format(
-            d=dir_name, p=path)
-    print('build_path={b}'.format(b=build_path))
-    sys.path.append(build_path)
-    import lbann
-    import lbann.contrib.lc.launcher
-
-    # Setup LBANN experiment
-    trainer, model, data_reader, optimizer = setup_experiment(lbann)
-
-    # Run LBANN experiment
-    kwargs = {
-        'account': 'guests',
-        'nodes': 1,
-        'partition': 'pbatch'
-    }
-    experiment_dir = '{d}/bamboo/unit_tests/experiments/{t}_{c}'.format(
-        d=dir_name, t=_test_name, c=compiler_name)
-    error_file_name = '{e}/err.log'.format(
-        e=experiment_dir, c=compiler_name)
-    return_code = lbann.contrib.lc.launcher.run(
-        trainer=trainer,
-        model=model,
-        data_reader=data_reader,
-        optimizer=optimizer,
-        experiment_dir=experiment_dir,
-        job_name='lbann_{}'.format(_test_name),
-        **kwargs)
-    tools.assert_success(return_code, error_file_name)
-
-# Construct methods that will be detected by PyTest
-def _test_clang6(cluster, exes, dirname):
-    _test(cluster, exes, dirname, 'clang6')
-def _test_gcc7(cluster, exes, dirname):
-    _test(cluster, exes, dirname, 'gcc7')
-def _test_intel19(cluster, exes, dirname):
-    _test(cluster, exes, dirname, 'intel19')
-def _test_exe(cluster, dirname, exe):
-    if exe is None:
-        e = 'test_{}_exe: Non-local testing'.format(_test_name)
-        print('Skip - ' + e)
-        pytest.skip(e)
-    exes = {'exe': exe}
-    _test(cluster, exes, dirname, 'exe')
-globals()['{}_clang6'.format(_test_name)] = _test_clang6
-globals()['{}_gcc7'.format(_test_name)] = _test_gcc7
-globals()['{}_intel19'.format(_test_name)] = _test_intel19
-globals()['{}_exe'.format(_test_name)] = _test_exe
+for test in tools.create_tests(setup_experiment, _test_name):
+    globals()[test.__name__] = test
