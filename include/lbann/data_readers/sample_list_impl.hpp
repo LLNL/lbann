8f6c921a771877d8a1e2739a6b348cf7c339a85e
David A. Hysom <hysom@llnl.gov>
2019-10-14 15:00:32 -0700

    Made "broadcast" version of sample_list::all_gather_archive() the default. The version that uses all_gather can be selected via the cmd line flag: --all_gather_old

--- include/lbann/data_readers/sample_list_impl.hpp
+++ include/lbann/data_readers/sample_list_impl.hpp
@@ -338,7 +338,7 @@ inline void sample_list<sample_name_t>
 ::all_gather_archive(const std::string &archive,
                      std::vector<std::string>& gathered_archive,
                      lbann_comm& comm) {
-  if (options::get()->get_bool("all_gather_new")) {
+  if (!options::get()->get_bool("all_gather_old")) {
     all_gather_archive_new(archive, gathered_archive, comm);
     return;
   }
@@ -386,7 +386,7 @@ inline void sample_list<sample_name_t>
 
 template <typename sample_name_t>
 inline void sample_list<sample_name_t>
-::all_gather_archive_new(const std::string &archive,
+::all_gather_archive(const std::string &archive,
                      std::vector<std::string>& gathered_archive,
                      lbann_comm& comm) {
 
