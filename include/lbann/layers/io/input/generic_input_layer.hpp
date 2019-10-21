3ff2c0f14ab60204ddc6e8bee7eeb83b8210d40a
Tom Benson <30674819+benson31@users.noreply.github.com>
2019-10-21 14:35:21 -0700

    Fix model copy issue (#1311)
    * fix an issue where null pointers could be dereferenced

--- include/lbann/layers/io/input/generic_input_layer.hpp
+++ include/lbann/layers/io/input/generic_input_layer.hpp
@@ -126,7 +126,7 @@ class generic_input_layer : public io_layer {
       io_buffer = io_buffer->copy();
     }
     for (auto& dr : m_data_readers) {
-      dr.second = dr.second->copy();
+      dr.second = dr.second ? dr.second->copy() : nullptr;
     }
   }
 
@@ -136,7 +136,7 @@ class generic_input_layer : public io_layer {
       io_buffer = io_buffer->copy();
     }
     for (auto& dr : m_data_readers) {
-      dr.second = dr.second->copy();
+      dr.second = dr.second ? dr.second->copy() : nullptr;
     }
     return *this;
   }
