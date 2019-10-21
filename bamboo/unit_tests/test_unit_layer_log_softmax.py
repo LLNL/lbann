Fix indexing bugs in log-softmax layer CUDA kernels

bamboo/unit_tests/test_unit_layer_log_softmax.py
@@ -1,56 +1,210 @@
-import sys
-sys.path.insert(0, '../common_python')
-import tools
-import pytest
+import functools
+import operator
 import os
+import os.path
+import sys
+import numpy as np
 
+# Local files
+current_file = os.path.realpath(__file__)
+current_dir = os.path.dirname(current_file)
+sys.path.insert(0, os.path.join(os.path.dirname(current_dir), 'common_python'))
+import tools
 
-def skeleton_layer_log_softmax(cluster, executables, dir_name, compiler_name,
-                               weekly, data_reader_percent):
-    if compiler_name not in executables:
-      e = 'skeleton_layer_log_softmax: default_exes[%s] does not exist' % compiler_name
-      print('Skip - ' + e)
-      pytest.skip(e)
-    output_file_name = '%s/bamboo/unit_tests/output/layer_log_softmax_%s_output.txt' % (dir_name, compiler_name)
-    error_file_name  = '%s/bamboo/unit_tests/error/layer_log_softmax_%s_error.txt' % (dir_name, compiler_name)
-    command = tools.get_command(
-        cluster=cluster, executable=executables[compiler_name],
-        num_nodes=1,
-        time_limit=10,
-        num_processes=2, dir_name=dir_name,
-        data_reader_name='synthetic',
-        data_reader_percent=data_reader_percent,
-        model_folder='tests/layer_tests', model_name='log_softmax',
-        optimizer_name='sgd',
-        output_file_name=output_file_name, error_file_name=error_file_name, weekly=weekly)
-    return_code = os.system(command)
-    tools.assert_success(return_code, error_file_name)
+# ==============================================
+# Objects for Python data reader
+# ==============================================
+# Note: The Python data reader imports this file as a module and calls
+# the functions below to ingest data.
 
+# Data
+np.random.seed(201910213)
+_num_samples = 15
+_sample_size = 11
+_samples = np.random.normal(size=(_num_samples,_sample_size)).astype(np.float32)
 
-def test_unit_layer_log_softmax_clang6(cluster, exes, dirname,
-                                       weekly, data_reader_percent):
-    skeleton_layer_log_softmax(cluster, exes, dirname, 'clang6',
-                               weekly, data_reader_percent)
+# Sample access functions
+def get_sample(index):
+    return _samples[index,:]
+def num_samples():
+    return _num_samples
+def sample_dims():
+    return (_sample_size,)
 
+# ==============================================
+# NumPy softmax
+# ==============================================
 
-def test_unit_layer_log_softmax_gcc7(cluster, exes, dirname,
-                                     weekly, data_reader_percent):
-    skeleton_layer_log_softmax(cluster, exes, dirname, 'gcc7',
-                               weekly, data_reader_percent)
+def numpy_log_softmax(x):
+    """NumPy implementation of log-softmax."""
+    x = x.astype(np.float64)
+    x = x - np.max(x)
+    return x - np.log(np.sum(np.exp(x)))
 
+# ==============================================
+# Setup LBANN experiment
+# ==============================================
 
-def test_unit_layer_log_softmax_intel19(cluster, exes, dirname,
-                                        weekly, data_reader_percent):
-    skeleton_layer_log_softmax(cluster, exes, dirname, 'intel19',
-                               weekly, data_reader_percent)
+def setup_experiment(lbann):
+    """Construct LBANN experiment.
 
+    Args:
+        lbann (module): Module for LBANN Python frontend
 
-# Run with python3 -m pytest -s test_unit_ridge_regression.py -k 'test_unit_layer_log_softmax_exe' --exe=<executable>
-def test_unit_layer_log_softmax_exe(cluster, dirname, exe, weekly, data_reader_percent):
-    if exe is None:
-        e = 'test_unit_layer_log_softmax_exe: Non-local testing'
-        print('Skip - ' + e)
-        pytest.skip(e)
-    exes = {'exe': exe}
-    skeleton_layer_log_softmax(cluster, exes, dirname, 'exe',
-                               weekly, data_reader_percent)
+    """
+    trainer = lbann.Trainer()
+    model = construct_model(lbann)
+    data_reader = construct_data_reader(lbann)
+    optimizer = lbann.NoOptimizer()
+    return trainer, model, data_reader, optimizer
+
+def construct_model(lbann):
+    """Construct LBANN model.
+
+    Args:
+        lbann (module): Module for LBANN Python frontend
+
+    """
+
+    # Convenience function to convert list to a space-separated string
+    def str_list(it):
+        return ' '.join([str(i) for i in it])
+
+    # Convenience function to compute L2 norm squared with NumPy
+    def l2_norm2(x):
+        x = x.reshape(-1).astype(np.float64)
+        return np.inner(x, x)
+
+    # Input data
+    # Note: Sum with a weights layer so that gradient checking will
+    # verify that error signals are correct.
+    x_weights = lbann.Weights(optimizer=lbann.SGD(),
+                              initializer=lbann.ConstantInitializer(value=0.0))
+    x0 = lbann.WeightsLayer(weights=x_weights, dims=str(_sample_size))
+    x1 = lbann.Identity(lbann.Input())
+    x = lbann.Sum([x0, x1])
+    x_lbann = x
+
+    # Objects for LBANN model
+    obj = []
+    metrics = []
+    callbacks = []
+
+    # ------------------------------------------
+    # Data-parallel layout
+    # ------------------------------------------
+
+    # LBANN implementation
+    x = x_lbann
+    y = lbann.LogSoftmax(x, data_layout='data_parallel')
+    z = lbann.L2Norm2(y)
+    obj.append(z)
+    metrics.append(lbann.Metric(z, name='data-parallel output'))
+
+    # NumPy implementation
+    vals = []
+    for i in range(num_samples()):
+        x = get_sample(i)
+        y = numpy_log_softmax(x)
+        z = l2_norm2(y)
+        vals.append(z)
+    val = np.mean(vals)
+    tol = 8 * val * np.finfo(np.float32).eps
+    callbacks.append(lbann.CallbackCheckMetric(
+        metric=metrics[-1].name,
+        lower_bound=val-tol,
+        upper_bound=val+tol,
+        error_on_failure=True,
+        execution_modes='test'))
+
+    # ------------------------------------------
+    # Model-parallel layout
+    # ------------------------------------------
+
+    # LBANN implementation
+    x = x_lbann
+    y = lbann.LogSoftmax(x, data_layout='model_parallel')
+    z = lbann.L2Norm2(y)
+    obj.append(z)
+    metrics.append(lbann.Metric(z, name='model-parallel output'))
+
+    # NumPy implementation
+    vals = []
+    for i in range(num_samples()):
+        x = get_sample(i)
+        y = numpy_log_softmax(x)
+        z = l2_norm2(y)
+        vals.append(z)
+    val = np.mean(vals)
+    tol = 8 * val * np.finfo(np.float32).eps
+    callbacks.append(lbann.CallbackCheckMetric(
+        metric=metrics[-1].name,
+        lower_bound=val-tol,
+        upper_bound=val+tol,
+        error_on_failure=True,
+        execution_modes='test'))
+
+    # ------------------------------------------
+    # Gradient checking
+    # ------------------------------------------
+
+    callbacks.append(lbann.CallbackCheckGradients(error_on_failure=True))
+
+    # ------------------------------------------
+    # Construct model
+    # ------------------------------------------
+
+    mini_batch_size = num_samples() // 2
+    num_epochs = 0
+    return lbann.Model(mini_batch_size,
+                       num_epochs,
+                       layers=lbann.traverse_layer_graph(x_lbann),
+                       objective_function=obj,
+                       metrics=metrics,
+                       callbacks=callbacks)
+
+def construct_data_reader(lbann):
+    """Construct Protobuf message for Python data reader.
+
+    The Python data reader will import the current Python file to
+    access the sample access functions.
+
+    Args:
+        lbann (module): Module for LBANN Python frontend
+
+    """
+
+    # Note: The training data reader should be removed when
+    # https://github.com/LLNL/lbann/issues/1098 is resolved.
+    message = lbann.reader_pb2.DataReader()
+    message.reader.extend([
+        tools.create_python_data_reader(
+            lbann,
+            current_file,
+            'get_sample',
+            'num_samples',
+            'sample_dims',
+            'train'
+        )
+    ])
+    message.reader.extend([
+        tools.create_python_data_reader(
+            lbann,
+            current_file,
+            'get_sample',
+            'num_samples',
+            'sample_dims',
+            'test'
+        )
+    ])
+    return message
+
+# ==============================================
+# Setup PyTest
+# ==============================================
+
+# Create test functions that can interact with PyTest
+# Note: Create test name by removing ".py" from file name
+_test_name = os.path.splitext(os.path.basename(current_file))[0]
+for test in tools.create_tests(setup_experiment, _test_name):
+    globals()[test.__name__] = test