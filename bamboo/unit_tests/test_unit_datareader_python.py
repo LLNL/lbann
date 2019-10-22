Bugfix for softmax and log-softmax layers (#1318)
* Fix bug in GPU log-softmax layer

A CUDA kernel was sometimes launched with a grid size of 0

* Fix bug in GPU softmax layer

A CUDA kernel was sometimes launched with a grid size of 0

* Make sure Bamboo layer unit tests have a last mini-batch of size 1

Helps catch bugs when a process has no local data.

* Fix indexing bugs in log-softmax layer CUDA kernels

* Document softmax and log-softmax layers in layers.proto

bamboo/unit_tests/test_unit_datareader_python.py
@@ -17,7 +17,7 @@ import tools
 
 # Data
 np.random.seed(20190708)
-_num_samples = 23
+_num_samples = 29
 _sample_size = 7
 _samples = np.random.normal(size=(_num_samples,_sample_size))
 _samples = _samples.astype(np.float32)
@@ -78,7 +78,7 @@ def construct_model(lbann):
         execution_modes='test'))
 
     # Construct model
-    mini_batch_size = 5
+    mini_batch_size = num_samples() // 4
     num_epochs = 0
     return lbann.Model(mini_batch_size,
                        num_epochs,

bamboo/unit_tests/test_unit_layer_argmax.py
@@ -19,7 +19,7 @@ import tools
 
 # Data
 np.random.seed(20190911)
-_num_samples = 31
+_num_samples = 35
 _sample_dims = (11,)
 _sample_size = functools.reduce(operator.mul, _sample_dims)
 _samples = np.random.normal(size=(_num_samples,_sample_size)).astype(np.float32)
@@ -98,7 +98,7 @@ def construct_model(lbann):
         execution_modes='test'))
 
     # Construct model
-    mini_batch_size = 17
+    mini_batch_size = num_samples() // 2
     num_epochs = 0
     return lbann.Model(mini_batch_size,
                        num_epochs,