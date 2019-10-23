a3e55e4d5126bd9c660d28c9ee44699bb28f227b
Tim Moon <moon13@llnl.gov>
2019-10-23 15:09:18 -0700

    Add Bamboo integration tests for LeNet, AlexNet, and ResNet-50
    Remove the existing integration tests

--- bamboo/allocate_and_run.sh
+++ bamboo/allocate_and_run.sh
@@ -35,7 +35,7 @@ fi
 if [ "${CLUSTER}" = 'lassen' ]; then
     ALLOCATION_TIME_LIMIT=600
     if [ ${WEEKLY} -ne 0 ]; then
-        timeout -k 5 24h bsub -G guests -Is -q pbatch -nnodes 16 -W ${ALLOCATION_TIME_LIMIT} ./run.sh --weekly
+        timeout -k 5 24h bsub -G guests -Is -q pbatch -nnodes 4 -W ${ALLOCATION_TIME_LIMIT} ./run.sh --weekly
     else
         timeout -k 5 24h bsub -G guests -Is -q pbatch -nnodes 2 -W ${ALLOCATION_TIME_LIMIT} ./run.sh
     fi
@@ -49,7 +49,7 @@ elif [ "${CLUSTER}" = 'ray' ]; then
 elif [ "${CLUSTER}" = 'catalyst' ] || [ "${CLUSTER}" = 'corona' ] || [ "${CLUSTER}" = 'pascal' ]; then
     ALLOCATION_TIME_LIMIT=960
     if [ ${WEEKLY} -ne 0 ]; then
-        timeout -k 5 24h salloc -N16 --partition=pbatch -t ${ALLOCATION_TIME_LIMIT} ./run.sh --weekly
+        timeout -k 5 24h salloc -N4 --partition=pbatch -t ${ALLOCATION_TIME_LIMIT} ./run.sh --weekly
         if [ "${CLUSTER}" = 'catalyst' ]; then
             cd integration_tests
             python -m pytest -s test_integration_performance.py -k test_integration_performance_full_alexnet_clang6 --weekly --run --junitxml=../full_alexnet_clang6/results.xml
---
+++ bamboo/integration_tests/test_integration_lenet.py
@@ -0,0 +1,204 @@
+import functools
+import operator
+import os
+import os.path
+import re
+import sys
+import numpy as np
+import google.protobuf.text_format
+import pytest
+
+# Local files
+current_file = os.path.realpath(__file__)
+current_dir = os.path.dirname(current_file)
+sys.path.insert(0, os.path.join(os.path.dirname(current_dir), 'common_python'))
+import tools
+
+# ==============================================
+# Options
+# ==============================================
+
+# Training options
+num_epochs = 5
+mini_batch_size = 64
+num_nodes = 2
+
+# Classification accuracy (percent)
+expected_train_accuracy_range = (98.5, 99.5)
+expected_test_accuracy_range = (98, 99)
+
+# Average mini-batch time (in sec) for each LC system
+expected_mini_batch_times = {
+    'pascal':   0.0040,
+    'catalyst': 0.0055,
+    'lassen':   0.0020,
+    'ray':      -1,
+    'corona':   -1,
+}
+
+# ==============================================
+# Setup LBANN experiment
+# ==============================================
+
+def setup_experiment(lbann):
+    """Construct LBANN experiment.
+
+    Args:
+        lbann (module): Module for LBANN Python frontend
+
+    """
+    trainer = lbann.Trainer()
+    model = construct_model(lbann)
+    data_reader = construct_data_reader(lbann)
+    optimizer = lbann.SGD(learn_rate=0.01, momentum=0.9)
+    return trainer, model, data_reader, optimizer
+
+def construct_model(lbann):
+    """Construct LBANN model.
+
+    Args:
+        lbann (module): Module for LBANN Python frontend
+
+    """
+
+    # TODO (tym): Figure out how to switch between LBANN builds. See
+    # GitHub Issue #1289.
+    import lbann.models
+
+    # Layer graph
+    input_ = lbann.Input()
+    images = lbann.Identity(input_)
+    labels = lbann.Identity(input_)
+    x = lbann.models.LeNet(10)(images)
+    probs = lbann.Softmax(x)
+    loss = lbann.CrossEntropy([probs, labels])
+    acc = lbann.CategoricalAccuracy([probs, labels])
+
+    # Objects for LBANN model
+    callbacks = [lbann.CallbackPrint(), lbann.CallbackTimer()]
+    metrics = [lbann.Metric(acc, name='accuracy', unit='%')]
+
+    # Construct model
+    return lbann.Model(mini_batch_size,
+                       num_epochs,
+                       layers=lbann.traverse_layer_graph(input_),
+                       objective_function=loss,
+                       metrics=metrics,
+                       callbacks=callbacks)
+
+def construct_data_reader(lbann):
+    """Construct Protobuf message for Python data reader.
+
+    The Python data reader will import the current Python file to
+    access the sample access functions.
+
+    Args:
+        lbann (module): Module for LBANN Python frontend
+
+    """
+
+    # TODO (tym): Figure out how to switch between LBANN builds. See
+    # GitHub Issue #1289.
+    import lbann.contrib.lc.paths
+
+    # Load data readers from prototext
+    dirname = os.path.dirname
+    lbann_dir = dirname(dirname(dirname(os.path.realpath(__file__))))
+    pb_file = os.path.join(lbann_dir,
+                           'model_zoo',
+                           'data_readers',
+                           'data_reader_mnist.prototext')
+    message = lbann.lbann_pb2.LbannPB()
+    with open(pb_file, 'r') as f:
+        google.protobuf.text_format.Merge(f.read(), message)
+    message = message.data_reader
+
+    # Set location of MNIST data
+    for reader in message.reader:
+        reader.data_filedir = lbann.contrib.lc.paths.mnist_dir()
+
+    # No validation set
+    message.reader[0].validation_percent = 0
+
+    return message
+
+# ==============================================
+# Setup PyTest
+# ==============================================
+
+def augment_test_func(test_func):
+    """Augment test function to parse log files.
+
+    `tools.create_tests` creates functions that run an LBANN
+    experiment. This function creates augmented functions that parse
+    the log files after LBANN finishes running, e.g. to check metrics
+    or runtimes.
+
+    Note: The naive approach is to define the augmented test functions
+    in a loop. However, Python closures are late binding. In other
+    words, the function would be overwritten every time we define it.
+    We get around this overwriting problem by defining the augmented
+    function in the local scope of another function.
+
+    Args:
+        test_func (function): Test function created by
+            `tools.create_tests`.
+
+    Returns:
+        function: Test that can interact with PyTest.
+
+    """
+    test_name = test_func.__name__
+
+    # Define test function
+    def func(cluster, exes, dirname):
+
+        # Run LBANN experiment
+        experiment_output = test_func(cluster, exes, dirname)
+
+        # Parse LBANN log file
+        train_accuracy = None
+        test_accuracy = None
+        mini_batch_times = []
+        with open(experiment_output['stdout_log_file']) as f:
+            for line in f:
+                match = re.search('training epoch [0-9]+ accuracy : ([0-9.]+)%', line)
+                if match:
+                    train_accuracy = float(match.group(1))
+                match = re.search('test accuracy : ([0-9.]+)%', line)
+                if match:
+                    test_accuracy = float(match.group(1))
+                match = re.search('training epoch [0-9]+ mini-batch time statistics : ([0-9.]+)s mean', line)
+                if match:
+                    mini_batch_times.append(float(match.group(1)))
+
+        # Check if training accuracy is within expected range
+        assert (expected_train_accuracy_range[0]
+                < train_accuracy
+                < expected_train_accuracy_range[1]), \
+                'train accuracy is outside expected range'
+
+        # Check if testing accuracy is within expected range
+        assert (expected_test_accuracy_range[0]
+                < test_accuracy
+                < expected_test_accuracy_range[1]), \
+                'test accuracy is outside expected range'
+
+        # Check if mini-batch time is within expected range
+        # Note: Skip first epoch since its runtime is usually an outlier
+        mini_batch_times = mini_batch_times[1:]
+        mini_batch_time = sum(mini_batch_times) / len(mini_batch_times)
+        assert (0.75 * expected_mini_batch_times[cluster]
+                < mini_batch_time
+                < 1.25 * expected_mini_batch_times[cluster]), \
+                'average mini-batch time is outside expected range'
+
+    # Return test function from factory function
+    func.__name__ = test_name
+    return func
+
+# Create test functions that can interact with PyTest
+for _test_func in tools.create_tests(setup_experiment,
+                                     __file__,
+                                     nodes=num_nodes):
+    globals()[_test_func.__name__] = augment_test_func(_test_func)
