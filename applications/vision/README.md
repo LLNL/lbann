2e8efe6c92b8bf7ad8cbd4c00121cd4a2f286efd
Tim Moon <moon13@llnl.gov>
2019-11-01 16:00:38 -0700

    Create vision application and add LeNet

---
+++ applications/vision/README.md
@@ -0,0 +1,6 @@
+# Example models for computer vision
+
+This directory contains LBANN implementations of widely-used vision
+models. They are intended to validate and benchmark LBANN's vision
+functionality, and are also suitable as pedagogical tools for using
+LBANN.
---
+++ applications/vision/lenet.py
@@ -0,0 +1,104 @@
+import argparse
+import os.path
+import google.protobuf.text_format as txtf
+import lbann
+import data
+
+# ----------------------------------
+# Command-line arguments
+# ----------------------------------
+
+desc = ('Train LeNet on MNIST data using LBANN.')
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument(
+    '--partition', action='store', type=str,
+    help='scheduler partition', metavar='NAME')
+parser.add_argument(
+    '--account', action='store', type=str,
+    help='scheduler account', metavar='NAME')
+args = parser.parse_args()
+
+# ----------------------------------
+# Construct layer graph
+# ----------------------------------
+
+# Input data
+input = lbann.Input()
+images = lbann.Identity(input)
+labels = lbann.Identity(input)
+
+# LeNet
+x = lbann.Convolution(images,
+                      num_dims = 2,
+                      num_output_channels = 6,
+                      num_groups = 1,
+                      conv_dims_i = 5,
+                      conv_strides_i = 1,
+                      conv_dilations_i = 1,
+                      has_bias = True)
+x = lbann.Relu(x)
+x = lbann.Pooling(x,
+                  num_dims = 2,
+                  pool_dims_i = 2,
+                  pool_strides_i = 2,
+                  pool_mode = "max")
+x = lbann.Convolution(x,
+                      num_dims = 2,
+                      num_output_channels = 16,
+                      num_groups = 1,
+                      conv_dims_i = 5,
+                      conv_strides_i = 1,
+                      conv_dilations_i = 1,
+                      has_bias = True)
+x = lbann.Relu(x)
+x = lbann.Pooling(x,
+                  num_dims = 2,
+                  pool_dims_i = 2,
+                  pool_strides_i = 2,
+                  pool_mode = "max")
+x = lbann.FullyConnected(x, num_neurons = 120, has_bias = True)
+x = lbann.Relu(x)
+x = lbann.FullyConnected(x, num_neurons = 84, has_bias = True)
+x = lbann.Relu(x)
+x = lbann.FullyConnected(x, num_neurons = 10, has_bias = True)
+probs = lbann.Softmax(x)
+
+# Loss function and accuracy
+loss = lbann.CrossEntropy([probs, labels])
+acc = lbann.CategoricalAccuracy([probs, labels])
+
+# ----------------------------------
+# Setup experiment
+# ----------------------------------
+
+# Setup model
+mini_batch_size = 64
+num_epochs = 20
+model = lbann.Model(mini_batch_size,
+                    num_epochs,
+                    layers=lbann.traverse_layer_graph(input),
+                    objective_function=loss,
+                    metrics=[lbann.Metric(acc, name='accuracy', unit='%')],
+                    callbacks=[lbann.CallbackPrintModelDescription(),
+                               lbann.CallbackPrint(),
+                               lbann.CallbackTimer()])
+
+# Setup optimizer
+opt = lbann.SGD(learn_rate=0.01, momentum=0.9)
+
+# Load data reader from prototext
+data_reader = data.make_mnist_data_reader()
+
+# Setup trainer
+trainer = lbann.Trainer()
+
+# ----------------------------------
+# Run experiment
+# ----------------------------------
+# Note: Use `lbann.contrib.lc.launcher.run` instead for optimized
+# defaults on LC systems.
+
+kwargs = {}
+if args.partition: kwargs['partition'] = args.partition
+if args.account: kwargs['account'] = args.account
+lbann.run(trainer, model, data_reader, opt, **kwargs)
