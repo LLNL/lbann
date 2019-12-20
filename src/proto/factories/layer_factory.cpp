41cdb70a9b6cc3b56fe1166d09857c0377186446
Tim Moon <moon13@llnl.gov>
2019-12-20 15:53:58 -0800

    Fix segfault when slice layer gets slice points from reader

--- src/proto/factories/layer_factory.cpp
+++ src/proto/factories/layer_factory.cpp
@@ -469,8 +469,10 @@ std::unique_ptr<Layer> construct_layer_legacy(
       slice_point_method_name = "'get_slice_points_from_reader'";
       const auto dr_generic  = lbann::peek_map(data_readers, execution_mode::training);
       const std::string& var = params.get_slice_points_from_reader();
-      const auto& _slice_points = get_slice_points_from_reader(dr_generic, var, is_supported);
-      std::copy(_slice_points.begin(), _slice_points.end(), slice_points.begin());
+      for (const auto& slice_point
+             : get_slice_points_from_reader(dr_generic, var, is_supported)) {
+        slice_points.push_back(slice_point);
+      }
     } else {
       slice_point_method_name = "'slice_points'";
       slice_points = parse_list<size_t>(params.slice_points());
