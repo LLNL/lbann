ca396a4b0aff52250dce0ce79951b2fc54e93781
Tim Moon <moon13@llnl.gov>
2019-12-20 14:39:59 -0800

    Optimize GPU slice layer backprop

--- src/layers/transform/slice.cu
+++ src/layers/transform/slice.cu
@@ -170,7 +170,6 @@ void fp_compute_impl(
   // Tensor views have already been setup in fp_setup_outputs
 }
 
-#if 0 /// @todo Restore
 template <typename TensorDataType>
 void bp_compute_impl(
   slice_layer<TensorDataType,data_layout::MODEL_PARALLEL,El::Device::GPU>& l) {
@@ -180,7 +179,7 @@ void bp_compute_impl(
   auto& input_grad = l.get_error_signals();
   std::unique_ptr<El::AbstractDistMatrix<TensorDataType>> input_grad_v(
     input_grad.Construct(input_grad.Grid(), input_grad.Root()));
-  size_t offset = 0;
+  size_t offset = l.m_slice_points.front();
   for (size_t j=0; j<static_cast<size_t>(l.get_num_children()); ++j) {
     const auto& output_grad = l.get_prev_error_signals(j);
     El::View(*input_grad_v, input_grad,
@@ -190,7 +189,6 @@ void bp_compute_impl(
   }
 
 }
-#endif // 0
 
 template <typename TensorDataType>
 void fp_compute_impl(
@@ -336,29 +334,55 @@ void fp_compute_impl(
 
 }
 
-#if 0 /// @todo Restore
 template <typename TensorDataType>
 void bp_compute_impl(
-  slice_layer<TensorDataType,data_layout::DATA_PARALLEL,El::Device::GPU>& l,
-  size_t concat_dim) {
+  slice_layer<TensorDataType,data_layout::DATA_PARALLEL,El::Device::GPU>& l) {
 
   // Check that number of dimensions is valid
   /// @todo Support tensors with arbitrary number of dimensions
-  const size_t num_dims = l.get_output_dims().size();
+  const auto& input_dims = l.get_input_dims();
+  const size_t num_dims = input_dims.size();
   if (num_dims > 3) {
     LBANN_ERROR(l.get_type()," layer \"",l.get_name(),"\" ",
                 "is operating on ",num_dims,"-D tensors, ",
                 "but only 3-D tensors are currently supported");
   }
 
-  // Get dimensions and strides for each input tensor
-  const size_t num_inputs = l.get_num_parents();
-  std::vector<const TensorDataType*> input_buffer_list;
-  std::vector<dim4> input_dims_list, input_strides_list;
-  size_t max_input_size = 0;
-  for (size_t j=0; j<num_inputs; ++j) {
-    const auto& input = l.get_prev_activations(j);
-    const auto& input_dims = l.get_input_dims(j);
+  // Get dimensions and strides for each output gradient tensor
+  const size_t num_outputs = l.get_num_children();
+  std::vector<const TensorDataType*> output_grad_buffer_list;
+  std::vector<dim4> output_grad_dims_list, output_grad_strides_list;
+  size_t max_output_grad_size = 0;
+  for (size_t j=0; j<num_outputs; ++j) {
+    const auto& output_grad = l.get_prev_error_signals(j);
+    const auto& output_grad_dims = l.get_output_dims(j);
+
+    // Construct dimensions and strides in reverse order
+    // Note: Assume each mini-batch sample is fully packed.
+    std::vector<size_t> rdims(output_grad_dims.rbegin(), output_grad_dims.rend());
+    std::vector<size_t> rstrides(output_grad_dims.size(), 1);
+    for (size_t d=1; d<output_grad_dims.size(); ++d) {
+      rstrides[d] = rdims[d-1] * rstrides[d-1];
+    }
+    rdims.push_back(output_grad.LocalWidth());
+    rstrides.push_back(output_grad.LDim());
+
+    // Pad tensor dimensions to 4D
+    rdims.resize(4, 1);
+    rstrides.resize(4, rstrides.back());
+
+    output_grad_buffer_list.push_back(output_grad.LockedBuffer());
+    output_grad_dims_list.push_back({rdims[3], rdims[2], rdims[1], rdims[0]});
+    output_grad_strides_list.push_back(
+      {rstrides[3], rstrides[2], rstrides[1], rstrides[0]});
+    max_output_grad_size = std::max(max_output_grad_size,
+                                    rdims[3]*rdims[2]*rdims[1]*rdims[0]);
+  }
+
+  // Get strides for input gradient tensor
+  dim4 input_grad_strides;
+  auto& input_grad = l.get_error_signals();
+  {
 
     // Construct dimensions and strides in reverse order
     // Note: Assume each mini-batch sample is fully packed.
@@ -367,74 +391,43 @@ void bp_compute_impl(
     for (size_t d=1; d<input_dims.size(); ++d) {
       rstrides[d] = rdims[d-1] * rstrides[d-1];
     }
-    rdims.push_back(input.LocalWidth());
-    rstrides.push_back(input.LDim());
+    rdims.push_back(input_grad.LocalWidth());
+    rstrides.push_back(input_grad.LDim());
 
     // Pad tensor dimensions to 4D
     rdims.resize(4, 1);
     rstrides.resize(4, rstrides.back());
 
-    input_buffer_list.push_back(input.LockedBuffer());
-    input_dims_list.push_back({rdims[3], rdims[2], rdims[1], rdims[0]});
-    input_strides_list.push_back(
-      {rstrides[3], rstrides[2], rstrides[1], rstrides[0]});
-    max_input_size = std::max(max_input_size,
-                              rdims[3]*rdims[2]*rdims[1]*rdims[0]);
+    input_grad_strides = {rstrides[3], rstrides[2], rstrides[1], rstrides[0]};
   }
 
-  // Get strides for output tensor
-  dim4 output_strides;
-  auto& output = l.get_activations();
-  {
-    const auto& output_dims = l.get_output_dims();
-
-    // Construct dimensions and strides in reverse order
-    // Note: Assume each mini-batch sample is fully packed.
-    std::vector<size_t> rdims(output_dims.rbegin(), output_dims.rend());
-    std::vector<size_t> rstrides(output_dims.size(), 1);
-    for (size_t d=1; d<output_dims.size(); ++d) {
-      rstrides[d] = rdims[d-1] * rstrides[d-1];
-    }
-    rdims.push_back(output.LocalWidth());
-    rstrides.push_back(output.LDim());
-
-    // Pad tensor dimensions to 4D
-    rdims.resize(4, 1);
-    rstrides.resize(4, rstrides.back());
-
-    output_strides = {rstrides[3], rstrides[2], rstrides[1], rstrides[0]};
-  }
-
-  // Compute each input tensor's offset in output tensor
-  concat_dim += 4 - num_dims;   // Tensor has been padded to 4-D
-  std::vector<size_t> output_offset_list;
-  output_offset_list.push_back(0);
-  for (const auto& input_dims : input_dims_list) {
-    auto offset = output_offset_list.back();
-    offset += input_dims[concat_dim] * output_strides[concat_dim];
-    output_offset_list.push_back(offset);
+  // Compute offsets in input gradient tensor
+  const size_t slice_dim_stride = input_grad_strides[l.m_slice_dim+(4-num_dims)];
+  std::vector<size_t> input_grad_offset_list;
+  for (const auto& slice_point : l.m_slice_points) {
+    input_grad_offset_list.push_back(slice_point * slice_dim_stride);
   }
 
   // Pack tensor data into a CPU buffer
   l.m_workspace_event.synchronize();
   l.m_workspace.resize(
-    sizeof(TensorDataType*) * input_buffer_list.size()
-    + sizeof(dim4) * input_dims_list.size()
-    + sizeof(dim4) * input_strides_list.size()
-    + sizeof(size_t) * output_offset_list.size());
+    sizeof(TensorDataType*) * output_grad_buffer_list.size()
+    + sizeof(dim4) * output_grad_dims_list.size()
+    + sizeof(dim4) * output_grad_strides_list.size()
+    + sizeof(size_t) * input_grad_offset_list.size());
   size_t pos = 0;
-  std::memcpy(&l.m_workspace[pos], input_buffer_list.data(),
-              sizeof(TensorDataType*) * input_buffer_list.size());
-  pos += sizeof(TensorDataType*) * input_buffer_list.size();
-  std::memcpy(&l.m_workspace[pos], input_dims_list.data(),
-              sizeof(dim4) * input_dims_list.size());
-  pos += sizeof(dim4) * input_dims_list.size();
-  std::memcpy(&l.m_workspace[pos], input_strides_list.data(),
-              sizeof(dim4) * input_strides_list.size());
-  pos += sizeof(dim4) * input_strides_list.size();
-  std::memcpy(&l.m_workspace[pos], output_offset_list.data(),
-              sizeof(size_t) * output_offset_list.size());
-  pos += sizeof(size_t) * output_offset_list.size();
+  std::memcpy(&l.m_workspace[pos], output_grad_buffer_list.data(),
+              sizeof(TensorDataType*) * output_grad_buffer_list.size());
+  pos += sizeof(TensorDataType*) * output_grad_buffer_list.size();
+  std::memcpy(&l.m_workspace[pos], output_grad_dims_list.data(),
+              sizeof(dim4) * output_grad_dims_list.size());
+  pos += sizeof(dim4) * output_grad_dims_list.size();
+  std::memcpy(&l.m_workspace[pos], output_grad_strides_list.data(),
+              sizeof(dim4) * output_grad_strides_list.size());
+  pos += sizeof(dim4) * output_grad_strides_list.size();
+  std::memcpy(&l.m_workspace[pos], input_grad_offset_list.data(),
+              sizeof(size_t) * input_grad_offset_list.size());
+  pos += sizeof(size_t) * input_grad_offset_list.size();
 
   // Copy tensor data to GPU
   auto&& stream = El::GPUManager::Stream();
@@ -447,38 +440,37 @@ void bp_compute_impl(
                   stream);
   l.m_workspace_event.record(stream);
   pos = 0;
-  auto&& device_input_buffer_list
+  auto&& device_output_grad_buffer_list
     = reinterpret_cast<const TensorDataType**>(device_workspace_ptr+pos);
-  pos += sizeof(TensorDataType*) * input_buffer_list.size();
-  auto&& device_input_dims_list
+  pos += sizeof(TensorDataType*) * output_grad_buffer_list.size();
+  auto&& device_output_grad_dims_list
     = reinterpret_cast<const dim4*>(device_workspace_ptr+pos);
-  pos += sizeof(dim4) * input_dims_list.size();
-  auto&& device_input_strides_list
+  pos += sizeof(dim4) * output_grad_dims_list.size();
+  auto&& device_output_grad_strides_list
     = reinterpret_cast<const dim4*>(device_workspace_ptr+pos);
-  pos += sizeof(dim4) * input_strides_list.size();
-  auto&& device_output_offset_list
+  pos += sizeof(dim4) * output_grad_strides_list.size();
+  auto&& device_input_grad_offset_list
     = reinterpret_cast<const size_t*>(device_workspace_ptr+pos);
-  pos += sizeof(size_t) * output_offset_list.size();
+  pos += sizeof(size_t) * input_grad_offset_list.size();
 
   // Launch CUDA kernel
-  if (max_input_size > 0) {
+  if (max_output_grad_size > 0) {
     constexpr size_t block_size = 256;
     dim3 block_dims, grid_dims;
     block_dims.x = block_size;
-    grid_dims.x = (max_input_size + block_size - 1) / block_size;
-    grid_dims.y = num_inputs;
+    grid_dims.x = (max_output_grad_size + block_size - 1) / block_size;
+    grid_dims.y = num_outputs;
     concat4d_kernel<<<grid_dims, block_dims, 0, stream>>>(
-      num_inputs,
-      device_input_buffer_list,
-      device_input_dims_list,
-      device_input_strides_list,
-      output.Buffer(),
-      output_strides,
-      device_output_offset_list);
+      num_outputs,
+      device_output_grad_buffer_list,
+      device_output_grad_dims_list,
+      device_output_grad_strides_list,
+      input_grad.Buffer(),
+      input_grad_strides,
+      device_input_grad_offset_list);
   }
 
 }
-#endif // 0
 
 // Explicit instantiation
 #define PROTO(T)                                        \
