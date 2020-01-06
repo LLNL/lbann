8473d7e040b457b3190fba17df99db4e52ba16c4
Brian C. Van Essen <vanessen1@llnl.gov>
2020-01-06 14:29:43 -0800

    Update the random gaussian fill to use a float for generating the numbers when using cpu_fp16 data type.

--- src/utils/random.cpp
+++ src/utils/random.cpp
@@ -362,14 +362,16 @@ void uniform_fill(El::AbstractDistMatrix<TensorDataType>& mat, El::Int m, El::In
 template <typename TensorDataType>
 void gaussian_fill_procdet(El::AbstractDistMatrix<TensorDataType>& mat, El::Int m, El::Int n,
                            TensorDataType mean, TensorDataType stddev) {
-  CircMatDT<TensorDataType, El::Device::CPU> vals(m, n, mat.Grid(), 0);
+  using RandDataType = typename std::conditional<
+    std::is_same<TensorDataType,cpu_fp16>::value, float, TensorDataType>::type;
+  CircMatDT<RandDataType, El::Device::CPU> vals(m, n, mat.Grid(), 0);
   if (vals.Participating()) {
     auto& local_vals = vals.Matrix();
     auto& gen = get_generator();
-    std::normal_distribution<TensorDataType> dist(mean, stddev);
+    std::normal_distribution<RandDataType> dist(mean, stddev);
     for (El::Int col = 0; col < local_vals.Width(); ++col) {
       for (El::Int row = 0; row < local_vals.Height(); ++row) {
-        local_vals(row, col) = dist(gen);
+        local_vals(row, col) = El::To<TensorDataType>(dist(gen));
       }
     }
   }
