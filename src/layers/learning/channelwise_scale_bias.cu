5d34ad03043c2dd78ec822b6dd13c807857ed309
Thomas R. Benson <benson31@llnl.gov>
2020-01-08 09:39:04 -0800

    various fixes

--- src/layers/learning/channelwise_scale_bias.cu
+++ src/layers/learning/channelwise_scale_bias.cu
@@ -276,7 +276,7 @@ void channelwise_scale_bias_layer<TensorDataType, T_layout, Dev>::bp_compute() {
   template class channelwise_scale_bias_layer<        \
     T, data_layout::DATA_PARALLEL, El::Device::GPU>;
 
-#define LBANN_INSTANTIATE_CPU_HALF
+#define LBANN_INSTANTIATE_GPU_HALF
 #include "lbann/macros/instantiate.hpp"
 
 } // namespace lbann
--- src/layers/learning/entrywise_scale_bias.cu
+++ src/layers/learning/entrywise_scale_bias.cu
@@ -205,7 +205,7 @@ void entrywise_scale_bias_layer<TensorDataType, Layout, Device>::bp_compute() {
   template class entrywise_scale_bias_layer<         \
     T, data_layout::MODEL_PARALLEL, El::Device::GPU>
 
-#define LBANN_INSTANTIATE_CPU_HALF
+#define LBANN_INSTANTIATE_GPU_HALF
 #include "lbann/macros/instantiate.hpp"
 
 } // namespace lbann
