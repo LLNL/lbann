f68049787fa660482a0907c3c5c91096f9b8183b
David A. Hysom <hysom@llnl.gov>
2020-01-16 11:31:24 -0800

    Added a long comment, and the MIT license from the cnpy repo

--- include/lbann/utils/cnpy_llnl.hpp
+++ include/lbann/utils/cnpy_llnl.hpp
@@ -31,10 +31,51 @@
 
 nammespace cnpy {
 
+/**
+ * This file contains additional function for the cnpy package
+ * ( https://github.com/davidHysom/lbann/pull/new/cnpy_extended )
+ * The original code only loads a numpy array for a filename;
+ * we have need (e.g, our sample_list class) to open a file
+ * prior to loading, and to load from a filestream instead of
+ * a filename.
+ *
+ * load(std::istream), below, is largely a re-write of the 
+ * original cnpy::npz_t cnpy::npz_load(std::string fname) function.
+ *
+ * The MIT license from the cnpy package is included below.
+ */
+
 std::ifstream open_file(std::string fname);
 
 npz_t load(std::istream);
 
 } //namespace cnpy 
+
+
+
+/** License from the original cnpy repo: 
+ *
+ * The MIT License
+ *
+ * Copyright (c) Carl Rogers, 2011
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #endif  //LBANN_UTILS_CNPY_LLNL_HPP
