c3a93c44649b108469f62ca4fe421425da687509
Sam Ade Jacobs <jacobs32@llnl.gov>
2020-02-23 14:35:17 -0800

    LBANN implementation of MaCC surrogate model

---
+++ applications/physics/ICF/macc_models.py
@@ -0,0 +1,115 @@
+import lbann
+import lbann.modules.base
+
+
+#Synonymous to fc_gen0
+class MACCForward(lbann.modules.Module):
+
+    global_count = 0  # Static counter, used for default names
+
+    def __init__(self, out_dim,name=None):
+       self.instance = 0
+       self.name = (name if name
+                     else 'macc_forward{0}'.format(MACCForward.global_count))
+
+       fc = lbann.modules.FullyConnectedModule
+       
+       #generator #fc2_gen0
+       g_neurons = [32,256,1024]
+       self.gen_fc = [fc(g_neurons[i],activation=lbann.Relu, name=self.name+'gen_fc'+str(i))
+                      for i in range(len(g_neurons))]
+       self.predy = fc(out_dim,name=self.name+'pred_out')
+      
+    def forward(self,x):
+        return self.predy(self.gen_fc[2](self.gen_fc[1](self.gen_fc[0](x))))
+ 
+#Synonymous to fc_gen1
+class MACCInverse(lbann.modules.Module):
+
+    global_count = 0  # Static counter, used for default names
+
+    def __init__(self, out_dim,name=None):
+       self.instance = 0
+       self.name = (name if name
+                     else 'macc_inverse{0}'.format(MACCInverse.global_count))
+
+       fc = lbann.modules.FullyConnectedModule
+       
+       #generator #fc_gen1
+       g_neurons = [16,128,64]
+       self.gen_fc = [fc(g_neurons[i],activation=lbann.Relu, name=self.name+'gen_fc'+str(i))
+                      for i in range(len(g_neurons))]
+       self.predx = fc(out_dim,name=self.name+'pred_out')
+
+    def forward(self,y):
+        return self.predx(self.gen_fc[2](self.gen_fc[1](self.gen_fc[0](y))))
+
+
+class MACCWAE(lbann.modules.Module):
+
+    global_count = 0  # Static counter, used for default names
+
+    def __init__(self, encoder_out_dim, decoder_out_dim, name=None):
+       self.instance = 0
+       self.name = (name if name
+                     else 'macc_wae{0}'.format(MACCWAE.global_count))
+
+
+       fc = lbann.modules.FullyConnectedModule
+       conv = lbann.modules.Convolution2dModule
+
+       disc_neurons = [128,64,1]
+       encoder_neurons = [32,256,128]
+       decoder_neurons = [64,128,256]
+
+       
+       #Encoder
+       self.enc_fc0 = fc(encoder_neurons[0],activation=lbann.Elu,name=self.name+'_enc_fc0')
+       self.enc_fc1 = fc(encoder_neurons[1],activation=lbann.Tanh,name=self.name+'_enc_fc1')
+       self.enc_fc2 = fc(encoder_neurons[2],activation=lbann.Tanh,name=self.name+'_enc_fc2')
+       self.enc_out = fc(encoder_out_dim,name=self.name+'enc_out')
+     
+       #Decoder
+       self.dec_fc0 = fc(decoder_neurons[0],activation=lbann.Elu,name=self.name+'_dec_fc0')
+       self.dec_fc1 = fc(decoder_neurons[1],activation=lbann.Tanh,name=self.name+'_dec_fc1')
+       self.dec_fc2 = fc(decoder_neurons[2],activation=lbann.Tanh,name=self.name+'_dec_fc2')
+       self.dec_out = fc(decoder_out_dim,name=self.name+'pred_y')
+       
+       #Discriminator1
+       self.d0_fc0 = fc(disc_neurons[0],activation=lbann.Relu,name=self.name+'_disc0_fc0')
+       self.d0_fc1 = fc(disc_neurons[1],activation=lbann.Relu,name=self.name+'_disc0_fc1')
+       self.d0_fc2 = fc(disc_neurons[2],name=self.name+'_disc0_fc2')
+
+       #Discriminator2
+       #stacked_discriminator, this will be frozen, no optimizer, 
+       #layer has to be named for replace layer callback 
+       self.d1_fc0 = fc(disc_neurons[0],activation=lbann.Relu,name=self.name+'_disc1_fc0')
+       self.d1_fc1 = fc(disc_neurons[1],activation=lbann.Relu,name=self.name+'_disc1_fc1')
+       self.d1_fc2 = fc(disc_neurons[2],name=self.name+'_disc1_fc2')
+
+    def forward(self, z, y):
+         
+        z_sample = self.encoder(y)
+
+        y_recon = self.decoder(z_sample)
+
+        # d real/fake share weights, shared weights is copied to d_adv 
+        #(through replace weight callback) and freeze
+        d_real = self.discriminator0(lbann.Concatenation([y,z],axis=0))  
+        y_z_sample = lbann.Concatenation([y,z_sample],axis=0)
+        d_fake = self.discriminator0(lbann.StopGradient(y_z_sample)) 
+        d_adv = self.discriminator1(y_z_sample) #freeze
+
+        return d_real, d_fake, d_adv,y_recon
+
+    def encoder(self,y):
+        return self.enc_out(self.enc_fc2(self.enc_fc1(self.enc_fc0(y))))
+
+    def decoder(self,z):
+        return self.dec_out(self.dec_fc2(self.dec_fc1(self.dec_fc0(z))))
+
+    def discriminator0(self,input):
+        return self.d0_fc2(self.d0_fc1(self.d0_fc0(input)))
+        
+    def discriminator1(self,input):
+        return self.d1_fc2(self.d1_fc1(self.d1_fc0(input)))
