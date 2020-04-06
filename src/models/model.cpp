60df8dfb00a0511113ee5d92d1e08eba6c113e9b
Brian C. Van Essen <vanessen1@llnl.gov>
2020-04-06 14:59:16 -0700

    Removed legacy storage of checkpoint and restart logic to persist binary blobs in favor of Cereal serialization.

--- src/models/model.cpp
+++ src/models/model.cpp
@@ -1280,8 +1280,6 @@ bool model::save_to_checkpoint_shared(persist& p) {
   m_comm->trainer_barrier();
   // write out fields we need to save for model
   if (m_comm->am_trainer_master()) {
-    //    p.write_uint64(persist_type::model, "max_mini_batch_size",      (uint64_t) m_max_mini_batch_size);
-    //    p.write_uint32(persist_type::model, "persist_callback_type",      (uint32_t) p.get_cb_type());
     write_cereal_archive<model>(*this, p, "model.xml");
   }
 
@@ -1304,21 +1302,7 @@ bool model::save_to_checkpoint_shared(persist& p) {
 bool model::load_from_checkpoint_shared(persist& p) {
   std::string trainer_dir = p.get_checkpoint_dir();
   p.open_restart(p.get_checkpoint_dir() + '/' + get_name() + '/');
-  // have rank 0 read the file
-  // read state from file
-  struct lbann_model_header header;
   // Assume checkpoint reload from epoch end not step end
-  // if (m_comm->am_trainer_master()) {
-  //   //    p.read_uint64(persist_type::model, "max_mini_batch_size",      &header.max_mini_batch_size);
-  //   p.read_uint32(persist_type::model, "persist_callback_type",     &header.callback_type);
-  // }
-  // // TODO: this assumes homogeneous processors
-  // // broadcast state from rank 0
-  // m_comm->trainer_broadcast(0, header);
-  // // set our member params from values read from disk
-  // //  m_max_mini_batch_size = (size_t)           header.max_mini_batch_size;
-  // // set state of persist object to know which type of ckpt we are returning from.
-  // p.set_cb_type((callback_type) header.callback_type);
 
   load_from_shared_cereal_archive<model>(*this, p, *get_comm(), "model.xml");
 
@@ -1351,9 +1335,6 @@ bool model::save_to_checkpoint_distributed(persist& p){
   p.open_checkpoint_dir(p.get_checkpoint_dir() + '/' + get_name() + '/', true);
   // Make sure that the master has had a chance to create the directories
   m_comm->trainer_barrier();
-  // write out fields we need to save for model
-  //  p.write_uint64(persist_type::model, "max_mini_batch_size",  (uint64_t) m_max_mini_batch_size);
-  p.write_uint32(persist_type::train, "persist_callback_type",(uint32_t) p.get_cb_type());
 
   write_cereal_archive<model>(*this, p, "model.xml");
 
@@ -1378,13 +1359,6 @@ bool model::save_to_checkpoint_distributed(persist& p){
 bool model::load_from_checkpoint_distributed(persist& p){
   std::string trainer_dir = p.get_checkpoint_dir();
   p.open_restart(p.get_checkpoint_dir() + '/' + get_name() + '/');
-  struct lbann_model_header header;
-  //  p.read_uint64(persist_type::model, "max_mini_batch_size",      &header.max_mini_batch_size);
-  p.read_uint32(persist_type::train, "persist_callback_type",     &header.callback_type);
-
-  //  m_max_mini_batch_size = (size_t)           header.max_mini_batch_size;
-
-  p.set_cb_type((callback_type) header.callback_type);
 
   read_cereal_archive<model>(*this, p, "model.xml");
 
