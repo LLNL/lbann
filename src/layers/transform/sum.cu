ec6465ff38b43c48aceaf950b383f3f486af28f1
Naoya Maruyama <maruyama3@llnl.gov>
2020-04-08 15:50:27 -0700

    Do not include distconv headers when not used

--- src/layers/transform/sum.cu
+++ src/layers/transform/sum.cu
@@ -28,7 +28,9 @@
 #include "lbann/layers/transform/sum.hpp"
 #include "lbann/utils/exception.hpp"
 
+#ifdef LBANN_HAS_DISTCONV
 #include "distconv/tensor/algorithms_cuda.hpp"
+#endif // LBANN_HAS_DISTCONV
 
 namespace lbann {
 
