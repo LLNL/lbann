6487da1d8c2f5c3c85c7492ded7b79c7fd722616
Sam Ade Jacobs <jacobs32@llnl.gov>
2020-04-09 17:54:47 -0700

    Delete chembl_train.sh

--- applications/ATOM/chembl_train.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-python train_atom_char_rnn.py --job_name chembl_1_7m --embedding-dim 42 --num-embeddings 42 --data-path data/chembl/chembl_data_1_7m_train.npy --pad-index 40 --account baasic --partition pvis
-
