1a3ec31155e9ee729c349b3d1ae0acde7022c7fa
Naoya Maruyama <maruyama3@llnl.gov>
2020-04-15 11:47:26 -0700

    Tighter control of enabling distconv for concatenate

--- include/lbann/layers/transform/concatenate.hpp
+++ include/lbann/layers/transform/concatenate.hpp
@@ -109,7 +109,11 @@ private:
 #ifdef LBANN_HAS_DISTCONV
   friend class concatenate_distconv_adapter<TensorDataType, Layout, Device>;
  protected:
-  bool is_distconv_supported() const override { return true; }
+  bool is_distconv_supported() const override {
+    // Only supported for the channel dimension
+    return Device == El::Device::GPU && Layout == data_layout::DATA_PARALLEL
+        && m_concat_dim == 0;
+  }
   void setup_distconv_adapter() override {
     this->get_distconv_adapter_ptr() = make_unique<
       concatenate_distconv_adapter<TensorDataType, Layout, Device>>(*this);
