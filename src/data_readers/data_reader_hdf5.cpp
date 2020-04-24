cddb25d8274c1684a356df2a80e174e993a852e4
Naoya Maruyama <maruyama3@llnl.gov>
2020-04-23 21:49:30 -0700

    Clean up data_reader_hdf5
    Removed code for rank reordering.

--- src/data_readers/data_reader_hdf5.cpp
+++ src/data_readers/data_reader_hdf5.cpp
@@ -1,40 +1,41 @@
 ////////////////////////////////////////////////////////////////////////////////
-//// Copyright (c) 2014-2019, Lawrence Livermore National Security, LLC.
-//// Produced at the Lawrence Livermore National Laboratory.
-//// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
-//// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
-////
-//// LLNL-CODE-697807.
-//// All rights reserved.
-////
-//// This file is part of LBANN: Livermore Big Artificial Neural Network
-//// Toolkit. For details, see http://software.llnl.gov/LBANN or
-//// https://github.com/LLNL/LBANN.
-////
-//// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
-//// may not use this file except in compliance with the License.  You may
-//// obtain a copy of the License at:
-////
-//// http://www.apache.org/licenses/LICENSE-2.0
-////
-//// Unless required by applicable law or agreed to in writing, software
-//// distributed under the License is distributed on an "AS IS" BASIS,
-//// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-//// implied. See the License for the specific language governing
-//// permissions and limitations under the license.
-////
-///////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014-2019, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+//
+// LLNL-CODE-697807.
+// All rights reserved.
+//
+// This file is part of LBANN: Livermore Big Artificial Neural Network
+// Toolkit. For details, see http://software.llnl.gov/LBANN or
+// https://github.com/LLNL/LBANN.
+//
+// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the license.
+//
+/////////////////////////////////////////////////////////////////////////////////
 #include "lbann/data_readers/data_reader_hdf5.hpp"
 #include "lbann/utils/profiling.hpp"
+#include "lbann/utils/distconv.hpp"
+#include "conduit/conduit_relay.hpp"
+#include "conduit/conduit_relay_io_hdf5.hpp"
+
 #include <cstdio>
 #include <string>
 #include <fstream>
 #include <unordered_set>
 #include <iostream>
 #include <cstring>
-#include "lbann/utils/distconv.hpp"
-#include "conduit/conduit_relay.hpp"
-#include "conduit/conduit_relay_io_hdf5.hpp"
 
 namespace {
 inline hid_t check_hdf5(hid_t hid, const char *file, int line) {
@@ -45,11 +46,12 @@ inline hid_t check_hdf5(hid_t hid, const char *file, int line) {
   }
   return hid;
 }
-}
+} // namespace
 
 #define CHECK_HDF5(call) check_hdf5(call, __FILE__, __LINE__)
 
 namespace lbann {
+
 const std::string hdf5_reader::HDF5_KEY_DATA = "full";
 const std::string hdf5_reader::HDF5_KEY_RESPONSES = "unitPar";
 
@@ -71,7 +73,6 @@ hdf5_reader& hdf5_reader::operator=(const hdf5_reader& rhs) {
   copy_members(rhs);
   return (*this);
 }
-
 
 void hdf5_reader::copy_members(const hdf5_reader &rhs) {
   if(rhs.m_data_store != nullptr) {
@@ -95,9 +96,6 @@ void hdf5_reader::copy_members(const hdf5_reader &rhs) {
 
 void hdf5_reader::read_hdf5_hyperslab(hsize_t h_data, hsize_t filespace,
                                       int rank, short *sample) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   prof_region_begin("read_hdf5_hyperslab", prof_colors[0], false);
   // this is the splits, right now it is hard coded to split along the
   // z axis
@@ -118,19 +116,17 @@ void hdf5_reader::read_hdf5_hyperslab(hsize_t h_data, hsize_t filespace,
   // block -> the size of the block selected from the dataspace
   //hsize_t status;
 
-  CHECK_HDF5(H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count,
+  CHECK_HDF5(H5Sselect_hyperslab(filespace, H5S_SELECT_SET,
+                                 offset, NULL, count,
                                  m_hyperslab_dims.data()));
 
-  CHECK_HDF5(H5Dread(h_data, H5T_NATIVE_SHORT, memspace, filespace, m_dxpl, sample));
+  CHECK_HDF5(H5Dread(h_data, H5T_NATIVE_SHORT, memspace,
+                     filespace, m_dxpl, sample));
   prof_region_end("read_hdf5_hyperslab", false);
-#endif // LBANN_HAS_DISTCONV
 }
 
 void hdf5_reader::read_hdf5_sample(int data_id, short *sample) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
-  int world_rank = dc::get_input_rank(*get_comm());
+  int world_rank = get_comm()->get_rank_in_trainer();
   auto file = m_file_paths[data_id];
   hid_t h_file = CHECK_HDF5(H5Fopen(file.c_str(), H5F_ACC_RDONLY, m_fapl));
 
@@ -155,35 +151,30 @@ void hdf5_reader::read_hdf5_sample(int data_id, short *sample) {
   }
   CHECK_HDF5(H5Fclose(h_file));
   return;
-#endif // LBANN_HAS_DISTCONV
 }
 
 void hdf5_reader::load() {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   lbann_comm* l_comm = get_comm();
-  if(dc::get_rank_stride() != 1) {
-    LBANN_ERROR("HDF5 MPI-IO data reader requires DistConv rank stride = 1");
-  }
-  MPI_Comm mpi_comm = dc::get_input_comm(*l_comm);
-  int world_rank = dc::get_input_rank(*l_comm); // Should probably be trainer rank
-  int color = world_rank/dc::get_number_of_io_partitions();
+  MPI_Comm mpi_comm = l_comm->get_trainer_comm().GetMPIComm();
+  int world_rank = l_comm->get_rank_in_trainer();
+  int color = world_rank / dc::get_number_of_io_partitions();
   MPI_Comm_split(mpi_comm, color, world_rank, &m_comm);
   m_shuffled_indices.clear();
   m_shuffled_indices.resize(m_file_paths.size());
   std::iota(m_shuffled_indices.begin(), m_shuffled_indices.end(), 0);
   int nprocs;
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-  if ((nprocs%dc::get_number_of_io_partitions()) !=0) {
+  if ((nprocs % dc::get_number_of_io_partitions()) !=0) {
     LBANN_ERROR("nprocs should be divisible by num of io partitions otherwise this wont work");
   }
 
   // Read the dimension size of the first sample,
   // assuming that all of the samples have the same dimension size
   if (m_file_paths.size() > 0) {
-    const hid_t h_file = CHECK_HDF5(H5Fopen(m_file_paths[0].c_str(), H5F_ACC_RDONLY, H5P_DEFAULT));
-    const hid_t h_data = CHECK_HDF5(H5Dopen(h_file, HDF5_KEY_DATA.c_str(), H5P_DEFAULT));
+    const hid_t h_file = CHECK_HDF5(H5Fopen(m_file_paths[0].c_str(),
+                                            H5F_ACC_RDONLY, H5P_DEFAULT));
+    const hid_t h_data = CHECK_HDF5(H5Dopen(h_file, HDF5_KEY_DATA.c_str(),
+                                            H5P_DEFAULT));
     const hid_t h_space = CHECK_HDF5(H5Dget_space(h_data));
     if (CHECK_HDF5(H5Sget_simple_extent_ndims(h_space)) != 4) {
       LBANN_ERROR("The number of dimensions of HDF5 data samples should be 4");
@@ -210,7 +201,6 @@ void hdf5_reader::load() {
 
 #define DATA_READER_HDF5_USE_MPI_IO
 #ifdef DATA_READER_HDF5_USE_MPI_IO
-  dc::MPIRootPrintStreamDebug() << "data_reader_hdf5 is compiled with MPI-IO enabled";
   m_fapl = CHECK_HDF5(H5Pcreate(H5P_FILE_ACCESS));
   CHECK_HDF5(H5Pset_fapl_mpio(m_fapl, m_comm, MPI_INFO_NULL));
   m_dxpl = CHECK_HDF5(H5Pcreate(H5P_DATASET_XFER));
@@ -223,31 +213,13 @@ void hdf5_reader::load() {
   options *opts = options::get();
   if (opts->get_bool("preload_data_store")) {
     LBANN_ERROR("preload_data_store not supported on HDF5 data reader");
-#if 0
-    int np = l_comm->get_procs_per_trainer();
-    int base_files_per_rank = m_shuffled_indices.size() / np;
-    int extra = m_shuffled_indices.size() - (base_files_per_rank*np);
-    if (extra > np) {
-      LBANN_ERROR("extra > np");
-    }
-    local_list_sizes.resize(np, 0);
-    for (int j=0; j<np; j++) {
-      local_list_sizes[j] = base_files_per_rank;
-      if (j < extra) {
-        local_list_sizes[j] += 1;
-      }
-    }
-#endif
   }
   if (m_use_data_store) {
     instantiate_data_store();
   }
 
   select_subset_of_data();
-  if (dc::get_rank_stride() == 1) {
-    MPI_Comm_dup(dc::get_mpi_comm(), &m_response_gather_comm);
-  }
-#endif // LBANN_HAS_DISTCONV
+  MPI_Comm_dup(dc::get_mpi_comm(), &m_response_gather_comm);
 }
 
 bool hdf5_reader::fetch_label(Mat& Y, int data_id, int mb_idx) {
@@ -255,9 +227,6 @@ bool hdf5_reader::fetch_label(Mat& Y, int data_id, int mb_idx) {
 }
 
 bool hdf5_reader::fetch_datum(Mat& X, int data_id, int mb_idx) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   prof_region_begin("fetch_datum", prof_colors[0], false);
 
   // In the Cosmoflow case, each minibatch should have only one
@@ -274,13 +243,9 @@ bool hdf5_reader::fetch_datum(Mat& X, int data_id, int mb_idx) {
   }
   prof_region_end("fetch_datum", false);
   return true;
-#endif // LBANN_HAS_DISTCONV
 }
 
 void hdf5_reader::fetch_datum_conduit(Mat& X, int data_id) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   const std::string conduit_key = LBANN_DATA_ID_STR(data_id);
   // Create a node to hold all of the data
   conduit::Node node;
@@ -289,7 +254,7 @@ void hdf5_reader::fetch_datum_conduit(Mat& X, int data_id) {
     const conduit::Node& ds_node = m_data_store->get_conduit_node(data_id);
     node.set_external(ds_node);
     prof_region_end("get_conduit_node", false);
-  }else {
+  } else {
     auto &conduit_obj = node[conduit_key + "/slab"];
     conduit_obj.set(conduit::DataType::int16(
         m_num_features / dc::get_number_of_io_partitions()));
@@ -309,14 +274,10 @@ void hdf5_reader::fetch_datum_conduit(Mat& X, int data_id) {
   prof_region_begin("copy_to_buffer", prof_colors[0], false);
   std::memcpy(X.Buffer(), data, slab.dtype().number_of_elements()*slab.dtype().element_bytes());
   prof_region_end("copy_to_buffer", false);
-#endif // LBANN_HAS_DISTCONV
 }
 
 //get from a cached response
 bool hdf5_reader::fetch_response(Mat& Y, int data_id, int mb_idx) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   prof_region_begin("fetch_response", prof_colors[0], false);
   assert_eq(Y.Height(), m_num_response_features);
   float *buf = nullptr;
@@ -331,21 +292,15 @@ bool hdf5_reader::fetch_response(Mat& Y, int data_id, int mb_idx) {
   }
   std::memcpy(Y.Buffer(), buf,
               m_num_response_features*sizeof(DataType));
-  if (dc::get_rank_stride() == 1) {
-    gather_responses(Y.Buffer());
-  }
+  gather_responses(Y.Buffer());
   prof_region_end("fetch_response", false);
   return true;
-#endif // LBANN_HAS_DISTCONV
 }
 
 // Gather scattered responses to the first N ranks, where N is the
 // mini-batch size. This is not necessary when the rank reordering
 // is used.
 void hdf5_reader::gather_responses(float *responses) {
-#ifndef LBANN_HAS_DISTCONV
-  LBANN_ERROR("HDF5 data reader requires Distconv");
-#else
   float recv_buf[m_num_response_features];
   const int rank = dc::get_mpi_rank();
   const int num_part = dc::get_number_of_io_partitions();
@@ -358,7 +313,6 @@ void hdf5_reader::gather_responses(float *responses) {
 
   // send
   if (rank % num_part == 0) {
-    //dc::MPIPrintStreamInfo() << "Sending to " << dst_rank;
     MPI_Isend(responses, m_num_response_features, MPI_FLOAT, dst_rank,
               tag, m_response_gather_comm, &req[req_idx]);
     ++req_idx;
@@ -366,20 +320,16 @@ void hdf5_reader::gather_responses(float *responses) {
 
   // recv
   if (rank < mini_batch_size) {
-    //dc::MPIPrintStreamInfo() << "Receiving from " << src_rank;
     MPI_Irecv(recv_buf, m_num_response_features, MPI_FLOAT, src_rank, tag,
               m_response_gather_comm, &req[req_idx]);
     ++req_idx;
   }
 
   if (req_idx > 0) {
-    //dc::MPIPrintStreamInfo() << "Waiting for response gather completion";
     MPI_Waitall(req_idx, req, MPI_STATUS_IGNORE);
-    //dc::MPIRootPrintStreamInfo() << "Gather done";
   }
 
   std::memcpy(responses, recv_buf, sizeof(float) * m_num_response_features);
-#endif // LBANN_HAS_DISTCONV
 }
 
 } // namespace lbann
