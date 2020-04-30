452d564ed7d11cfa78f4966e972babcee24553ca
Brian C. Van Essen <vanessen1@llnl.gov>
2020-04-30 11:01:08 -0700

    Cleaned up and set back to pbatch

--- bamboo/allocate_and_run.sh
+++ bamboo/allocate_and_run.sh
@@ -36,14 +36,12 @@ ALLOCATION_TIME_LIMIT_NIGHTLY=45
 ALLOCATION_TIME_LIMIT_WEEKLY=90
 
 if [ "${CLUSTER}" = 'lassen' ]; then
-#    ALLOCATION_TIME_LIMIT=600
     if [ ${WEEKLY} -ne 0 ]; then
         timeout -k 5 24h bsub -G guests -Is -q pbatch -nnodes 4 -W ${ALLOCATION_TIME_LIMIT_WEEKLY} ./run.sh --weekly
     else
         timeout -k 5 24h bsub -G guests -Is -q pbatch -nnodes 2 -W ${ALLOCATION_TIME_LIMIT_NIGHTLY} ./run.sh
     fi
 elif [ "${CLUSTER}" = 'ray' ]; then
-#    ALLOCATION_TIME_LIMIT=240
     if [ ${WEEKLY} -ne 0 ]; then
         timeout -k 5 24h bsub -Is -q pbatch -nnodes 4 -W ${ALLOCATION_TIME_LIMIT_WEEKLY} ./run.sh --weekly
     else
@@ -65,14 +63,12 @@ elif [ "${CLUSTER}" = 'corona' ]; then
         fi
     fi
 elif [ "${CLUSTER}" = 'pascal' ]; then
-#    ALLOCATION_TIME_LIMIT=960
     if [ ${WEEKLY} -ne 0 ]; then
         timeout -k 5 24h salloc -N4 --partition=pbatch -t ${ALLOCATION_TIME_LIMIT_WEEKLY} ./run.sh --weekly
     else
-#        ALLOCATION_TIME_LIMIT=90 # Start with 1.5 hrs; may adjust for CPU clusters
         if [[ $(mjstat -c | awk 'match($1, "pbatch") && NF < 7 { print $5 }') -ne "0" ]];
         then
-            timeout -k 5 24h salloc -N2 --partition=pvis -t ${ALLOCATION_TIME_LIMIT_NIGHTLY} ./run.sh
+            timeout -k 5 24h salloc -N2 --partition=pbatch -t ${ALLOCATION_TIME_LIMIT_NIGHTLY} ./run.sh
         else
             echo "Partition \"pbatch\" on cluster \"${CLUSTER}\" appears to be down."
         fi
