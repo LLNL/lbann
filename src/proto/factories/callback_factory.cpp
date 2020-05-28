44da54fc059ff7da51d97b3358d7ffce15b41ad0
Sam Ade Jacobs <jacobs32@llnl.gov>
2020-05-27 19:46:27 -0700

    Set weight value callback (#1529)
    * add account and ppn to python front end
    
    * Add account to PFE
    
    * implement set weight value callback
    
    * Apply "set weights value" callback at given mini-batch step
    
    Previously, the callback was applied at a given epoch.
    
    * Add unit test for "set weights value" callback
    
    Co-authored-by: Tim Moon <moon13@llnl.gov>

--- src/proto/factories/callback_factory.cpp
+++ src/proto/factories/callback_factory.cpp
@@ -64,6 +64,7 @@
 #include "lbann/callbacks/timeline.hpp"
 #include "lbann/callbacks/timer.hpp"
 #include "lbann/callbacks/variable_minibatch.hpp"
+#include "lbann/callbacks/set_weights_value.hpp"
 
 #include "lbann/proto/factories.hpp"
 #include "lbann/proto/helpers.hpp"
@@ -184,6 +185,8 @@ void register_default_builders(factory_type& factory)
                            build_timeline_callback_from_pbuf);
   factory.register_builder("CallbackTimer",
                            build_timer_callback_from_pbuf);
+  factory.register_builder("CallbackSetWeightsValue",
+                           build_set_weights_value_callback_from_pbuf);
 }
 
 // Manage a global factory
