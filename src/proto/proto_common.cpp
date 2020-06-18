c3748e99a356335480ec73d2f0d24f68c8cdbc5f
Yosuke Oyama <17844184+oyamay@users.noreply.github.com>
2020-06-18 05:47:31 -0700

    Give a warning for the old CosmoFLow data reader

--- src/proto/proto_common.cpp
+++ src/proto/proto_common.cpp
@@ -179,25 +179,28 @@ void init_data_readers(
       reader_numpy_npz->set_scaling_factor_int16(readme.scaling_factor_int16());
       reader = reader_numpy_npz;
 #ifdef LBANN_HAS_DISTCONV
-    } else if (name=="cosmoflow_hdf5") {
+    } else if (name == "cosmoflow_hdf5" || name == "hdf5") {
+      if(name == "cosmoflow_hdf5") {
+        LBANN_WARNING("The \"cosmoflow_hdf5\" data reader is deprecated. Use \"hdf5\" instead.");
+      }
       const auto key_data = readme.hdf5_key_data();
       const auto key_labels = readme.hdf5_key_labels();
       const auto key_responses = readme.hdf5_key_responses();
       const auto hyperslab_labels = readme.hdf5_hyperslab_labels();
-      auto* reader_cosmo_hdf5 = new hdf5_reader<DataType>(shuffle, key_data,
+      auto* reader_hdf5 = new hdf5_reader<DataType>(shuffle, key_data,
                                                           key_labels,
                                                           key_responses,
                                                           hyperslab_labels);
-      reader_cosmo_hdf5->set_has_labels(!readme.disable_labels());
-      reader_cosmo_hdf5->set_has_responses(!readme.disable_responses());
-      reader_cosmo_hdf5->set_num_responses(!readme.num_responses());
+      reader_hdf5->set_has_labels(!readme.disable_labels());
+      reader_hdf5->set_has_responses(!readme.disable_responses());
+      reader_hdf5->set_num_responses(!readme.num_responses());
       auto filedir = readme.data_filedir();
       if(!endsWith(filedir, "/")) {
         filedir = filedir + "/";
       }
       const auto paths = glob(filedir +readme.data_file_pattern());
-      reader_cosmo_hdf5->set_hdf5_paths(paths);
-      reader = reader_cosmo_hdf5;
+      reader_hdf5->set_hdf5_paths(paths);
+      reader = reader_hdf5;
 #endif // LBANN_HAS_DISTCONV
     } else if (name == "pilot2_molecular_reader") {
       pilot2_molecular_reader* reader_pilot2_molecular = new pilot2_molecular_reader(readme.num_neighbors(), readme.max_neighborhood(), shuffle);
