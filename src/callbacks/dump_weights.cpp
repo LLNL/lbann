23f20f686d85878fefdedbc88fcc071a3c9cd04d
Brian C. Van Essen <vanessen1@llnl.gov>
2020-06-19 15:15:36 -0700

    Updated the dump weights callback to properly handly output from multiple models and trainers so that they don't clobber each other.

--- src/callbacks/dump_weights.cpp
+++ src/callbacks/dump_weights.cpp
@@ -29,6 +29,8 @@
 #include "lbann/callbacks/dump_weights.hpp"
 #include "lbann/utils/memory.hpp"
 #include "lbann/weights/data_type_weights.hpp"
+//#include "lbann/utils/file_utils.hpp"
+#include "lbann/utils/trainer_file_utils.hpp"
 
 #include <callbacks.pb.h>
 
@@ -38,30 +40,28 @@ namespace lbann {
 namespace callback {
 
 void dump_weights::on_train_begin(model *m) {
-  do_dump_weights(m, "initial");
+  do_dump_weights(*m, "initial");
 }
 
 void dump_weights::on_epoch_end(model *m) {
-  do_dump_weights(m);
+  do_dump_weights(*m);
 }
 
-void dump_weights::do_dump_weights(model *m, std::string s) {
-  const auto& c = static_cast<const sgd_execution_context&>(m->get_execution_context());
+void dump_weights::do_dump_weights(const model& m, std::string s) {
+  const auto& c = static_cast<const sgd_execution_context&>(m.get_execution_context());
 
   if(c.get_epoch() % m_epoch_interval != 0)  return;
 
-  makedir(m_basename.c_str());
-  for (weights *w : m->get_weights()) {
-    std::string epoch = "-epoch" + std::to_string(c.get_epoch()-1);
-    if(s != "") {
-      epoch = "-" + s;
-    }
-    const std::string file
-      = (m_basename
-         + "/" + m->get_name()
-         + epoch
-         + "-" + w->get_name()
-         + "-Weights");
+  // Create directory
+  const std::string root_file_path = get_multi_trainer_model_path(m, m_directory);
+  file::trainer_master_make_directory(root_file_path, m.get_comm());
+
+  //  makedir(m_directory.c_str());
+  for (weights *w : m.get_weights()) {
+    const std::string file = (root_file_path
+                              + c.get_state_string()
+                              + "-" + w->get_name()
+                              + "-Weights");
     const auto* dtw = dynamic_cast<const data_type_weights<DataType>*>(w);
     El::Write(dtw->get_values(), file, El::ASCII);
   }
