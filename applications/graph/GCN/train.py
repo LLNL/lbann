738e93e9f56453615226a6a76a8b658e5415258c
Shehtab Zaman <szaman5@binghamton.edu>
2020-07-07 17:57:58 -0700

    Added new graph kernels and new dataset - Added new graph kernel implementations: GCN, GIN, Graph - Created new method for handling slicing in data

--- applications/graph/GCN/train.py
+++ applications/graph/GCN/train.py
@@ -1,6 +1,52 @@
 import lbann
-from GCN_Layer import GCN_Layer
+from Graph_Kernels import GCN_Layer
 from lbann.util import str_list
+
+class lbann_Data_Mat:
+    def __init__(self, list_of_layers, layer_size):
+        self.size = (len(list_of_lbann_layers), layer_size)
+        self.layers = list_of_layers
+    
+    def __getitem__(self, row):
+        return self.layers[row]
+
+class lbann_Graph_Data:
+    def __init__(self, input_layer, num_vertices, num_features, num_classes):
+        self.num_vertices = num_vertices 
+        self.num_features = num_features 
+        self.num_classes = num_classes 
+
+        self.x, self.adj, self.y = gen_data()
+
+    def generate_slice_points (self):
+        slices_points = [for i in range(self.num_vertices * self.num_features + 1, self.num_features)]
+        adj_mat = slice_points[-1] + self.num_vertices * self.num_vertices 
+        slices_points.append(adj_mat)
+        targets = slice_points[-1] + self.num_classes 
+        slice_points.append(targets)
+        return str_list(slice_points)
+    
+    def gen_data(self, input_layer):
+        slice_points = generate_slice_points()
+        sliced_graph  = lbann.Slice(input_layer, axis = 0, slice_points = slice_points, name="Sliced_Input")
+
+        node_features = [] 
+
+        for i in range(self.num_vertices):
+            temp = lbann.Identity(sliced_graph)
+            node_features.append(lbann.Reshape(temp, dims=str(self.num_vertices)))
+
+        x = lbann_Data_Mat(node_features, self.num_features)
+        
+        adj_mat_in = lbann.Identity(sliced_graph) 
+        adj_mat = lbann.Reshape(adj_mat_in, dims = str_list([self.num_vertices, self.num_vertices])) 
+
+        y_ind = lbann.Identity(sliced_graph)
+        y = lbann.Reshape(sliced_graph, dims=str(self.num_classes))
+        return x, adj_mat, y 
+
+
+
 
 def make_model(num_vertices = None, 
                node_features = None, 
@@ -76,15 +122,25 @@ def make_model(num_vertices = None,
     
    # print("Warning: Not using GCN layer and forwaring orignal feature matrix to reduction step. Should only use this when testing dataset / data reader")
     
-    out_channel = 4
+    out_channel_1 = 1024
+    out_channel_2 = 256
+    out_channel_3 = 128
     
-    gcn = GCN_Layer(input_channels = node_features, output_channels = out_channel)
-
+    gcn = GCN_Layer(input_channels = node_features, output_channels = out_channel_1)
+    gcn2 = GCN_Layer(input_channels = out_channel_1, output_channels = out_channel_2)
+    gcn3 = GCN_Layer(input_channels = out_channel_2, output_channels = out_channel_3)
+    
+    out_channel = out_channel_3 
+    
     x = gcn(feature_matrix, adj_matrix )
-  
-    x = lbann.Relu(x,name="GCN_activation") 
+    x = lbann.Relu(x,name="GCN1_activation") 
 
+    x = gcn2(x, adj_matrix)
+    x = lbann.Relu(x, name="GCN2_activation")
     
+    x = gcn3 (x, adj_matrix)
+    x = lbann.Relu(x, name="GCN3_activation")
+
     #----------------------------------
     # Apply Reduction on Node Features
     #----------------------------------
@@ -93,6 +149,8 @@ def make_model(num_vertices = None,
     x = lbann.MatMul(average_vector,x, name="Node_Feature_Reduction") # X is now a vector with output_channel dimensions 
     
     x = lbann.Reshape(x, dims= str_list([out_channel]), name="Squeeze")
+    x = lbann.FullyConnected(x, num_neurons=64, name="hidden_layer_1")
+    x = lbann.Relu(x, name="hidden_layer_1_activation")
     x = lbann.FullyConnected(x, num_neurons=num_classes, name="Output_Fully_Connected")
     
     #----------------------------------
