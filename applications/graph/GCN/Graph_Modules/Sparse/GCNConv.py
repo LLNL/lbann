9138b018174082b9df19d8a2cd6609dacd39bacb
Shehtab Zaman <szaman5@binghamton.edu>
2020-07-15 10:31:33 -0700

    Refactoreed to mimic proper modules structure

---
+++ applications/graph/GCN/Graph_Modules/Sparse/GCNConv.py
@@ -0,0 +1,42 @@
+import lbann
+from lbann.modules import Module
+from lbann.util import str_list
+from Graph_Data import lbann_Data_Mat
+import lbann.modules.base
+import math 
+
+class GCNConv(Module):
+    global_count = 0
+
+    def __init__(self, input_channels, output_channels, name=None, activation = None):
+        super().__init__()
+        GCNConv.global_count +=1
+        self.name = (self.name if name else 'GCN_{}'.format(GCN_Layer.global_count))
+        
+        std_dev = math.sqrt(6/ (input_channels + output_channels))
+
+        self.weights = lbann.Weights(initializer = lbann.NormalInitializer(
+                                                   mean = 0,
+                                                   standard_deviation=std_dev),
+                                    name = self.name+'_Weights')
+
+        self.W = lbann.WeightsLayer(dims = str_list([input_channels, output_channels]),
+                                    name = self.name+'_layer',
+                                    weights = self.weights)
+        
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+    
+    def forward(self, X, A, activation = lbann.Relu):
+        # Assume X is a lbann data object
+        for i in range(X.shape[0]):
+            X[i] = lbann.MatMul(X[i], self.W, name=self.name+'_message_'+str(i))
+        out = X.get_mat()
+        out = lbann.MatMul(A, out, name=self.name+'_aggregate')
+
+        out = activation(out, name = self.name+'_activation')
+
+        out = lbann_Data_Mat.mat_to_data(out, X.shape[0], self.output_channels)
+        return out 
+ 
+
---
+++ applications/graph/GCN/Graph_Modules/Sparse/GINConv.py
@@ -0,0 +1,33 @@
+import lbann 
+from lbann.modules import Module 
+from Graph_Data import lbann_Data_Mat as Matrix
+
+class GINConv(Module):
+    global_count = 0; 
+
+    def __init__(self, sequential_nn, output_channels, eps = 1e-6,name = None):
+        GINConv.global_count += 1
+        self.name = (self.name if name else 'GIN_{}'.format(GINConv.global_count))
+
+        self.nn = sequential_nn
+        
+        self.eps = eps
+        
+        self.output_channels = output_channels
+
+
+    def forward(self, X, A):
+        in_channel = X.shape[1]
+        
+        eps = lbann.Constant(value=self.eps,num_neurons = str(in_channel))
+
+        for layer in self.nn:
+            for node_feature in range(X.shape[0]):
+                X[node_feature] = layer(X[node_feature])
+
+        out = X.get_mat() #Gather the rows 
+
+        out = lbann.MatMul(A, out)
+
+        return Matrix.mat_to_data(out, X.shape[0], self.output_channels) 
+
