901ff7a38d0502ad70f52f7b2dc0e4fcf78309b1
Shehtab Zaman <szaman5@binghamton.edu>
2020-07-31 11:44:26 -0700

    Added testing files for sparse graph layers

--- applications/graph/GNN/data/MNIST_Superpixel/__init__.py
+++ applications/graph/GNN/data/MNIST_Superpixel/__init__.py
@@ -14,7 +14,7 @@ def make_data_reader(): #TO DO: Extend this to use this for validation / test se
     _reader.role = 'train'
     _reader.shuffle = False #Turn off shuffle for debugging 
     _reader.percent_of_data_to_use = 1.0 
-    _reader.python.module = 'dataset'
+    _reader.python.module = 'MNIST_Superpixel_Dataset'
     _reader.python.module_dir = os.path.dirname(os.path.realpath(__file__))
     _reader.python.sample_function = 'get_train'
     _reader.python.num_samples_function = 'num_train_samples' 
