3fb1d574d367d14220758a326fb6edee75837f40
Sam Ade Jacobs <jacobs32@llnl.gov>
2020-08-10 23:06:17 -0700

    Add mcf to eval model

--- applications/physics/ICF/eval_macc_surrogate.py
+++ applications/physics/ICF/eval_macc_surrogate.py
@@ -50,8 +50,11 @@ parser.add_argument(
     '--xdim', action='store', default=5, type=int,
     help='input (x) dim (default: 5)', metavar='NUM')
 parser.add_argument(
-    '--mcf', action='store', default=1, type=int,
-    help='model capacity factor (default: 1)', metavar='NUM')
+    '--wae_mcf', action='store', default=1, type=int,
+    help='wae model capacity factor (default: 1)', metavar='NUM')
+parser.add_argument(
+    '--surrogate_mcf', action='store', default=1, type=int,
+    help='surrogate model capacity factor (default: 1)', metavar='NUM')
 parser.add_argument(
     '--lamda-cyc', action='store', default=1e-3, type=float,
     help='lamda-cyc (default: 1e-3)', metavar='NUM')
@@ -110,9 +113,9 @@ def construct_model():
 
 
     z = lbann.Gaussian(mean=0.0,stdev=1.0, neuron_dims="20")
-    wae = macc_models.MACCWAE(args.zdim,args.ydim,cf=args.mcf,use_CNN=args.useCNN) #pretrained, freeze
-    inv = macc_models.MACCInverse(args.xdim,cf=args.mcf)
-    fwd = macc_models.MACCForward(args.zdim,cf=args.mcf)
+    wae = macc_models.MACCWAE(args.zdim,args.ydim,cf=args.wae_mcf,use_CNN=args.useCNN) #pretrained, freeze
+    inv = macc_models.MACCInverse(args.xdim,cf=args.surrogate_mcf)
+    fwd = macc_models.MACCForward(args.zdim,cf=args.surrogate_mcf)
 
 
     y_pred_fwd = wae.encoder(gt_y)
