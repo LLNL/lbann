ceef9edd1a1af7246c5c29eb74e06bf4479325c0
Brian C. Van Essen <vanessen1@llnl.gov>
2020-08-27 13:57:59 -0700

    Updating the spack environments to be compatible with Spack v0.15.

--- spack_environments/llnl_lc/externals-linux-rhel7-haswell.sh
+++ spack_environments/llnl_lc/externals-linux-rhel7-haswell.sh
@@ -3,13 +3,14 @@
 EXTERNAL_ALL_PACKAGES=$(cat <<EOF
     all:
       providers:
-        mpi: [mvapich2@2.3 arch=linux-rhel7-haswell]
-        lapack: [openblas threads=openmp]
-        blas: [openblas threasd=openmp]
+        mpi:
+          - mvapich2@2.3 arch=linux-rhel7-haswell
+        lapack:
+          - openblas threads=openmp
+        blas:
+          - openblas threasd=openmp
       buildable: true
       version: []
-      paths: {}
-      modules: {}
 EOF
 )
 
@@ -17,59 +18,80 @@ EXTERNAL_PACKAGES=$(cat <<EOF
     cmake::
       buildable: True
       variants: ~openssl ~ncurses
-      version: [3.18.0]
-      modules:
-        cmake@3.18.0 arch=linux-rhel7-haswell:  cmake/3.18.0
+      version:
+      - 3.18.0
+      externals:
+      - spec: cmake@3.18.0 arch=linux-rhel7-haswell
+        modules:
+        -  cmake/3.18.0
 
     cuda::
       buildable: False
-      version: [10.2.89]
-      modules:
-        cuda@10.2.89 arch=linux-rhel7-haswell: cuda/10.2.89
+      version:
+      - 10.2.89
+      externals:
+      - spec: cuda@10.2.89 arch=linux-rhel7-haswell
+        modules:
+        - cuda/10.2.89
 
     cudnn::
       buildable: true
-      version: [7.6.5.32-10.1-linux-x64]
+      version:
+      - 7.6.5.32-10.1-linux-x64
 
     gcc::
-       buildable: False
-       version: [7.3.0]
-       modules:
-         gcc@7.3.0 arch=linux-rhel7-haswell: gcc/7.3.0
+      buildable: False
+      version:
+      - 7.3.0
+      externals:
+      - spec: gcc@7.3.0 arch=linux-rhel7-haswell
+        modules:
+        - gcc/7.3.0
 
     hwloc::
       buildable: False
-      version: [2.0.2]
-      paths:
-        hwloc@2.0.2 arch=linux-rhel7-haswell: /usr/lib64/libhwloc.so
+      version:
+      - 2.0.2
+      externals:
+      - spec: hwloc@2.0.2 arch=linux-rhel7-haswell
+        prefix: /usr/lib64/libhwloc.so
 
     mvapich2::
       buildable: True
-      version: [2.3]
-      paths:
-        mvapich2@2.3%gcc@7.3.0 arch=linux-rhel7-haswell: /usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.3.0/
+      version:
+      - 2.3
+      externals:
+      - spec: mvapich2@2.3%gcc@7.3.0 arch=linux-rhel7-haswell
+        prefix: /usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.3.0/
 
     openblas::
       buildable: True
       variants: threads=openmp
-      version: [0.3.10]
+      version:
+      - 0.3.10
 
     opencv::
       buildable: true
       variants: build_type=RelWithDebInfo ~calib3d+core~cuda~dnn~eigen+fast-math~features2d~flann~gtk+highgui+imgproc~ipp~ipp_iw~jasper~java+jpeg~lapack~ml~opencl~opencl_svm~openclamdblas~openclamdfft~openmp+png~powerpc~pthreads_pf~python~qt+shared~stitching~superres+tiff~ts~video~videoio~videostab~vsx~vtk+zlib
-      version: [4.1.0]
+      version:
+      - 4.1.0
 
     python::
       buildable: True
       variants: +shared ~readline ~zlib ~bz2 ~lzma ~pyexpat
-      version: [3.7.2]
-      modules:
-        python@3.7.2 arch=linux-rhel7-haswell: python/3.7.2
+      version:
+      - 3.7.2
+      externals:
+      - spec: python@3.7.2 arch=linux-rhel7-haswell
+        modules:
+        - python/3.7.2
 
     rdma-core::
       buildable: False
-      version: [20]
-      paths:
-        rdma-core@20 arch=linux-rhel7-haswell: /usr
+      version:
+      - 20
+      externals:
+      - spec: rdma-core@20 arch=linux-rhel7-haswell
+        prefix: /usr
 EOF
 )
--- spack_environments/llnl_lc/externals-linux-rhel7-power9le.sh
+++ spack_environments/llnl_lc/externals-linux-rhel7-power9le.sh
@@ -3,13 +3,14 @@
 EXTERNAL_ALL_PACKAGES=$(cat <<EOF
     all:
       providers:
-        mpi: [spectrum-mpi@rolling-release arch=linux-rhel7-power9le]
-        lapack: [openblas threads=openmp]
-        blas: [openblas threasd=openmp]
+        mpi:
+          - spectrum-mpi@rolling-release arch=linux-rhel7-power9le
+        lapack:
+          - openblas threads=openmp
+        blas:
+          - openblas threasd=openmp
       buildable: true
       version: []
-      paths: {}
-      modules: {}
 EOF
 )
 
@@ -18,58 +19,77 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       buildable: True
       variants: ~openssl ~ncurses
       version: [3.18.0]
-      modules:
-        cmake@3.18.0 arch=linux-rhel7-power9le: cmake/3.18.0
+      externals:
+      - spec: cmake@3.18.0 arch=linux-rhel7-power9le
+        modules:
+        - cmake/3.18.0
 
     cuda::
       buildable: False
       version: [10.1.168]
-      modules:
-        cuda@10.1.168 arch=linux-rhel7-power9le: cuda/10.1.168
+      externals:
+      - spec: cuda@10.1.168 arch=linux-rhel7-power9le
+        modules:
+        - cuda/10.1.168
 
     cudnn::
       buildable: true
-      version: [8.0.2.39-10.1-linux-ppc64le]
+      version:
+      - 8.0.2.39-10.1-linux-ppc64le
 
     gcc::
-       buildable: False
-       version: [7.3.1]
-       modules:
-         gcc@7.3.1 arch=linux-rhel7-power9le: gcc/7.3.1
+      buildable: False
+      version:
+      - 7.3.1
+      externals:
+      - spec: gcc@7.3.1 arch=linux-rhel7-power9le
+        modules:
+        - gcc/7.3.1
 
     hwloc::
       buildable: False
-      version: [2.0.2]
-      paths:
-        hwloc@2.0.2 arch=linux-rhel7-power9le: /usr/lib64/libhwloc.so
+      version:
+      - 2.0.2
+      externals:
+      - spec: hwloc@2.0.2 arch=linux-rhel7-power9le
+        prefix: /usr/lib64/libhwloc.so
 
     openblas::
       buildable: True
       variants: threads=openmp ~avx2 ~avx512
-      version: [0.3.10]
+      version:
+      - 0.3.10
 
     opencv::
       buildable: true
       variants: build_type=RelWithDebInfo ~calib3d+core~cuda~dnn~eigen+fast-math~features2d~flann~gtk+highgui+imgproc~ipp~ipp_iw~jasper~java+jpeg~lapack~ml~opencl~opencl_svm~openclamdblas~openclamdfft~openmp+png+powerpc~pthreads_pf~python~qt+shared~stitching~superres+tiff~ts~video~videoio~videostab+vsx~vtk+zlib
-      version: [4.1.0]
+      version:
+      - 4.1.0
 
     python::
       buildable: True
       variants: +shared ~readline ~zlib ~bz2 ~lzma ~pyexpat
-      version: [3.7.2]
-      modules:
-        python@3.7.2 arch=linux-rhel7-power9le: python/3.7.2
+      version:
+      - 3.7.2
+      externals:
+      - spec: python@3.7.2 arch=linux-rhel7-power9le
+        modules:
+        - python/3.7.2
 
     rdma-core::
       buildable: False
-      version: [20]
-      paths:
-        rdma-core@20 arch=linux-rhel7-power9le: /usr
+      version:
+      - 20
+      externals:
+      - spec: rdma-core@20 arch=linux-rhel7-power9le
+        prefix: /usr
 
     spectrum-mpi::
       buildable: False
-      version: [rolling-release]
-      paths:
-        spectrum-mpi@rolling-release %gcc@7.3.1 arch=linux-rhel7-power9le: /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-gcc-7.3.1
+      version:
+      - rolling-release
+      externals:
+      - spec: spectrum-mpi@rolling-release %gcc@7.3.1 arch=linux-rhel7-power9le
+        prefix: /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-gcc-7.3.1
 EOF
 )
