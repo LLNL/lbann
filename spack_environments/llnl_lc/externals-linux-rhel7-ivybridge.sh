4c6260bf9283ccc1fdb15726e08f02ce8f551381
Brian C. Van Essen <vanessen1@llnl.gov>
2020-08-31 23:04:50 -0700

    Removed problematic whitespace.

--- spack_environments/llnl_lc/externals-linux-rhel7-ivybridge.sh
+++ spack_environments/llnl_lc/externals-linux-rhel7-ivybridge.sh
@@ -24,7 +24,6 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       - spec: cmake@3.18.0 arch=linux-rhel7-ivybridge
         modules:
         -  cmake/3.18.0
-
     cuda::
       buildable: False
       version:
@@ -33,12 +32,10 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       - spec: cuda@10.2.89 arch=linux-rhel7-ivybridge
         modules:
         - cuda/10.2.89
-
     cudnn::
       buildable: true
       version:
       - 8.0.0.180-10.2-linux-x64
-
     gcc::
       buildable: False
       version:
@@ -47,7 +44,6 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       - spec: gcc@7.3.0 arch=linux-rhel7-ivybridge
         modules:
         - gcc/7.3.0
-
     hwloc::
       buildable: False
       version:
@@ -55,7 +51,6 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       externals:
       - spec: hwloc@2.0.2 arch=linux-rhel7-ivybridge
         prefix: /usr/lib64/libhwloc.so
-
     mvapich2::
       buildable: True
       version:
@@ -63,19 +58,16 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       externals:
       - spec: mvapich2@2.3%gcc@7.3.0 arch=linux-rhel7-ivybridge
         prefix: /usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.3.0/
-
     openblas::
       buildable: True
       variants: threads=openmp
       version:
       - 0.3.10
-
     opencv::
       buildable: true
       variants: build_type=RelWithDebInfo ~calib3d+core~cuda~dnn~eigen+fast-math~features2d~flann~gtk+highgui+imgproc~ipp~ipp_iw~jasper~java+jpeg~lapack~ml~opencl~opencl_svm~openclamdblas~openclamdfft~openmp+png~powerpc~pthreads_pf~python~qt+shared~stitching~superres+tiff~ts~video~videoio~videostab~vsx~vtk+zlib
       version:
       - 4.1.0
-
     python::
       buildable: True
       variants: +shared ~readline ~zlib ~bz2 ~lzma ~pyexpat
@@ -85,7 +77,6 @@ EXTERNAL_PACKAGES=$(cat <<EOF
       - spec: python@3.7.2 arch=linux-rhel7-ivybridge
         modules:
         - python/3.7.2
-
     rdma-core::
       buildable: False
       version:
