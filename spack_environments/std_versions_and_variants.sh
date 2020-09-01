4e78d24be648ced2c43967bf250e3c40e9f6d941
Brian C. Van Essen <vanessen1@llnl.gov>
2020-09-01 19:55:48 -0400

    Updated gcc versions for modules

--- spack_environments/std_versions_and_variants.sh
+++ spack_environments/std_versions_and_variants.sh
@@ -54,6 +54,7 @@ STD_MODULES=$(cat <<EOF
       core_compilers:
         - 'gcc@7.3.0'
         - 'gcc@7.3.1'
+        - 'gcc@7.4.0'
         - 'gcc@8.1.1'
       projections:
         all: '\${PACKAGE}/\${VERSION}-\${COMPILERNAME}-\${COMPILERVER}'
@@ -80,6 +81,7 @@ STD_MODULES=$(cat <<EOF
       core_compilers:
         - 'gcc@7.3.0'
         - 'gcc@7.3.1'
+        - 'gcc@7.4.0'
         - 'gcc@8.1.1'
       projections:
         all: '\${PACKAGE}/\${VERSION}-\${COMPILERNAME}-\${COMPILERVER}'
