dab07ff8abf90faae2bd354a312382851bd72021
Tim Moon <moon13@llnl.gov>
2020-10-05 12:16:40 -0700

    Update ATOM parse script for recon loss to print test loss

--- applications/ATOM/parse_recon.sh
+++ applications/ATOM/parse_recon.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
 fi
 log_file=$(realpath "$1")
 
-# Parse log file
+# Parse log file for training recon loss
 cat ${log_file} \
     | grep "^.*(instance [0-9]*) training epoch [0-9]* recon" \
     | sed "s/^.*(instance \([0-9]*\)) training epoch \([0-9]*\).*: \(.*\)/\1\t\2\t\3/" \
@@ -34,5 +34,37 @@ END {
   for (i=0;i<epochs;++i) {
     mean = sums[i]/trainers;
     sqmean = sqsums[i]/trainers;
-    print("Epoch ",i,", recon : mean=",mean,", stdev=",sqmean-mean*mean,", min=",mins[i],", max=",maxs[i]);  }
-}' \
+    print("Epoch ",i,", train recon : mean=",mean,", stdev=",sqmean-mean*mean,", min=",mins[i],", max=",maxs[i]);  }
+}'
+
+# Parse log file for test recon loss
+cat ${log_file} \
+    | grep "^.*(instance [0-9]*) test recon" \
+    | sed "s/^.*(instance \([0-9]*\)) test recon : \(.*\)/\1\t\2/" \
+    | awk '
+function max(x,y) {
+  if (x == "") { return y; }
+  if (y == "") { return x; }
+  return x > y ? x : y;
+}
+function min(x,y) {
+  if (x == "") { return y; }
+  if (y == "") { return x; }
+  return x < y ? x : y;
+}
+{
+  epoch = int(epochs[$1]);
+  epochs[$1]++;
+  sums[epoch] += $2;
+  sqsums[epoch] += $2*$2;
+  maxs[epoch] = max($2,maxs[epoch]);
+  mins[epoch] = min($2,mins[epoch]);
+  num_trainers = max(num_trainers,$1+1);
+  num_epochs = max(num_epochs,epoch+1);
+}
+END {
+  for (i=0;i<num_epochs-1;++i) {
+    mean = sums[i]/num_trainers;
+    sqmean = sqsums[i]/num_trainers;
+    print("Epoch ",i,", test recon : mean=",mean,", stdev=",sqmean-mean*mean,", min=",mins[i],", max=",maxs[i]);  }
+}'
