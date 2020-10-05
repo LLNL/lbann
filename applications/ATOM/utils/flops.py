5974bc1d8d4afb48673554d4a46d306e07c90773
Tim Moon <moon13@llnl.gov>
2020-10-04 22:15:06 -0700

    Update ATOM FLOP script WAE model

--- applications/ATOM/utils/flops.py
+++ applications/ATOM/utils/flops.py
@@ -4,9 +4,10 @@ def encoder_fp_flops(seq_len, embed_dim, hidden_dim, latent_dim):
 
     # RNN
     flops += 2 * (3*hidden_dim*embed_dim + 3*hidden_dim**2) * seq_len
+    flops += 2 * (3*hidden_dim) * seq_len
 
-    # 2 FCs (mu, logvar)
-    flops += 2 * 2 * (latent_dim*hidden_dim)
+    # FC
+    flops += 2 * (latent_dim*hidden_dim) + latent_dim
 
     return flops
 
@@ -15,27 +16,67 @@ def decoder_fp_flops(seq_len, vocab_size, embed_dim, latent_dim, hidden_dim):
     flops = 0
 
     # FC (init hidden)
-    flops += 2 * (hidden_dim*latent_dim)
+    flops += 2 * (hidden_dim*latent_dim) + hidden_dim
 
     # RNN 1
     flops += 2 * (3*hidden_dim*(embed_dim+latent_dim) + 3*hidden_dim**2) * seq_len
+    flops += 2 * (3*hidden_dim) * seq_len
 
-    # RNN 2-3
-    flops += 2 * 2 * (3*hidden_dim**2 + 3*hidden_dim**2) * seq_len
+    # RNN 2
+    flops += 2 * (3*hidden_dim**2 + 3*hidden_dim**2) * seq_len
+    flops += 2 * (3*hidden_dim) * seq_len
+
+    # RNN 3
+    flops += 2 * (3*hidden_dim**2 + 3*hidden_dim**2) * seq_len
+    flops += 2 * (3*hidden_dim) * seq_len
 
     # FC (preds)
     flops += 2 * (vocab_size*hidden_dim) * seq_len
+    flops += vocab_size * seq_len
+
+    return flops
+
+def discriminator_fp_flops(discriminator_dims, seq_len, embed_dim, latent_dim):
+    flops = 0
+
+    # FC 1
+    flops += 2 * discriminator_dims[0] * ((embed_dim+latent_dim)*seq_len)
+    flops += discriminator_dims[0]
+
+    # Remaining FCs
+    for i in range(1, len(discriminator_dims)):
+        flops += 2 * discriminator_dims[i] * discriminator_dims[i-1]
+        flops += discriminator_dims[i]
 
     return flops
 
 if __name__ == "__main__":
 
-    # Forward prop
-    flops = encoder_fp_flops(seq_len=100, embed_dim=40, hidden_dim=256, latent_dim=128)
-    flops += decoder_fp_flops(seq_len=100, vocab_size=40, embed_dim=40, latent_dim=128, hidden_dim=512)
+    # Encoder
+    flops = 3 * encoder_fp_flops(
+        seq_len=100,
+        embed_dim=40,
+        hidden_dim=256,
+        latent_dim=128,
+    )
 
-    # Backprop
-    flops *= 3
+    # Decoder
+    flops += 3 * decoder_fp_flops(
+        seq_len=100,
+        vocab_size=40,
+        embed_dim=40,
+        latent_dim=128,
+        hidden_dim=512,
+    )
+
+    # Discriminator
+    # Note: Called 3 times, one time without weight gradients
+    flops += 8 * discriminator_fp_flops(
+        discriminator_dims=[128, 64, 1],
+        seq_len=100,
+        embed_dim=40,
+        latent_dim=128,
+    )
 
     # Print PFLOPs
     print("PFLOPs/sample = ",flops/1e15)
