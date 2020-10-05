96d1e41894fb016f999e0cf92fefd97d93be48e2
Brian C. Van Essen <vanessen1@llnl.gov>
2020-10-04 18:34:46 -0700

    Updated the launch_merlin.sh script to take command line arguments in a more flexible manner.  Added support for scaling the learning rate manually.  Added a training yaml file for the WAE model.

--- applications/ATOM/launch_merlin.sh
+++ applications/ATOM/launch_merlin.sh
@@ -1,18 +1,73 @@
 #!/bin/bash
 # ./launch_merlin.sh  "num_nodes:4 8"  "mb_size:512 2048"  "ppt:16 32"  "ipt:1024"
+# ./launch_merlin.sh -y train_atom_wae.yaml -n "4 16" -b 4096 -p "4 16" -i 100
+
+echo "Running job: $0 $@"
+
+NUM_NODES="num_nodes:1"
+MB_SIZE="mb_size:1"
+PPT="ppt:1"
+IPT="ipt:1"
+YAML=train_atom.yaml
+LR_SCALING="lr_scaling:False"
+
+usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+
+[ $# -eq 0 ] && usage
+while getopts ":hn:b:i:p:y:l:" arg; do
+  case $arg in
+    y) # Yaml file
+      YAML=${OPTARG}
+      echo $YAML
+      ;;
+    n) # Number of nodes
+      NUM_NODES="num_nodes:${OPTARG}"
+      echo $NUM_NODES
+      ;;
+    b) # mini-batch size
+      MB_SIZE="mb_size:${OPTARG}"
+      echo $MB_SIZE
+      ;;
+    p) # processors per trainer
+      PPT="ppt:${OPTARG}"
+      echo $PPT
+      ;;
+    i) # iterations per tournament
+      IPT="ipt:${OPTARG}"
+      echo $IPT
+      ;;
+    l) # lr_scaling (True or False)
+      LR_SCALING="lr_scaling:${OPTARG}"
+      echo $LR_SCALING
+      ;;
+    h | *) # Display help.
+      usage
+      exit 0
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+#if [ -z "${n}" ] || [ -z "${b}" ]  || [ -z "${p}" ]  || [ -z "${i}" ]; then
+# echo "${n}"
+# echo "${b}"
+# echo "${p}"
+# echo "${i}"
+# if [ -z "${n}" ] || [ -z "${b}" ]  || [ -z "${p}" ]  || [ -z "${i}" ]; then
+#     echo "Missing required arguments"
+#     usage
+# fi
 
 # Turn off core files to work aroung flux exec issue.
 ulimit -c 0
-
-YAML=train_atom.yaml
 
 merlin stop-workers --spec ${YAML}
 merlin purge -f ${YAML}
 # Send all tasks to the broker
 # Use a custom generator to give good names to each test
-CMD="merlin run train_atom.yaml --pgen merlin_pgen_make_test_permutations.py --parg \"$1\" --parg \"$2\" --parg \"$3\" --parg \"$4\""
+CMD="merlin run ${YAML} --pgen merlin_pgen_make_test_permutations.py --parg \"$NUM_NODES\" --parg \"$MB_SIZE\" --parg \"$PPT\" --parg \"$IPT\" --parg \"$LR_SCALING\""
 echo $CMD
-merlin run train_atom.yaml --pgen merlin_pgen_make_test_permutations.py --parg "$1" --parg "$2" --parg "$3" --parg "$4"
+merlin run ${YAML} --pgen merlin_pgen_make_test_permutations.py --parg "$NUM_NODES" --parg "$MB_SIZE" --parg "$PPT" --parg "$IPT" --parg "$LR_SCALING"
 
 # Show the workers command
 merlin run-workers ${YAML} --echo
--- applications/ATOM/merlin_pgen_make_test_permutations.py
+++ applications/ATOM/merlin_pgen_make_test_permutations.py
@@ -12,18 +12,24 @@ def get_custom_generator(env, **kwargs):
     mb_size = kwargs.get('mb_size', '1').split(" ")
     procs_per_trainer = kwargs.get('ppt', '1').split(" ")
     iter_per_tournament = kwargs.get('ipt', '1')
+    lr_scaling = kwargs.get('lr_scaling', 'False').split(" ")
 
     num_nodes_values = []
     mb_sizes_values = []
     procs_per_trainer_values = []
     ltfbbi_values = []
+    lr_values = []
     trial_values = []
 
-    for trial, param_combo in enumerate(iter.product(num_nodes,mb_size,procs_per_trainer)):
+    for trial, param_combo in enumerate(iter.product(num_nodes,mb_size,procs_per_trainer,lr_scaling)):
         num_nodes_values.append(param_combo[0])
         mb_sizes_values.append(param_combo[1])
         procs_per_trainer_values.append(param_combo[2])
         ltfbbi_values.append(int(int(iter_per_tournament) / int(param_combo[1])))
+        if param_combo[3] == 'True':
+            lr_values.append(3e-4 * (int(param_combo[1])/512))
+        else:
+            lr_values.append(3e-4)
         trial_values.append(trial)
 
     params = {
@@ -46,6 +52,10 @@ def get_custom_generator(env, **kwargs):
         "4LTFBBI": {
             "values": ltfbbi_values,
             "label": "LTFBbi%%"
+        },
+        "5LR": {
+            "values": lr_values,
+            "label": "lr%%"
         },
     }
 
