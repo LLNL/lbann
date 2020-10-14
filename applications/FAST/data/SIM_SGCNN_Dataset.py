d21a8a190cf163fdc36ac2ccca0bb7fae0f86df0
Shehtab Zaman <szaman5@binghamton.edu>
2020-10-14 11:58:41 -0700

    Added full FAST model with run files and simulated data.

--- applications/FAST/data/SIM_SGCNN_Dataset.py
+++ applications/FAST/data/SIM_SGCNN_Dataset.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-class SimDataset(object):
+class Sim_GCNN_Dataset(object):
     """docstring for SimDataset"""
     def __init__(self,
                  num_samples,
                  num_nodes,
                  node_features,
                  edge_features):
-        super(SimDataset, self).__init__()
+        super(Sim_GCNN_Dataset, self).__init__()
         self.num_samples = num_samples
         self.num_nodes = num_nodes
         self.node_features = node_features
@@ -17,7 +17,7 @@ class SimDataset(object):
 
     def __generate_data(self):
         sample_size = self.sample_size()
-        return np.zeros((self.num_samples, sample_size + 1), dtype=np.float32)
+        return np.zeros((self.num_samples, sample_size), dtype=np.float32)
 
     def __len__(self):
         return self.num_samples
@@ -29,7 +29,7 @@ class SimDataset(object):
         number_edges = int((self.num_nodes*(self.num_nodes-1))/2)
         node_feat_mat = self.num_nodes * self.node_features
         edge_feat_mat = number_edges * self.edge_features
-        edge_adj = self.num_nodes **3
+        edge_adj = self.num_nodes ** 3
         covalent_mat = self.num_nodes ** 2
         non_covalent_mat = self.num_nodes ** 2
         ligand_only_mat = self.num_nodes ** 2
@@ -37,7 +37,7 @@ class SimDataset(object):
             + non_covalent_mat + ligand_only_mat + 1
 
 
-dataset = SimDataset(100, 50, 19, 1)
+dataset = Sim_GCNN_Dataset(100, 50, 19, 1)
 
 
 def get_train(index):
---
+++ applications/FAST/run_sgcnn.py
@@ -0,0 +1,102 @@
+import argparse
+import lbann
+import lbann.contrib.launcher
+import lbann.contrib.args
+from SGCNN.model import SGCNN
+from data_util import slice_graph_data
+import os
+import os.path as osp
+
+desc = ("Training SGCNN on a Protein-ligand graphs using LBANN")
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument("--mini-batch-size",
+                    action='store', default=32, type=int,
+                    help="Mini batch size {default: 32}", metavar='NUM')
+parser.add_argument("--num-epochs",
+                    action='store', default=1, type=int,
+                    help="Number of epochs {default: 1}", metavar='NUM')
+parser.add_argument("--job-name",
+                    action='store', default="SGCNN", type=str,
+                    help="Job name {default: SGCNN}", metavar='NAME')
+lbann.contrib.args.add_scheduler_arguments(parser)
+args = parser.parse_args()
+kwargs = lbann.contrib.args.get_scheduler_kwargs(args)
+data_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def make_data_reader(module_name='SIM_SGCNN_Dataset',
+                     module_dir=None,
+                     sample_function='get_train',
+                     num_samples_function='num_train_samples',
+                     sample_dims='sample_dims'):
+    data_dir = osp.join(osp.dirname(os.path.realpath(__file__)), 'data')
+
+    module_dir = (module_dir if module_dir else data_dir)
+    reader = lbann.reader_pb2.DataReader()
+    _reader = reader.reader.add()
+    _reader.name = 'python'
+    _reader.role = 'train'
+    _reader.shuffle = False  # Turn off shuffle for debugging
+    _reader.percent_of_data_to_use = 1.0
+    _reader.python.module = module_name
+    _reader.python.module_dir = module_dir
+    _reader.python.sample_function = sample_function
+    _reader.python.num_samples_function = num_samples_function
+    _reader.python.sample_dims_function = sample_dims
+
+    return reader
+
+
+def make_model(num_epochs):
+    sgcnn_model = SGCNN()
+
+    input_ = lbann.Input(target_mode='N/A')
+    X, cov_adj, noncov_adj, edge_ft, edge_adj, ligand_only, target = \
+        slice_graph_data(input_)
+
+    predicted = sgcnn_model(X,
+                            cov_adj,
+                            noncov_adj,
+                            edge_ft,
+                            edge_adj,
+                            ligand_only)
+
+    loss = lbann.MeanAbsoluteError([predicted, target], name='MAE_loss')
+    layers = lbann.traverse_layer_graph(input_)
+    metrics = [lbann.Metric(loss, name='MAE')]
+    # Prints initial Model after Setup
+    print_model = lbann.CallbackPrintModelDescription()
+    # Prints training progress
+    training_output = lbann.CallbackPrint(interval=1,
+                                          print_global_stat_only=False)
+    gpu_usage = lbann.CallbackGPUMemoryUsage()
+    timer = lbann.CallbackTimer()
+    callbacks = [print_model, training_output, gpu_usage, timer]
+    model = lbann.Model(num_epochs,
+                        layers=layers,
+                        objective_function=loss,
+                        metrics=metrics,
+                        callbacks=callbacks
+                        )
+    return model
+
+
+def main():
+    optimizer = lbann.Adam(learn_rate=1e-3,
+                           beta1=0.9,
+                           beta2=0.99,
+                           eps=1e-8)
+    mini_batch_size = args.mini_batch_size
+    num_epochs = args.num_epochs
+    job_name = args.job_name
+    data_reader = make_data_reader()
+    model = make_model(num_epochs)
+    trainer = lbann.Trainer(mini_batch_size=mini_batch_size)
+
+    lbann.contrib.launcher.run(trainer, model, data_reader, optimizer,
+                               job_name=job_name,
+                               **kwargs)
+
+
+if __name__ == '__main__':
+    main()
