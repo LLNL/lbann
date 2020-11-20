LLNL/lbann

commit 24d66c34957fdb43a06d044616ef415f85e0eac1

Remove learning_layer, regularizer_layer, and transform_layer (#1704)

include/lbann/layers/regularizers/local_response_normalization.hpp
@@ -28,7 +28,6 @@
 #define LBANN_LAYER_LOCAL_RESPONSE_NORMALIZATION_HPP_INCLUDED
 
 #include <vector>
-#include "lbann/layers/regularizers/regularizer.hpp"
 #include "lbann/utils/cudnn.hpp"
 #include "lbann/utils/exception.hpp"
 #include "lbann/utils/dnn_lib/cudnn/local_response_normalization.hpp"
@@ -47,7 +46,7 @@ namespace lbann {
 template <typename TensorDataType,
           data_layout T_layout = data_layout::DATA_PARALLEL,
           El::Device Dev = El::Device::CPU>
-class local_response_normalization_layer : public regularizer_layer<TensorDataType> {
+class local_response_normalization_layer : public data_type_layer<TensorDataType> {
 #ifdef LBANN_HAS_CUDNN
   using ScalingType = cudnn::ScalingParamType<TensorDataType>;
 #else
@@ -63,7 +62,7 @@ public:
                                      TensorDataType alpha,
                                      TensorDataType beta,
                                      TensorDataType k)
-    : regularizer_layer<TensorDataType>(comm),
+    : data_type_layer<TensorDataType>(comm),
       m_window_width(window_width), m_alpha(alpha), m_beta(beta), m_k(k)
 #ifdef LBANN_HAS_CUDNN
     , m_tensors_cudnn_desc(this)
@@ -72,7 +71,7 @@ public:
 
   local_response_normalization_layer(
     const local_response_normalization_layer& other)
-    : regularizer_layer<TensorDataType>(other),
+    : data_type_layer<TensorDataType>(other),
       m_window_width(other.m_window_width),
       m_alpha(other.m_alpha),
       m_beta(other.m_beta),
@@ -89,7 +88,7 @@ public:
 
   local_response_normalization_layer& operator=(
     const local_response_normalization_layer& other) {
-    regularizer_layer<TensorDataType>::operator=(other);
+    data_type_layer<TensorDataType>::operator=(other);
     m_window_width = other.m_window_width;
     m_alpha = other.m_alpha;
     m_beta = other.m_beta;
@@ -112,7 +111,7 @@ public:
   El::Device get_device_allocation() const override { return Dev; }
 
   description get_description() const override {
-    auto desc = regularizer_layer<TensorDataType>::get_description();
+    auto desc = data_type_layer<TensorDataType>::get_description();
     desc.add("alpha", m_alpha);
     desc.add("beta", m_beta);
     desc.add("k", m_k);
@@ -122,13 +121,13 @@ public:
 protected:
 
   void setup_dims(DataReaderMetaData& dr_metadata) override {
-    regularizer_layer<TensorDataType>::setup_dims(dr_metadata);
+    data_type_layer<TensorDataType>::setup_dims(dr_metadata);
     this->set_output_dims(this->get_input_dims());
   }
 
   /// Initialize GPU objects
   void setup_gpu() override {
-    regularizer_layer<TensorDataType>::setup_gpu();
+    data_type_layer<TensorDataType>::setup_gpu();
 #ifndef LBANN_HAS_CUDNN
     LBANN_ERROR("cuDNN not detected");
 #else

include/lbann/layers/learning/CMakeLists.txt
@@ -10,7 +10,6 @@ set_full_path(THIS_DIR_HEADERS
   fully_connected.hpp
   fully_connected_cuda.hpp
   gru.hpp
-  learning.hpp
   )
 
 # Propagate the files up the tree

include/lbann/layers/learning/fully_connected.hpp
@@ -27,7 +27,7 @@
 #ifndef LBANN_LAYERS_LEARNING_FULLY_CONNECTED_HPP_INCLUDED
 #define LBANN_LAYERS_LEARNING_FULLY_CONNECTED_HPP_INCLUDED
 
-#include "lbann/layers/learning/learning.hpp"
+#include "lbann/layers/data_type_layer.hpp"
 #include "lbann/models/model.hpp"
 
 #include <string>
@@ -48,7 +48,7 @@ namespace lbann {
  *  initialized to zero.
  */
 template <typename TensorDataType, data_layout T_layout, El::Device Dev>
-class fully_connected_layer : public learning_layer<TensorDataType> {
+class fully_connected_layer : public data_type_layer<TensorDataType> {
 public:
   /** @name Public Types */
   ///@{

include/lbann/layers/learning/learning.hpp
@@ -1,46 +1,0 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2014-2019, Lawrence Livermore National Security, LLC.
-// Produced at the Lawrence Livermore National Laboratory.
-// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
-// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
-//
-// LLNL-CODE-697807.
-// All rights reserved.
-//
-// This file is part of LBANN: Livermore Big Artificial Neural Network
-// Toolkit. For details, see http://software.llnl.gov/LBANN or
-// https://github.com/LLNL/LBANN.
-//
-// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
-// may not use this file except in compliance with the License.  You may
-// obtain a copy of the License at:
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-// implied. See the License for the specific language governing
-// permissions and limitations under the license.
-////////////////////////////////////////////////////////////////////////////////
-
-#ifndef LBANN_LAYER_LEARNING_HPP_INCLUDED
-#define LBANN_LAYER_LEARNING_HPP_INCLUDED
-
-#include "lbann/layers/data_type_layer.hpp"
-
-namespace lbann {
-
-/** @todo Remove. Layers should inherit directly from the base layer
- *  class.
- */
-
-template <typename TensorDataType>
-class learning_layer : public data_type_layer<TensorDataType> {
- public:
-  learning_layer(lbann_comm *comm) : data_type_layer<TensorDataType>(comm) {}
-};
-
-} // namespace lbann
-
-#endif // LBANN_LAYER_LEARNING_HPP_INCLUDED

include/lbann/layers/regularizers/CMakeLists.txt
@@ -5,7 +5,6 @@ set_full_path(THIS_DIR_HEADERS
   entrywise_batch_normalization.hpp
   layer_norm.hpp
   local_response_normalization.hpp
-  regularizer.hpp
   selu_dropout.hpp
   )
 

include/lbann/layers/regularizers/batch_normalization.hpp
@@ -27,7 +27,7 @@
 #ifndef LBANN_LAYER_REGULARIZER_BATCH_NORMALIZATION_HPP_INCLUDED
 #define LBANN_LAYER_REGULARIZER_BATCH_NORMALIZATION_HPP_INCLUDED
 
-#include "lbann/layers/regularizers/regularizer.hpp"
+#include "lbann/layers/data_type_layer.hpp"
 #include "lbann/models/model.hpp"
 #include "lbann/utils/distconv.hpp"
 
@@ -86,7 +86,7 @@ class batch_normalization_distconv_adapter: public data_type_distconv_adapter<Te
  *  pp. 448-456. 2015.
  */
 template <typename TensorDataType, data_layout T_layout, El::Device Dev>
-class batch_normalization_layer : public regularizer_layer<TensorDataType> {
+class batch_normalization_layer : public data_type_layer<TensorDataType> {
   static_assert(T_layout == data_layout::DATA_PARALLEL,
                 "batch normalization only supports DATA_PARALLEL");
 public:
@@ -159,7 +159,7 @@ public:
                             TensorDataType decay=0.9,
                             TensorDataType epsilon=1e-5,
                             int statistics_group_size=1)
-    : regularizer_layer<TensorDataType>(comm),
+    : data_type_layer<TensorDataType>(comm),
       m_decay(decay),
       m_epsilon(epsilon),
       m_statistics_group_size(statistics_group_size) {
@@ -170,7 +170,7 @@ public:
   }
 
   batch_normalization_layer(const batch_normalization_layer& other)
-    : regularizer_layer<TensorDataType>(other),
+    : data_type_layer<TensorDataType>(other),
       m_decay(other.m_decay),
       m_epsilon(other.m_epsilon),
       m_statistics_group_size(other.m_statistics_group_size),
@@ -191,7 +191,7 @@ public:
                       other.m_bias_gradient->Copy() : nullptr) {}
 
   batch_normalization_layer& operator=(const batch_normalization_layer& other) {
-    regularizer_layer<TensorDataType>::operator=(other);
+    data_type_layer<TensorDataType>::operator=(other);
     m_decay = other.m_decay;
     m_epsilon = other.m_epsilon;
     m_statistics_group_size = other.m_statistics_group_size;
@@ -224,7 +224,7 @@ public:
   El::Device get_device_allocation() const override { return Dev; }
 
   description get_description() const override {
-    auto desc = regularizer_layer<TensorDataType>::get_description();
+    auto desc = data_type_layer<TensorDataType>::get_description();
     desc.add("Decay", m_decay);
     desc.add("Epsilon", m_epsilon);
     desc.add("Statistics group size", m_statistics_group_size);
@@ -234,7 +234,7 @@ public:
 protected:
 
   void setup_matrices(const El::Grid& grid) override {
-    regularizer_layer<TensorDataType>::setup_matrices(grid);
+    data_type_layer<TensorDataType>::setup_matrices(grid);
     m_mean_and_var.reset(new StarMatDT<TensorDataType, Dev>(grid));
     m_mean_v.reset(new StarMatDT<TensorDataType, Dev>(grid));
     m_var_v.reset(new StarMatDT<TensorDataType, Dev>(grid));
@@ -246,12 +246,12 @@ protected:
   }
 
   void setup_dims(DataReaderMetaData& dr_metadata) override {
-    regularizer_layer<TensorDataType>::setup_dims(dr_metadata);
+    data_type_layer<TensorDataType>::setup_dims(dr_metadata);
     this->set_output_dims(this->get_input_dims());
   }
 
   void setup_data(size_t max_mini_batch_size) override {
-    regularizer_layer<TensorDataType>::setup_data(max_mini_batch_size);
+    data_type_layer<TensorDataType>::setup_data(max_mini_batch_size);
     const auto& output_dims = this->get_output_dims();
     const auto& num_channels = output_dims[0];
 

include/lbann/layers/regularizers/dropout.hpp
@@ -27,7 +27,6 @@
 #ifndef LBANN_LAYER_REGULARIZER_DROPOUT_HPP_INCLUDED
 #define LBANN_LAYER_REGULARIZER_DROPOUT_HPP_INCLUDED
 
-#include "lbann/layers/regularizers/regularizer.hpp"
 #include "lbann/models/model.hpp"
 #include "lbann/utils/cudnn.hpp"
 #include "lbann/utils/random_number_generators.hpp"
@@ -47,7 +46,7 @@ namespace lbann {
  *  Learning Research 15, no. 1 (2014): 1929-1958.
  */
 template <typename TensorDataType, data_layout T_layout, El::Device Dev>
-class dropout : public regularizer_layer<TensorDataType> {
+class dropout : public data_type_layer<TensorDataType> {
 public:
   /** @name Public Types */
   ///@{
@@ -61,7 +60,7 @@ public:
   /** Keep units with probabiliy keep_prob. */
   dropout(lbann_comm *comm,
           EvalType keep_prob = EvalType(0.5))
-    : regularizer_layer<TensorDataType>(comm),
+    : data_type_layer<TensorDataType>(comm),
       m_keep_prob(keep_prob)
 #ifdef LBANN_HAS_CUDNN
     , m_tensors_cudnn_desc(this)
@@ -77,7 +76,7 @@ public:
   }
 
   dropout(const dropout& other)
-    : regularizer_layer<TensorDataType>(other),
+    : data_type_layer<TensorDataType>(other),
       m_keep_prob(other.m_keep_prob),
       m_mask(other.m_mask ? other.m_mask->Copy() : nullptr)
 #ifdef LBANN_HAS_CUDNN
@@ -96,7 +95,7 @@ public:
   }
 
   dropout& operator=(const dropout& other) {
-    regularizer_layer<TensorDataType>::operator=(other);
+    data_type_layer<TensorDataType>::operator=(other);
     m_keep_prob = other.m_keep_prob;
     m_mask = other.m_mask ? std::unique_ptr<AbsDistMatrixType>(other.m_mask->Copy()) : nullptr;
 #ifdef LBANN_HAS_CUDNN
@@ -119,7 +118,7 @@ public:
   El::Device get_device_allocation() const override { return Dev; }
 
   description get_description() const override {
-    auto desc = regularizer_layer<TensorDataType>::get_description();
+    auto desc = data_type_layer<TensorDataType>::get_description();
     desc.add("Keep probability", m_keep_prob);
     return desc;
   }
@@ -135,17 +134,17 @@ public:
 protected:
 
   void setup_dims(DataReaderMetaData& dr_metadata) override {
-    regularizer_layer<TensorDataType>::setup_dims(dr_metadata);
+    data_type_layer<TensorDataType>::setup_dims(dr_metadata);
     this->set_output_dims(this->get_input_dims());
   }
 
   void setup_matrices(const El::Grid& grid) override {
-    regularizer_layer<TensorDataType>::setup_matrices(grid);
+    data_type_layer<TensorDataType>::setup_matrices(grid);
     m_mask = std::unique_ptr<AbsDistMatrixType>(this->get_activations().Copy());
   }
 
   void setup_gpu() override {
-    regularizer_layer<TensorDataType>::setup_gpu();
+    data_type_layer<TensorDataType>::setup_gpu();
 #ifndef LBANN_HAS_CUDNN
     LBANN_ERROR("cuDNN not detected");
 #else

include/lbann/layers/regularizers/regularizer.hpp
@@ -1,44 +1,0 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2014-2019, Lawrence Livermore National Security, LLC.
-// Produced at the Lawrence Livermore National Laboratory.
-// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
-// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
-//
-// LLNL-CODE-697807.
-// All rights reserved.
-//
-// This file is part of LBANN: Livermore Big Artificial Neural Network
-// Toolkit. For details, see http://software.llnl.gov/LBANN or
-// https://github.com/LLNL/LBANN.
-//
-// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
-// may not use this file except in compliance with the License.  You may
-// obtain a copy of the License at:
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-// implied. See the License for the specific language governing
-// permissions and limitations under the license.
-////////////////////////////////////////////////////////////////////////////////
-
-#ifndef LBANN_LAYER_REGULARIZER_HPP_INCLUDED
-#define LBANN_LAYER_REGULARIZER_HPP_INCLUDED
-#include "lbann/layers/data_type_layer.hpp"
-
-namespace lbann {
-
-/** @todo Remove. Layers should inherit directly from the base layer
- *  class.
- */
-template <typename TensorDataType>
-class regularizer_layer : public data_type_layer<TensorDataType> {
- public:
-  regularizer_layer(lbann_comm *comm) : data_type_layer<TensorDataType>(comm) {}
-};
-
-} // namespace lbann
-
-#endif // LBANN_LAYER_REGULARIZER_HPP_INCLUDED

commit eb922f91a9456141e286b0f14bdde3119bcc67df

Fixed bug introduced in #1704

include/lbann/layers/regularizers/local_response_normalization.hpp
@@ -28,6 +28,7 @@
 #define LBANN_LAYER_LOCAL_RESPONSE_NORMALIZATION_HPP_INCLUDED
 
 #include <vector>
+#include "lbann/layers/data_type_layer.hpp"
 #if defined LBANN_HAS_DNN_LIB
 #include "lbann/utils/dnn_lib/helpers.hpp"
 #include "lbann/utils/dnn_lib/local_response_normalization.hpp"