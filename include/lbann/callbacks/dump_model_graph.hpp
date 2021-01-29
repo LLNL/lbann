c26c9154e625d33820fb4fc1bca728fd23ef29f3
Brian Van Essen <vanessen1@llnl.gov>
2021-01-28 23:14:47 -0800

    Update dump_model_graph.hpp

--- include/lbann/callbacks/dump_model_graph.hpp
+++ include/lbann/callbacks/dump_model_graph.hpp
@@ -22,8 +22,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 // implied. See the License for the specific language governing
 // permissions and limitations under the license.
-//
-// check_nan .hpp .cpp - Check matrices for invalid numbers
 ////////////////////////////////////////////////////////////////////////////////
 
 #ifndef LBANN_CALLBACKS_CALLBACK_DUMP_MODEL_GRAPH_HPP_INCLUDED
