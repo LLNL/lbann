f75f739e4d765f3f69a3d1d6b1409dd14f23e9e3
Michael Wyatt <wyatt5@llnl.gov>
2021-01-29 17:11:51 -0800

    driver now loads trainer and model from checkpoint files

--- core-driver/main.cpp
+++ core-driver/main.cpp
@@ -35,10 +35,46 @@ int main(int argc, char *argv[]) {
     std::cout << "MPI_THREAD_MULTIPLE not supported" << std::endl;
   }
 
+  // Comms
   std::cout << "initializing LBANN..." << std::endl;
   auto lbann_comm = lbann::driver_init(MPI_COMM_WORLD);
+
+  int ppt = lbann_comm->get_procs_in_world();
+  if (ppt != lbann_comm->get_procs_per_trainer()) {
+    lbann_comm->split_trainers(ppt);
+  }
+
+
+  // Data Coordinator
+  std::unique_ptr<lbann::data_coordinator> dc;
+  dc = lbann::make_unique<lbann::buffered_data_coordinator<float>>(lbann_comm.get());
+
+  // Trainer
+  int mbs = 64;
+  auto t = lbann::make_unique<lbann::trainer>(lbann_comm.get(), mbs, std::move(dc));
+
+  // Checkpoint location
+  auto& p = t->get_persist_obj();
+  std::string cp_loc;
+  cp_loc = "/usr/workspace/wyatt5/cp_models/trainer0/sgd.shared.epoch_begin.epoch.10.step.8440/";
+  p.open_restart(cp_loc.c_str());
+
+  // Load trainer from checkpoint
+  auto t_flag = t->load_from_checkpoint_shared(p);
+  std::cout << "trainer load: " << t_flag << std::endl;
+
+  // Model
+  auto m = lbann::make_unique<lbann::directed_acyclic_graph_model>(lbann_comm.get(), nullptr, nullptr);
+
+  // Load model from checkpoint
+  auto m_flag = m->load_from_checkpoint_shared(p);
+  std::cout << "model load: " << m_flag << std::endl;
+
+  // Close restart
+  p.close_restart();
+
+  // Close comms
   lbann::finalize();
-  std::cout << "LBANN initialized." << std::endl;
 
   MPI_Finalize();
   return 0;
