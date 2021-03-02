930e7783116b1ef1a7cd17cbe282352309eb7973
Michael Wyatt <wyatt5@llnl.gov>
2021-03-02 11:59:17 -0800

    trimming down on content of batch_inf_alg

--- include/lbann/execution_algorithms/batch_inference_algorithm.hpp
+++ include/lbann/execution_algorithms/batch_inference_algorithm.hpp
@@ -50,61 +50,24 @@ public:
   /** Copy training_algorithm. */
   //  virtual batch_inference_algorithm* copy() const = default;
 
-  std::string get_name() const override { return "sgd"; }
+  std::string get_name() const override { return "batch_inference"; }
 
   // ===========================================
   // Execution
   // ===========================================
 
-  /** Apply the training algorithm to the model with the provided
-      context and execution mode */
-  void apply(execution_context& c,
+  /** Infer on samples with a given model. */
+  void infer(inf_execution_context& c,
              model& model,
              data_coordinator& dc,
-             execution_mode mode,
-             termination_criteria const& term_criteria) override;
-
-  /** Train a model using an iterative SGD solver. */
-  void train(sgd_execution_context& c,
-             model& model,
-             data_coordinator& dc,
-             size_t num_epochs, size_t num_batches=0);
-
-  /** Evaluate a model using the forward pass of an SGD solver. */
-  void evaluate(sgd_execution_context& c,
-                model& model,
-                data_coordinator& dc,
-                execution_mode mode, size_t num_batches=0);
+             size_t num_batches=0);
 
 protected:
-  /** Train model on one step / mini-batch of an SGD forward pass */
-  virtual bool train_mini_batch(sgd_execution_context& c, model& model, data_coordinator& dc);
-
   /** Evaluate model on one step / mini-batch of an SGD forward pass */
-  virtual bool evaluate_mini_batch(sgd_execution_context& c, model& model, data_coordinator& dc, execution_mode mode);
+  virtual bool infer_mini_batch(inf_execution_context& c, model& model, data_coordinator& dc);
 
-  ////////////////////////////////////////////////////////////
-  // Callbacks
-  ////////////////////////////////////////////////////////////
-
-  /** Execute callbacks at start of training. */
-  virtual void do_train_begin_cbs(model& model);
-  /** Execute callbacks at end of training. */
-  virtual void do_train_end_cbs(model& model);
-  /** Execute callbacks at start of evaluation. */
-  virtual void do_evaluate_begin_cbs(model& model, execution_mode mode);
-  /** Execute callbacks at end of evaluation. */
-  virtual void do_evaluate_end_cbs(model& model, execution_mode mode);
-  /** Execute callbacks at start of epoch. */
-  virtual void do_epoch_begin_cbs(model& model);
-  /** Execute callbacks at end of epoch. */
-  virtual void do_epoch_end_cbs(model& model);
-  /** Execute callbacks at start of mini-batch. */
-  virtual void do_batch_begin_cbs(model& model, execution_mode mode);
-  /** Execute callbacks at end of mini-batch. */
-  virtual void do_batch_end_cbs(model& model, execution_mode mode);
 };
 
 }  // namespace lbann
 
-#endif  // LBANN_SGD_TRAINING_ALGORITHM_HPP
+#endif  // LBANN_BATCH_INFERENCE_ALGORITHM_HPP
