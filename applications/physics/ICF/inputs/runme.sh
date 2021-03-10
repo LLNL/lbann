6691ecb53bad57d5b9a73e0e9427e23f2614470d
David A. Hysom <hysom@llnl.gov>
2021-03-10 12:51:29 -0800

    removing 1) json files, since the reader how uses yaml files; 2) files that shouldn't have been checked in; 3) everything else, until I come up with a more comprehensive plan as to where things should go

--- applications/physics/ICF/inputs/runme.sh
+++
@@ -1,7 +0,0 @@
-export AL_PROGRESS_RANKS_PER_NUMA_NODE=2
-export OMP_NUM_THREADS=8
-export MV2_USE_RDMA_CM=0
-my_hash=spack-build-6xey4bu
-my_build=/usr/workspace/wsb/hysom/hdf5_reader/$my_hash
-
-srun --nodes=1 --ntasks=1 --ntasks-per-node=2   --mpibind=off --nvidia_compute_mode=default --cpu_bind=mask_cpu:0x1ff0000001ff,0x3fe0000003fe00 $my_build/model_zoo/lbann --prototext=experiment_train_jag_wae.prototext
