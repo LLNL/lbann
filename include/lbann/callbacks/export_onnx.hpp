0a36b13a1d00271c22e408f6e3f87cb3e85664e5
Katie Graham <graham63@llnl.gov>
2021-03-13 16:08:39 -0800

    export_onnx.hpp/.cpp

---
+++ include/lbann/callbacks/export_onnx.hpp
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014-2019, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+// Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+// the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+//
+// LLNL-CODE-697807.
+// All rights reserved.
+//
+// This file is part of LBANN: Livermore Big Artificial Neural Network
+// Toolkit. For details, see http://software.llnl.gov/LBANN or
+// https://github.com/LLNL/LBANN.
+//
+// Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the license.
+//
+// export_onnx .hpp .cpp - Exports trained model to onnx format
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef LBANN_CALLBACKS_EXPORT_ONNX_HPP_INCLUDED
+#define LBANN_CALLBACKS_EXPORT_ONNX_HPP_INCLUDED
+
+#include "lbann/callbacks/callback.hpp"
+#include <onnx/onnx_pb.h>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace onnx;
+
+namespace lbann {
+namespace callback {
+
+/** @class export_onnx
+ *  @brief Callback to export a trained model to onnx format
+ */
+class export_onnx : public callback_base {
+
+public:
+  /** @brief export_onnx Constructor. */
+  export_onnx();
+
+
+
+private:
+
+
+
+}; // class export_onnx
+
+std::unique_ptr<callback_base>
+build_export_onnx_callback_from_pbuf(
+  const google::protobuf::message&,
+  const std::shared_ptr<lbann_summary>&);
+
+} // namespace callback
+} // namespace lbann
+
+#endif  // LBANN_CALLBACKS_EXPORT_ONNX_HPP_INCLUDED
