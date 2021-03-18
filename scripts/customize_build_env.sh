a89b037a01d5f944e1807bf04c73ef9e1c2c18fb
Michael Wyatt <wyatt5@llnl.gov>
2021-03-18 15:11:26 -0700

    force spack install to use system openmpi instead of install its own

--- scripts/customize_build_env.sh
+++ scripts/customize_build_env.sh
@@ -93,7 +93,7 @@ set_center_specific_modules()
                 ;;
             "zen" | "zen2") # Corona
                 # Don't load OpenMPI get the HIP Clang to build it
-                MODULE_CMD="module --force unload StdEnv; module load clang/11.0.0 python/3.7.2 opt rocm/4.0.0"
+                MODULE_CMD="module --force unload StdEnv; module load clang/11.0.0 python/3.7.2 opt rocm/4.0.0 openmpi-gnu/4.0"
                 ;;
             *)
                 echo "No pre-specified modules found for this system. Make sure to setup your own"
@@ -282,6 +282,13 @@ cat <<EOF  >> ${yaml}
       externals:
       - spec: rdma-core@20 arch=${spack_arch}
         prefix: /usr
+    openmpi:
+      buildable: False
+      version:
+      - 4.0
+      externals:
+      - spec: openmpi@4.0.0 arch=${spack_arch}
+        prefix: /opt/openmpi/4.0/gnu
 EOF
                 ;;
             *)
