4e53aefc94ab8bd7a578ddf3c3639b90fcfb3842
Brian C. Van Essen <vanessen1@llnl.gov>
2021-03-30 13:43:09 -0700

    Updated the build for different platforms to enable the proper variants.

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -6,14 +6,25 @@ import os, re, subprocess
 
 
 def test_compiler_build_script(cluster, dirname):
-    if cluster not in ['catalyst', 'corona', 'lassen', 'pascal', 'ray']:
+    # if cluster not in ['catalyst', 'corona', 'lassen', 'pascal', 'ray']:
+    #     e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
+    #     print('Skip - ' + e)
+    #     pytest.skip(e)
+    output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
+    error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
+    if cluster in ['lassen', 'pascal', 'ray']:
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+            dirname, output_file_name, error_file_name)
+    elif cluster in ['corona']:
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +rocm +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+            dirname, output_file_name, error_file_name)
+    elif cluster in ['catalyst']:
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+            dirname, output_file_name, error_file_name)
+    else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
-    output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
-    error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
-    # --test should be included but currently fails
-    command = '%s/scripts/build_lbann.sh -d -l bamboo -- +cuda +deterministic +fft +vision +numpy > %s 2> %s' % (
-        dirname, output_file_name, error_file_name)
+        
     return_code = os.system(command)
     tools.assert_success(return_code, error_file_name)
