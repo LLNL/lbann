37d10b5f53e3def814efda6537daaf074d09ad93
Katie Graham <graham63@llnl.gov>
2021-04-26 16:29:42 -0700

    latest version of onnx callback

--- include/lbann/layers/layer.hpp
+++ include/lbann/layers/layer.hpp
@@ -45,6 +45,7 @@
 #endif // LBANN_HAS_DISTCONV
 #include <string>
 #include <vector>
+#include <onnx/onnx_pb.h>
 
 /** @brief A utility macro for easily defining default-constructed sub-class
  *  builders.*/
@@ -469,6 +470,9 @@ public:
 
   /** @brief Write layer to proto file */
   virtual void write_proto(lbann_data::Layer* proto) const;
+
+  /** @brief Add layer specific data to onnx node */
+  virtual void fill_onnx_node(onnx::NodeProto& node) const;
 
   const Layer& get_parent_layer(size_t index=0) const;
   const Layer& get_child_layer(size_t index=0) const;
--- src/callbacks/export_onnx.cpp
+++ src/callbacks/export_onnx.cpp
@@ -45,7 +45,89 @@ namespace callback {
 export_onnx::export_onnx(std::shared_ptr<lbann_summary> const& summarizer)
   : m_summarizer(summarizer)
 {
-  std::cout << "I do stuff!" << std::endl;
+  std::cout << "I do stuff! different?" << std::endl;
+}
+
+void export_onnx::on_setup_end(model* m)
+{
+  std::cout << "TEST" << std::endl;
+
+  mp_.set_ir_version(7);
+  //FIXME: what goes here? ONNX operators?
+  // The empty string ("") domain indicates the operators defined
+  // as part of the ONNX specification; other domains correspond
+  // to operator sets of other vendors (e.g., they can be used to
+  // provide vendor-specific extensions to ONNX)
+  auto* opset = mp_.add_opset_import();
+  opset->set_domain("");
+  opset->set_version(1);
+
+  mp_.set_producer_name("LBANN");
+  mp_.set_producer_version(LBANN_MAKE_STR(LBANN_VERSION));
+  mp_.set_domain("lbann/LLNL/com.github");
+  // FIXME: model version is version of graph encoded.
+  //        Should this be passed in?
+  mp_.set_model_version(1);
+  mp_.set_doc_string("Livermore Big Artificial Neural Network");
+  // FIXME: what should go here??
+  auto* metadata = mp_.add_metadata_props();
+  metadata->set_key("name of thing");
+  metadata->set_value("thing");
+}
+
+void export_onnx::on_train_begin(model* m)
+{
+  // graph info
+  auto* gp = mp_.mutable_graph();
+  auto const layers = m->get_layers();
+  // node info
+  for (auto const* layer : layers)
+  {
+    auto* np = gp->add_node();
+    layer->fill_onnx_node(*np);
+    std::cout << "name: " << np->name() << std::endl;
+  }
+  gp->set_name(m->get_name());
+
+  // FIXME: Dims and data type for graph?
+  auto* initializer = gp->add_initializer();
+  int32_t dims[] = {1, 2, 3};
+  for( auto dim : dims )
+  {
+    initializer->add_dims(dim);
+  }
+  // FIXME: Get TensorDataType?
+  initializer->set_data_type(0);
+
+  // FIXME: What goes here?
+  auto* sparse_initializer = gp->add_sparse_initializer();
+  auto* sparse_values = sparse_initializer->mutable_values();
+  for( auto dim : dims )
+  {
+    sparse_values->add_dims(dim);
+  }
+  // FIXME: Get TensorDataType?
+  sparse_values->set_data_type(0);
+
+  auto* sparse_indices = sparse_initializer->mutable_indices();
+  for( auto dim : dims )
+  {
+    sparse_indices->add_dims(dim);
+  }
+  // FIXME: Get TensorDataType?
+  sparse_indices->set_data_type(0);
+
+
+  // FIXME: Name, layers, get_type
+  gp->set_doc_string(m->get_name());
+
+  // FIXME: What is graph input and output?
+  auto* input = gp->add_input();
+  auto* output = gp->add_output();
+
+  // FIXME: What is this stuff?
+  auto* value_info = gp->add_value_info();
+  auto* quantization_annotation = gp->add_quantization_annotation();
 }
 
 std::unique_ptr<callback_base>
