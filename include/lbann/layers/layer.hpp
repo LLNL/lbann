bdf29968f36e2b4cf593471dd0c2d162074a7ce6
Katie Graham <graham63@llnl.gov>
2021-04-26 22:31:20 -0700

    latest version of onnx callback

--- include/lbann/layers/layer.hpp
+++ include/lbann/layers/layer.hpp
@@ -496,6 +496,9 @@ public:
   //** @brief Get ONNX operator type */
   virtual std::string get_onnx_op_type() const;
 
+  //** @brief Get ONNX operator type */
+  virtual std::string get_onnx_op_type() const;
+
   const Layer& get_parent_layer(size_t index=0) const;
   const Layer& get_child_layer(size_t index=0) const;
 
