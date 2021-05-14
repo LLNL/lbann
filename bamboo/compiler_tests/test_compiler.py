47590d5b6833c768f1dbfb314c0a44c06b5431a1
Brian C. Van Essen <vanessen1@llnl.gov>
2021-05-14 16:26:23 -0700

    Cleaning up debugging code

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -6,10 +6,6 @@ import os, re, subprocess
 
 
 def test_compiler_build_script(cluster, dirname):
-    # if cluster not in ['catalyst', 'corona', 'lassen', 'pascal', 'ray']:
-    #     e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
-    #     print('Skip - ' + e)
-    #     pytest.skip(e)
     output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
     error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
     if cluster in ['lassen', 'pascal', 'ray']:
