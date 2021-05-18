a2b8e36bcc944766ce2825343264c5f7a4c15a5f
Brian C. Van Essen <vanessen1@llnl.gov>
2021-05-18 11:16:24 -0700

    Bamboo builds should use a clean build directory

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -9,13 +9,13 @@ def test_compiler_build_script(cluster, dirname):
     output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
     error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
     if cluster in ['lassen', 'pascal', 'ray']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     elif cluster in ['corona']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +rocm +deterministic +fft +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +rocm +deterministic +fft +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     elif cluster in ['catalyst']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
