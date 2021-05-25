447aa41055d643a43f57a57a6c7e6390b8197f0d
Brian C. Van Essen <vanessen1@llnl.gov>
2021-05-25 10:53:45 -0700

    Renamed the fixed function HDF5 data reader that was tuned for Cosmoflow to data_reader_hdf5_legacy.[hpp, cpp].

--- include/lbann/lbann.hpp
+++ include/lbann/lbann.hpp
@@ -146,7 +146,8 @@
 #include "lbann/data_readers/data_reader_pilot2_molecular.hpp"
 #endif // LBANN_HAS_CNPY
 #ifdef LBANN_HAS_DISTCONV
-#include "lbann/data_readers/data_reader_hdf5.hpp"
+#include "lbann/data_readers/data_reader_hdf5_legacy.hpp"
+#include "lbann/data_readers/data_reader_HDF5.hpp"
 #endif // LBANN_HAS_DISTCONV
 #include "lbann/data_readers/data_reader_smiles.hpp"
 #include "lbann/data_readers/data_reader_node2vec.hpp"
