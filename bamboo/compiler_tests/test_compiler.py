e2964557d7a7c8fa6728f19c436a918535c29973
Brian Van Essen <vanessen1@llnl.gov>
2021-06-30 14:31:42 -0700

    Added -j parallel build flags to bamboo builds (#1904)

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -9,18 +9,18 @@ def test_compiler_build_script(cluster, dirname):
     output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
     error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
     if cluster in ['lassen', 'pascal', 'ray']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     elif cluster in ['corona']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     elif cluster in ['catalyst']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
             dirname, output_file_name, error_file_name)
     else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
-        
+
     return_code = os.system(command)
     tools.assert_success(return_code, error_file_name)
