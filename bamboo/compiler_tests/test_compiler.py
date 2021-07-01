41c5325df4fb629f246a7122f4d58c89a04fd683
Brian Van Essen <vanessen1@llnl.gov>
2021-06-30 18:38:48 -0700

    Force bamboo to include the Agent ID in the spack environment name (#1905)
    * Force bamboo to include the Agent ID in the spack environment name
    
    * Added the unique Bamboo agent ID to the run script

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -8,15 +8,17 @@ import os, re, subprocess
 def test_compiler_build_script(cluster, dirname):
     output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
     error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
+    # Get environment variables
+    BAMBOO_AGENT = os.getenv('bamboo_agentId')
     if cluster in ['lassen', 'pascal', 'ray']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, output_file_name, error_file_name)
+        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
     elif cluster in ['corona']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
-            dirname, output_file_name, error_file_name)
+        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
+            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
     elif cluster in ['catalyst']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, output_file_name, error_file_name)
+        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
+            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
     else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
