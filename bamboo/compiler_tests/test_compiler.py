79c4eada0f30a0c3129325d41ca7250297590ddf
Thomas R. Benson <benson31@llnl.gov>
2021-07-07 08:41:00 -0700

    save spack artifacts from compile test

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -6,23 +6,29 @@ import os, re, subprocess
 
 
 def test_compiler_build_script(cluster, dirname):
-    output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
-    error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
+    bamboo_base_dir = '%s/bamboo/compiler_tests' % (dirname)
+    output_file_name = '%s/output/build_script_output.txt' % (bamboo_base_dir)
+    error_file_name = '%s/error/build_script_error.txt' % (bamboo_base_dir)
+
     # Get environment variables
     BAMBOO_AGENT = os.getenv('bamboo_agentId')
+
+    common_cmd = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy' % (dirname, BAMBOO_AGENT)
     if cluster in ['lassen', 'pascal', 'ray']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +cuda +half +fft ^hydrogen@develop > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     elif cluster in ['corona']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +rocm ^hydrogen@develop > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     elif cluster in ['catalyst']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +onednn +half +fft ^hydrogen@develop > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
 
     return_code = os.system(command)
+
+    gather_artifacts_cmd = 'cp %s/spack-*.txt %s/output' % (dirname, bamboo_base_dir)
+    os.system(gather_artifacts_cmd)
+
     tools.assert_success(return_code, error_file_name)
+
