8c61ac0f62688d0220f53378a15785c721635e4b
Tom Benson <30674819+benson31@users.noreply.github.com>
2021-07-09 12:26:34 -0400

    Run Catch2 tests and save Spack artifacts in Bamboo (#1909)
    * save spack artifacts from compile test
    
    * add catch tests
    
    * attempt to fix the artifact copying
    
    * try to cleanup gpu issues with catch tests

--- bamboo/compiler_tests/test_compiler.py
+++ bamboo/compiler_tests/test_compiler.py
@@ -2,27 +2,38 @@ import sys
 sys.path.insert(0, '../common_python')
 import tools
 import pytest
-import os, re, subprocess
-
+import os, re
+import shutil
 
 def test_compiler_build_script(cluster, dirname):
-    output_file_name = '%s/bamboo/compiler_tests/output/build_script_output.txt' % (dirname)
-    error_file_name = '%s/bamboo/compiler_tests/error/build_script_error.txt' % (dirname)
+    bamboo_base_dir = os.path.join(dirname, 'bamboo', 'compiler_tests')
+    output_file_name = os.path.join(bamboo_base_dir, 'output', 'build_script_output.txt')
+    error_file_name = os.path.join(bamboo_base_dir, 'error', 'build_script_error.txt')
+
     # Get environment variables
     BAMBOO_AGENT = os.getenv('bamboo_agentId')
+
+    common_cmd = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy' % (dirname, BAMBOO_AGENT)
     if cluster in ['lassen', 'pascal', 'ray']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +cuda +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +cuda +half +fft > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     elif cluster in ['corona']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +rocm +deterministic +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +rocm > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     elif cluster in ['catalyst']:
-        command = '%s/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +onednn +deterministic +half +fft +vision +numpy > %s 2> %s' % (
-            dirname, BAMBOO_AGENT, output_file_name, error_file_name)
+        command = '%s +onednn +half +fft > %s 2> %s' % (common_cmd, output_file_name, error_file_name)
     else:
         e = 'test_compiler_build_script: Unsupported Cluster %s' % cluster
         print('Skip - ' + e)
         pytest.skip(e)
 
     return_code = os.system(command)
+
+    artifact_dir = os.path.join(bamboo_base_dir, 'output')
+    with os.scandir(dirname) as it:
+        for entry in it:
+            if entry.is_file() and re.match(r'spack-.*txt', entry.name):
+                (base, ext) = os.path.splitext(entry.name)
+                new_file_name = base + '_output' + ext
+                shutil.copyfile(entry.path, os.path.join(artifact_dir, new_file_name))
+
     tools.assert_success(return_code, error_file_name)
+
