ae6ffae21beb714fbbe0e6d683c2a82f166098ca
Tom Benson <30674819+benson31@users.noreply.github.com>
2021-07-09 10:55:19 -0400

    Fix Corona build (#1912)
    * fix declarations on overridden virtual functions
    
    * fix a linker error
    
    * remove debugging output
    
    * remove symbol-inducing references to kfac_block_gru
    
    * Fixed compile error
    
    Co-authored-by: Michael Wyatt <mrwyattii@gmail.com>

--- src/execution_algorithms/kfac.cpp
+++ src/execution_algorithms/kfac.cpp
@@ -440,8 +440,19 @@ void KFAC::on_forward_prop_end(
         block = std::make_shared<kfac_block_bn<Device>>(
             l, &context, layer_id, proc_rank);
       } else if(is_gru) {
+#ifdef LBANN_GRU_LAYER_CUDNN_SUPPORTED
         block = std::make_shared<kfac_block_gru<Device>>(
             l, &context, layer_id, proc_rank);
+#else
+        if constexpr (Device == El::Device::CPU) {
+          block = std::make_shared<kfac_block_gru<Device>>(
+            l, &context, layer_id, proc_rank);
+        }
+        else {
+          block = nullptr;
+          LBANN_ERROR("GRU not supported on GPU without cuDNN support.");
+        }
+#endif
       }
 
       context.m_blocks.push_back(std::move(block));
