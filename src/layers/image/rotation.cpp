e84b37e791d39618521f4f63879b88ba6cf91cdd
tnat410 <tnat410@gmail.com>
2021-07-22 14:06:02 -0400

    Update src/layers/image/rotation.cpp
    Co-authored-by: Tim Moon <moon13@llnl.gov>

--- src/layers/image/rotation.cpp
+++ src/layers/image/rotation.cpp
@@ -48,12 +48,9 @@ void rotation_layer<TensorDataType, Layout, Device>::fp_compute() {
   const auto& input_dims = this->get_input_dims(0);
   const El::Int num_dims = input_dims.size();
   const auto& num_samples = local_input.Width();
-  const El::Int num_channels = std::accumulate(input_dims.begin(),
-                                               input_dims.end()-2,
-                                               1,
-                                               std::multiplies<int>());
-  const El::Int input_height = input_dims[num_dims-2];
-  const El::Int input_width = input_dims[num_dims-1];
+  const El::Int num_channels = input_dims[0]
+  const El::Int input_height = input_dims[1];
+  const El::Int input_width = input_dims[2];
 
   // Get rotation angle
   const auto& angles = this->get_prev_activations(1);
