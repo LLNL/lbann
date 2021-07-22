d1c87d987a40742c1efda644c9a3de49f7d8382c
Thomas R. Benson <benson31@llnl.gov>
2021-07-22 12:31:01 -0400

    Move non-IO operations to helpers library and test.
    The main TODO is to test the sample list formatting routines. I need
    to understand the sample list formats before doing that, though.

--- data_utils/hdf5/generate_HDF5_schema_and_sample_list.cpp
+++ data_utils/hdf5/generate_HDF5_schema_and_sample_list.cpp
@@ -27,136 +27,103 @@
 // Author: David Hysom (hysom1@llnl.gov)
 // Issues: https://github.com/LLNL/lbann/issues/new
 
-#include "conduit/conduit.hpp"
-#include "conduit/conduit_relay.hpp"
-#include "conduit/conduit_schema.hpp"
+#include "helpers.hpp"
+
+#include <clara.hpp>
+
+#include <conduit/conduit.hpp>
+#include <conduit/conduit_relay.hpp>
+#include <conduit/conduit_relay_hdf5.hpp>
+#include <conduit/conduit_relay_io.hpp>
+#include <conduit/conduit_relay_io_identify_protocol_api.hpp>
+#include <conduit/conduit_schema.hpp>
+#include <conduit/conduit_utils.hpp>
 
 #include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <set>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
+using conduit::Node;
 using namespace std;
 
-const string sample_list_inclusive_fn("inclusive.sample_list");
-const string sample_list_exclusive_fn("exclusive.sample_list");
-const string yaml_fn("data_schema.yaml");
+string const sample_list_inclusive_fn("inclusive.sample_list");
+string const sample_list_exclusive_fn("exclusive.sample_list");
+string const yaml_fn("data_schema.yaml");
 
-const conduit::Node* get_sample(const vector<string>& tokens,
-                                const conduit::Node& nd)
+conduit::Node build_node_from_file(std::string const& filename,
+                                   std::string const& protocol = "")
 {
-  const conduit::Node* node = &nd;
-  size_t count = 0;
-  for (size_t j = 0; j < tokens.size(); j++) {
-    if (node->has_child(tokens[j])) {
-      ++count;
-      node = &(node->child(tokens[j]));
-    }
-  }
-  if (count == tokens.size()) {
-    return node;
-  }
-
-  // recursion
-  conduit::NodeConstIterator iter = nd.children();
-  while (iter.has_next()) {
-    const conduit::Node& child = iter.next();
-    return get_sample(tokens, child);
-  }
-  return nullptr;
-}
-void get_sample_ids(const string& schema_str,
-                    const conduit::Node& nd,
-                    vector<string>& sample_ids)
-{
-  const string candidate = nd.schema().to_string();
-  if (candidate == schema_str) {
-    sample_ids.push_back(nd.path());
-  }
-  // recursion
-  conduit::NodeConstIterator iter = nd.children();
-  while (iter.has_next()) {
-    const conduit::Node& child = iter.next();
-    get_sample_ids(schema_str, child, sample_ids);
-  }
+  conduit::Node node;
+  if (protocol.empty())
+    conduit::relay::io::load(filename, node);
+  else
+    conduit::relay::io::load(filename, protocol, node);
+  return node;
 }
 
-void get_all_sample_ids(const string& filelist,
-                        const string& schema_str,
-                        map<string, vector<string>>& sample_ids)
+map<string, vector<string>> get_all_sample_ids(conduit::Schema const& schema,
+                                               vector<string> const& filelist,
+                                               std::string const& protocol)
 {
-  cout << "getting list of sample IDs for all files\n";
-  ifstream in(filelist);
-  string filename;
-  while (in >> filename) {
+  map<string, vector<string>> sample_ids;
+  for (auto const& filename : filelist) {
     if (filename.size() < 2) {
       continue;
     }
-    conduit::Node nd;
     cout << "  loading: " << filename << endl;
-    conduit::relay::io::load(filename.c_str(), "hdf5", nd);
-    get_sample_ids(schema_str, nd, sample_ids[filename]);
+    Node nd = build_node_from_file(filename, protocol);
+    sample_ids[filename] = data_utils::get_matching_node_paths(nd, schema);
   }
+  return sample_ids;
 }
 
-void print_leading_spaces(ofstream& out, int n)
+void print_leading_spaces(ostream& out, size_t n) { out << string(n, ' '); }
+
+bool is_leaf(Node const& nd) { return nd.number_of_children() == 0L; }
+
+void write_yaml_header(ostream& out)
 {
-  for (int k = 0; k < n; k++) {
-    out << " ";
-  }
+  out << R"YAML(#You must edit this file prior to use as follows.
+#
+#  Uncomment the 'pack' entries for whichever data fields you wish to use.
+#  pack's value should be one of the following: datum, label, response.
+#  If you do not wish to use a data field, simply leave 'pack' commented out
+#
+#  For normalization, uncomment and add appropriate values to the 'scale'
+#  and 'bias' entries.  For images, the 'scale' and 'bias' entries should be
+#  lists with one entry per channel. You should also add the following
+#  entries for images: dims, channels. Here is an example of the metadata
+#  entries for images:
+#    dims: [300,300]
+#    channels: 2
+#    scale: [1.23, 4.56]
+#    bias:  [1.0, 2.0]
+#
+#  The 'ordering' entries determine how data is packed into a vector
+#  e.g, when you have multiple 'pack: response' entries. The values in the
+#  ordering entries need not be unique and are relative
+#
+)YAML";
 }
 
-bool is_leaf(const conduit::Node& nd) { return !nd.number_of_children(); }
-
-void write_yaml_header(ofstream& out)
-{
-  out
-    << "#You must edit this file prior to use as follows.\n"
-       "#\n"
-       "#  Uncomment the 'pack' entries for whichever data fields you wish to "
-       "use.\n"
-       "#  pack's value should be one of the following: datum, label, "
-       "response.\n"
-       "#  If you do not wish to use a data field, simply leave 'pack' "
-       "commented out\n"
-       "#\n"
-       "#  For normalization, uncomment and add appropriate values to the "
-       "'scale' \n"
-       "#  and 'bias' entries.  For images, the 'scale' and 'bias' entries "
-       "should be\n"
-       "#  lists with one entry per channel. You should also add the following "
-       "\n"
-       "#  entries for images: dims, channels. Here is an example of the "
-       "metadata\n"
-       "#  entries for images:\n"
-       "#    dims: [300,300]\n"
-       "#    channels: 2\n"
-       "#    scale: [1.23, 4.56]\n"
-       "#    bias:  [1.0, 2.0]\n"
-       "#\n"
-       "#  The 'ordering' entries determine how data is packed into a vector \n"
-       "#  e.g, when you have multiple 'pack: response' entries. The values in "
-       "the \n"
-       "#  ordering entries need not be unique and are relative\n"
-       "#\n";
-}
-
-void write_yaml_file(ofstream& out,
-                     const conduit::Node* nd,
+void write_yaml_file(ostream& out,
+                     Node const& nd,
                      int indent = 0,
                      int ordering = 10)
 {
   if (indent == 0) {
-    cout << ">>>>>>>>>> WRITING " << yaml_fn << endl;
     write_yaml_header(out);
   }
-  conduit::NodeConstIterator iter = nd->children();
+  conduit::NodeConstIterator iter = nd.children();
   while (iter.has_next()) {
-    const conduit::Node& child = iter.next();
+    Node const& child = iter.next();
     int n = indent;
     print_leading_spaces(out, n);
     out << child.name() << ":\n";
@@ -177,15 +144,14 @@ void write_yaml_file(ofstream& out,
       out << "#bias:\n";
       ordering += 10;
     }
-    write_yaml_file(out, &child, indent + 2, ordering);
+    write_yaml_file(out, child, indent + 2, ordering);
   }
 }
 
-void write_exclusive_sample_list(ofstream& out,
-                                 const map<string, vector<string>>& sample_ids,
-                                 const string& base_dir)
+void write_exclusive_sample_list(ostream& out,
+                                 map<string, vector<string>> const& sample_ids,
+                                 string const& base_dir)
 {
-  cout << ">>>>>>>>>> WRITING exclustive sample list\n";
   out << "CONDUIT_HDF5_EXCLUSION\n";
   size_t good = 0;
   for (const auto& t : sample_ids) {
@@ -197,16 +163,15 @@ void write_exclusive_sample_list(ofstream& out,
   if (base_dir == ".") { // edge case
     idx = 0;
   }
-  for (const auto& t : sample_ids) {
-    out << t.first.substr(idx) << " " << t.second.size() << " 0\n";
+  for (const auto& [filename, samples] : sample_ids) {
+    out << filename.substr(idx) << " " << samples.size() << " 0\n";
   }
 }
 
-void write_inclusive_sample_list(ofstream& out,
-                                 const map<string, vector<string>>& sample_ids,
-                                 const string& base_dir)
+void write_inclusive_sample_list(ostream& out,
+                                 map<string, vector<string>> const& sample_ids,
+                                 string const& base_dir)
 {
-  cout << ">>>>>>>>>> WRITING inclustive sample list\n";
   out << "CONDUIT_HDF5_INCLUSION\n";
   size_t good = 0;
   for (const auto& t : sample_ids) {
@@ -218,152 +183,126 @@ void write_inclusive_sample_list(ofstream& out,
   if (base_dir == ".") { // edge case
     idx = 0;
   }
-  for (const auto& t : sample_ids) {
-    out << t.first.substr(idx) << " " << t.second.size() << " 0";
-    for (const auto& sample_id : t.second) {
+  for (const auto& [filename, samples] : sample_ids) {
+    out << filename.substr(idx) << " " << samples.size() << " 0";
+    for (const auto& sample_id : samples) {
       out << " " << sample_id;
     }
     out << endl;
   }
 }
 
-void open_file(string filename, ofstream& out)
+// get vector containing hdf5 filenames
+vector<string> get_file_names(std::string const& file_list_file_name)
 {
-  out.open(filename);
-  if (!out) {
-    cerr << "failed to open " << filename << " for WRITING\n";
-    exit(9);
-  }
-}
-
-string get_base_dir(const string& filelist)
-{
-  // this is very inefficient; there are many better approaches
-  // for finding longest common prefix; but this is easy to code.
-
-  // get vector containing hdf5 filenames
-  vector<string> f;
-  ifstream in(filelist);
-  string filename;
-  while (in >> filename) {
-    if (filename.size() > 2) {
-      f.push_back(filename);
+  vector<string> file_list;
+  ifstream in(file_list_file_name);
+  string file_name;
+  file_name.reserve(1024);
+  while (in >> file_name) {
+    if (file_name.size() > 2) {
+      file_list.emplace_back(data_utils::normalize_path(file_name));
     }
   }
-  if (f.size() < 1) {
-    cerr << "get_base_dir; possibly empty filelist\n";
-    exit(0);
-  }
-
-  // edge case
-  if (f.size() == 1) {
-    size_t jj = f[0].rfind('/');
-    if (jj == string::npos) {
-      cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>. string::npos\n";
-      return ".";
-    }
-    else {
-      cout << "NOT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>. string::npos\n";
-      return f[0].substr(0, jj);
-    }
-  }
-
-  // find longest common prefix, which is the base directory
-  string b;
-  int index = 0;
-  while (true) {
-    size_t j1 = f[0].find('/', index + 1);
-    if (j1 == string::npos) {
-      break;
-    }
-    for (size_t i = 1UL; i < f.size(); i++) {
-      size_t j2 = f[i].find('/', index + 1);
-      if (j2 != j1) {
-        break;
-      }
-    }
-    index = j1;
-  }
-  b = f[0].substr(0, index);
-
-  return b;
+  return file_list;
 }
 
 int main(int argc, char** argv)
 {
-  if (argc != 3) {
-    cout << "usage: " << argv[0]
-         << " filelist sample_id\n"
-            "\n"
-            "where: 'filelist' contains the names of one or more hdf5 files;\n"
-            "\n"
-            "example invocations:\n"
-            "  generate_yaml filelist_PROBIES.txt RUN_ID/000000000\n"
-            "  generate_yaml filelist_carbon.txt e1/s100\n"
-            "  generate_yaml filelist_jag.txt 0.0.96.7.0:1\n"
-            "\n";
-    exit(9);
-  }
-  string filelist(argv[1]);
-  string sample_id(argv[2]);
+  string filelist;
+  string sample_id;
+  string protocol;
+  bool print_help = false;
 
-  // get base directory, which is the longest common prefix
-  string base_dir = get_base_dir(filelist);
+  auto cli =
+    clara::Help(print_help).optional() |
+    clara::Arg(filelist, "data file list")(
+      "The name of a file containing the names of one or more HDF5 files")
+      .required() |
+    clara::Arg(sample_id, "sample id")(
+      "The path to a prototypical sample in the first HDF5 file")
+      .required() |
+    clara::Opt(protocol, "data protocol")["-p"]["--protocol"](
+      "The conduit-compatible protocol string for these files");
+
+  auto result = cli.parse({argc, argv});
+  if (!result) {
+    cerr << "Error: Parsing arguments failed with message: "
+         << result.errorMessage() << endl;
+    return EXIT_FAILURE;
+  }
+
+  if (print_help || sample_id.empty() || filelist.empty()) {
+    cout << cli << endl
+         << "example invocations:\n"
+         << "  generate_yaml filelist_PROBIES.txt RUN_ID/000000000\n"
+         << "  generate_yaml filelist_carbon.txt e1/s100\n"
+         << "  generate_yaml filelist_jag.txt 0.0.96.7.0:1\n"
+         << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Get the list of sample files
+  auto const file_names = get_file_names(filelist);
+
+  // Since every file is expected to be the same format, and the list
+  // of files may be long, we do this once up front.
+  if (protocol.empty())
+    conduit::relay::io::identify_protocol(file_names.front(), protocol);
+
+  // Get base directory, which is the longest common prefix
+  string const base_dir = data_utils::get_longest_common_prefix(file_names);
+  cout << "Common base dir: " << base_dir << endl;
 
   // open output files
-  ofstream out_sample_list_inclusive;
-  ofstream out_sample_list_exclusive;
-  ofstream out_yaml;
-  open_file(sample_list_inclusive_fn, out_sample_list_inclusive);
-  open_file(sample_list_exclusive_fn, out_sample_list_exclusive);
-  open_file(yaml_fn, out_yaml);
 
   // load the first hdf5 file from the filelist
-  ifstream in(filelist.c_str());
-  if (!in) {
-    cerr << "failed to open " << filelist << " for reading\n";
-    exit(9);
-  }
-  string filename;
-  in >> filename;
-  in.close();
-  conduit::Node nd;
-  cout << "loading: " << filename << endl;
-  conduit::relay::io::load(filename, "hdf5", nd);
+  try {
+    string const& filename = file_names.front();
 
-  // tokenize the sample id: split on the '/' character
-  replace(sample_id.begin(), sample_id.end(), '/', ' ');
-  istringstream iss(sample_id);
-  string w;
-  vector<string> tokens;
-  while (iss >> w) {
-    tokens.push_back(w);
-  }
+    cout << "Searching for sample ID \"" << sample_id
+         << "\" in file: " << filename << endl;
 
-  // get a node whose children compose a sample, then write the yaml file
-  const conduit::Node* sample_node = get_sample(tokens, nd);
-  if (sample_node == nullptr) {
-    cerr << "failed to find your specified sample_id: " << sample_id << endl;
-    for (long long j = 0L; j < nd.number_of_children(); j++) {
-      cout << nd.child(j).path() << endl; // XX
+    Node node = build_node_from_file(filename, protocol);
+    auto const& prototype_sample =
+      data_utils::get_prototype_sample(node, sample_id);
+
+    {
+      cout << "Writing yaml file (\"" << yaml_fn << "\")... ";
+      ofstream out_yaml(yaml_fn);
+      write_yaml_file(out_yaml, prototype_sample);
+      cout << "done." << endl;
     }
-    exit(9);
+
+    // get pathnames for all sample IDs
+    auto sample_ids =
+      get_all_sample_ids(prototype_sample.schema(), file_names, protocol);
+
+    // write the sample lists
+    {
+      cout << "Writing exclusive sample list (\"" << sample_list_exclusive_fn
+           << "\")... ";
+      ofstream out_sample_list_exclusive(sample_list_exclusive_fn);
+
+      write_exclusive_sample_list(out_sample_list_exclusive,
+                                  sample_ids,
+                                  base_dir);
+      cout << "done." << endl;
+    }
+    {
+      cout << "Writing inclusive sample list (\"" << sample_list_inclusive_fn
+           << "\")... ";
+      ofstream out_sample_list_inclusive(sample_list_inclusive_fn);
+      write_inclusive_sample_list(out_sample_list_inclusive,
+                                  sample_ids,
+                                  base_dir);
+      cout << "done." << endl;
+    }
   }
-  write_yaml_file(out_yaml, sample_node);
+  catch (conduit::Error const& e) {
+    cerr << "Error detected by Conduit:\n" << e.what() << endl;
+    return EXIT_FAILURE;
+  }
 
-  // get pathnames for all sample IDs
-  map<string, vector<string>> sample_ids; // maps: filename -> list of sample
-                                          // IDs
-  const string schema_str = sample_node->schema().to_string();
-  get_all_sample_ids(filelist, schema_str, sample_ids);
-
-  // write the sample lists
-  cout << "base dir: " << base_dir << endl;
-  write_exclusive_sample_list(out_sample_list_exclusive, sample_ids, base_dir);
-  write_inclusive_sample_list(out_sample_list_inclusive, sample_ids, base_dir);
-
-  // close output files
-  out_yaml.close();
-  out_sample_list_exclusive.close();
-  out_sample_list_inclusive.close();
+  return EXIT_SUCCESS;
 }
