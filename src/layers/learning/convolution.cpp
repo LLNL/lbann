c539c1db595b8ebe8cd75d4f9f958c0541c799f5
Tim Moon <moon13@llnl.gov>
2021-08-03 16:14:30 -0700

    Revert to old distconv API
    Make sure kernel gradient buffer is allocated so it can be used to autotune convolution algorithm.

--- src/layers/learning/convolution.cpp
+++ src/layers/learning/convolution.cpp
@@ -270,11 +270,6 @@ void convolution_distconv_adapter<TensorDataType, Layout, Device>::setup_layer(
     this->m_bwd_filter_algo = dc::get_convolution_bwd_filter_algorithm();
   }
 
-  // Allocate temporary buffer for kernel
-  // Note: Needed for autotuning the convolution algorithm
-  El::simple_buffer<TensorDataType,Device> temp(this->m_kernel->get_local_size());
-  assert0(dc::tensor::View(*this->m_kernel, temp.data()));
-
   std::vector<int> pads = layer.m_pads;
   std::reverse(pads.begin(), pads.end());
   std::vector<int> strides = layer.m_strides;
@@ -282,6 +277,16 @@ void convolution_distconv_adapter<TensorDataType, Layout, Device>::setup_layer(
   std::vector<int> dilations = layer.m_dilations;
   std::reverse(dilations.begin(), dilations.end());
 
+  // Allocate temporary buffer for kernel gradient buffer, if needed
+  // Note: Needed for autotuning the convolution algorithm
+  El::simple_buffer<TensorDataType,Device> temp;
+  TensorDataType* kernel_gradient_buffer = this->m_kernel_gradient->get_buffer();
+  if (kernel_gradient_buffer == nullptr) {
+    temp.allocate(this->m_kernel_gradient->get_local_size());
+    assert0(dc::tensor::View(*this->m_kernel_gradient, temp.data()));
+  }
+
+  // Setup
   this->m_conv->setup(this->get_prev_activations(),
                       *(this->m_kernel), this->get_activations(),
                       this->get_error_signals(),
@@ -291,6 +296,10 @@ void convolution_distconv_adapter<TensorDataType, Layout, Device>::setup_layer(
                       this->m_fwd_algo, this->m_bwd_data_algo,
                       this->m_bwd_filter_algo,
                       workspace_capacity);
+
+  // Clean up
+  assert0(dc::tensor::View(*this->m_kernel_gradient, kernel_gradient_buffer));
+
 }
 #endif // defined LBANN_HAS_DISTCONV
 
