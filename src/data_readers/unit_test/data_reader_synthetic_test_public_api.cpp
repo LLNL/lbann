3ac5bb8ae6573f8763e57b4fc89e67d3652cce62
Brian C. Van Essen <vanessen1@llnl.gov>
2021-08-31 11:10:00 -0700

    Added data reader public API test for responses.  Improved the tests to sweep multiple variables through the public API.

--- src/data_readers/unit_test/data_reader_synthetic_test_public_api.cpp
+++ src/data_readers/unit_test/data_reader_synthetic_test_public_api.cpp
@@ -68,93 +68,182 @@ TEST_CASE("Synthetic data reader public API classification tests",
   // Create a local copy of the RNG to check the synthetic data reader
   lbann::fast_rng_gen ref_fast_generator;
   ref_fast_generator.seed(lbann::hash_combine(42, 0));
-  std::normal_distribution<lbann::DataType> dist(float(0), float(1));
 
   // Initalize a per-trainer I/O thread pool
   auto io_thread_pool = lbann::make_unique<lbann::thread_pool>();
   io_thread_pool->launch_pinned_threads(1, 1);
 
-  El::Int num_samples = 7;
-  El::Int num_labels = 10;
-  std::vector<int> dims = {4, 4};
-  lbann::data_reader_synthetic* dr = new lbann::data_reader_synthetic(
-          num_samples,
-          dims,
-          num_labels,
-          false);
-  dr->setup(io_thread_pool->get_num_threads(),
-            io_thread_pool.get());
-  dr->set_rank(0);
-  dr->set_comm(&comm);
-  dr->set_num_parallel_readers(1);
-  dr->load();
-  dr->set_mini_batch_size(num_samples);
-  dr->set_last_mini_batch_size(num_samples);
-  dr->set_initial_position();
-
-  DataReaderSyntheticWhiteboxTester white_box_tester;
-
   std::vector<std::string> active_data_fields = {"samples", "labels"};
-  //  std::vector<std::string> active_data_fields = {"samples", "labels", "responses"};
-  std::map<lbann::input_data_type, lbann::CPUMat*> local_input_buffers;
-  for (auto& data_field : active_data_fields) {
-    lbann::input_data_type data_field_hack;
-    lbann::CPUMat* local_mat = new lbann::CPUMat();
-    if (data_field == INPUT_DATA_TYPE_SAMPLES) {
-      data_field_hack = lbann::input_data_type::SAMPLES;
-      local_mat->Resize(dims[0]*dims[1], num_samples);
-      El::Zeros_seq(*local_mat, dims[0]*dims[1], num_samples);
-    }
-    else if (data_field == INPUT_DATA_TYPE_LABELS) {
-      data_field_hack = lbann::input_data_type::LABELS;
-      local_mat->Resize(num_labels, num_samples);
-      El::Zeros_seq(*local_mat, num_labels, num_samples);
-    }
-    else if (data_field == INPUT_DATA_TYPE_LABELS) {
-      data_field_hack = lbann::input_data_type::RESPONSES;
-    }
-    local_input_buffers[data_field_hack] = local_mat;
-  }
-  El::Matrix<El::Int> indices_fetched;
-  El::Zeros_seq(indices_fetched, num_samples, 1);
+  for(auto s = 1; s <= 10; s++) {
+    El::Int num_samples = s;
+    std::vector<int> dims = {s,s};;
+    El::Int num_labels = s*2;
 
-  // for(auto k = 1; k < 10; k++) {
-  //   dims[0] = k;
-  //   for(auto l = 1; l < 10; l++) {
-  //     response_dims[0] = l;
+    std::map<lbann::input_data_type, std::unique_ptr<lbann::CPUMat>> owning_local_input_buffers;
+    std::map<lbann::input_data_type, lbann::CPUMat*> local_input_buffers;
+    for (auto& data_field : active_data_fields) {
+      lbann::input_data_type data_field_hack;
+      auto local_mat = std::make_unique<lbann::CPUMat>();
+      if (data_field == INPUT_DATA_TYPE_SAMPLES) {
+        data_field_hack = lbann::input_data_type::SAMPLES;
+        local_mat->Resize(dims[0] * dims[1], num_samples);
+        El::Zeros_seq(*local_mat, dims[0] * dims[1], num_samples);
+      }
+      else if (data_field == INPUT_DATA_TYPE_LABELS) {
+        data_field_hack = lbann::input_data_type::LABELS;
+        local_mat->Resize(num_labels, num_samples);
+        El::Zeros_seq(*local_mat, num_labels, num_samples);
+      }
+      local_input_buffers[data_field_hack] = local_mat.get();
+      owning_local_input_buffers[data_field_hack] = std::move(local_mat);
+    }
+    El::Matrix<El::Int> indices_fetched;
+    El::Zeros_seq(indices_fetched, num_samples, 1);
 
-      SECTION("fetch data and label")
-      {
-        dr->fetch(local_input_buffers, indices_fetched);
+    SECTION("fetch data and label s=" + std::to_string(s))
+    {
+      auto dr = std::make_unique<lbann::data_reader_synthetic>(
+        num_samples,
+        dims,
+        num_labels,
+        false);
+      dr->setup(io_thread_pool->get_num_threads(), io_thread_pool.get());
+      dr->set_rank(0);
+      dr->set_comm(&comm);
+      dr->set_num_parallel_readers(1);
+      dr->load();
+      dr->set_mini_batch_size(num_samples);
+      dr->set_last_mini_batch_size(num_samples);
+      dr->set_initial_position();
 
-        for(auto& [field, buf] : local_input_buffers) {
-          std::cout << "For field " << to_string(field) << std::endl;
-          El::Print(*buf);
+      dr->fetch(local_input_buffers, indices_fetched);
+
+      // for (auto& [field, buf] : local_input_buffers) {
+      //   std::cout << "For field " << to_string(field) << std::endl;
+      //   El::Print(*buf);
+      // }
+
+      auto& X = *(local_input_buffers[lbann::input_data_type::SAMPLES]);
+      auto& Y = *(local_input_buffers[lbann::input_data_type::LABELS]);
+      CHECK(X.Width() == Y.Width());
+
+      for (El::Int j = 0; j < num_samples; j++) {
+        // Create a new normal distribution for each sample.  This ensures
+        // that the behavior matches the implementation in the synthetic data
+        // reader and handles the case of odd numbers of entries with a normal
+        // distriubtion implementation. (Specifically that entries for a
+        // normal distribution are generated in pairs.)
+        std::normal_distribution<lbann::DataType> dist(float(0), float(1));
+        for (El::Int i = 0; i < X.Height(); i++) {
+          CHECK(X(i, j) == dist(ref_fast_generator));
         }
-
-        auto& X = *(local_input_buffers[lbann::input_data_type::SAMPLES]);
-        auto& Y = *(local_input_buffers[lbann::input_data_type::LABELS]);
-        CHECK(X.Width() == Y.Width());
-
-        for(El::Int j = 0; j < Y.Width(); j++) {
-          for(El::Int i = 0; i < X.Height(); i++) {
-            CHECK(X(i,j) == dist(ref_fast_generator));
+        auto index = lbann::fast_rand_int(ref_fast_generator, num_labels);
+        // std::cout << "Here is the reference value " << index << std::endl;
+        for (El::Int i = 0; i < Y.Height(); i++) {
+          if (index == i) {
+            CHECK(Y(i, j) == 1);
           }
-          auto index = lbann::fast_rand_int(ref_fast_generator, num_labels);
-          std::cout << "Here is the reference value " << index << std::endl;
-          for(El::Int i = 0; i < Y.Height(); i++) {
-            if(index == i) {
-              CHECK(Y(i,j) == 1);
-            }else {
-              CHECK(Y(i,j) == 0);
-            }
+          else {
+            CHECK(Y(i, j) == 0);
           }
         }
       }
-  //   }
-  // }
+    }
+  }
+}
 
-  for(auto& [field, buf] : local_input_buffers) {
-    delete buf;
+TEST_CASE("Synthetic data reader public API regression tests",
+          "[mpi][data_reader][synthetic][regression][public]")
+{
+  // initialize stuff (boilerplate)
+  auto& comm = unit_test::utilities::current_world_comm();
+  lbann::init_random(42, 1);
+  lbann::init_data_seq_random(42);
+
+  // Create a local copy of the RNG to check the synthetic data reader
+  lbann::fast_rng_gen ref_fast_generator;
+  ref_fast_generator.seed(lbann::hash_combine(42, 0));
+
+  // Initalize a per-trainer I/O thread pool
+  auto io_thread_pool = lbann::make_unique<lbann::thread_pool>();
+  io_thread_pool->launch_pinned_threads(1, 1);
+
+  std::vector<std::string> active_data_fields = {"samples", "responses"};
+  for(auto s = 1; s <= 10; s++) {
+    El::Int num_samples = s;
+    std::vector<int> dims = {s,s};;
+    std::vector<int> response_dims = {s+1, s+1};
+
+    SECTION("fetch data and response s=" + std::to_string(s))
+    {
+      std::map<lbann::input_data_type, std::unique_ptr<lbann::CPUMat>> owning_local_input_buffers;
+      std::map<lbann::input_data_type, lbann::CPUMat*> local_input_buffers;
+      for (auto& data_field : active_data_fields) {
+        lbann::input_data_type data_field_hack;
+        auto local_mat = std::make_unique<lbann::CPUMat>();
+        if (data_field == INPUT_DATA_TYPE_SAMPLES) {
+          data_field_hack = lbann::input_data_type::SAMPLES;
+          local_mat->Resize(dims[0] * dims[1], num_samples);
+          El::Zeros_seq(*local_mat, dims[0] * dims[1], num_samples);
+        }
+        else if (data_field == INPUT_DATA_TYPE_RESPONSES) {
+          data_field_hack = lbann::input_data_type::RESPONSES;
+          local_mat->Resize(response_dims[0] * response_dims[1], num_samples);
+        }
+        local_input_buffers[data_field_hack] = local_mat.get();
+        owning_local_input_buffers[data_field_hack] = std::move(local_mat);
+      }
+      El::Matrix<El::Int> indices_fetched;
+      El::Zeros_seq(indices_fetched, num_samples, 1);
+
+      auto dr = std::make_unique<lbann::data_reader_synthetic>(num_samples,
+                                                               dims,
+                                                               response_dims,
+                                                               false);
+      dr->setup(io_thread_pool->get_num_threads(), io_thread_pool.get());
+      dr->set_rank(0);
+      dr->set_comm(&comm);
+      dr->set_num_parallel_readers(1);
+      dr->load();
+      dr->set_mini_batch_size(num_samples);
+      dr->set_last_mini_batch_size(num_samples);
+      dr->set_initial_position();
+
+      dr->fetch(local_input_buffers, indices_fetched);
+
+      // for (auto& [field, buf] : local_input_buffers) {
+      //   std::cout << "For field " << to_string(field) << std::endl;
+      //   El::Print(*buf);
+      // }
+
+      auto& X = *(local_input_buffers[lbann::input_data_type::SAMPLES]);
+      auto& Y = *(local_input_buffers[lbann::input_data_type::RESPONSES]);
+      CHECK(X.Width() == Y.Width());
+
+      for (El::Int j = 0; j < num_samples; j++) {
+        {
+          // Create a new normal distribution for each sample.  This ensures
+          // that the behavior matches the implementation in the synthetic data
+          // reader and handles the case of odd numbers of entries with a normal
+          // distriubtion implementation. (Specifically that entries for a
+          // normal distribution are generated in pairs.)
+          std::normal_distribution<lbann::DataType> dist(float(0), float(1));
+          for (El::Int i = 0; i < X.Height(); i++) {
+            CHECK(X(i, j) == dist(ref_fast_generator));
+          }
+        }
+        {
+          // Create a new normal distribution for each sample.  This ensures
+          // that the behavior matches the implementation in the synthetic data
+          // reader and handles the case of odd numbers of entries with a normal
+          // distriubtion implementation. (Specifically that entries for a
+          // normal distribution are generated in pairs.)
+          std::normal_distribution<lbann::DataType> dist(float(0), float(1));
+          for (El::Int i = 0; i < Y.Height(); i++) {
+            CHECK(Y(i, j) == dist(ref_fast_generator));
+          }
+        }
+      }
+    }
   }
 }
