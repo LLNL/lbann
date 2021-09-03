284d1c4a1534837d24233800c4bc3224a160acc0
Thomas R. Benson <benson31@llnl.gov>
2021-09-03 14:32:31 -0400

    quick factoring to use the common macro

--- include/lbann/operators/math/binary.hpp
+++ include/lbann/operators/math/binary.hpp
@@ -27,88 +27,37 @@
 #ifndef LBANN_INCLUDE_LBANN_OPERATORS_MATH_BINARY_HPP_INCLUDED
 #define LBANN_INCLUDE_LBANN_OPERATORS_MATH_BINARY_HPP_INCLUDED
 
-#include "lbann_config.hpp"
-
-#include "lbann/operators/elementwise_operator.hpp"
-#include "lbann/utils/cloneable.hpp"
-
-#include <operators.pb.h>
+#include "lbann/operators/declare_stateless_op.hpp"
 
 namespace lbann {
 
-#define LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(OP_NAME, OP_STRING)            \
-  template <typename DataT, El::Device D>                                      \
-  class OP_NAME##Operator final                                                \
-    : public Cloneable<OP_NAME##Operator<DataT, D>,                            \
-                       ElementwiseOperator<DataT, DataT, D>>                   \
-  {                                                                            \
-    using BaseType = Cloneable<OP_NAME##Operator<DataT, D>,                    \
-                               ElementwiseOperator<DataT, DataT, D>>;          \
-    using LocalInputTensorType = typename BaseType::LocalInputTensorType;      \
-    using LocalOutputTensorType = typename BaseType::LocalOutputTensorType;    \
-    using ConstLocalInputTensorType =                                          \
-      typename BaseType::ConstLocalInputTensorType;                            \
-    using ConstLocalOutputTensorType =                                         \
-      typename BaseType::ConstLocalOutputTensorType;                           \
-                                                                               \
-  public:                                                                      \
-    OP_NAME##Operator() = default;                                             \
-    OP_NAME##Operator(OP_NAME##Operator&&) = default;                          \
-    OP_NAME##Operator(OP_NAME##Operator const&) = default;                     \
-    OP_NAME##Operator& operator=(OP_NAME##Operator&&) = default;               \
-    OP_NAME##Operator& operator=(OP_NAME##Operator const&) = default;          \
-    ~OP_NAME##Operator() = default;                                            \
-    std::string get_type() const final { return OP_STRING; }                   \
-    template <typename ArchiveT>                                               \
-    void serialize(ArchiveT& ar)                                               \
-    {                                                                          \
-      using OperatorType = ElementwiseOperator<DataT, DataT, D>;               \
-      ar(::cereal::make_nvp("ElementwiseOperator",                             \
-                            ::cereal::base_class<OperatorType>(this)));        \
-    }                                                                          \
-                                                                               \
-  private:                                                                     \
-    void                                                                       \
-    fp_compute_local(std::vector<ConstLocalInputTensorType> inputs,            \
-                     std::vector<LocalOutputTensorType> outputs) const final;  \
-    void bp_compute_local(                                                     \
-      std::vector<ConstLocalInputTensorType> inputs,                           \
-      std::vector<ConstLocalOutputTensorType> grads_wrt_outputs,               \
-      std::vector<LocalInputTensorType> grads_wrt_inputs) const final;         \
-    void set_proto_params(lbann_data::Operator& msg) const final               \
-    {                                                                          \
-      msg.mutable_parameters()->PackFrom(lbann_data::OP_NAME##Operator{});     \
-    }                                                                          \
-    void do_fill_description(description&) const final {}                      \
-  }
-
 // Arithmetic operations
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Add, "add");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Subtract, "subtract");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Multiply, "multiply");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Divide, "divide");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Mod, "modulo");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Pow, "power");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(SafeDivide, "safe divide");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(SquaredDifference, "squared difference");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Add, "add");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Subtract, "subtract");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Multiply, "multiply");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Divide, "divide");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Mod, "modulo");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Pow, "power");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(SafeDivide, "safe divide");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(SquaredDifference,
+                                             "squared difference");
 
 // Comparison operations
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Max, "maximum");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Min, "minimum");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Equal, "equal");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(NotEqual, "not equal");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Less, "less than");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(LessEqual, "less than or equal");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(Greater, "greater than");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(GreaterEqual, "greater than or equal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Max, "maximum");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Min, "minimum");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Equal, "equal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(NotEqual, "not equal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Less, "less than");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(LessEqual, "less than or equal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Greater, "greater than");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(GreaterEqual,
+                                             "greater than or equal");
 
 // Logical operations
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(LogicalAnd, "logical and");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(LogicalOr, "logical or");
-LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR(LogicalXor, "logical xor");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(LogicalAnd, "logical and");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(LogicalOr, "logical or");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(LogicalXor, "logical xor");
 
 } // namespace lbann
-
-#undef LBANN_DECLARE_ENTRYWISE_BINARY_OPERATOR
 
 #endif // LBANN_INCLUDE_LBANN_OPERATORS_MATH_BINARY_HPP_INCLUDED
--- include/lbann/operators/math/unary.hpp
+++ include/lbann/operators/math/unary.hpp
@@ -27,110 +27,57 @@
 #ifndef LBANN_INCLUDE_LBANN_OPERATORS_MATH_UNARY_HPP_INCLUDED
 #define LBANN_INCLUDE_LBANN_OPERATORS_MATH_UNARY_HPP_INCLUDED
 
-#include "lbann_config.hpp"
-
-#include "lbann/operators/elementwise_operator.hpp"
-#include "lbann/utils/cloneable.hpp"
-
-#include <operators.pb.h>
+#include "lbann/operators/declare_stateless_op.hpp"
 
 namespace lbann {
 
 // These are all single-type operators.
 
-#define LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(OP_NAME, OP_STRING)             \
-  template <typename DataT, El::Device D>                                      \
-  class OP_NAME##Operator final                                                \
-    : public Cloneable<OP_NAME##Operator<DataT, D>,                            \
-                       ElementwiseOperator<DataT, DataT, D>>                   \
-  {                                                                            \
-    using BaseType = Cloneable<OP_NAME##Operator<DataT, D>,                    \
-                               ElementwiseOperator<DataT, DataT, D>>;          \
-    using LocalInputTensorType = typename BaseType::LocalInputTensorType;      \
-    using LocalOutputTensorType = typename BaseType::LocalOutputTensorType;    \
-    using ConstLocalInputTensorType =                                          \
-      typename BaseType::ConstLocalInputTensorType;                            \
-    using ConstLocalOutputTensorType =                                         \
-      typename BaseType::ConstLocalOutputTensorType;                           \
-                                                                               \
-  public:                                                                      \
-    OP_NAME##Operator() = default;                                             \
-    OP_NAME##Operator(OP_NAME##Operator&&) = default;                          \
-    OP_NAME##Operator(OP_NAME##Operator const&) = default;                     \
-    OP_NAME##Operator& operator=(OP_NAME##Operator&&) = default;               \
-    OP_NAME##Operator& operator=(OP_NAME##Operator const&) = default;          \
-    ~OP_NAME##Operator() = default;                                            \
-    std::string get_type() const final { return OP_STRING; }                   \
-    template <typename ArchiveT>                                               \
-    void serialize(ArchiveT& ar)                                               \
-    {                                                                          \
-      using OperatorType = ElementwiseOperator<DataT, DataT, D>;               \
-      ar(::cereal::make_nvp("ElementwiseOperator",                             \
-                            ::cereal::base_class<OperatorType>(this)));        \
-    }                                                                          \
-                                                                               \
-  private:                                                                     \
-    void                                                                       \
-    fp_compute_local(std::vector<ConstLocalInputTensorType> inputs,            \
-                     std::vector<LocalOutputTensorType> outputs) const final;  \
-    void bp_compute_local(                                                     \
-      std::vector<ConstLocalInputTensorType> inputs,                           \
-      std::vector<ConstLocalOutputTensorType> grads_wrt_outputs,               \
-      std::vector<LocalInputTensorType> grads_wrt_inputs) const final;         \
-    void set_proto_params(lbann_data::Operator& msg) const final               \
-    {                                                                          \
-      msg.mutable_parameters()->PackFrom(lbann_data:: OP_NAME##Operator{}); \
-    }                                                                          \
-    void do_fill_description(description&) const final {}                      \
-  }
-
 // Logical operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(LogicalNot, "logical not");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(LogicalNot, "logical not");
 
 // Sign operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Negative, "negative");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Sign, "sign");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Negative, "negative");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Sign, "sign");
 
 // Rounding operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Round, "round");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Ceil, "ceil");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Floor, "floor");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Round, "round");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Ceil, "ceil");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Floor, "floor");
 
 // Power operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Reciprocal, "reciprocal");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Square, "square");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Sqrt, "square root");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Rsqrt, "reciprocal square root");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(SafeReciprocal, "safe reciprocal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Reciprocal, "reciprocal");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Square, "square");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Sqrt, "square root");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Rsqrt, "reciprocal square root");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(SafeReciprocal, "safe reciprocal");
 
 // Exponential and logarithmic operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Exp, "exponential");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Expm1, "expm1");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Log, "natural logarithm");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Log1p, "log1p");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Exp, "exponential");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Expm1, "expm1");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Log, "natural logarithm");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Log1p, "log1p");
 
 // Trigonometric operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Cos, "cosine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Sin, "sine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Tan, "tangent");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Acos, "arccosine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Asin, "arcsine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Atan, "arctangent");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Cos, "cosine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Sin, "sine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Tan, "tangent");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Acos, "arccosine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Asin, "arcsine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Atan, "arctangent");
 
 // Hyperbolic operations
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Cosh, "hyperbolic cosine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Sinh, "hyperbolic sine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Tanh, "hyperbolic tangent");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Acosh, "hyperbolic arccosine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Asinh, "hyperbolic arcsine");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Atanh, "hyperbolic arctangent");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Cosh, "hyperbolic cosine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Sinh, "hyperbolic sine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Tanh, "hyperbolic tangent");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Acosh, "hyperbolic arccosine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Asinh, "hyperbolic arcsine");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Atanh, "hyperbolic arctangent");
 
 // Error function
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(Erf, "error function");
-LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR(ErfInv, "inverse error function");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(Erf, "error function");
+LBANN_DECLARE_STATELESS_ELEMENTWISE_OPERATOR(ErfInv, "inverse error function");
 
 } // namespace lbann
-
-#undef LBANN_DECLARE_ENTRYWISE_UNARY_OPERATOR
 
 #endif // LBANN_INCLUDE_LBANN_OPERATORS_MATH_UNARY_HPP_INCLUDED
