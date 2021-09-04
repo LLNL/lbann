b8128fdf95df3afc88cf1e8468d8dd0a715dfb20
Katie Graham <graham63@llnl.gov>
2021-09-03 17:13:35 -0700

    minor fixes to copyright and include files

--- include/lbann/callbacks/export_onnx.hpp
+++ include/lbann/callbacks/export_onnx.hpp
@@ -1,3 +1,4 @@
+
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2014-2021, Lawrence Livermore National Security, LLC.
 // Produced at the Lawrence Livermore National Laboratory.
