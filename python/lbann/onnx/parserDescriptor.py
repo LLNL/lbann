290cc93c4fb0d309cc08fb5bd9f9c5235999f6cc
Tim Moon <moon13@llnl.gov>
2021-11-09 10:58:15 -0800

    Remove deprecated Python scripts for ONNX and plotting

--- python/lbann/onnx/parserDescriptor.py
+++
@@ -1,8 +0,0 @@
-def parserDescriptor(convertedLayers=[], arithmetic=False, stub=False):
-    def _parserDescriptor(cl):
-        cl.convertedLayers = convertedLayers
-        cl.arithmetic = arithmetic
-        cl.stub = stub
-        return cl
-
-    return _parserDescriptor
