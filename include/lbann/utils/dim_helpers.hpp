f6c87534284e84c6be3ad24f6255e4d848b09fbb
Thomas R. Benson <benson31@llnl.gov>
2021-11-22 15:23:29 -0500

    Tensor dims are explicitly created by the operator layer.

--- include/lbann/utils/dim_helpers.hpp
+++ include/lbann/utils/dim_helpers.hpp
@@ -28,37 +28,34 @@
 
 #include <lbann/utils/exception.hpp>
 
-namespace lbann
-{
+namespace lbann {
 
 template <typename T>
 auto get_linear_size(std::vector<T> const& dims)
 {
-  return (dims.size()
-          ? std::accumulate(begin(dims), end(dims), T(1), std::multiplies<T>())
-          : T(0));
+  return (
+    dims.size()
+      ? std::accumulate(begin(dims), end(dims), T(1), std::multiplies<T>())
+      : T(0));
 }
 
 template <typename T>
 auto get_linear_size(size_t ndims, T const* dims)
 {
   return (ndims
-          ? std::accumulate(dims, dims+ndims, T(1), std::multiplies<T>())
-          : T(0));
+            ? std::accumulate(dims, dims + ndims, T(1), std::multiplies<T>())
+            : T(0));
 }
-
 
 template <typename T>
 auto get_strides(size_t ndims, T const* dims, T const& lowest_stride)
 {
   std::vector<T> strides(ndims, lowest_stride);
-  if (ndims > 0)
-  {
-    for (size_t ii = ndims-1; ii != 0; --ii)
-    {
+  if (ndims > 0) {
+    for (size_t ii = ndims - 1; ii != 0; --ii) {
       if (dims[ii] == T(0))
-        LBANN_ERROR("Zero-sized dimension not allowed. Dims[",ii,"] = 0.");
-      strides[ii-1] = strides[ii]*dims[ii];
+        LBANN_ERROR("Zero-sized dimension not allowed. Dims[", ii, "] = 0.");
+      strides[ii - 1] = strides[ii] * dims[ii];
     }
   }
   return strides;
@@ -82,5 +79,46 @@ auto get_packed_strides(std::vector<T> const& dims)
   return get_strides(dims.size(), dims.data(), T(1));
 }
 
-}// namespace lbann
+namespace details {
+
+template <typename T, typename... ArgTs>
+std::enable_if_t<std::is_integral_v<T>>
+accumulate_dims(std::vector<size_t>& acc, T const& x, ArgTs&&... rest);
+
+template <typename T, typename... ArgTs>
+std::enable_if_t<std::is_integral_v<T>>
+accumulate_dims(std::vector<size_t>& acc,
+                std::vector<T> const& x,
+                ArgTs&&... rest);
+
+inline void accumulate_dims(std::vector<size_t>&) {}
+
+template <typename T, typename... ArgTs>
+std::enable_if_t<std::is_integral_v<T>>
+accumulate_dims(std::vector<size_t>& acc, T const& x, ArgTs&&... rest)
+{
+  acc.push_back(x);
+  accumulate_dims(acc, std::forward<ArgTs>(rest)...);
+}
+
+template <typename T, typename... ArgTs>
+std::enable_if_t<std::is_integral_v<T>>
+accumulate_dims(std::vector<size_t>& acc,
+                std::vector<T> const& x,
+                ArgTs&&... rest)
+{
+  acc.insert(end(acc), cbegin(x), cend(x));
+  accumulate_dims(acc, std::forward<ArgTs>(rest)...);
+}
+} // namespace details
+
+template <typename... ArgTs>
+std::vector<size_t> splice_dims(ArgTs&&... args)
+{
+  std::vector<size_t> dims;
+  details::accumulate_dims(dims, std::forward<ArgTs>(args)...);
+  return dims;
+}
+
+} // namespace lbann
 #endif // LBANN_UTILS_DIM_HELPERS_HPP_
