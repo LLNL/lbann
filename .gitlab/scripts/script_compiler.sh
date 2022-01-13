9433c810814c992dd5b115c4fce0199a6858ab88
Brian C. Van Essen <vanessen1@llnl.gov>
2022-01-12 17:06:16 -0800

    Cleaning up build command

--- .gitlab/scripts/script_compiler.sh
+++ .gitlab/scripts/script_compiler.sh
@@ -7,5 +7,5 @@
 #python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
 #salloc -N1 -t 600 python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
 export SPACK_ROOT=/g/g19/vanessen/spack.git; . $SPACK_ROOT/share/spack/setup-env.sh
-${CI_PROJECT_DIR}/scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy
+${CI_PROJECT_DIR}/scripts/build_lbann.sh -d -l bamboo --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy
 exit 0
