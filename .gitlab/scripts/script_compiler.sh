74261e68ef49085c2c9b59120889d6e984697c21
Brian C. Van Essen <vanessen1@llnl.gov>
2022-01-12 16:36:35 -0800

    Testing spack build on Ray

--- .gitlab/scripts/script_compiler.sh
+++ .gitlab/scripts/script_compiler.sh
@@ -1,7 +1,11 @@
 # source /etc/bashrc
 
 #!/bin/bash -l
-module load cmake/3.9.2
-salloc -N1 -t 600 python -m pytest -s --junitxml=results.xml
+#module load cmake/3.9.2
+#salloc -N1 -t 600 python -m pytest -s --junitxml=results.xml
+#cd bamboo/compiler_tests
+#python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
 #salloc -N1 -t 600 python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
+spack_LBANN
+./scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy
 exit 0
