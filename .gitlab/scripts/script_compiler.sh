884631e45c2c63004dc7ac60a606ba4716343eaf
Brian C. Van Essen <vanessen1@llnl.gov>
2022-01-12 16:47:14 -0800

    Fixed spack setup

--- .gitlab/scripts/script_compiler.sh
+++ .gitlab/scripts/script_compiler.sh
@@ -6,6 +6,6 @@
 #cd bamboo/compiler_tests
 #python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
 #salloc -N1 -t 600 python -m pytest -s test_compiler.py -k 'test_compiler_build_script' --junitxml=results.xml
-spack_LBANN
+export SPACK_ROOT=/g/g19/vanessen/spack.git; . $SPACK_ROOT/share/spack/setup-env.sh
 ../scripts/build_lbann.sh -d -l bamboo-%s --test --clean-build -j $(($(nproc)+2)) -- +deterministic +vision +numpy
 exit 0
