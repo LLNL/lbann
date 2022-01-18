1e69fc8b2c118ecc3c55ee91529c8ef562313322
Michael Wyatt <wyatt5@llnl.gov>
2022-01-18 13:22:09 -0800

    synthetic dataset is now 1024 samples

--- applications/nlp/RoBERTa/dataset.py
+++ applications/nlp/RoBERTa/dataset.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+data = np.random.randint(50265, size=(1024,513))
+data[:,0] = data[:,0] % 10
+
 def get_sample(i):
-    vals = range(513)
-    return np.array(vals).flatten().astype(np.float64)
+    vals = data[i]
+    return vals.flatten().astype(np.float32)
 
 def num_samples():
-    return 1
+    return 1024
 
 def sample_dims():
     return (513,)
