db33e9a654eddda307395087ed098541cfd5fb41
Tim Moon <moon13@llnl.gov>
2022-01-18 16:20:20 -0800

    Add PyTest tests for sub-grid-parallel split, sum, slice, concat layers

--- bamboo/unit_tests/test_unit_subgrid_concat.py
+++ bamboo/unit_tests/test_unit_subgrid_concat.py
@@ -7,7 +7,6 @@ import numpy as np
 
 # Bamboo utilities
 current_file = os.path.realpath(__file__)
-print("Current file:",current_file, __file__)
 current_dir = os.path.dirname(current_file)
 sys.path.insert(0, os.path.join(os.path.dirname(current_dir), 'common_python'))
 import tools
@@ -19,11 +18,9 @@ import tools
 # the functions below to ingest data.
 
 # Data
-np.random.seed(2019102455)
-_num_samples = 128
-_sample_size = 64
-_sample_dims = (2,2,16)
-_sample_size = functools.reduce(operator.mul, _sample_dims)
+np.random.seed(202201181)
+_num_samples = 19
+_sample_size = 5
 _samples = np.random.normal(size=(_num_samples,_sample_size)).astype(np.float32)
 
 # Sample access functions
@@ -45,7 +42,6 @@ def setup_experiment(lbann):
         lbann (module): Module for LBANN Python frontend
 
     """
-    assert False, 'Temporarily disabled' ### @todo Remove
     mini_batch_size = num_samples() // 2
     trainer = lbann.Trainer(mini_batch_size)
     model = construct_model(lbann)
@@ -68,9 +64,9 @@ def construct_model(lbann):
                               initializer=lbann.ConstantInitializer(value=0.0),
                               name='input_weights')
     x = lbann.Sum(lbann.Reshape(lbann.Input(data_field='samples'),
-                                dims=tools.str_list(_sample_dims)),
+                                dims=tools.str_list(_sample_size)),
                   lbann.WeightsLayer(weights=x_weights,
-                                     dims=tools.str_list(_sample_dims)))
+                                     dims=tools.str_list(_sample_size)))
     x_lbann = x
 
     # Objects for LBANN model
@@ -78,58 +74,34 @@ def construct_model(lbann):
     metrics = []
     callbacks = []
 
-    branches = 2
-
     # ------------------------------------------
-    # Data-parallel layout
+    # LBANN implementation
     # ------------------------------------------
 
     # LBANN implementation
-    x = x_lbann
-    y = lbann.Identity(x, data_layout='data_parallel')
-
-    slice_points = (0, 8, 16)
-    x_slice = lbann.Slice(x, axis=2, slice_points=tools.str_list(slice_points),parallel_strategy = {'sub_branch_tag':0,'enable_subgraph':True})
-
-    branch1 = lbann.Identity(x_slice, data_layout='data_parallel',parallel_strategy = {'sub_branch_tag':1,'enable_subgraph':True})
-    branch2 = lbann.Identity(x_slice, data_layout='data_parallel',parallel_strategy = {'sub_branch_tag':2,'enable_subgraph':True})
-
-    grid_slice = lbann.Cross_Grid_Sum_Slice([branch1,branch2])
-
-    branch1 = lbann.Identity(grid_slice)
-    branch2 = lbann.Identity(grid_slice)
-
-    sum_branch = lbann.Sum([branch1,branch2],parallel_strategy = {'sub_branch_tag':0,'enable_subgraph':True})
-    z = lbann.L2Norm2(sum_branch)
+    ### @todo Layers with optimized inter-grid communication
+    x = lbann.Identity(x_lbann)
+    y1 = lbann.Sin(x, parallel_strategy = {'grid_tag':1})
+    y2 = lbann.Cos(x, parallel_strategy = {'grid_tag':2})
+    y = lbann.Concatenation(
+        y1,
+        y2,
+        parallel_strategy = {'grid_tag':0})
+    z = lbann.L2Norm2(y)
     obj.append(z)
-    metrics.append(lbann.Metric(z, name='data-parallel layout'))
+    metrics.append(lbann.Metric(z, name='obj'))
 
+    # ------------------------------------------
     # NumPy implementation
+    # ------------------------------------------
+
     vals = []
     for i in range(num_samples()):
-        x = get_sample(i).reshape(_sample_dims).astype(np.float64)
-        y = []
-
-        cross_sum = 0
-        for j in range(len(slice_points)-1):
-            x_slice = x[:,:,slice_points[j]:slice_points[j+1]]
-
-            if(j==0):
-                cross_sum = x_slice
-            else:
-                cross_sum += x_slice
-
-        last_dim = int(_sample_dims[-1]/(branches*branches))
-        sum_slices = None
-        for j in range(branches):
-            if(j==0):
-                sum_slices = cross_sum[:,:,:last_dim]
-            else:
-                sum_slices += cross_sum[:,:,j*last_dim:(j+1)*last_dim]
-
-        z = tools.numpy_l2norm2(sum_slices)
+        x = get_sample(i).astype(np.float64)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+        z = tools.numpy_l2norm2(y1) + tools.numpy_l2norm2(y2)
         vals.append(z)
-
     val = np.mean(vals)
     tol = 8 * val * np.finfo(np.float32).eps
     callbacks.append(lbann.CallbackCheckMetric(
@@ -138,8 +110,6 @@ def construct_model(lbann):
         upper_bound=val+tol,
         error_on_failure=True,
         execution_modes='test'))
-
-
 
     # ------------------------------------------
     # Gradient checking
@@ -152,8 +122,7 @@ def construct_model(lbann):
     # ------------------------------------------
 
     num_epochs = 0
-
-    return lbann.Model(num_epochs,subgraph_communication=lbann.SubgraphCommunication.COLL_OPT,
+    return lbann.Model(num_epochs,
                        layers=lbann.traverse_layer_graph(x_lbann),
                        objective_function=obj,
                        metrics=metrics,
@@ -200,5 +169,8 @@ def construct_data_reader(lbann):
 # ==============================================
 
 # Create test functions that can interact with PyTest
-for _test_func in tools.create_tests(setup_experiment, __file__):
+for _test_func in tools.create_tests(
+        setup_experiment,
+        __file__,
+        environment={'LBANN_NUM_SUBGRIDS': 2}):
     globals()[_test_func.__name__] = _test_func
