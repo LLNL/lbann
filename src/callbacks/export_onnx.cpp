38484292ba4da761f830daad2cd66527493f82d0
Thomas R. Benson <benson31@llnl.gov>
2022-02-24 13:50:54 -0500

    Clang-format and quick fix for parallel copy to CIRC,CIRC

--- src/callbacks/export_onnx.cpp
+++ src/callbacks/export_onnx.cpp
@@ -39,7 +39,6 @@
 #include <iostream>
 #include <string>
 
-
 namespace lbann {
 namespace callback {
 
@@ -67,62 +66,63 @@ void export_onnx::on_train_begin(model* m)
   auto* gp = mp_.mutable_graph();
   gp->set_name(m->get_name());
 
-  auto rank = m->get_comm()->get_rank_in_trainer();
-  if( rank == 0 ) {
-    auto const weights_vec = m->get_weights();
-    for (auto const weights : weights_vec) {
-      auto* initializer = gp->add_initializer();
-      auto dims = weights->get_dims();
-      for (auto const dim : dims) {
-        initializer->add_dims(dim);
-      }
-      const auto& values = dynamic_cast<El::AbstractDistMatrix<DataType>&>(
-        weights->get_values());
+  auto const rank = m->get_comm()->get_rank_in_trainer();
 
-      El::DistMatrix<DataType, El::CIRC, El::CIRC, El::ELEMENT,
-                     El::Device::CPU> tmp(values.Grid(), 0);
-
-      El::Copy(values, tmp);
-
-      if( tmp.CrossRank() == tmp.Root()) {
-        auto const& local = tmp.LockedMatrix();
-        auto const mat_height = tmp.Height();
-        auto const mat_width = tmp.Width();
-
-        if (sizeof(DataType) == 4 || sizeof(DataType) == 2 ) {
-          initializer->set_data_type(onnx::TensorProto::FLOAT);
-          for (auto col = decltype(mat_width){0}; col < mat_width; ++col) {
-            for (auto row = decltype(mat_height){0}; row < mat_height; ++row) {
-              initializer->add_float_data(local.CRef(row,col));
-            }
-          }
-        }
-        else if (sizeof(DataType) == 8) {
-          initializer->set_data_type(onnx::TensorProto::DOUBLE);
-          for (auto col = decltype(mat_width){0}; col < mat_width; ++col) {
-            for (auto row = decltype(mat_height){0}; row < mat_height; ++row) {
-              initializer->add_double_data(local.CRef(row,col));
-            }
-          }
-        }
-        else
-          LBANN_ERROR("Unsupported DataType. Export onnx callback supports float, double, and half.");
-      }
-      initializer->set_name(weights->get_name());
-      initializer->set_doc_string(weights->get_name());
+  auto const weights_vec = m->get_weights();
+  for (auto const weights : weights_vec) {
+    auto* initializer = gp->add_initializer();
+    auto dims = weights->get_dims();
+    for (auto const dim : dims) {
+      initializer->add_dims(dim);
     }
+    const auto& values =
+      dynamic_cast<El::AbstractDistMatrix<DataType>&>(weights->get_values());
+
+    El::DistMatrix<DataType, El::CIRC, El::CIRC, El::ELEMENT, El::Device::CPU>
+      tmp(values.Grid(), 0);
+
+    El::Copy(values, tmp);
+
+    if (tmp.CrossRank() == tmp.Root()) {
+      auto const& local = tmp.LockedMatrix();
+      auto const mat_height = tmp.Height();
+      auto const mat_width = tmp.Width();
+
+      if constexpr (std::is_same_v<DataType, float>) {
+        initializer->set_data_type(onnx::TensorProto::FLOAT);
+        for (auto col = decltype(mat_width){0}; col < mat_width; ++col) {
+          for (auto row = decltype(mat_height){0}; row < mat_height; ++row) {
+            initializer->add_float_data(local.CRef(row, col));
+          }
+        }
+      }
+      else if constexpr (std::is_same_v<DataType, double>) {
+        initializer->set_data_type(onnx::TensorProto::DOUBLE);
+        for (auto col = decltype(mat_width){0}; col < mat_width; ++col) {
+          for (auto row = decltype(mat_height){0}; row < mat_height; ++row) {
+            initializer->add_double_data(local.CRef(row, col));
+          }
+        }
+      }
+      else
+        LBANN_ERROR("Unsupported DataType. Export onnx callback supports "
+                    "float and double.");
+    }
+    initializer->set_name(weights->get_name());
+    initializer->set_doc_string(weights->get_name());
   }
+
   auto const layers = m->get_layers();
   for (auto const* layer : layers) {
     layer->fill_onnx_node(*gp);
   }
   gp->set_doc_string(m->get_name());
 
-  if(rank == 0) {
+  if (rank == 0) {
     std::ofstream onnx_out(m_output_file);
     mp_.SerializeToOstream(&onnx_out);
 
-    if(m_print_debug_string) {
+    if (m_print_debug_string) {
       std::ofstream debug("onnx_debug.txt");
       debug << mp_.DebugString();
     }
@@ -130,9 +130,9 @@ void export_onnx::on_train_begin(model* m)
 }
 
 std::unique_ptr<callback_base>
-build_export_onnx_callback_from_pbuf(
-  const google::protobuf::Message& proto_msg,
-  const std::shared_ptr<lbann_summary>&) {
+build_export_onnx_callback_from_pbuf(const google::protobuf::Message& proto_msg,
+                                     const std::shared_ptr<lbann_summary>&)
+{
   const auto& params =
     dynamic_cast<const lbann_data::Callback::CallbackExportOnnx&>(proto_msg);
   return std::make_unique<export_onnx>(
@@ -141,5 +141,5 @@ build_export_onnx_callback_from_pbuf(
      ? std::string("lbann_output.onnx")
      : params.output_file()));
 }
-}// namespace callback
-}// namespace lbann
+} // namespace callback
+} // namespace lbann
