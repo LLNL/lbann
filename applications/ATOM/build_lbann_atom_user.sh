6e1e05ede23175bf53b006aec32fa5424801cab0
Brian C. Van Essen <vanessen1@llnl.gov>
2022-03-08 17:21:57 -0800

    Fixed name of requirements.txt file

--- applications/ATOM/build_lbann_atom_user.sh
+++ applications/ATOM/build_lbann_atom_user.sh
@@ -26,6 +26,6 @@ SPACK_ARCH_TARGET=$(spack arch -t)
 set_center_specific_variants ${CENTER} ${SPACK_ARCH_TARGET}
 
 #CMD="./build_lbann.sh -j $(($(nproc)+2)) -d -l atom -s -u develop -e external_packages_atom.sh -- ${CENTER_USER_VARIANTS}"
-CMD="./build_lbann.sh -j $(($(nproc)+2)) -d -l atom -s -u develop --pip requirements -p rdkit -- ${CENTER_USER_VARIANTS}"
+CMD="./build_lbann.sh -j $(($(nproc)+2)) -d -l atom -s -u develop --pip requirements.txt -p rdkit -- ${CENTER_USER_VARIANTS}"
 echo ${CMD}
 ${CMD}
