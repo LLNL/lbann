9ad7f205afd41411ff4dd0a9c99c72323df53e79
Brian C. Van Essen <vanessen1@llnl.gov>
2022-03-08 16:42:21 -0800

    Temporarily overriding the path to the LBANN repo to the user and ATOM install scripts

--- scripts/.build_lbann_as_user.sh
+++ scripts/.build_lbann_as_user.sh
@@ -1,4 +1,5 @@
-LBANN_REPO_PATH="https://github.com/LLNL/lbann/raw/develop"
+#LBANN_REPO_PATH="https://github.com/LLNL/lbann/raw/develop"
+LBANN_REPO_PATH="https://github.com/bvanessen/lbann/raw/feature_add_pip_deps"
 curl -fsSL -O ${LBANN_REPO_PATH}/scripts/build_lbann.sh
 curl -fsSL -O ${LBANN_REPO_PATH}/scripts/customize_build_env.sh
 curl -fsSL -O ${LBANN_REPO_PATH}/scripts/utilities.sh
