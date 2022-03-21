afd93454f50fc53e1059a9d5c67f05061a248558
Brian C. Van Essen <vanessen1@llnl.gov>
2022-03-21 12:16:31 -0700

    Updated LTFB test with weekly parameter

--- ci_test/unit_tests/test_unit_callback_ltfb.py
+++ ci_test/unit_tests/test_unit_callback_ltfb.py
@@ -173,10 +173,10 @@ def augment_test_func(test_func):
     test_name = test_func.__name__
 
     # Define test function
-    def func(cluster, dirname):
+    def func(cluster, dirname, weekly):
 
         # Run LBANN experiment
-        experiment_output = test_func(cluster, dirname)
+        experiment_output = test_func(cluster, dirname, weekly)
 
         # Parse LBANN log file
         num_trainers = None
