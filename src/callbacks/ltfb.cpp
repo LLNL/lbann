ed7970e210870259206ad7d115150a59a3ce8abe
Thomas R. Benson <benson31@llnl.gov>
2022-03-28 15:52:02 -0400

    Apply the same fix to the LTFB callback.

--- src/callbacks/ltfb.cpp
+++ src/callbacks/ltfb.cpp
@@ -256,6 +256,12 @@ void restore_model_weights(
 std::string sendrecv_string(lbann_comm const& c, std::string const& src,
                             El::Int partner_trainer)
 {
+#ifdef LBANN_HAS_ALUMINUM
+  El::mpi::EnsureComm<size_t, El::Collective::SENDRECV>(
+    c.get_world_comm(),
+    El::SyncInfo<El::Device::CPU>{});
+#endif
+
   if (!c.am_trainer_master())
     return "";
 
