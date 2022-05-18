9f0aba67b296450e19c7394a0a23563cd7a125c8
Sam Ade Jacobs <jacobs32@llnl.gov>
2022-05-17 22:40:43 -0700

    Code refactor for integration test, support for interactive allocation, additional code clean up

--- applications/nas/nasnet/cifar_search.py
+++ applications/nas/nasnet/cifar_search.py
@@ -1,15 +1,8 @@
-# NASNet Search Space https://arxiv.org/pdf/1707.07012.pdf
-# code modified from DARTS https://github.com/quark0/darts
 import numpy as np
 import sys
 import os
 import time
-from collections import namedtuple
 import lbann
-import lbann.models
-import lbann.models.resnet
-import data.cifar10
-from search import micro_encoding
 import argparse
 import lbann.contrib.args
 import lbann.contrib.launcher
@@ -17,38 +10,8 @@ from os.path import join
 import subprocess
 
 sys.path.insert(0, os.getenv('PWD'))
-import search.model as cifar
+import cifar_networks
 
-
-Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
-Genotype_norm = namedtuple('Genotype', 'normal normal_concat')
-Genotype_redu = namedtuple('Genotype', 'reduce reduce_concat')
-
-# what you want to search should be defined here and in micro_operations
-PRIMITIVES = [
-    'max_pool_3x3',
-    'avg_pool_3x3',
-    'skip_connect',
-    'sep_conv_3x3',
-    'sep_conv_5x5',
-    'dil_conv_3x3',
-    'dil_conv_5x5',
-    'sep_conv_7x7',
-    'conv_7x1_1x7',
-]
-'''
-
-PRIMITIVES = [
-    'none',
-    'max_pool_3x3',
-    'avg_pool_3x3',
-    'skip_connect',
-    'sep_conv_3x3',
-    'sep_conv_5x5',
-    'dil_conv_3x3',
-    'dil_conv_5x5',
-]
-'''
 
 # ----------------------------------
 # Command-line arguments
@@ -59,9 +22,6 @@ parser = argparse.ArgumentParser(description=desc)
 #lbann.contrib.args.add_scheduler_arguments(parser)
 
 #NAS parameters
-parser.add_argument(
-    '--pop-size', action='store', default=16, type=int,
-    help='Population size (default: 16)')
 parser.add_argument(
     '--num-blocks', action='store', default=5, type=int,
     help='Number of blocks per cell (default: 5)')
@@ -80,16 +40,16 @@ parser.add_argument(
     '--mini-batch-size', action='store', default=64, type=int,
     help='mini-batch size (default: 64)', metavar='NUM')
 parser.add_argument(
-    '--num-epochs', action='store', default=200, type=int,
-    help='number of epochs (default: 10)', metavar='NUM')
+    '--num-epochs', action='store', default=20, type=int,
+    help='number of epochs (default: 20)', metavar='NUM')
 
 #Compute (job) parameters
 parser.add_argument(
     '--nodes', action='store', default=4, type=int,
     help='Num of compute nodes (default: 4)')
 parser.add_argument(
-    '--ppn', action='store', default=4, type=int,
-    help='Processes per node (default: 4)')
+    '--ppn', action='store', default=2, type=int,
+    help='Processes per node (default: 2)')
 parser.add_argument("--ppt", type=int, default=2) 
 parser.add_argument(
     '--job-name', action='store', default='denas_cifar10', type=str,
@@ -101,112 +61,7 @@ parser.add_argument(
 lbann.contrib.args.add_optimizer_arguments(parser, default_learning_rate=0.1)
 args = parser.parse_args()
 
-
-def generate(expd):
-    # design to debug the encoding scheme
-    seed = 0
-    np.random.seed(seed)
-    pop_size = int(args.nodes*args.ppn/args.ppt)
-    #B, n_ops, n_cell = 5, 7, 2
-    B, n_ops, n_cell = args.num_blocks, args.n_ops, args.n_cell
-    networks = []
-    network_id = 0
-        
-    # Setup data reader
-    data_reader = data.cifar10.make_data_reader(num_classes=10)
-       
-    while len(networks) < pop_size:
-        bit_string = []
-        for c in range(n_cell):
-            for b in range(B):
-                bit_string += [np.random.randint(n_ops),
-                               np.random.randint(b + 2),
-                               np.random.randint(n_ops),
-                               np.random.randint(b + 2)
-                               ]
-
-        genome = micro_encoding.convert(bit_string)
-        # check against evaluated networks in case of duplicates
-        doTrain = True
-        for network in networks:
-            if micro_encoding.compare(genome, network):
-                doTrain = False
-                break
-
-        if doTrain:
-            genotype = micro_encoding.decode(genome)
-            print("Newtwork id, bitstring, genome, genotype ", network_id, bit_string, genome, genotype)
-            mymodel = cifar.NetworkCIFAR(16, 10, 8, False, genotype)
-            networks.append(genome)
-
-            images = lbann.Input(data_field='samples')
-            labels = lbann.Input(data_field='labels')
-
-            preds,_ = mymodel(images)
-            probs = lbann.Softmax(preds)
-            cross_entropy = lbann.CrossEntropy(probs, labels)
-            top1 = lbann.CategoricalAccuracy(probs, labels)
-
-            obj = lbann.ObjectiveFunction([cross_entropy])
     
-
-            metrics = lbann.Metric(top1, name='accuracy', unit='%')
-
-            callbacks = [lbann.CallbackPrint(),
-                         lbann.CallbackTimer()]
-
-
-            model = lbann.Model(epochs=args.num_epochs,
-                       layers=[images,labels],
-                       objective_function=obj,
-                       metrics=metrics,
-                       callbacks=callbacks)
-
-            opt = lbann.contrib.args.create_optimizer(args)
-
-            # Setup trainer
-            trainer = lbann.Trainer(mini_batch_size=args.mini_batch_size)
-            
-            if args.use_ltfb:
-                print("Using LTFB ")
-                SGD = lbann.BatchedIterativeOptimizer
-                RPE = lbann.RandomPairwiseExchange
-                ES = lbann.RandomPairwiseExchange.ExchangeStrategy(strategy='checkpoint_binary')
-                #            checkpoint_dir=os.getcwd())
-                metalearning = RPE(
-                   metric_strategies={'accuracy': RPE.MetricStrategy.HIGHER_IS_BETTER},
-                   exchange_strategy=ES)  
-                ltfb = lbann.LTFB("ltfb",
-                              metalearning=metalearning,
-                              local_algo=SGD("local sgd", num_iterations=625), 
-                              #local_algo=SGD("local sgd", num_iterations=62),
-                              metalearning_steps=args.num_epochs)
-
-                trainer = lbann.Trainer(mini_batch_size=args.mini_batch_size,
-                                    training_algo=ltfb)
-
-            
-    
-            # Run LBANN
-            try:
-                lbann.contrib.launcher.run(trainer=trainer,
-                    model=model,
-                    time_limit=360,
-                    account='hpcml',
-                    experiment_dir=expd,
-                    nodes=args.nodes,
-                    procs_per_node=args.ppn,
-                    data_reader=data_reader,
-                    optimizer=opt,
-                    setup_only=True,
-                    proto_file_name="experiment.prototext.trainer"+str(network_id),
-                    lbann_args=f"--generate_multi_proto --procs_per_trainer={args.ppt}",
-                    job_name=args.job_name)
-            except:
-                pass
-            
-            network_id += 1
-
 
 if __name__ == "__main__":
     tag = 'ltfb' if args.use_ltfb else 'random'
@@ -214,8 +69,31 @@ if __name__ == "__main__":
     if not os.path.exists(expd):
         os.mkdir(expd)
     print('Experiment dir : {}'.format(expd))
-    generate(expd)
+
+    script = lbann.launcher.make_batch_script(nodes=args.nodes,
+                                              procs_per_node=args.ppn,
+                                              experiment_dir=expd)
+    pop_size = int(args.nodes*args.ppn/args.ppt)
+
+    cifar_networks.create_networks(expd,
+                    args.num_epochs,
+                    args.mini_batch_size,
+                    pop_size,
+                    use_ltfb=args.use_ltfb,
+                    num_blocks=args.num_blocks,
+                    num_ops=args.n_ops,
+                    num_cells=args.n_cell)
+
+    proto_file = os.path.join(script.work_dir,'experiment.prototext.trainer0')
+    command = [
+                lbann.lbann_exe(),
+                f'--procs_per_trainer={args.ppt}',
+                '--generate_multi_proto',
+                f'--prototext={proto_file}']
+    script.add_parallel_command(command)
     
+    # Run script
+    script.run(True)
     
     
                      
