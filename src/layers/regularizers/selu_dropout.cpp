e56b65f7e72445c74658031a8617e4f3dbf0871c
Katie Graham <graham63@llnl.gov>
2022-06-21 13:01:36 -0700

    Added scale and alpha_prime to serialize lbann in selu dropout

--- src/layers/regularizers/selu_dropout.cpp
+++ src/layers/regularizers/selu_dropout.cpp
@@ -134,10 +134,8 @@ void selu_dropout<T,L,D>::write_specific_proto(lbann_data::Layer& proto) const {
   proto.set_datatype(proto::ProtoDataType<T>);
   auto* msg = proto.mutable_selu_dropout();
   msg->set_keep_prob(m_keep_prob);
-  //FIXME(KLG): This is wrong (Tom's problem)
-  //msg->set_alpha(m_alpha_prime);
-  //FIXME(KLG): How do I get this? (Tom's problem)
-  //msg->set_scale(scale);
+  msg->set_alpha(-m_alpha_prime);
+  msg->set_scale(El::To<T>(1));
 }
 
 template <typename T, data_layout L, El::Device D>
