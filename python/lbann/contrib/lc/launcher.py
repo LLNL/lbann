72db0be80559d0ebc7733225f8354887062aa624
Brian C. Van Essen <vanessen1@llnl.gov>
2022-09-01 13:42:01 -0700

    Removed OpenMPI flags that are no longer necessary with TCE OpenMPI module.

--- python/lbann/contrib/lc/launcher.py
+++ python/lbann/contrib/lc/launcher.py
@@ -80,9 +80,9 @@ def make_batch_script(
     set_environment('MV2_USE_RDMA_CM', 0)
 
     # Optimizations for Corona
-    if system in ('corona'):
-        set_environment('OMPI_MCA_btl', '^openib')
-        set_environment('OMPI_MCA_osc', 'ucx')
+    # if system in ('corona'):
+    #     set_environment('OMPI_MCA_btl', '^openib')
+    #     set_environment('OMPI_MCA_osc', 'ucx')
 
     # Optimizations for Sierra-like systems
     if system in ('sierra', 'lassen', 'rzansel'):
