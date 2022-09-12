725c3e896ad45e96278ad5699a90f7a90d210983
Brian C. Van Essen <vanessen1@llnl.gov>
2022-09-12 14:20:58 -0700

    Debugging

--- .gitlab/common/run-catch-tests-flux.sh
+++ .gitlab/common/run-catch-tests-flux.sh
@@ -53,7 +53,7 @@ LBANN_HASH=$(spack find --format {hash:7} lbann@${SPACK_ENV_NAME}-${SPACK_ARCH_T
 SPACK_BUILD_DIR="spack-build-${LBANN_HASH}"
 cd ${SPACK_BUILD_DIR}
 flux proxy ${JOB_ID} flux mini run -N 1 -n 1 -g 1 -t 5m \
-     . /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
+     source /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
      spack env activate -p lbann-${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}; \
      ./unit_test/seq-catch-tests \
      -r JUnit \
@@ -68,7 +68,7 @@ LBANN_NNODES=$(flux jobs -no {id}:{name}:{nnodes} | grep ${JOB_NAME} | awk -F: '
 flux proxy ${JOB_ID} flux mini run \
      -N ${LBANN_NNODES} -n $(($TEST_TASKS_PER_NODE * ${LBANN_NNODES})) \
      -g 1 -t 5m -o gpu-affinity=per-task -o cpu-affinity=per-task \
-     . /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
+     source /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
      spack env status; \
      spack env activate -p lbann-${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}; \
      spack env status; \
@@ -84,7 +84,7 @@ fi
 flux proxy ${JOB_ID} flux mini run \
      -N ${LBANN_NNODES} -n $(($TEST_TASKS_PER_NODE * ${LBANN_NNODES})) \
      -g 1 -t 5m -o gpu-affinity=per-task -o cpu-affinity=per-task \
-     . /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
+     source /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
      spack env status; \
      spack env activate -p lbann-${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}; \
      spack env status; \
