1fb6f747332ebc4d1ae9f9c9fc0806814fae50bf
Shehtab Zaman <szaman5@binghamton.edu>
2022-12-14 15:36:12 -0800

    Passing unit tests

--- ci_test/unit_tests/test_unit_layer_batched_matmul_distconv.py
+++ ci_test/unit_tests/test_unit_layer_batched_matmul_distconv.py
@@ -225,7 +225,7 @@ def construct_model(lbann):
     # Gradient checking
     # ------------------------------------------
 
-    # callbacks.append(lbann.CallbackCheckGradients(error_on_failure=True))
+    callbacks.append(lbann.CallbackCheckGradients(error_on_failure=True))
 
     # ------------------------------------------
     # Construct model
