ac8b6c0d3ddafb4f0ab90386947338ec55318aec
Brian C. Van Essen <vanessen1@llnl.gov>
2023-02-16 00:48:20 -0800

    Added comm.hpp to the activations.

--- src/layers/activations/activation_layer_builders.cpp
+++ src/layers/activations/activation_layer_builders.cpp
@@ -24,6 +24,7 @@
 // permissions and limitations under the license.
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "lbann/comm.hpp"
 #include "lbann/layers/activations/activation_layer_builders.hpp"
 
 #include "lbann/layers/activations/elu.hpp"
