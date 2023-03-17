d89c6070007fed118afd4a6cecbb091bfa70408d
Tal Ben-Nun <tbennun@users.noreply.github.com>
2023-03-16 22:27:30 -0700

    Add contributor guidelines and pre-commit hooks (#2197)
    * Add contributor guidelines and pre-commit hooks
    
    * Switch to local clang-format hook
    
    * Remove proto formatting
    
    * Switch from pre-commit to a simple script
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Brian Van Essen <vanessen1@llnl.gov>

---
+++ scripts/pre-commit-hook.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+CLANG_FORMAT="${CLANG_FORMAT:-$(which clang-format)}"
+
+# Test for modified files
+echo -n "Testing modified files... "
+files=$(git diff --cached --name-only --diff-filter=ACM | grep -E "(pp|cu|proto)$")
+if [ "$files" == "" ]; then
+    echo "No files to test."
+    exit 0
+fi
+
+if ! [ -x "$CLANG_FORMAT" ]; then
+    echo "clang-format not found (CLANG_FORMAT=${CLANG_FORMAT}). Either install it or set the CLANG_FORMAT environment variable to its path"
+    exit 1
+fi
+
+# Run clang-format
+olddiff=$(git diff)
+$CLANG_FORMAT -i $files
+newdiff=$(git diff)
+
+# Test if diff changed
+if [ "$olddiff" != "$newdiff" ]; then
+    echo "Files were modified, re-commit."
+    exit 1
+fi
+echo "Success!"
