a726db845c09216031a17c6c6019221a50cd7386
Brian C. Van Essen <vanessen1@llnl.gov>
2023-03-17 10:52:11 -0700

    Fixed the test_catch2_unit_tests.py file so that it executes from the build directory, which allows it to find local files that are required for the catch2 testing.  Additionally, changed how the return code is checked and avoid looking for error files that aren't created.

--- ci_test/unit_tests/test_catch2_unit_tests.py
+++ ci_test/unit_tests/test_catch2_unit_tests.py
@@ -51,10 +51,10 @@ def test_run_sequential_catch_tests(cluster, dirname):
     seq_launch = get_system_seq_launch(cluster)
     seq_output_file_name = 'seq_catch_tests_output-%s.xml' % (cluster)
     seq_output_file = os.path.join(output_dir, seq_output_file_name)
-    seq_error_file = os.path.join(output_dir, "error", "seq-catch-test-error.log")
     seq_catch_args = [seq_catch_exe, '-r', 'junit', '-o', seq_output_file]
-    output = sp.run(seq_launch + seq_catch_args)
-    tools.assert_success(output.returncode, seq_error_file)
+    output = sp.run(seq_launch + seq_catch_args, cwd=build_dir)
+    if output.returncode != 0:
+        raise AssertionError('return_code={%d}' % output.returncode)
 
 def test_run_parallel_catch_tests(cluster, dirname):
     if cluster != 'lassen':
@@ -71,10 +71,10 @@ def test_run_parallel_catch_tests(cluster, dirname):
     mpi_launch = get_system_mpi_launch(cluster)
     mpi_output_file_name = 'mpi_catch_tests_output-%s-rank=%%r-size=%%s.xml' % (cluster)
     mpi_output_file = os.path.join(output_dir, mpi_output_file_name)
-    mpi_error_file = os.path.join(output_dir, "error", "mpi-catch-test-error.log")
     mpi_catch_args = [mpi_catch_exe, '-r', 'junit', '-o', mpi_output_file]
-    output = sp.run(mpi_launch + mpi_catch_args)
-    tools.assert_success(output.returncode, mpi_error_file)
+    output = sp.run(mpi_launch + mpi_catch_args, cwd=build_dir)
+    if output.returncode != 0:
+        raise AssertionError('return_code={%d}' % output.returncode)
 
 def test_run_parallel_filesystem_catch_tests(cluster, dirname):
     if cluster != 'lassen':
@@ -91,7 +91,7 @@ def test_run_parallel_filesystem_catch_tests(cluster, dirname):
     mpi_launch = get_system_mpi_launch(cluster)
     mpi_output_file_name = 'mpi_filesystem_catch_tests_output-%s-rank=%%r-size=%%s.xml' % (cluster)
     mpi_output_file = os.path.join(output_dir, mpi_output_file_name)
-    mpi_error_file = os.path.join(output_dir, "error", "mpi-filesystem-catch-test-error.log")
     mpi_catch_args = [mpi_catch_exe, '"[filesystem]"', '-r', 'junit', '-o', mpi_output_file]
-    output = sp.run(mpi_launch + mpi_catch_args)
-    tools.assert_success(output.returncode, mpi_error_file)
+    output = sp.run(mpi_launch + mpi_catch_args, cwd=build_dir)
+    if output.returncode != 0:
+        raise AssertionError('return_code={%d}' % output.returncode)
