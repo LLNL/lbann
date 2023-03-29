830e24a4043bd4ee47d4bb0c23e3b4132c60950b
Pier Fiedorowicz <fiedorowicz1@llnl.gov>
2023-03-29 08:57:38 -0700

    Update path for cached huggingface wmt16 dataset

--- applications/nlp/utils/paths.py
+++ applications/nlp/utils/paths.py
@@ -18,8 +18,19 @@ def root_dir():
     """Root directory for LBANN NLP application."""
     return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+def wmt_dir():
+    """Data directory for the WMT 2016 dataset.
 
-def wmt_dir(system=system()):
+    See https://huggingface.co/datasets/wmt16
+
+    The dataset has already been downloaded on LLNL LC systems and is
+    available to anyone in the "lbann" group.
+
+    """
+
+    return '/p/vast1/lbann/datasets/wmt16_en_de_huggingface'
+
+def wmt_dir_old(system=system()):
     """Data directory for the WMT 2014 dataset.
 
     See https://pytorchnlp.readthedocs.io/en/latest/source/torchnlp.datasets.html#torchnlp.datasets.wmt_dataset.
