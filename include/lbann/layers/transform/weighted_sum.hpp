1458d818e24249968e22e2a5ee6f732173e6eeac
Tal Ben-Nun <talbn@llnl.gov>
2023-05-16 19:31:00 -0700

    Make weighted sum in-place-capable, ensure backprop runs all the way through in testing

--- include/lbann/layers/transform/weighted_sum.hpp
+++ include/lbann/layers/transform/weighted_sum.hpp
@@ -66,7 +66,7 @@ public:
   std::string get_type() const override { return "weighted sum"; }
   data_layout get_data_layout() const override { return T_layout; }
   El::Device get_device_allocation() const override { return Dev; }
-  bool can_run_inplace() const override { return false; }
+  bool can_run_inplace() const override { return true; }
   int get_backprop_requirements() const override { return ERROR_SIGNALS; }
 
   description get_description() const override
