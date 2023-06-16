691865e9f7a78d7eee019f7f32dc93cb7b9f94cd
Brian C. Van Essen <vanessen1@llnl.gov>
2023-06-15 18:55:19 -0700

    Cleaned up deadcode.

--- .gitlab/common/run-catch-tests-flux.sh
+++ .gitlab/common/run-catch-tests-flux.sh
@@ -31,19 +31,8 @@
 source ${HOME}/${SPACK_REPO}/share/spack/setup-env.sh
 
 # Load the LBANN module
-echo "BVE Starting catch tests from ${PWD}"
-echo "${LBANN_MODFILES_DIR}"
-#echo "source LBANN_${SYSTEM_NAME}_${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}_setup_module_path.sh"
-file LBANN_${SYSTEM_NAME}_${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}_setup_module_path.sh
-file ${PWD}/LBANN_${SYSTEM_NAME}_${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}_setup_module_path.sh
-#source LBANN_${SYSTEM_NAME}_${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}_setup_module_path.sh
-echo "$(which lbann)"
 ml load lbann
 echo "$(which lbann)"
-
-# Load up the spack environment
-#spack env activate -p lbann-${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}
-#spack load lbann@${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET} arch=${SPACK_ARCH}
 
 # Configure the output directory
 OUTPUT_DIR=${CI_PROJECT_DIR}/${RESULTS_DIR}
@@ -65,29 +54,16 @@ export MV2_USE_RDMA_CM=0
 export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${ROCM_PATH}/lib:${LD_LIBRARY_PATH}
 
-# LBANN_HASH=$(spack find --format {hash:7} lbann@${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET})
-# SPACK_BUILD_DIR="spack-build-${LBANN_HASH}"
-
-# cd ${SPACK_BUILD_DIR}
-
 cd ${LBANN_BUILD_DIR}
 
-flux resource list
-#flux proxy ${JOB_ID} flux resource list
-
-#flux mini run -N1 -n1 env
+#flux resource list
 
 flux run --label-io -n4 -N2 -g 1 -o cpu-affinity=per-task -o gpu-affinity=per-task sh -c 'taskset -cp $$; printenv | grep VISIBLE' | sort
 
 flux run --label-io -n4 -N2 -g 1 -o cpu-affinity=off -o gpu-affinity=per-task sh -c 'taskset -cp $$; printenv | grep VISIBLE' | sort
-#flux proxy ${JOB_ID} flux mini run --label-io -n4 -N2 -g 1 -o cpu-affinity=per-task -o gpu-affinity=per-task sh -c 'taskset -cp $$; printenv | grep VISIBLE' | sort
 
-flux run -N 1 -n 1 -g 1 -t 5m rocm-smi
+#flux run -N 1 -n 1 -g 1 -t 5m rocm-smi
 
-     # module load gcc-tce/10.3.1 rocm/5.2.0 openmpi-tce/4.1.2; \
-     # source /g/g14/lbannusr/spack_repos/spack_corona.git/share/spack/setup-env.sh; \
-     # spack env activate -p lbann-${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}; \
-#flux proxy ${JOB_ID} flux mini run -N 1 -n 1 -g 1 -t 5m \
 flux run -N 1 -n 1 -g 1 -t 5m \
      ./unit_test/seq-catch-tests \
      -r JUnit \
@@ -96,11 +72,6 @@ if [[ $? -ne 0 ]]; then
     FAILED_JOBS+=" seq"
 fi
 
-#     --ntasks-per-node=$TEST_TASKS_PER_NODE \
-# ${TEST_MPIBIND_FLAG}
-    #LBANN_NNODES=1
-#TEST_TASKS_PER_NODE=4
-#flux proxy ${JOB_ID} flux mini run \
 flux run \
      -N ${LBANN_NNODES} -n $((${TEST_TASKS_PER_NODE} * ${LBANN_NNODES})) \
      -g 1 -t 5m -o gpu-affinity=per-task -o cpu-affinity=per-task -o mpibind=off \
@@ -111,9 +82,6 @@ if [[ $? -ne 0 ]]; then
     FAILED_JOBS+=" mpi"
 fi
 
-#     --ntasks-per-node=$TEST_TASKS_PER_NODE \
-# ${TEST_MPIBIND_FLAG}
-#flux proxy ${JOB_ID} flux mini run \
 flux run \
      -N ${LBANN_NNODES} -n $((${TEST_TASKS_PER_NODE} * ${LBANN_NNODES})) \
      -g 1 -t 5m -o gpu-affinity=per-task -o cpu-affinity=per-task -o mpibind=off \
