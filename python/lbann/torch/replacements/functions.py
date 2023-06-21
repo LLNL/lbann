9e0314dbd8a853dd82fae8c93cb7734588882901
Tal Ben-Nun <talbn@llnl.gov>
2023-06-20 22:27:40 -0700

    Basic set of module and function replacements

---
+++ python/lbann/torch/replacements/functions.py
@@ -0,0 +1,268 @@
+################################################################################
+# Copyright (c) 2014-2023, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+# the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+#
+# LLNL-CODE-697807.
+# All rights reserved.
+#
+# This file is part of LBANN: Livermore Big Artificial Neural Network
+# Toolkit. For details, see http://software.llnl.gov/LBANN or
+# https://github.com/LLNL/LBANN.
+#
+# Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the license.
+#
+################################################################################
+"""
+Specific function converters for the LBANN PyTorch frontend.
+"""
+
+import functools
+import lbann
+from lbann.torch.converters import register_function
+import torch.nn as nn
+import warnings
+
+
+@register_function('aten.full.default')
+def _full(shape, value, **kwargs):
+    return lbann.Constant(num_neurons=shape[-1], value=value)
+
+
+@register_function('aten.view.default')
+def reshape(x, new_shape, **kwargs):
+    return lbann.Reshape(x, dims=new_shape[1:])
+
+
+@register_function('aten.slice.Tensor')
+def _slice(x, dim, start, end):
+    return lbann.Slice(x, axis=dim, slice_points=[start, end])
+
+
+@register_function('aten.unsqueeze.default')
+def unsqueeze(x, dim):
+    new_shape = list(x.shape[0:dim]) + [1] + list(x.shape[dim:])
+    return lbann.Reshape(x, dims=new_shape[1:])
+
+
+@register_function('<built-in function add>')  # operator.add
+# TODO: Ensure this works properly!
+@register_function('<built-in function iadd>')  # operator.iadd
+@register_function('aten.add.Tensor')
+def add(x, y, **kwargs):
+    if isinstance(y, (int, float)):
+        return lbann.AddConstant(x, constant=y)
+    if isinstance(x, (int, float)):
+        return lbann.AddConstant(y, constant=x)
+
+    return lbann.Add(x, y)
+
+
+@register_function('<built-in function sub>')  # operator.sub
+@register_function('<built-in function isub>')  # operator.isub
+@register_function('aten.sub.Tensor')
+def sub(x, y, **kwargs):
+    if isinstance(y, (int, float)):
+        return lbann.SubtractConstant(x, constant=y)
+    if isinstance(x, (int, float)):
+        return lbann.ConstantSubtract(y, constant=x)
+
+    return lbann.Subtract(x, y)
+
+
+@register_function('<built-in function mul>')  # operator.mul
+@register_function('<built-in function imul>')  # operator.imul
+@register_function('aten.mul.Tensor')
+def mul(x, y, **kwargs):
+    if isinstance(y, (int, float)):
+        return lbann.Scale(x, constant=y)
+    if isinstance(x, (int, float)):
+        return lbann.Scale(y, constant=x)
+
+    return lbann.Multiply(x, y)
+
+
+@register_function('<built-in function truediv>')  # operator.truediv
+@register_function('<built-in function itruediv>')  # operator.itruediv
+@register_function('aten.div.Tensor')
+def div(x, y, **kwargs):
+    if isinstance(y, (int, float)):
+        return lbann.Scale(x, constant=1 / y)
+    if isinstance(x, (int, float)):
+        return lbann.Scale(lbann.Reciprocal(y), constant=x)
+
+    return lbann.Divide(x, y)
+
+
+@register_function('torch.flatten')
+def flatten_impl(x, start_dim=1, end_dim=-1):
+    if start_dim == 0 or end_dim == 0:
+        raise ValueError('Cannot flatten batch dimension in LBANN')
+
+    shp = x.shape
+    n = len(shp)
+    start_dim = start_dim if start_dim > 0 else n + start_dim
+    end_dim = end_dim if end_dim >= 0 else n + end_dim + 1
+
+    new_shape = list(shp[:start_dim]) + [
+        functools.reduce(lambda a, b: a * b, shp[start_dim:end_dim], 1)
+    ] + list(shp[end_dim:])
+
+    return lbann.Reshape(x, dims=new_shape[1:])
+
+
+@register_function('torch.nn.functional.batch_norm')
+def batchnorm_impl(x,
+                   running_mean,
+                   running_var,
+                   w,
+                   b=None,
+                   training=False,
+                   momentum=0.1,
+                   eps=1e-5):
+    return lbann.BatchNormalization(x, decay=1 - momentum, epsilon=eps)
+
+
+@register_function('torch.nn.functional.softmax')
+def softmax_impl(x, dim=None, **kwargs):
+    return lbann.Softmax(x)
+
+
+@register_function('prims.convert_element_type.default')
+def conv(x, dtype, **kwargs):
+    warnings.warn(f'Casting to {dtype} will not be converted')
+    return x
+
+
+@register_function('aten.permute.default')
+def permute(x, dims, **kwargs):
+    if dims[0] != 0:
+        raise NotImplementedError('Batch dimension cannot be permuted when '
+                                  'converting to LBANN')
+    new_dims = [d - 1 for d in dims[1:]]
+    return lbann.TensorPermute(x, axes=new_dims)
+
+
+@register_function('aten.clone.default')
+def clone(x, **kwargs):
+    return lbann.Identity(x)
+
+
+@register_function('aten.clamp_min.default')
+def clamp_min(x, clampval):
+    return lbann.Clamp(x, min=clampval)
+
+
+@register_function('aten.clamp_max.default')
+def clamp_max(x, clampval):
+    return lbann.Clamp(x, max=clampval)
+
+
+@register_function('aten.tanh.default')
+def tanh(x):
+    return lbann.Tanh(x)
+
+
+@register_function('aten.mean.dim')
+def mean(x, dims, keepdim=False, **kwargs):
+    return lbann.ChannelwiseMean(x)
+
+
+@register_function('torch._C._nn.linear')
+def linear(x, weight, bias=None, **kwargs):
+    return lbann.FullyConnected(
+        x,
+        num_neurons=weight.shape[0],
+        has_bias=(True if (bias is not None and bias.shape) else False))
+
+
+@register_function('torch.nn.functional.relu')
+def relu(x):
+    return lbann.Relu(x)
+
+
+@register_function('torch.nn.functional.max_pool2d')
+def mp2d(x, kernel_size, strides=None, padding=0, **kwargs):
+    assert len(x.shape) == 4  # Batch, channels
+    return lbann.Pooling(x,
+                         pool_mode='max',
+                         num_dims=2,
+                         has_vectors=True,
+                         pool_dims=kernel_size,
+                         pool_pads=padding or [0, 0],
+                         pool_strides=strides or [1, 1])
+
+
+@register_function('torch.cat')
+def cat(args, dim=1):
+    return lbann.Concatenation(*args, axis=dim)
+
+
+@register_function('torch.tile')
+def tile(x, dims):
+    ndims = len(x.shape) - 1  # Remove batch dimension
+    if len(dims) > ndims:
+        dims = dims[1:]
+
+    if len(dims) < ndims:  # Pad with ones
+        dims = [1] * (ndims - len(dims)) + dims
+
+    new_shape = [s * d for s, d in zip(x.shape[1:], dims)]
+    return lbann.Tessellate(x, dims=new_shape)
+
+
+@register_function('aten.expand.default')
+def expand(x, dims):
+    if tuple(dims) == tuple(x.shape):
+        return x
+    raise NotImplementedError('Expand not implemented for differing shapes')
+
+
+@register_function('aten.bmm.default')
+def bmm(x, y, **kwargs):
+    return lbann.MatMul(x, y)
+
+
+@register_function('aten.erf.default')
+def erf(x):
+    return lbann.Erf(x)
+
+
+@register_function('aten.exp.default')
+def exp(x):
+    return lbann.Exp(x)
+
+
+@register_function('aten.select.int')
+def select_int(x, dim: int, index: int):
+    return lbann.Slice(x, axis=dim, slice_points=[index, index])
+
+
+@register_function('aten.amax.default')
+def amax(x, dims, keepdim=False):
+    # Dimension-wise max
+    if len(dims) == 1 and x.shape[dims[0]] == 1 and keepdim:
+        return x
+    raise NotImplementedError('Dimension-wise max not implemented')
+
+
+@register_function('torch.full')
+def full(size, fill_value, **kwargs):
+    return lbann.Constant(value=fill_value, num_neurons=size)
+
+
+@register_function('torch.addmm')
+def addmm(input, mat1, mat2, *, beta=1, alpha=1):
+    rhs = lbann.Scale(lbann.MatMul(mat1, mat2), constant=alpha)
+    return lbann.Add(lbann.Scale(input, constant=beta), rhs)
---
+++ python/lbann/torch/replacements/modules.py
@@ -0,0 +1,233 @@
+################################################################################
+# Copyright (c) 2014-2023, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+# the CONTRIBUTORS file. <lbann-dev@llnl.gov>
+#
+# LLNL-CODE-697807.
+# All rights reserved.
+#
+# This file is part of LBANN: Livermore Big Artificial Neural Network
+# Toolkit. For details, see http://software.llnl.gov/LBANN or
+# https://github.com/LLNL/LBANN.
+#
+# Licensed under the Apache License, Version 2.0 (the "Licensee"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the license.
+#
+################################################################################
+"""
+Specific PyTorch Module converters for the LBANN PyTorch frontend.
+"""
+import functools
+import lbann
+from lbann.torch.converters import register_module, register_opaque_shape_inference
+import torch.nn as nn
+
+
+def convnd_layer(c, dims: int, args, kwargs):
+    return lbann.Convolution(*args,
+                             **kwargs,
+                             num_dims=dims,
+                             out_channels=c.out_channels,
+                             kernel_size=c.kernel_size,
+                             padding=c.padding,
+                             stride=c.stride,
+                             dilation=c.dilation,
+                             has_bias=c.bias is not None,
+                             groups=c.groups)
+
+
+def deconvnd_layer(c: nn.ConvTranspose2d, dims: int, args, kwargs):
+    return lbann.Deconvolution(*args,
+                               **kwargs,
+                               num_dims=dims,
+                               out_channels=c.out_channels,
+                               kernel_size=c.kernel_size,
+                               padding=c.padding,
+                               output_padding=c.output_padding,
+                               stride=c.stride,
+                               dilation=c.dilation,
+                               has_bias=c.bias is not None,
+                               groups=c.groups)
+
+
+@register_module(nn.Conv2d)
+def conv2d(c: nn.Conv2d, *args, **kwargs):
+    return convnd_layer(c, 2, args, kwargs)
+
+
+@register_module(nn.Conv3d)
+def conv3d(c: nn.Conv3d, *args, **kwargs):
+    return convnd_layer(c, 3, args, kwargs)
+
+
+@register_module(nn.ConvTranspose1d)
+def deconv1d(c: nn.ConvTranspose1d, *args, **kwargs):
+    return deconvnd_layer(c, 1, args, kwargs)
+
+
+@register_module(nn.ConvTranspose2d)
+def deconv2d(c: nn.ConvTranspose2d, *args, **kwargs):
+    return deconvnd_layer(c, 2, args, kwargs)
+
+
+@register_module(nn.ConvTranspose3d)
+def deconv3d(c: nn.ConvTranspose3d, *args, **kwargs):
+    return deconvnd_layer(c, 3, args, kwargs)
+
+
+@register_module(nn.LeakyReLU)
+def leaky_relu_impl(origmod: nn.LeakyReLU, *args, **kwargs):
+    return lbann.LeakyRelu(*args,
+                           **kwargs,
+                           negative_slope=origmod.negative_slope)
+
+
+@register_module(nn.AvgPool3d)
+def avgpool3d_impl(origmod: nn.AvgPool3d, *args, **kwargs):
+    return lbann.Pooling(*args,
+                         **kwargs,
+                         pool_mode='average',
+                         num_dims=3,
+                         has_vectors=False,
+                         pool_dims_i=origmod.kernel_size,
+                         pool_pads_i=origmod.padding,
+                         pool_strides_i=origmod.stride)
+
+
+@register_module(nn.Dropout)
+def dropout_impl(mod: nn.Dropout, *args, **kwargs):
+    return lbann.Dropout(*args, **kwargs, keep_prob=(1 - mod.p))
+
+
+@register_module(nn.Linear)
+def linear_impl(origmod: nn.Linear, *args, **kwargs):
+    return lbann.FullyConnected(*args,
+                                **kwargs,
+                                num_neurons=origmod.out_features,
+                                has_bias=origmod.bias is not None,
+                                transpose=False)
+
+
+@register_module(nn.Flatten)
+def flatten_impl(mod: nn.Flatten, x, **kwargs):
+    if mod.start_dim == 0 or mod.end_dim == 0:
+        raise ValueError('Cannot flatten batch dimension in LBANN')
+
+    shp = x.shape
+    n = len(shp)
+    start_dim = mod.start_dim if mod.start_dim > 0 else n + mod.start_dim
+    end_dim = mod.end_dim if mod.end_dim >= 0 else n + mod.end_dim + 1
+
+    new_shape = list(shp[:start_dim]) + [
+        functools.reduce(lambda a, b: a * b, shp[start_dim:end_dim], 1)
+    ] + list(shp[end_dim:])
+
+    return lbann.Reshape(x, dims=new_shape[1:])
+
+
+@register_module(nn.ReLU)
+def relu_impl(mod: nn.ReLU, *args, **kwargs):
+    return lbann.Relu(*args, **kwargs)
+
+
+@register_module(nn.Identity)
+def identity_impl(mod: nn.Identity, x, *args, **kwargs):
+    return x  #return lbann.Identity(x)
+
+
+@register_module(nn.Embedding)
+def embedding_impl(mod: nn.Embedding, x, *args, **kwargs):
+    return lbann.Embedding(x,
+                           num_embeddings=mod.num_embeddings,
+                           embedding_dim=mod.embedding_dim,
+                           padding_idx=mod.padding_idx)
+
+
+@register_module(nn.LayerNorm)
+def ln_impl(mod: nn.LayerNorm, x, *args, **kwargs):
+    return lbann.LayerNorm(x, epsilon=mod.eps)
+
+
+@register_module([nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d])
+def bn_impl(mod: nn.BatchNorm2d, x, *args, **kwargs):
+    return lbann.BatchNormalization(x, decay=1 - mod.momentum, epsilon=mod.eps)
+
+
+def make_vector(val, dims):
+    if isinstance(val, int):
+        return [val] * dims
+    return val
+
+
+def pool_nd_impl(mod: nn.MaxPool2d, x, dims: int):
+    assert len(x.shape) == dims + 2  # Batch, channels
+    return lbann.Pooling(x,
+                         pool_mode='max',
+                         num_dims=dims,
+                         has_vectors=True,
+                         pool_dims=make_vector(mod.kernel_size, dims),
+                         pool_pads=make_vector(mod.padding, dims),
+                         pool_strides=make_vector(mod.stride, dims))
+
+
+@register_module(nn.MaxPool1d)
+def _impl(mod: nn.MaxPool1d, x):
+    return pool_nd_impl(mod, x, 1)
+
+
+@register_module(nn.MaxPool2d)
+def _impl(mod: nn.MaxPool2d, x):
+    return pool_nd_impl(mod, x, 2)
+
+
+@register_module(nn.MaxPool3d)
+def _impl(mod: nn.MaxPool3d, x):
+    return pool_nd_impl(mod, x, 3)
+
+
+def aap_nd(x, dims: int):
+    assert len(x.shape) == dims + 2  # Batch, channels
+    return lbann.ChannelwiseMean(x)
+
+
+@register_module(nn.AdaptiveAvgPool1d)
+def _impl(mod: nn.AdaptiveAvgPool1d, x):
+    return aap_nd(x, 1)
+
+
+@register_module(nn.AdaptiveAvgPool2d)
+def _impl(mod: nn.AdaptiveAvgPool2d, x):
+    return aap_nd(x, 2)
+
+
+@register_module(nn.AdaptiveAvgPool3d)
+def _impl(mod: nn.AdaptiveAvgPool3d, x):
+    return aap_nd(x, 3)
+
+
+# PyTorch Geometric
+try:
+    from torch_geometric.nn import GCNConv
+
+    @register_opaque_shape_inference(GCNConv)
+    def infer(mod: GCNConv, x, edge_index, edge_weight=None):
+        new_shape = (x.shape[0], mod.out_channels)
+        return x.dtype, new_shape, x.device
+
+    @register_module(GCNConv)
+    def _impl(mod: GCNConv, x, edge_index, edge_weight=None):
+        raise NotImplementedError
+
+except (ImportError, ModuleNotFoundError):
+    # No PyTorch Geometric installation found, skip
+    pass
