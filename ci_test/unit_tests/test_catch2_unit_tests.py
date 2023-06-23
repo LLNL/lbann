2121221b5ebdaad5071fd37f8066ec5c5be1eb8d
Brian C. Van Essen <vanessen1@llnl.gov>
2023-06-23 10:22:25 -0700

    Fix how the catch tests are found on Lassen.

--- ci_test/unit_tests/test_catch2_unit_tests.py
+++ ci_test/unit_tests/test_catch2_unit_tests.py
@@ -5,14 +5,16 @@ import pytest
 import os, re
 import subprocess as sp
 
+# BVE this should work for now, but needs to be cleaned up more
 def hack_find_spack_build_dir(basedir):
-    if os.getenv('SPACK_BUILD_DIR', default=None):
-        build_dir = os.getenv('SPACK_BUILD_DIR')
+    if os.getenv('LBANN_BUILD_DIR', default=None):
+        build_dir = os.getenv('LBANN_BUILD_DIR')
         return basedir + '/' + build_dir
     else:
+        build_dir = basedir + '/builds'
         with os.scandir(basedir) as it:
             for entry in it:
-                if entry.is_dir() and re.match(r'spack-.*', entry.name):
+                if entry.is_dir() and re.match(r'lbann_.*', entry.name):
                     return entry.path
 
 def get_system_seq_launch(cluster):
