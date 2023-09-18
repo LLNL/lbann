1fc2359e409f95f70b564e109cd32fa0d1d78f1e
Tal Ben-Nun <talbn@llnl.gov>
2023-09-18 12:49:01 -0700

    Add test

--- src/models/unit_test/model_test.cpp
+++ src/models/unit_test/model_test.cpp
@@ -32,6 +32,7 @@
 #include "lbann/objective_functions/objective_function.hpp"
 #include <lbann/base.hpp>
 #include <lbann/layers/io/input_layer.hpp>
+#include <lbann/layers/layer.hpp>
 #include <lbann/models/model.hpp>
 #include <lbann/proto/factories.hpp>
 #include <lbann/utils/lbann_library.hpp>
@@ -58,10 +59,11 @@ auto mock_datareader_metadata()
 }
 
 template <typename T>
-auto make_model(lbann::lbann_comm& comm)
+auto make_model(lbann::lbann_comm& comm,
+                const std::string& model_contents = model_prototext)
 {
   lbann_data::LbannPB my_proto;
-  if (!pb::TextFormat::ParseFromString(model_prototext, &my_proto))
+  if (!pb::TextFormat::ParseFromString(model_contents, &my_proto))
     throw "Parsing protobuf failed.";
   // Construct a trainer so that the model can register the input layer
   lbann::construct_trainer(&comm, my_proto.mutable_trainer(), my_proto);
@@ -162,4 +164,135 @@ TEST_CASE("Serializing models", "[mpi][model][serialize]")
     }
   }
 #endif // LBANN_HAS_CEREAL_XML_ARCHIVES
+}
+
+// Prototext graphs
+const std::string backprop_test_model = R"""(
+model {
+  layer {
+    name: "inp"
+    children: "layer1"
+    weights: "dummy_inputs"
+    weights_layer {
+      dims: 3
+    }
+  }
+  layer {
+    name: "layer1"
+    parents: "inp"
+    children: "stopgrad"
+    relu {
+    }
+  }
+  layer {
+    name: "stopgrad"
+    parents: "layer1"
+    children: "layer2"
+    stop_gradient {
+    }
+  }
+  layer {
+    name: "layer2"
+    parents: "stopgrad"
+    relu {
+    }
+  }
+  weights {
+    name: "dummy_inputs"
+    initializer {
+      value_initializer {
+        values: -1.2
+        values: 3.4
+        values: -5.67
+      }
+    }
+  }
+}
+)""";
+
+class backprop_test_layer final : public lbann::data_type_layer<float>
+{
+public:
+  backprop_test_layer(lbann::lbann_comm* comm)
+    : lbann::data_type_layer<float>(comm),
+      m_fprop_computed(false),
+      m_bprop_computed(false)
+  {}
+  backprop_test_layer* copy() const final
+  {
+    return new backprop_test_layer(*this);
+  }
+
+  std::string get_type() const final { return "backprop_test_layer"; }
+  lbann::data_layout get_data_layout() const final
+  {
+    return lbann::data_layout::DATA_PARALLEL;
+  }
+  El::Device get_device_allocation() const final { return El::Device::CPU; }
+  void write_specific_proto(lbann_data::Layer& proto) const final {}
+  bool fprop_computed() const { return m_fprop_computed; }
+  bool bprop_computed() const { return m_bprop_computed; }
+
+private:
+  friend class cereal::access;
+  backprop_test_layer() : backprop_test_layer(nullptr) {}
+
+  void setup_dims(lbann::DataReaderMetaData& dr_metadata) final
+  {
+    lbann::data_type_layer<float>::setup_dims(dr_metadata);
+    this->set_output_dims(this->get_input_dims());
+  }
+  void fp_setup_outputs(El::Int mini_batch_size) override
+  {
+    El::LockedView(this->get_activations(), this->get_prev_activations());
+  }
+  void bp_setup_gradient_wrt_inputs(El::Int mini_batch_size) override
+  {
+    El::LockedView(this->get_error_signals(), this->get_prev_error_signals());
+  }
+
+  void fp_compute() final { m_fprop_computed = true; }
+
+  void bp_compute() final { m_bprop_computed = true; }
+
+  bool m_fprop_computed, m_bprop_computed;
+};
+
+TEST_CASE("Gradient backpropagation stop", "[mpi][model][backprop]")
+{
+  using DataType = float;
+
+  auto& comm = unit_test::utilities::current_world_comm();
+
+  auto& g = comm.get_trainer_grid();
+  lbann::utils::grid_manager mgr(g);
+  std::unique_ptr<lbann::model> model =
+    make_model<DataType>(comm, backprop_test_model);
+
+  std::shared_ptr<backprop_test_layer> before_stopgrad =
+    std::make_shared<backprop_test_layer>(&comm);
+  std::shared_ptr<backprop_test_layer> after_stopgrad =
+    std::make_shared<backprop_test_layer>(&comm);
+
+  model->insert_layer(before_stopgrad, "layer1");
+  model->insert_layer(after_stopgrad, "layer2");
+
+  // Reset the model after adding the new layers
+  auto metadata = lbann::DataReaderMetaData();
+  model->setup(1UL, metadata, {&g}, true);
+
+  REQUIRE(!before_stopgrad->fprop_computed());
+  REQUIRE(!before_stopgrad->bprop_computed());
+  REQUIRE(!after_stopgrad->fprop_computed());
+  REQUIRE(!after_stopgrad->bprop_computed());
+
+  // Run forward
+  REQUIRE_NOTHROW(model->forward_prop(lbann::execution_mode::training));
+  REQUIRE(before_stopgrad->fprop_computed());
+  REQUIRE(after_stopgrad->fprop_computed());
+
+  // Run backprop
+  REQUIRE_NOTHROW(model->backward_prop(false));
+  REQUIRE(!before_stopgrad->bprop_computed());
+  REQUIRE(after_stopgrad->bprop_computed());
 }
