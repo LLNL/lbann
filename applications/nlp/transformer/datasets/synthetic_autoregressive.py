444cd02a582a45929e03f0ade3211b32091ff15e
Tal Ben-Nun <talbn@llnl.gov>
2023-09-21 14:25:27 -0700

    Add new PyTorch references for transformers

---
+++ applications/nlp/transformer/datasets/synthetic_autoregressive.py
@@ -0,0 +1,52 @@
+"""
+Synthetic dataset for benchmarking transformers on autoregressive tasks (e.g.,
+in causal language models).
+"""
+import numpy as np
+
+# ----------------------------------------------
+# Options
+# ----------------------------------------------
+
+sequence_length = 64
+_vocab_size = 32000
+
+# ----------------------------------------------
+# Setup
+# ----------------------------------------------
+
+pad_index = 0
+
+# ----------------------------------------------
+# Sample access functions
+# ----------------------------------------------
+
+
+def get_train_sample(index):
+    return np.random.randint(0,
+                             _vocab_size,
+                             size=(sequence_length, ),
+                             dtype=np.int32)
+
+
+def get_val_sample(index):
+    return np.random.randint(0,
+                             _vocab_size,
+                             size=(sequence_length, ),
+                             dtype=np.int32)
+
+
+def num_train_samples():
+    return 100000
+
+
+def num_val_samples():
+    return 100
+
+
+def sample_dims():
+    return (sequence_length, )
+
+
+def vocab_size():
+    return _vocab_size
---
+++ applications/nlp/transformer/datasets/thepile.py
@@ -0,0 +1,113 @@
+"""The Pile dataset."""
+import os.path
+import sys
+
+import numpy as np
+from datasets import load_dataset
+from tokenizers import Tokenizer
+
+# Local imports
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(current_dir, '..', '..'))
+import utils.paths
+
+# ----------------------------------------------
+# Options
+# ----------------------------------------------
+
+sequence_length = 512
+
+# ----------------------------------------------
+# Setup
+# ----------------------------------------------
+
+# Load the dataset with HuggingFace datasets
+data_dir = '/p/vast1/data/datasets/the-pile-huggingface'
+dataset_train, dataset_val = load_dataset(os.path.join(data_dir, 'pile.py'),
+                                          'all',
+                                          split=('train', 'validation'),
+                                          cache_dir=os.path.join(
+                                              data_dir, 'cache'))
+
+# Use the WMT 2016 tokenizer (?)
+tokenizer = Tokenizer.from_file(
+    os.path.join(utils.paths.wmt_dir(), 'tokenizer-wmt16.json'))
+pad_index = tokenizer.token_to_id('<pad>')
+bos_index = tokenizer.token_to_id('<s>')
+eos_index = tokenizer.token_to_id('</s>')
+
+# ----------------------------------------------
+# Tokenization
+# ----------------------------------------------
+
+
+def tokenize(text):
+    """Convert string to list of token indices.
+
+    Use byte-pair encoding trained on WMT-16. We add BOS and EOS tokens.
+    """
+    return tokenizer.encode('<s>' + text + '</s>').ids
+
+
+def detokenize(indices):
+    """Convert token indices to string.
+
+    Stops at the first EOS token. All other special tokens are ignored.
+    """
+    return tokenizer.decode(indices,
+                            skip_special_tokens=True).replace(' ##', '')
+
+
+# ----------------------------------------------
+# Sample access functions
+# ----------------------------------------------
+
+
+def get_train_sample(index):
+    """Token indices for a data sample from the training set.
+
+    The English and German text samples are tokenized,
+    padded/subsampled to sequence_length tokens, and concatenated.
+
+    """
+
+    # Tokenize text data
+    text = dataset_train[index]['text']
+    sample = tokenize(text)
+
+    # Randomly subsample sequences if they are too long
+    if len(sample) > sequence_length:
+        pos = np.random.rand()
+        offset = (len(sample) - sequence_length + 1) * pos
+        offset = int(np.floor(offset))
+        sample = sample[offset:offset + sequence_length]
+
+    # Pad sequences if they are too short
+    if len(sample) < sequence_length:
+        sample_pad = np.full(sequence_length, pad_index, dtype=int)
+        sample_pad[0:len(sample)] = sample
+        return sample_pad
+
+    return sample
+
+
+def get_val_sample(index):
+    """Token indices for a data sample from the validation set."""
+    text = dataset_train[index]['text']
+    return tokenize(text)
+
+
+def num_train_samples():
+    return len(dataset_train)
+
+
+def num_val_samples():
+    return len(dataset_val)
+
+
+def sample_dims():
+    return (sequence_length, )
+
+
+def vocab_size():
+    return tokenizer.get_vocab_size()
