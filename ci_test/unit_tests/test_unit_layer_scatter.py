2d8e54bd3e52c6c1c6ecc40ebd3788caee5202b0
Brian C. Van Essen <vanessen1@llnl.gov>
2023-10-05 12:42:31 -0700

    Removing debuggin code

--- ci_test/unit_tests/test_unit_layer_scatter.py
+++ ci_test/unit_tests/test_unit_layer_scatter.py
@@ -128,7 +128,6 @@ def construct_model(lbann):
         upper_bound=val+tol,
         error_on_failure=True,
         execution_modes='test'))
-    callbacks.append(lbann.CallbackPrintModelDescription())
 
     ######################################################################
     #
@@ -182,7 +181,6 @@ def construct_model(lbann):
         upper_bound=val+tol,
         error_on_failure=True,
         execution_modes='test'))
-    callbacks.append(lbann.CallbackPrintModelDescription())
     ######################################################################
     #
     #          2D Values , 1D Input, Axis = 1
@@ -238,7 +236,6 @@ def construct_model(lbann):
     # Gradient checking
 
     callbacks.append(lbann.CallbackCheckGradients(error_on_failure=True))
-    callbacks.append(lbann.CallbackPrintModelDescription())
 
     # Construct model
     num_epochs = 0
