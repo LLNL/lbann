3aadb2871d721f538d0cbf60dcae242b25d1ae20
Brian C. Van Essen <vanessen1@llnl.gov>
2023-10-26 18:17:47 -0700

    Cleaned up the reporting which data set is used.

--- src/callbacks/evaluate_progress.cpp
+++ src/callbacks/evaluate_progress.cpp
@@ -31,17 +31,13 @@
 #include "lbann/metrics/metric.hpp"
 #include "lbann/models/model.hpp"
 #include "lbann/objective_functions/objective_function.hpp"
-// #include "lbann/optimizers/adam.hpp"
-// #include "lbann/optimizers/sgd.hpp"
 #include "lbann/proto/proto_common.hpp"
 #include "lbann/trainers/trainer.hpp"
 #include "lbann/utils/cloneable.hpp"
 #include "lbann/utils/memory.hpp"
 #include "lbann/utils/protobuf.hpp"
-// #include "lbann/utils/random_number_generators.hpp"
 #include "lbann/utils/serialize.hpp"
 #include "lbann/utils/timer.hpp"
-// #include "lbann/weights/data_type_weights.hpp"
 
 #include "lbann/proto/callbacks.pb.h"
 
@@ -61,34 +57,13 @@ namespace callback {
 namespace {
 
 /** Get mean metric value with validation set. */
-EvalType evaluate(model& m, const std::string& metric_name)
+EvalType evaluate(model& m, const std::string& metric_name, execution_mode mode)
 {
   auto& c = m.get_execution_context();
   // Make sure data readers finish asynchronous work
   const auto original_mode = c.get_execution_mode();
   data_coordinator& dc = get_trainer().get_data_coordinator();
   dc.collect_background_data_fetch(original_mode);
-
-  auto mode = execution_mode::invalid;
-  auto modes = {execution_mode::tournament,
-                execution_mode::validation,
-                execution_mode::testing};
-  for (auto em : modes) {
-    if (dc.is_execution_mode_valid(em)) {
-      mode = em;
-      break;
-    }
-  }
-  if (mode == execution_mode::invalid) {
-    LBANN_WARNING("evaluate_progress requires ",
-                  to_string(execution_mode::tournament),
-                  " or ",
-                  to_string(execution_mode::validation),
-                  " or ",
-                  to_string(execution_mode::testing),
-                  " execution modes");
-    return 0;
-  }
 
   // Mark the data store as loading - Note that this is a temporary fix
   // for the current use of the tournament
@@ -157,12 +132,34 @@ void evaluate_progress::on_batch_begin(model* m)
     return;
   }
 
+  data_coordinator& dc = get_trainer().get_data_coordinator();
+  auto evaluation_mode = execution_mode::invalid;
+  auto modes = {execution_mode::tournament,
+                execution_mode::validation,
+                execution_mode::testing};
+  for (auto em : modes) {
+    if (dc.is_execution_mode_valid(em)) {
+      evaluation_mode = em;
+      break;
+    }
+  }
+  if (evaluation_mode == execution_mode::invalid) {
+    LBANN_WARNING("evaluate_progress requires ",
+                  to_string(execution_mode::tournament),
+                  " or ",
+                  to_string(execution_mode::validation),
+                  " or ",
+                  to_string(execution_mode::testing),
+                  " execution modes");
+    return;
+  }
+
   // Print message
   const auto message_prefix =
-    (std::string{} + "Evaluate progress using (" + to_string(mode) +
+    (std::string{} + "Evaluate progress using (" + to_string(evaluation_mode) +
      " data set) on " + "model \"" + local_model.get_name() + "\", " + "step " +
      std::to_string(step) + "): ");
-  auto local_score = evaluate(local_model, m_metric_name);
+  auto local_score = evaluate(local_model, m_metric_name, evaluation_mode);
 
   // Report evaluation results
   if (comm.am_trainer_master()) {
