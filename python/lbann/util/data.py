7f8f707cd6e4fc05174034a65b4735f2dad70ce2
Pier Fiedorowicz <fiedorowicz1@llnl.gov>
2024-01-16 23:08:04 -0800

    Add cosmoflow example and reader helper

--- python/lbann/util/data.py
+++ python/lbann/util/data.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+import os
+import inspect
+import pickle
+import lbann
 
 
 class Sample:
@@ -34,7 +38,7 @@ class DistConvDataset(Dataset):
     @property
     def rank(self):
         if not hasattr(self, '_rank'):
-            raise Exception('Rank is not set for distconv data reader')
+            self._rank = 0
         return self._rank
     
     @rank.setter
@@ -44,9 +48,42 @@ class DistConvDataset(Dataset):
     @property
     def num_io_partitions(self):
         if not hasattr(self, '_num_io_partitions'):
-            raise Exception('Number of IO partitions is not set for distconv data reader')
+            self._num_io_partitions = 1
         return self._num_io_partitions
 
     @num_io_partitions.setter
     def num_io_partitions(self, num_io_partitions):
         self._num_io_partitions = num_io_partitions
+
+
+def construct_python_dataset_reader(dataset,
+                                    dataset_path=None,
+                                    role='train',
+                                    shuffle=True,
+                                    fraction_of_data_to_use=1.0,
+                                    validation_fraction=0.,
+                                    load_module=True):
+    if dataset_path is None:
+        main_file = inspect.stack()[-1].filename
+        dataset_path = os.path.join(os.path.dirname(main_file), f'{role}_dataset.pkl')
+    
+    with open(dataset_path, 'wb') as f:
+        pickle.dump(dataset, f)
+
+    module_dir = None
+    if load_module:
+        module_dir = os.path.dirname(inspect.getsourcefile(type(dataset)))
+    
+    reader = lbann.reader_pb2.Reader(
+        name='python_dataset',
+        role=role,
+        shuffle=shuffle,
+        fraction_of_data_to_use=fraction_of_data_to_use,
+        validation_fraction=validation_fraction,
+        python_dataset=lbann.reader_pb2.PythonDatasetReader(
+            dataset_path=dataset_path,
+            module_dir=module_dir
+        )
+    )
+
+    return reader
