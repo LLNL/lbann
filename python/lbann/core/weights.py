2f691af1713981e43a376ea20433d6f14e148e14
Tal Ben-Nun <tbennun@users.noreply.github.com>
2024-02-14 09:29:56 -0800

    FSDP: Enable limiting scope using trainer grid rows/columns (#2424)

--- python/lbann/core/weights.py
+++ python/lbann/core/weights.py
@@ -1,6 +1,8 @@
 """Trainable model parameters."""
 import abc
 from lbann import weights_pb2
+from enum import Enum
+from typing import Optional
 import lbann.core.util
 
 class Initializer(abc.ABC):
@@ -22,19 +24,28 @@ if weights_pb2:
     for c in classes:
         globals()[c.__name__] = c
 
+
+class ShardingStrategy(Enum):
+        FULL = 0      # Sharded across all ranks (STAR x VC)
+        GRID_ROWS = 1 # Sharded across the process grid rows (STAR x MC)
+        GRID_COLS = 2 # Sharded across the process grid columns (STAR x MR)
+
+
 class Weights:
     """Trainable parameters for neural network."""
 
     global_count = 0  # Static counter, used for default names
 
-    def __init__(self, initializer=None, optimizer=None, name=None, datatype=None,
-                 sharded=None):
+    def __init__(self, initializer=None, optimizer=None, name=None,
+                 datatype=None, sharded=None,
+                 sharding_strategy: Optional[ShardingStrategy] = None):
         Weights.global_count += 1
         self.name = name if name else 'weights{0}'.format(Weights.global_count)
         self.initializer = initializer
         self.optimizer = optimizer
         self.datatype = datatype
         self.sharded = sharded
+        self.sharding_strategy = sharding_strategy
 
     def export_proto(self):
         """Construct and return a protobuf message."""
@@ -57,5 +68,8 @@ class Weights:
 
         if self.sharded:
             proto.sharded = self.sharded
+
+        if self.sharding_strategy is not None:
+            proto.sharding_strategy = self.sharding_strategy.value
 
         return proto
