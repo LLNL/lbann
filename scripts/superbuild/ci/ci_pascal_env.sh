40f4b5c8886d4f74a34caccee98b700d3205aee3
Brian C. Van Essen <vanessen1@llnl.gov>
2024-06-17 09:24:45 -0700

    Added a path to cuTensor for x86_64 platforms.

--- scripts/superbuild/ci/ci_pascal_env.sh
+++ scripts/superbuild/ci/ci_pascal_env.sh
@@ -40,5 +40,5 @@ INSTALL_ROOT=/usr/workspace/lbann/ci_stable_dependencies/pascal/cuda-11.8.0
 INSTALL_PREFIX_EXTERNALS=${INSTALL_ROOT}/openmpi-4.1.2
 
 # Location of external packages
-export CMAKE_PREFIX_PATH=${INSTALL_ROOT}/cudnn-8.9.4:${INSTALL_ROOT}/nccl-2.19.4
+export CMAKE_PREFIX_PATH=${INSTALL_ROOT}/cudnn-8.9.4:${INSTALL_ROOT}/nccl-2.19.4:${INSTALL_ROOT}/../../cutensor-2.0.1.2/libcutensor-linux-x86_64-2.0.1.2-archive
 CMAKE_CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH//:/;}
