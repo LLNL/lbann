22108ac5ab0b99727c46af21521f2cd8250a3384
Brian C. Van Essen <vanessen1@llnl.gov>
2024-06-25 11:44:07 -0700

    Force the installation of pip packages in the installed location to avoid bad system install.

--- scripts/build_lbann.sh
+++ scripts/build_lbann.sh
@@ -1278,7 +1278,7 @@ echo "To manipulate the version of python used it is:" | tee -a ${LOG}
 echo "  ${LBANN_PYTHON}" | tee -a ${LOG}
 echo "Additional Python packages for working with LBANN can be added either via PIP or by concretizing them together in spack., activate the spack environment then" | tee -a ${LOG}
 echo "To install them via PIP: 1) the spack environment (see above) and 2) issue the following command" | tee -a ${LOG}
-echo "  python3 -m pip install -r <requirements file>" | tee -a ${LOG}
+echo "  python3 -m pip install --force-reinstall -r <requirements file>" | tee -a ${LOG}
 echo "To install them via Spack: include them on the build_lbann.sh script command line argument via -e <path to text file of packages> or -p <spack package name>" | tee -a ${LOG}
 echo "##########################################################################################" | tee -a ${LOG}
 echo "All details of the run are logged to ${LOG}"
