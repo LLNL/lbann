3972df77bb7f3d85a792a903dc955d8e6299d742
Brian C. Van Essen <vanessen1@llnl.gov>
2024-07-30 13:32:57 -0700

    Added configure scripts for LBANN and a script to run the unit and integration tests.

---
+++ .gitlab/configure_lbann.sh
@@ -0,0 +1,62 @@
+if [[ "$cluster" == "lassen" ]]
+then
+    lapack_opt="-D BLA_VENDOR=Generic"
+else
+    lapack_opt=""
+fi
+
+cmake -G Ninja \
+      -S ${project_dir} \
+      -B ${build_dir}/build-lbann \
+      \
+      -D CMAKE_PREFIX_PATH=${CMAKE_CMAKE_PREFIX_PATH} \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=${prefix}/lbann \
+      \
+      -D CMAKE_BUILD_RPATH="${extra_rpaths//:/\;}" \
+      -D CMAKE_INSTALL_RPATH="${extra_rpaths//:/\;}" \
+      -D CMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+      \
+      -D CMAKE_CXX_STANDARD=17 \
+      -D CMAKE_CUDA_STANDARD=17 \
+      -D CMAKE_HIP_STANDARD=17 \
+      \
+      -D CMAKE_CUDA_ARCHITECTURES=${gpu_arch} \
+      -D CMAKE_HIP_ARCHITECTURES=${gpu_arch} \
+      \
+      -D BUILD_SHARED_LIBS=ON \
+      -D CMAKE_POSITION_INDEPENDENT_CODE=ON \
+      -D CMAKE_EXE_LINKER_FLAGS="${EXTRA_LINK_FLAGS}" \
+      -D CMAKE_SHARED_LINKER_FLAGS="${EXTRA_LINK_FLAGS}" \
+      \
+      -D BUILD_SHARED_LIBS=ON \
+      -D CMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      -D CMAKE_CXX_FLAGS="${EXTRA_CXX_FLAGS}" \
+      -D CMAKE_HIP_FLAGS="${EXTRA_HIP_FLAGS}" \
+      -D LBANN_DATATYPE=float \
+      -D LBANN_WITH_CALIPER=OFF \
+      -D LBANN_WITH_DISTCONV=${BUILD_WITH_DISTCONV} \
+      -D LBANN_WITH_TBINF=OFF \
+      -D LBANN_WITH_UNIT_TESTING=ON \
+      -D LBANN_WITH_CNPY=ON \
+      -D LBANN_DETERMINISTIC=ON \
+      -D LBANN_WITH_ADDRESS_SANITIZER=OFF \
+      -D LBANN_WITH_FFT=OFF \
+      -D LBANN_WITH_EMBEDDED_PYTHON=ON \
+      -D LBANN_WITH_PYTHON_FRONTEND=ON \
+      -D LBANN_WITH_VISION=ON
+
+
+#      -D LBANN_WITH_CUTENSOR=OFF \
+
+# \
+#       -D CMAKE_PREFIX_PATH=${FWD_CMAKE_PREFIX_PATH}
+
+#      -D LBANN_SB_LBANN_SOURCE_DIR=${LBANN_SRC_DIR} \
+
+      # -D LBANN_SB_DEFAULT_CUDA_OPTS=${cuda_platform} \
+      # -D LBANN_SB_DEFAULT_ROCM_OPTS=${rocm_platform} \
+      # -D LBANN_WITH_NVSHMEM=OFF \
+
+      # \
+      # -D LBANN_SB_DEFAULT_INSTALL_PATH_STRATEGY="PKG_LC" \
---
+++ .gitlab/run_unit_and_integration_tests.sh
@@ -0,0 +1,108 @@
+#!/bin/bash -l
+
+CLUSTER=$(hostname | sed 's/\([a-zA-Z][a-zA-Z]*\)[0-9]*/\1/g')
+LBANN_DIR=$(git rev-parse --show-toplevel)
+
+cd ${LBANN_DIR}/ci_test
+
+echo "${PWD}/run.sh CLUSTER=${CLUSTER}"
+
+PYTHON=python3
+LBANN_PYTHON=lbann_pfe.sh
+
+# WEEKLY=0
+# while :; do
+#     case ${1} in
+#         --weekly)
+#             # Run all tests. This is a weekly build.
+#             echo "Setting WEEKLY in run.sh"
+#             WEEKLY=1
+#             ;;
+#         -?*)
+#             # Unknown option
+#             echo "Unknown option (${1})" >&2
+#             exit 1
+#             ;;
+#         *)
+#             # Break loop if there are no more options
+#             break
+#     esac
+#     shift
+# done
+
+# # Use the spack provided by the CI
+# source ${HOME}/${SPACK_REPO}/share/spack/setup-env.sh
+
+# # "spack" is just a shell function; it may not be exported to this
+# # scope. Just to be sure, reload the shell integration.
+# if [ -n "${SPACK_ROOT}" ]; then
+#     source ${SPACK_ROOT}/share/spack/setup-env.sh
+# else
+#     echo "Spack required.  Please set SPACK_ROOT environment variable"
+#     exit 1
+# fi
+
+# SPACK_VERSION=$(spack --version | sed 's/-.*//g' | sed 's/[(].*[)]//g')
+# MIN_SPACK_VERSION=0.18.0
+
+# source ${LBANN_DIR}/scripts/utilities.sh
+
+# compare_versions ${SPACK_VERSION} ${MIN_SPACK_VERSION}
+# VALID_SPACK=$?
+
+# if [[ ${VALID_SPACK} -eq 2 ]]; then
+#     echo "Newer version of Spack required.  Detected version ${SPACK_VERSION} requires at least ${MIN_SPACK_VERSION}"
+#     exit 1
+# fi
+
+# echo "run.sh WEEKLY="
+# echo $WEEKLY
+
+# echo "Task: Cleaning"
+# ./clean.sh
+
+# echo "Discovered installed module file: ${LBANN_MODFILES_DIR}"
+# echo "Discovered Spack environment: ${SPACK_ENV_NAME}"
+# echo "Task: Compiler Tests"
+# cd compiler_tests
+# $PYTHON -m pytest -s -vv --durations=0 --junitxml=results.xml || exit 1
+
+# Find the correct module to load
+# SPACK_ARCH=$(spack arch)
+# SPACK_ARCH_TARGET=$(spack arch -t)
+# export LBANN_BUILD_LABEL="lbann_${SYSTEM_NAME}_${SPACK_ENV_NAME}-${SPACK_ARCH_TARGET}"
+# export LBANN_BUILD_PARENT_DIR="${CI_PROJECT_DIR}/builds/${LBANN_BUILD_LABEL}"
+# export LBANN_INSTALL_DIR="${LBANN_BUILD_PARENT_DIR}/install"
+# export LBANN_MODFILES_DIR="${LBANN_INSTALL_DIR}/etc/modulefiles"
+# ml use ${LBANN_MODFILES_DIR}
+# ml load lbann
+
+echo "Testing $(which lbann) from $(pwd)
+cd unit_tests"
+#cd ..
+
+flux run -N1 -n8 -g1 --exclusive lbann_pfe.sh -m pytest -s -vv --durations=0 --junitxml=results.xml
+
+exit 0
+# These tests are "allowed" to fail inside the script. That is, the
+# unit tests should be run even if these fail. The status is cached
+# for now.
+echo "Task: Integration Tests"
+cd integration_tests
+if [ ${WEEKLY} -ne 0 ]; then
+    $LBANN_PYTHON -m pytest -s -vv --durations=0 --weekly --junitxml=results.xml
+    status=$?
+else
+    $LBANN_PYTHON -m pytest -s -vv --durations=0 --junitxml=results.xml
+    status=$?
+fi
+cd ..
+
+echo "Task: Unit Tests"
+cd unit_tests
+OMP_NUM_THREADS=10 $LBANN_PYTHON -m pytest -s -vv --durations=0 --junitxml=results.xml
+status=$(($status + $?))
+cd ..
+
+echo "Task: Finished"
+exit $status
