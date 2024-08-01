ba047864deb1c60ff22d0c9ab964958e4a26891b
Brian C. Van Essen <vanessen1@llnl.gov>
2024-08-01 16:05:40 -0700

    Extend the mpi catch tests time limit.

--- .gitlab/run_catch_tests.sh
+++ .gitlab/run_catch_tests.sh
@@ -3,7 +3,7 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "~~~~~ Sequential catch tests"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-timeout -k 1m 2m \
+timeout -k 1m 10m \
         ${build_dir}/build-lbann/unit_test/seq-catch-tests \
         -r console \
         -r JUnit::out=${project_dir}/seq-tests_junit.xml || {
@@ -43,7 +43,7 @@ then
     case "${cluster}" in
         pascal)
             export OMPI_MCA_mpi_warn_on_fork=0
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     srun -N1 -n2 --ntasks-per-node=2 --mpibind=off \
                     -D ${build_dir}/build-lbann \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests "exclude:[random]" "exclude:[filesystem]" \\
@@ -55,7 +55,7 @@ then
                 echo "******************************"
 #                    -r mpicumulative \
             }
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     srun -N1 -n2 --ntasks-per-node=2 --mpibind=off \
                     -D ${build_dir}/build-lbann \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests -s "[filesystem]" \
@@ -69,7 +69,7 @@ then
             }
             ;;
         lassen)
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     jsrun -n1 -r1 -a4 -c40 -g4 -d packed -b packed:10 \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests "exclude:[random]" "exclude:[filesystem]" \
                     -r console::out=${project_dir}/mpi-tests-console-rank=%r-size=%s.log \
@@ -80,7 +80,7 @@ then
                 echo "******************************"
 #                    -r mpicumulative \
             }
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     jsrun -n1 -r1 -a4 -c40 -g4 -d packed -b packed:10 \
                     -h ${build_dir}/build-lbann \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests -s "[filesystem]" \
@@ -95,7 +95,7 @@ then
             ;;
         corona|tioga)
             export H2_SELECT_DEVICE_0=1
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     flux run -N1 -n8 -g1 --exclusive \
                     --cwd=${build_dir}/build-lbann \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests "exclude:[random]" "exclude:[filesystem]" \
@@ -109,7 +109,7 @@ then
             }
 # \
 #                    -o "${project_dir}/mpi-catch-filesystem-results-rank=%r-size=%s.xml"
-            timeout -k 1m 2m \
+            timeout -k 1m 10m \
                     flux run -N1 -n8 -g1 --exclusive \
                     --cwd=${build_dir}/build-lbann \
                     ${build_dir}/build-lbann/unit_test/mpi-catch-tests -s "[filesystem]" \
